73ea0d1632ee5132fd083796f6b9d4a9a0e38146
Lyashevsky <alyashev@amd.com>
2016-07-07 15:59:16 -0700

    to make it cosistent
    [ROCm/MIOpen commit: daee916408a36617a94e9e85c6d6956b4fb96522]

--- src/OCLKernel.cpp
+++ src/OCLKernel.cpp
@@ -2,26 +2,6 @@
 
 mlopenStatus_t OCLKernel::run(cl_command_queue &queue,
 	const int &work_dim,
-	const size_t &global_work_offset,
-	const size_t &global_work_dim,
-	const size_t &local_work_dim) {
-
-	cl_int status = clEnqueueNDRangeKernel(queue, _kernel,
-		work_dim,
-		&global_work_offset,
-		&global_work_dim,
-		&local_work_dim, 0, NULL, NULL);
-
-	//TODO: Check for error 
-	if (status != CL_SUCCESS) {
-		printf("kernelfailed\n");
-	}
-
-	return mlopenStatusSuccess;
-}
-
-mlopenStatus_t OCLKernel::run(cl_command_queue &queue,
-	const int &work_dim,
 	const size_t  * global_work_offset,
 	const size_t  * global_work_dim,
 	const size_t  * local_work_dim) {
