2e962565c1b7f72e3e09eac6707d45805a632af5
Paul <pfultz2@yahoo.com>
2016-07-21 11:38:38 -0500

    Initial setup for tests
    [ROCm/MIOpen commit: 9407ae55910dc22c57ab547a64ae44d2b03f4fcc]

--- test/forward_conv.cpp
+++ test/forward_conv.cpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <iterator>
 #include <memory>
+#include <utility>
 #include <iostream>
 #include <mlopenTensor.hpp>
 
@@ -17,7 +18,8 @@ struct handle_fixture
     {
         mlopenCreate(&handle);
         mlopenGetStream(handle, &q);
-        clGetCommandQueueInfo(q, CL_QUEUE_CONTEXT, sizeof(cl_context), &ctx, nullptr);
+        auto status = clGetCommandQueueInfo(q, CL_QUEUE_CONTEXT, sizeof(cl_context), &ctx, nullptr);
+        if (status != CL_SUCCESS) throw status;
     }
 
     template<class T>
@@ -45,7 +47,8 @@ struct handle_fixture
     std::vector<T> read(cl_mem data, int sz)
     {
         std::vector<T> result(sz);
-        clEnqueueReadBuffer(q, data, CL_TRUE, 0, sizeof(T)*sz, result.data(), 0, NULL, NULL);
+        auto status = clEnqueueReadBuffer(q, data, CL_TRUE, 0, sizeof(T)*sz, result.data(), 0, nullptr, nullptr);
+        if (status != CL_SUCCESS) throw status;
         return result;
     }
 
@@ -55,117 +58,126 @@ struct handle_fixture
     }
 };
 
-struct input_tensor_fixture
+template<class F>
+void for4(int n, int c, int h, int w, F f)
 {
-    mlopenTensorDescriptor_t inputTensor;
+    for(int i=0;i<n;i++)
+            for(int j=0;j<c;j++)
+                for(int k=0;k<h;k++)
+                    for(int m=0;m<w;m++)
+                        f(i, j, k, m);
+}
 
-    input_tensor_fixture()
+template<class T>
+struct input_tensor
+{
+    mlopenTensorDescriptor_t desc;
+    std::vector<T> data;
+
+    template<class Generate>
+    input_tensor(Generate g, int n, int c, int h, int w)
+    : data(n*c*h*w)
     {
-        mlopenCreateTensorDescriptor(&inputTensor);
+        mlopenCreateTensorDescriptor(&desc);
         mlopenSet4dTensorDescriptor(
-                inputTensor,
-                mlopenFloat,
-                100,
-                32,
-                8,
-                8);
-        
+                desc,
+                mlopenFloat, // TODO: Pick correct type
+                n,
+                c,
+                h,
+                w);
+
+        auto iterator = data.begin();
+        for4(n, c, h, w, [&](int i, int j, int k, int m)
+        {
+            assert(iterator < data.end());
+            *iterator = g(i, j, k, m);
+            ++iterator;
+        });        
     }
 
-    ~input_tensor_fixture()
+    ~input_tensor()
     {
-        mlopenDestroyTensorDescriptor(inputTensor);
+        mlopenDestroyTensorDescriptor(desc);
     }
 };
 
-struct conv_filter_fixture : virtual handle_fixture
+template<class T>
+struct conv_filter
 {
-    mlopenTensorDescriptor_t convFilter;
-    mlopenConvolutionDescriptor_t convDesc;
+    mlopenTensorDescriptor_t desc;
+    mlopenConvolutionDescriptor_t filter;
+    std::vector<T> data;
 
     static const mlopenConvolutionMode_t mode = mlopenConvolution;
 
-    conv_filter_fixture()
+    template<class Generate>
+    conv_filter(Generate g, int n, int c, int h, int w, int padh = 0, int padw = 0, int u = 1, int v = 1, int upx = 1, int upy = 1)
+    : data(n*c*h*w)
     {
-        mlopenCreateTensorDescriptor(&convFilter);
+        mlopenCreateTensorDescriptor(&desc);
         // weights
         mlopenSet4dTensorDescriptor(
-            convFilter,
-            mlopenFloat,
-            64,  // outputs
-            32,   // inputs
-            5,   // kernel size
-            5);
+            desc,
+            mlopenFloat, // TODO: Pick correct type
+            n,  // outputs
+            c,   // inputs
+            h,   // kernel size
+            w);
         
-        mlopenCreateConvolutionDescriptor(&convDesc);
+        mlopenCreateConvolutionDescriptor(&filter);
         // convolution with padding 2
-        mlopenInitConvolutionDescriptor(convDesc,
+        mlopenInitConvolutionDescriptor(filter,
                 mode,
-                0,
-                0,
-                1,
-                1,
-                1,
-                1);
+                padh,
+                padw,
+                u,
+                v,
+                upx,
+                upy);
+
+        auto iterator = data.begin();
+        for4(n, c, h, w, [&](int i, int j, int k, int m)
+        {
+            assert(iterator < data.end());
+            *iterator = g(i, j, k, m);
+            ++iterator;
+        }); 
         
     }
-    ~conv_filter_fixture()
+    ~conv_filter()
     {
-        mlopenDestroyTensorDescriptor(convFilter);
-        mlopenDestroyConvolutionDescriptor(convDesc);
-    }
-
-    void run()
-    {
-        mlopenConvolutionMode_t lmode = mode;
-        int pad_w, pad_h, u, v, upx, upy;
-        mlopenGetConvolutionDescriptor(convDesc,
-                &lmode,
-                &pad_h, &pad_w, &u, &v,
-                &upx, &upy);
-
-        CHECK(mode == 0);
-        CHECK(pad_h == 0);
-        CHECK(pad_w == 0);
-        CHECK(u == 1);
-        CHECK(v == 1);
-        CHECK(upx == 1);
-        CHECK(upy == 1);
+        mlopenDestroyTensorDescriptor(desc);
+        mlopenDestroyConvolutionDescriptor(filter);
     }
 };
 
-struct output_tensor_fixture : conv_filter_fixture, input_tensor_fixture
+template<class T>
+struct output_tensor
 {
-    mlopenTensorDescriptor_t outputTensor;
-    output_tensor_fixture()
+    mlopenTensorDescriptor_t desc;
+    std::vector<T> data;
+
+    output_tensor(const conv_filter<T>& filter, const input_tensor<T>& input)
     {
         int x, y, z, a;
-        mlopenGetConvolutionForwardOutputDim(convDesc, inputTensor, convFilter, &x, &y, &z, &a);
+        mlopenGetConvolutionForwardOutputDim(filter.filter, input.desc, filter.desc, &x, &y, &z, &a);
 
-        mlopenCreateTensorDescriptor(&outputTensor);
+        data.resize(x*y*z*a);
+
+        mlopenCreateTensorDescriptor(&desc);
 
         mlopenSet4dTensorDescriptor(
-            outputTensor,
+            desc,
             mlopenFloat,
             x,
             y,
             z,
             a);
     }
-    ~output_tensor_fixture()
+    ~output_tensor()
     {
-        mlopenDestroyTensorDescriptor(outputTensor);
-    }
-
-    void run()
-    {
-        int x, y, z, a;
-        mlopenGetConvolutionForwardOutputDim(convDesc, inputTensor, convFilter, &x, &y, &z, &a);
-
-        CHECK(x == 100);
-        CHECK(y == 64);
-        CHECK(z == 4);
-        CHECK(a == 4);
+        mlopenDestroyTensorDescriptor(desc);
     }
 };
 
@@ -255,75 +267,107 @@ std::vector<T> ForwardConvOnHost(
     return out;
 }
 
-struct conv_forward : output_tensor_fixture
+template<class T>
+void verify_forward_conv(const input_tensor<T>& input, const conv_filter<T>& filter)
 {
-    void run()
-    {
+    output_tensor<T> out{filter, input};
+    handle_fixture handle;
 
-        // Setup OpenCL buffers
+    cl_mem in_dev = handle.write(input.data);
+    cl_mem wei_dev = handle.write(filter.data);
+    cl_mem out_dev = handle.create<float>(out.data.size());
 
-        std::vector<float> in(mlopenGetTensorDescriptorElementSize(inputTensor));
-        std::vector<float> wei(mlopenGetTensorDescriptorElementSize(convFilter));
+    int ret_algo_count;
+    mlopenConvAlgoPerf_t perf;
 
-        for(auto&& x:in) {
-            x = rand() * (1.0 / RAND_MAX);
-        }
-        for (auto&& x:wei) {
-            x = (double)(rand() * (1.0 / RAND_MAX) - 0.5) * 0.001;
-        }
+    int alpha = 1, beta = 1;
 
-        cl_mem in_dev = this->write(in);
-        cl_mem wei_dev = this->write(wei);
-        cl_mem out_dev = this->create<float>(mlopenGetTensorDescriptorElementSize(outputTensor));
+    mlopenFindConvolutionForwardAlgorithm(handle.handle,
+        input.desc,
+        in_dev,
+        filter.desc,
+        wei_dev,
+        filter.filter,
+        out.desc,
+        out_dev,
+        1,
+        &ret_algo_count,
+        &perf,
+        mlopenConvolutionFastest,
+        NULL,
+        10);
 
-        int ret_algo_count;
-        mlopenConvAlgoPerf_t perf;
+    mlopenConvolutionForward(handle.handle,
+        &alpha,
+        input.desc,
+        in_dev,
+        filter.desc,
+        wei_dev,
+        filter.filter,
+        mlopenConvolutionFwdAlgoDirect,
+        &beta,
+        out.desc,
+        out_dev,
+        NULL,
+        0);
 
-        int alpha = 1, beta = 1;
+    out.data = handle.read<T>(out_dev, out.data.size());
 
-        mlopenFindConvolutionForwardAlgorithm(handle,
-            inputTensor,
-            in_dev,
-            convFilter,
-            wei_dev,
-            convDesc,
-            outputTensor,
-            out_dev,
-            1,
-            &ret_algo_count,
-            &perf,
-            mlopenConvolutionFastest,
-            NULL,
-            10);
+    auto host_out = ForwardConvOnHost(input.desc,
+        input.data,
+        filter.desc,
+        filter.data,
+        filter.filter,
+        out.desc);
 
-        mlopenConvolutionForward(handle,
-            &alpha,
-            inputTensor,
-            in_dev,
-            convFilter,
-            wei_dev,
-            convDesc,
-            mlopenConvolutionFwdAlgoDirect,
-            &beta,
-            outputTensor,
-            out_dev,
-            NULL,
-            0);
+    CHECK(out.data == host_out);
+}
 
-        std::vector<float> out = this->read<float>(out_dev, mlopenGetTensorDescriptorElementSize(outputTensor));
+template<class F, class... Ts>
+void each_args(F f, Ts&&... xs)
+{
+    std::initializer_list<int>{(f(std::forward<Ts>(xs)), 0)...};
+}
 
-        auto host_out = ForwardConvOnHost(inputTensor,
-            in,
-            convFilter,
-            wei,
-            convDesc,
-            outputTensor);
-
-        CHECK(out == host_out);
-
-    }
+struct swallow
+{
+    template<class... Ts>
+    swallow(Ts&&...) {}
 };
 
+template<class T, class Input, class Filter>
+void verify(int n, std::pair<int, int> ind, std::pair<int, int> fd, Input in, Filter f)
+{
+    verify_forward_conv<T>(
+        input_tensor<T>(in, 10, 16, ind.first, ind.second),
+        conv_filter<T>(f, n, 16, fd.first, fd.second)
+    );
+}
+
+template<class T, class... Gs>
+void verify_each_gen(int n, std::pair<int, int> ind, std::pair<int, int> fd, Gs... gs)
+{
+    each_args([&](auto g1)
+    {
+        each_args([&](auto g2)
+        {
+            verify<T>(n, ind, fd, g1, g2);
+        }, gs...);
+    }, gs...);
+}
+
 int main() {
-    run_test<conv_forward>();
+    auto g0 = [](int, int, int, int) { return 0; };
+    auto g1 = [](int, int, int, int) { return 1; };
+    auto g_id = [](int n, int c, int h, int w) { return h == w ? 1 : 0; };
+    auto g = [](int n, int c, int h, int w) { return n+c+h+w; };
+
+    verify_each_gen<float>(64, std::make_pair(8, 8), std::make_pair(5, 5), g0, g1, g, g_id);
+
+    // verify_sizes<float>(7, g, g);
+
+    // verify_forward_conv<float>(
+    //     input_tensor<float>(g, 10, 32, 8, 8),
+    //     conv_filter<float>(g, 64, 32, 5, 5)
+    // );
 }
