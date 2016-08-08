bcfc189fb6e38ec84cae07e94778a499bde69234
Paul <pfultz2@yahoo.com>
2016-08-08 15:22:35 -0500

    More updates to clang tidy
    [ROCm/MIOpen commit: dcfec0a785a0f8c6a437d4b4622c9b281b28e8cf]

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -8,18 +8,40 @@ find_program(CLANG_TIDY_EXE
         clang-tidy-3.7
         clang-tidy-3.6
         clang-tidy-3.5
+    PATHS
+        /usr/local/opt/llvm/bin
 )
+
+if( NOT CLANG_TIDY_EXE )
+    message( STATUS "Clang tidy not found" )
+endif()
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 macro(enable_clang_tidy)
-    set(options)
+    set(options ANALYZE_TEMPORARY_DTORS)
     set(oneValueArgs)
-    set(multiValueArgs CHECKS)
+    set(multiValueArgs CHECKS ERRORS EXTRA_ARGS)
 
     cmake_parse_arguments(PARSE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-    message(STATUS "Clang tidy checks: ${PARSE_CHECKS}")
-    set(CLANG_TIDY_COMMAND ${CLANG_TIDY_EXE} -p ${CMAKE_BINARY_DIR} -checks='${PARSE_CHECKS}')
+    string(REPLACE ";" "," CLANG_TIDY_CHECKS "${PARSE_CHECKS}")
+    string(REPLACE ";" "," CLANG_TIDY_ERRORS "${PARSE_ERRORS}")
+    string(REPLACE ";" " " CLANG_TIDY_EXTRA_ARGS "${PARSE_EXTRA_ARGS}")
+    
+    message(STATUS "Clang tidy checks: ${CLANG_TIDY_CHECKS}")
+
+    if (${PARSE_ANALYZE_TEMPORARY_DTORS})
+        set(CLANG_TIDY_ANALYZE_TEMPORARY_DTORS "-analyze-temporary-dtors")
+    endif()
+
+    set(CLANG_TIDY_COMMAND 
+        ${CLANG_TIDY_EXE} 
+        -p ${CMAKE_BINARY_DIR} 
+        -checks='${CLANG_TIDY_CHECKS}'
+        -warnings-as-errors='${CLANG_TIDY_ERRORS}'
+        -extra-arg='${CLANG_TIDY_EXTRA_ARGS}'
+        ${CLANG_TIDY_ANALYZE_TEMPORARY_DTORS}
+    )
     add_custom_target(tidy)
 endmacro()
 
