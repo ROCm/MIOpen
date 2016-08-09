57ac6c0e56170153dfcb057b095c1fb60c431dcd
Paul <pfultz2@yahoo.com>
2016-08-09 13:53:16 -0500

    Fix float_equal error

--- test/forward_conv.cpp
+++ test/forward_conv.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <array>
 #include <iterator>
+#include <numeric>
 #include <memory>
 #include <utility>
 #include <iostream>
@@ -235,17 +236,44 @@ struct float_equal_fn
     template<class T>
     bool operator()(T x, T y) const
     {
-        return std::fabs(x - y) < std::numeric_limits<T>::epsilon() * std::max(x, y);
+        return std::fabs(x - y) < std::max(std::numeric_limits<T>::epsilon() * std::max(x, y), std::numeric_limits<T>::epsilon());
     }
 };
+
+template<class R1, class R2>
+bool float_equal_range(R1&& r1, R2&& r2)
+{
+    return std::distance(r1.begin(), r1.end()) == std::distance(r2.begin(), r2.end()) &&
+        std::equal(r1.begin(), r1.end(), r2.begin(), float_equal_fn{});
+}
+
+template<class R1, class R2, class Op>
+float accumulate_difference(R1&& r1, R2&& r2, Op op)
+{
+    return std::inner_product(r1.begin(), r1.end(), r2.begin(), 0.0, op,
+        [](float x, float y) { return std::fabs(x - y); }
+    );
+}
 
 template<class T>
 void verify_forward_conv(mlopen::Context& handle, const tensor<T>& input, const tensor<T>& weights, const mlopen::ConvolutionDescriptor& filter, int bias = 0)
 {
     auto out_cpu = forward_conv(input, weights, filter, bias);
     auto out_gpu = forward_conv(handle, input, weights, filter, bias);
+    int size = std::distance(out_cpu.begin(), out_cpu.end());
     CHECK(std::distance(out_cpu.begin(), out_cpu.end()) == std::distance(out_gpu.begin(), out_gpu.end()));
-    CHECK(std::equal(out_cpu.begin(), out_cpu.end(), out_gpu.begin(), float_equal_fn{}));
+    if (!float_equal_range(out_cpu, out_gpu))
+    {
+        std::cout << "FAILED: " << std::endl;
+        std::cout 
+            << "Average difference: " 
+            << (accumulate_difference(out_cpu, out_gpu, std::plus<float>()) / size) 
+            << std::endl;
+        std::cout 
+            << "Max difference: " 
+            << (accumulate_difference(out_cpu, out_gpu, [](float x, float y) { return std::max(x, y); })) 
+            << std::endl;
+    }
     // CHECK(out_cpu == out_gpu);
 }
 
@@ -288,7 +316,7 @@ struct verify_both
             std::cout << "Weights tensor: " << input.desc.ToString() << std::endl;
 
             verify_forward_conv(handle, input, weights, filter);
-        });
+        }, 10);
     }
 };
 
