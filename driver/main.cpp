809072eb761501968c4d0f0d3083a469ad46457e
Lyashevsky <alyashev@amd.com>
2016-08-09 17:26:18 -0700

    first pooling forward run

--- driver/main.cpp
+++ driver/main.cpp
@@ -56,5 +56,88 @@ int main(int argc, char* argv[]) {
 		drv.RunBackwardDataGPU();
 	}
 
+	// pooling
+	{
+		mlopenHandle_t handle;
+		cl_command_queue q;
+
+		mlopenTensorDescriptor_t inputTensor;
+		mlopenTensorDescriptor_t outputTensor;
+		mlopenPoolingDescriptor_t poolDesc;
+
+		std::unique_ptr<GPUMem> in_dev;
+		std::unique_ptr<GPUMem> wei_dev;
+		std::unique_ptr<GPUMem> out_dev;
+
+		std::vector<float> in;
+		std::vector<float> out;
+		std::vector<float> outhost;
+
+
+		handle = drv.GetHandle();
+		mlopenGetStream(handle, &q);
+
+		mlopenCreateTensorDescriptor(&inputTensor);
+		mlopenCreateTensorDescriptor(&outputTensor);
+
+		mlopenCreatePoolingDescriptor(&poolDesc);
+
+		mlopenPoolingMode_t	mode = mlopenPoolingMax;
+		int	windowHeight = 3;
+		int	windowWidth = 3;
+		int	pad_h = 1;
+		int	pad_w = 1;
+		int	u = 2;
+		int	v = 2;
+		mlopenSet2dPoolingDescriptor(poolDesc, mode, windowHeight, windowWidth, pad_h, pad_w, u, v);
+
+		mlopenSet4dTensorDescriptor(
+			inputTensor,
+			mlopenFloat,
+			UNPACK_VEC4(in_len));
+
+		int out_n, out_c, out_h, out_w;
+		mlopenGetPoolingForwardOutputDim(poolDesc, inputTensor, &out_n, &out_c, &out_h, &out_w);
+		std::vector<int> out_pooling_len({ out_n, out_c, out_h, out_w });
+		mlopenSet4dTensorDescriptor(
+			outputTensor,
+			mlopenFloat,
+			UNPACK_VEC4(out_pooling_len));
+
+		int n_l, c_l, h_l, w_l;
+		mlopenGet4dTensorDescriptorLengths(inputTensor, &n_l, &c_l, &h_l, &w_l);
+
+		size_t in_sz = n_l * c_l * h_l *w_l;
+
+		mlopenGet4dTensorDescriptorLengths(outputTensor, &n_l, &c_l, &h_l, &w_l);
+
+		size_t out_sz = n_l * c_l * h_l *w_l;
+
+		cl_context ctx;
+		clGetCommandQueueInfo(q, CL_QUEUE_CONTEXT, sizeof(cl_context), &ctx, NULL);
+
+		in_dev = std::unique_ptr<GPUMem>(new GPUMem(ctx, in_sz, sizeof(float)));
+		out_dev = std::unique_ptr<GPUMem>(new GPUMem(ctx, out_sz, sizeof(float)));
+
+		in = std::vector<float>(in_sz);
+		out = std::vector<float>(out_sz, 0);
+		outhost = std::vector<float>(out_sz, 0);
+
+		for (int i = 0; i < in_sz; i++) {
+			in[i] = rand() * (1.0 / RAND_MAX);
+		}
+		
+
+		cl_int status;
+		status = in_dev->ToGPU(q, in.data());
+		status |= out_dev->ToGPU(q, out.data());
+
+		if (status != CL_SUCCESS)
+			printf("Error copying data to GPU\n");
+
+		float alpha = 1., beta = 1.;
+		mlopenPoolingForward(handle, poolDesc, &alpha, inputTensor, in_dev->GetMem(), &beta, outputTensor, out_dev->GetMem());
+
+	}
 	return 0;
 }
