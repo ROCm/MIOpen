ea601ccac5f233663f12791c79fee085f76a1a0c
Paul <pfultz2@yahoo.com>
2016-08-18 20:01:49 -0500

    Initial implementation of hip handle
    [ROCm/MIOpen commit: 9e3cfbdeaf73b73adb6decf9874a7f72e5f02cc5]

--- src/hip/handlehip.cpp
+++ src/hip/handlehip.cpp
@@ -1,25 +1,49 @@
 #include <mlopen/handle.hpp>
+#include <mlopen/errors.hpp>
+#include <algorithm>
 
 namespace mlopen {
 
 struct HandleImpl
 {
+    // typedef MLOPEN_MANAGE_PTR(hipStream_t, hipStreamDestroy) StreamPtr;
+    typedef std::shared_ptr<typename std::remove_pointer<hipStream_t>::type> StreamPtr;
+
+    StreamPtr create_stream()
+    {
+        hipStream_t result;
+        auto status = hipStreamCreate(&result);
+        if (status != hipSuccess) MLOPEN_THROW("Failed to allocate stream");
+        return StreamPtr{result, &hipStreamDestroy};
+    }
+
+    static StreamPtr reference_stream(hipStream_t s)
+    {
+        return StreamPtr{s, null_deleter{}};
+    }
+
+    std::vector<StreamPtr> streams;
 };
 
 Handle::Handle (int numStreams, mlopenAcceleratorQueue_t *streams) 
 : impl(new HandleImpl())
 {
+    std::transform(streams, streams+numStreams, std::back_inserter(this->impl->streams), [](hipStream_t x) {
+        return HandleImpl::reference_stream(x); 
+    });
 }
 
 Handle::Handle () 
 : impl(new HandleImpl())
-{}
+{
+    this->impl->streams.push_back(impl->create_stream());
+}
 
 Handle::~Handle() {}
 
 mlopenAcceleratorQueue_t Handle::GetStream() const
 {
-
+    return impl->streams.front().get();
 }
 
 void Handle::EnableProfiling(bool enable)
@@ -34,15 +58,21 @@ float Handle::GetKernelTime() const
 
 ManageDataPtr Handle::Create(int sz)
 {
-
+    void * result;
+    auto status = hipMalloc(&result, sz);
+    if (status != hipSuccess) MLOPEN_THROW("Hip error creating buffer: " + std::to_string(status));
+    return ManageDataPtr{result};
 }
 ManageDataPtr& Handle::WriteTo(const void* data, ManageDataPtr& ddata, int sz)
 {
-
+    auto status = hipMemcpy(ddata.get(), data, sz, hipMemcpyHostToDevice);
+    if (status != hipSuccess) MLOPEN_THROW("Hip error writing to buffer: " + std::to_string(status));
+    return ddata;
 }
 void Handle::ReadTo(void* data, const ManageDataPtr& ddata, int sz)
 {
-
+    auto status = hipMemcpy(data, ddata.get(), sz, hipMemcpyDeviceToHost);
+    if (status != hipSuccess) MLOPEN_THROW("Hip error reading from buffer: " + std::to_string(status));
 }
 }
 
