078fd780c2408f51dd1329eb6f44c995d5e5517a
Paul <pfultz2@yahoo.com>
2016-08-18 19:17:05 -0500

    Add stubs for hip api

--- src/hip/convolutionhip.cpp
+++ src/hip/convolutionhip.cpp
@@ -1,3 +1,73 @@
 #include <mlopen/convolution.hpp>
 
+namespace mlopen {
 
+void ConvolutionDescriptor::FindConvFwdAlgorithm(mlopen::Handle& handle,
+        const mlopen::TensorDescriptor& xDesc,
+        ConstData_t                 x,
+        const mlopen::TensorDescriptor& wDesc,
+        ConstData_t                 w,
+        const mlopen::TensorDescriptor& yDesc,
+        ConstData_t                 y,
+        const int                       requestAlgoCount,
+        int                             *returnedAlgoCount,
+        mlopenConvAlgoPerf_t            *perfResults,
+        mlopenConvPreference_t          preference,
+        void                            *workSpace,
+        size_t                          workSpaceSize,
+        bool                            exhaustiveSearch) const
+{
+
+}
+
+void ConvolutionDescriptor::ConvolutionForward(mlopen::Handle& handle,
+    const void                          *alpha,
+    const mlopen::TensorDescriptor&     xDesc,
+    ConstData_t                     x,
+    const mlopen::TensorDescriptor&     wDesc,
+    ConstData_t                     w,
+    mlopenConvFwdAlgorithm_t            algo,
+    const void                          *beta,
+    const mlopen::TensorDescriptor&     yDesc,
+    Data_t                              y,
+    void                                *workSpace,
+    size_t                              workSpaceSize) const
+{
+
+}
+
+void ConvolutionDescriptor::FindConvBwdDataAlgorithm(mlopen::Handle& handle,
+    const mlopen::TensorDescriptor& dyDesc,
+    ConstData_t                 dy,
+    const mlopen::TensorDescriptor& wDesc,
+    ConstData_t                 w,
+    const mlopen::TensorDescriptor& dxDesc,
+    ConstData_t                 dx,
+    const int                       requestAlgoCount,
+    int                             *returnedAlgoCount,
+    mlopenConvAlgoPerf_t            *perfResults,
+    mlopenConvPreference_t          preference,
+    void                            *workSpace,
+    size_t                          workSpaceSize,
+    bool                            exhaustiveSearch) const
+{
+
+}
+
+void ConvolutionDescriptor::ConvolutionBackwardData(mlopen::Handle& handle,
+    const void                          *alpha,
+    const mlopen::TensorDescriptor&     dyDesc,
+    ConstData_t                     dy,
+    const mlopen::TensorDescriptor&     wDesc,
+    ConstData_t                     w,
+    mlopenConvBwdDataAlgorithm_t        algo,
+    const void                          *beta,
+    const mlopen::TensorDescriptor&     dxDesc,
+    Data_t                              dx,
+    void                                *workSpace,
+    size_t                              workSpaceSize) const
+{
+
+}
+
+}
--- src/hip/handlehip.cpp
+++ src/hip/handlehip.cpp
@@ -1,5 +1,48 @@
 #include <mlopen/handle.hpp>
 
+namespace mlopen {
 
+struct HandleImpl
+{
+};
 
+Handle::Handle (int numStreams, mlopenAcceleratorQueue_t *streams) 
+: impl(new HandleImpl())
+{
+}
+
+Handle::Handle () 
+: impl(new HandleImpl())
+{}
+
+Handle::~Handle() {}
+
+mlopenAcceleratorQueue_t Handle::GetStream() const
+{
+
+}
+
+void Handle::EnableProfiling(bool enable)
+{
+
+}
+
+float Handle::GetKernelTime() const
+{
+    return 0.0;
+}
+
+ManageDataPtr Handle::Create(int sz)
+{
+
+}
+ManageDataPtr& Handle::WriteTo(const void* data, ManageDataPtr& ddata, int sz)
+{
+
+}
+void Handle::ReadTo(void* data, const ManageDataPtr& ddata, int sz)
+{
+
+}
+}
 
