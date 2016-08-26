e49f9d77fae518f0f66e3e3efb5669299e06465c
Mayank Daga <mayank.daga@amd.com>
2016-08-26 09:44:54 -0500

    fixed mlopen:: namespace in convolution apis

--- src/include/mlopen/convolution.hpp
+++ src/include/mlopen/convolution.hpp
@@ -13,44 +13,18 @@ struct ConvolutionDescriptor : mlopenConvolutionDescriptor {
 	ConvolutionDescriptor(int p_pad_h = 0, int p_pad_w = 0, int p_u = 1, int p_v = 1, int p_upscalex = 1, int p_upscaley = 1);
 	ConvolutionDescriptor(mlopenConvolutionMode_t p_mode, int p_pad_h = 0, int p_pad_w = 0, int p_u = 1, int p_v = 1, int p_upscalex = 1, int p_upscaley = 1);
 
-	std::tuple<int, int, int, int> GetForwardOutputDim(const TensorDescriptor& inputTensorDesc, const TensorDescriptor& filterDesc) const;
-	TensorDescriptor GetForwardOutputTensor(const TensorDescriptor& inputTensorDesc, const TensorDescriptor& filterDesc) const;
+	std::tuple<int, int, int, int> GetForwardOutputDim(const TensorDescriptor& inputTensorDesc,
+										const TensorDescriptor& filterDesc) const;
+	TensorDescriptor GetForwardOutputTensor(const TensorDescriptor& inputTensorDesc,
+										const TensorDescriptor& filterDesc) const;
 
-	void FindConvFwdAlgorithm(mlopen::Handle& handle,
-		const mlopen::TensorDescriptor&	xDesc,
-		ConstData_t					x,
-		const mlopen::TensorDescriptor&	wDesc,
-		ConstData_t					w,
-		const mlopen::TensorDescriptor&	yDesc,
-		ConstData_t					y,
-		const int						requestAlgoCount,
-		int								*returnedAlgoCount,
-		mlopenConvAlgoPerf_t			*perfResults,
-		mlopenConvPreference_t			preference,
-		void							*workSpace,
-		size_t							workSpaceSize,
-		bool							exhaustiveSearch) const;
-
-	void ConvolutionForward(mlopen::Handle& handle,
-		const void							*alpha,
-		const mlopen::TensorDescriptor&		xDesc,
+	void FindConvFwdAlgorithm(Handle& handle,
+		const TensorDescriptor&			xDesc,
 		ConstData_t						x,
-		const mlopen::TensorDescriptor&		wDesc,
+		const TensorDescriptor&			wDesc,
 		ConstData_t						w,
-		mlopenConvFwdAlgorithm_t			algo,
-		const void							*beta,
-		const mlopen::TensorDescriptor&		yDesc,
-		Data_t								y,
-		void								*workSpace,
-		size_t								workSpaceSize) const;
-
-	void FindConvBwdDataAlgorithm(mlopen::Handle& handle,
-		const mlopen::TensorDescriptor&	dyDesc,
-		ConstData_t					dy,
-		const mlopen::TensorDescriptor&	wDesc,
-		ConstData_t					w,
-		const mlopen::TensorDescriptor&	dxDesc,
-		ConstData_t					dx,
+		const TensorDescriptor&			yDesc,
+		ConstData_t						y,
 		const int						requestAlgoCount,
 		int								*returnedAlgoCount,
 		mlopenConvAlgoPerf_t			*perfResults,
@@ -59,18 +33,46 @@ struct ConvolutionDescriptor : mlopenConvolutionDescriptor {
 		size_t							workSpaceSize,
 		bool							exhaustiveSearch) const;
 
-	void ConvolutionBackwardData(mlopen::Handle& handle,
-		const void							*alpha,
-		const mlopen::TensorDescriptor&		dyDesc,
-		ConstData_t						dy,
-		const mlopen::TensorDescriptor&		wDesc,
+	void ConvolutionForward(Handle& handle,
+		const void						*alpha,
+		const TensorDescriptor&			xDesc,
+		ConstData_t						x,
+		const TensorDescriptor&			wDesc,
 		ConstData_t						w,
-		mlopenConvBwdDataAlgorithm_t		algo,
-		const void							*beta,
-		const mlopen::TensorDescriptor&		dxDesc,
-		Data_t								dx,
-		void								*workSpace,
-		size_t								workSpaceSize) const;
+		mlopenConvFwdAlgorithm_t		algo,
+		const void						*beta,
+		const TensorDescriptor&			yDesc,
+		Data_t							y,
+		void							*workSpace,
+		size_t							workSpaceSize) const;
+
+	void FindConvBwdDataAlgorithm(Handle& handle,
+		const TensorDescriptor&			dyDesc,
+		ConstData_t						dy,
+		const TensorDescriptor&			wDesc,
+		ConstData_t						w,
+		const TensorDescriptor&			dxDesc,
+		ConstData_t						dx,
+		const int						requestAlgoCount,
+		int								*returnedAlgoCount,
+		mlopenConvAlgoPerf_t			*perfResults,
+		mlopenConvPreference_t			preference,
+		void							*workSpace,
+		size_t							workSpaceSize,
+		bool							exhaustiveSearch) const;
+
+	void ConvolutionBackwardData(Handle& handle,
+		const void						*alpha,
+		const TensorDescriptor&			dyDesc,
+		ConstData_t						dy,
+		const TensorDescriptor&			wDesc,
+		ConstData_t						w,
+		mlopenConvBwdDataAlgorithm_t	algo,
+		const void						*beta,
+		const TensorDescriptor&			dxDesc,
+		Data_t							dx,
+		void							*workSpace,
+		size_t							workSpaceSize) const;
 
 	mlopenConvolutionMode_t mode;
 	int pad_h;
