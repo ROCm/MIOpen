2fcf2b30089e73d5f5fdb72e183b86f33c4585f7
Paul <pfultz2@yahoo.com>
2016-08-27 06:22:57 -0500

    Remove create kernel that loads program

--- src/include/mlopen/clhelper.hpp
+++ src/include/mlopen/clhelper.hpp
@@ -12,8 +12,11 @@ using ClProgramPtr = MLOPEN_MANAGE_PTR(cl_program, clReleaseProgram);
 using ClKernelPtr = MLOPEN_MANAGE_PTR(cl_kernel, clReleaseKernel);
 
 ClProgramPtr LoadProgram(cl_context ctx, cl_device_id device, const std::string &program_name, std::string params);
-ClKernelPtr CreateKernel(cl_context ctx, cl_device_id device, const std::string& kernel_name, const std::string &program_name, std::string params);
 ClKernelPtr CreateKernel(cl_program program, const std::string& kernel_name);
+inline ClKernelPtr CreateKernel(const ClProgramPtr& program, const std::string& kernel_name)
+{
+	return CreateKernel(program.get(), kernel_name);
+}
 
 cl_device_id GetDevice(cl_command_queue q);
 cl_context GetContext(cl_command_queue q);
