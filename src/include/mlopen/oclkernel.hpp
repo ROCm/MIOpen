3d3b8001fe809474da38a715c7eb78cbad969309
Mayank Daga <mayank.daga@amd.com>
2016-08-29 13:50:09 -0500

    removed SetArgs and run from OCLKernel

--- src/include/mlopen/oclkernel.hpp
+++ src/include/mlopen/oclkernel.hpp
@@ -80,21 +80,6 @@ public:
 
 	OCLKernelInvoke Invoke(cl_command_queue q, std::function<void(cl_event&)> callback=nullptr);
 
-	template<typename T, typename... Args>
-	mlopenStatus_t SetArgs(int i, const T& first, const Args&... rest);
-	template<typename... Args>
-	mlopenStatus_t SetArgs(int i, const LocalMemArg &lmem, const Args&... rest);
-	mlopenStatus_t SetArgs(int) {
-		return mlopenStatusSuccess;
-	}
-
-	mlopenStatus_t run(cl_command_queue &queue,
-		const int &work_dim,
-		const size_t * global_work_offset,
-		const size_t * global_work_dim,
-		const size_t * local_work_dim,
-		cl_event	 * event);
-
 	cl_kernel GetKernel() { return kernel.get(); } 
 
 	mlopenStatus_t GetKernelName(std::string &kernelName);
@@ -107,39 +92,6 @@ private:
 	std::vector<size_t> ldims;
 	std::vector<size_t> gdims;
 };
-
-template<typename T, typename... Args>
-mlopenStatus_t OCLKernel::SetArgs(int i, 
-		const T& first, 
-		const Args&... rest)
-{
-	cl_int status;
-
-	status = clSetKernelArg(kernel.get(), i++, sizeof(T), (void *)& first);
-	std::stringstream errStream;
-	errStream<<"OpenCL error setting kernel argument "<<i;
-//	clCheckStatus(status, errStream.str()) ;
-
-	status = SetArgs(i, rest...);
-	return mlopenStatusSuccess;
-
-}
-
-template<typename... Args>
-mlopenStatus_t OCLKernel::SetArgs(int i, 
-		const LocalMemArg &lmem, 
-		const Args&... rest)
-{
-	cl_int status;
-	status = clSetKernelArg(kernel.get(), i++, lmem.GetSize(), NULL);
-	std::stringstream errStream;
-	errStream<<"OpenCL error setting kernel argument (local memory) "<<i;
-	//clCheckStatus(status, errStream.str()) ;
-	
-	status = SetArgs(i, rest...);
-	return mlopenStatusSuccess;
-
-}
 
 }
 
--- src/ocl_kernel.cpp
+++ src/ocl_kernel.cpp
@@ -28,30 +28,6 @@ OCLKernelInvoke OCLKernel::Invoke(cl_command_queue q, std::function<void(cl_even
 	return result;
 }
 
-mlopenStatus_t OCLKernel::run(cl_command_queue &queue,
-	const int &work_dim,
-	const size_t  * global_work_offset,
-	const size_t  * global_work_dim,
-	const size_t  * local_work_dim,
-	cl_event	  * event) {
-
-	cl_int status = clEnqueueNDRangeKernel(queue, kernel.get(),
-		work_dim,
-		global_work_offset,
-		global_work_dim,
-		local_work_dim, 0, NULL, event);
-
-	//TODO: Check for error 
-	if (status != CL_SUCCESS) {
-		printf("kernel failed %d \n",status);
-	}
-	else if (event) {
-		clWaitForEvents(1, event);
-	}
-
-	return mlopenStatusSuccess;
-}
-
 mlopenStatus_t OCLKernel::GetKernelName(std::string &progName) {
 	
 	char *name = new char[200];
