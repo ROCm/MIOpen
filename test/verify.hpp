05d448ff3ee959422c59384a30fd9be27ace25e9
Paul <pfultz2@yahoo.com>
2016-09-30 14:59:40 -0500

    Compute error

--- test/verify.hpp
+++ test/verify.hpp
@@ -5,51 +5,57 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <mlopen/returns.hpp>
 
-struct float_equal_fn
+
+// Compute the value of a range
+template<class R>
+using range_value = typename std::decay<decltype(*std::declval<R>().begin())>::type;
+
+struct sum_fn
 {
-    template<class T>
-    bool operator()(T x, T y) const
-    {
-        return std::fabs(x - y) < std::max(std::numeric_limits<T>::epsilon() * std::max(x, y), std::numeric_limits<T>::epsilon());
-    }
+    template<class T, class U>
+    auto operator()(T x, U y) const MLOPEN_RETURNS(x + y);
+};
+
+struct max_mag_fn
+{
+    template<class T, class U>
+    auto operator()(T x, U y) const MLOPEN_RETURNS(std::max(std::fabs(x), std::fabs(y)));
+};
+
+struct square_diff_fn
+{
+    template<class T, class U>
+    auto operator()(T x, U y) const MLOPEN_RETURNS((x - y)*(x - y));
 };
 
 template<class R1, class R2>
-bool float_equal_range(R1&& r1, R2&& r2)
+range_value<R1> rms_range(R1&& r1, R2&& r2)
 {
-    return std::distance(r1.begin(), r1.end()) == std::distance(r2.begin(), r2.end()) &&
-        std::equal(r1.begin(), r1.end(), r2.begin(), float_equal_fn{});
-}
-
-template<class R1, class R2, class Op>
-float accumulate_difference(R1&& r1, R2&& r2, Op op)
-{
-    return std::inner_product(r1.begin(), r1.end(), r2.begin(), 0.0, op,
-        [](float x, float y) { return std::fabs(x - y); }
-    );
+    std::size_t n = std::distance(r1.begin(), r1.end());
+    if (n == std::distance(r2.begin(), r2.end())) 
+    {
+        auto square_diff = std::inner_product(r1.begin(), r1.end(), r2.begin(), 0.0, sum_fn{}, square_diff_fn{});
+        auto mag = std::inner_product(r1.begin(), r1.end(), r2.begin(), 0.0, sum_fn{}, max_mag_fn{});
+        return std::sqrt(square_diff / (n*mag));
+    }
+    else return std::numeric_limits<range_value<R1>>::max();
 }
 
 template<class V, class... Ts>
 void verify(V&& v, Ts&&... xs)
 {
+    const double tolerance = 10e-6;
     auto out_cpu = v.cpu(xs...);
     auto out_gpu = v.gpu(xs...);
-    int size = std::distance(out_cpu.begin(), out_cpu.end());
     CHECK(std::distance(out_cpu.begin(), out_cpu.end()) == std::distance(out_gpu.begin(), out_gpu.end()));
-    if (!float_equal_range(out_cpu, out_gpu))
+    auto error = rms_range(out_cpu, out_gpu);
+    if (error > tolerance)
     {
-        std::cout << "FAILED: " << std::endl;
-        v.fail(xs...);
-
-        std::cout 
-            << "Average difference: " 
-            << (accumulate_difference(out_cpu, out_gpu, std::plus<float>()) / size) 
-            << std::endl;
-        std::cout 
-            << "Max difference: " 
-            << (accumulate_difference(out_cpu, out_gpu, [](float x, float y) { return std::max(x, y); })) 
-            << std::endl;
+        std::cout << "FAILED: " << error << std::endl;
+        v.fail(error, xs...);
+        // TODO: Check if gpu data is all zeros
     }
 }
 
