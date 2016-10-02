7b3ce55f5d30148b523f1885e56f6b3b927f60e4
Alexander Lyashevsky <Alexander.Lyashevsky@amd.com>
2016-10-02 15:49:31 -0700

    new db and scripts
    [ROCm/MIOpen commit: 0fb675958973b3e23e0c5697804c450f2941fed4]

--- src/ocl/perf_cases_fcn.bat
+++ src/ocl/perf_cases_fcn.bat
@@ -23,12 +23,12 @@ echo "conv2/3x3 1x64x256x512x192x3x3"
 echo "conv2/relu_3x3 1x192x256x512x192"
 .\driver\Debug\MLOpenDriver.exe activ -t %T% -n %B%  -V %V% -W  512 -H 256 -c 192 -F 1
 echo "conv2/norm2 1x192x256x512x192x5x5"
-.\driver\Debug\MLOpenDriver.exe lrn -t %T% -n %B%  -V %V% -W  512 -H 256 -c 192 -k 192 -x 5 -y 5 -p 2 -q 2 -lrn 0 -F 1
+.\driver\Debug\MLOpenDriver.exe lrn -t %T% -n %B%  -V %V% -W  512 -H 256 -c 192 -x 5 -y 5 -p 2 -q 2 -lrn 0 -F 1
 echo "pool2/3x3_s2 1x192x256x512x192x3x3"
 .\driver\Debug\MLOpenDriver.exe pool -t %T% -n %B%  -V %V% -W  512 -H 256 -c 192 -k 192 -x 3 -y 3 -p 1 -q 1 -u 2 -v 2 -F 1
 echo "inception_3a/1x1 1x192x128x256x64x1x1"
 .\driver\Debug\MLOpenDriver.exe conv -i 1 -t %T% -n %B%  -V %V% -W  256 -H 128 -c 192 -k 64 -x 1 -y 1 -p 0 -q 0  -F 1 -s %S%
-echo "nception_3a/relu_1x1 1x64x128x256x64"
+echo "inception_3a/relu_1x1 1x64x128x256x64"
 .\driver\Debug\MLOpenDriver.exe activ -t %T% -n %B%  -V %V% -W  256 -H 128 -c 64 -F 1
 echo "inception_3a/3x3_reduce 1x192x128x256x96x3x3"
 .\driver\Debug\MLOpenDriver.exe conv -i 1 -t %T% -n %B%  -V %V% -W  256 -H 128 -c 192 -k 96 -x 1 -y 1 -p 0 -q 0  -F 1 -s %S%
