2011617ed381ad76fcdc7f0f54df73a70e8027a7
Paul <pfultz2@yahoo.com>
2016-10-03 15:04:16 -0500

    Add a driver for tests

--- test/forward_conv.cpp
+++ test/forward_conv.cpp
@@ -9,9 +9,10 @@
 #include <mlopen/convolution.hpp>
 #include <limits>
 
-#include "network_data.hpp"
+// #include "network_data.hpp"
 #include "tensor_holder.hpp"
 #include "verify.hpp"
+#include "driver.hpp"
 
 template<class T>
 tensor<T> get_output_tensor(const mlopen::ConvolutionDescriptor& filter, const tensor<T>& input, const tensor<T>& weights)
@@ -117,25 +118,7 @@ struct verify_conv_filter
     }
 };
 
-int main() {
-    // mlopen::Handle handle;
-    auto g0 = [](int, int, int, int) { return 0; };
-    auto g1 = [](int, int, int, int) { return 1; };
-    auto g_id = [](int, int, int h, int w) { return h == w ? 1 : 0; };
-    auto g = [](int n, int c, int h, int w)
-    {
-        double x = (547*n+701*c+877*h+1049*w+173)%1223;
-        return x/691.0;
-    };
-    (void)g0;
-    (void)g1;
-    (void)g_id;
-    (void)g;
-#if MLOPEN_TEST_ALL
-    printf("verify_all\n");
-    generate_all<float, network_visitor>(verify_conv_filter{}, g0,g1, g_id, g);
-#else
-    printf("verify_one\n");
-    generate_one<float>(verify_conv_filter{}, {16, 32, 8, 8}, {64, 32, 5, 5}, g);
-#endif
+int main(int argc, const char *argv[]) 
+{
+    test_drive<verify_conv_filter>(argc, argv);
 }
