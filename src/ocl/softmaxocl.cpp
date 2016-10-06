8b3e1874c057830adc1eba2cd13aaad8ea7f50e2
Mayank Daga <mayank.daga@amd.com>
2016-10-06 17:46:23 -0500

    csr-stream like softmax
    [ROCm/MIOpen commit: 90402c7b708a13527718148cedf9bd841a4141a8]

--- src/ocl/softmaxocl.cpp
+++ src/ocl/softmaxocl.cpp
@@ -4,6 +4,23 @@
 
 namespace mlopen {
 
+int nextPow2(int v) {
+	
+	if(v == 1) {
+		return (v << 1);
+	}
+	else {
+	v--;
+	v |= v >> 1;
+	v |= v >> 2;
+	v |= v >> 4;
+	v |= v >> 8;
+	v |= v >> 16;
+	v++;
+	return v;
+	}
+}
+
 mlopenStatus_t SoftmaxForward(
 		Handle						&handle,
 		const void					* /*alpha*/,
@@ -19,22 +36,51 @@ mlopenStatus_t SoftmaxForward(
 	//TODO: do we need to pass network_config?
 	std::string network = "placeholder";
 
-	size_t workgroups = std::min(n*h*w, 64*40*32);
+	// using workgroup size of 256 by default
+	int grid_size = n*h*w;
+	int spatial_dim = h*w;
+	int num_batch = c < 256 ? nextPow2(256/c) : 1;
 
-	const std::vector<size_t> vld(1, 64);
-	const std::vector<size_t> vgd(1, workgroups*vld[0]);
+	const std::vector<size_t> vld(1, 256);
 
-	cl_long  sz = n*h*w;
+	// compile parameters
+	std::string parms = "-DNUM_BATCH=" + std::to_string(num_batch);
 
-	handle.GetKernel("mlopenSoftmaxForward",
-			network,
-			program_name,
-			kernel_name,
-			vld,
-			vgd,
-			"")(y, c, sz);
+	// See Kernels/MLOpenSoftmax.cl for description
+	if(num_batch == 1) { // CSR-Vector like approach
 
+		// Control the max. number of workgroups launched so that we do not
+		// start getting workgroup scheduling overheads
+		size_t workgroups = std::min(grid_size, 64*40*8);
+		const std::vector<size_t> vgd(1, workgroups*vld[0]);
 
+		handle.GetKernel("mlopenSoftmaxForward",
+				network,
+				program_name,
+				kernel_name,
+				vld,
+				vgd,
+				parms)(y, c, grid_size, spatial_dim);
+	}
+	else { // CSR-Stream like approach
+
+		int batch_size = 256/num_batch;
+		int u_batch_size = c > batch_size ? nextPow2(c/batch_size) : 1;
+
+		const std::vector<size_t> vgd(1, grid_size/num_batch*vld[0]);
+
+		parms += " -DBATCH_SIZE=" + std::to_string(batch_size) + 
+			" -DU_BATCH_SIZE=" + std::to_string(u_batch_size);
+
+		handle.GetKernel("mlopenSoftmaxForward",
+				network,
+				program_name,
+				kernel_name,
+				vld,
+				vgd,
+				parms)(y, c, grid_size, spatial_dim);
+
+	}
 	return mlopenStatusSuccess;
 }
 
