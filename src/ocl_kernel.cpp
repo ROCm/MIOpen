01c69c835336c5b2fc5cb0571d1d322d53a727e8
Mayank Daga <mayank.daga@amd.com>
2016-11-23 11:42:25 -0600

    fixed passing local_work_size=0
    [ROCm/MIOpen commit: 7ec6c48e2dcf758d1a15b2ad0db9e3393fefc7f3]

--- src/ocl_kernel.cpp
+++ src/ocl_kernel.cpp
@@ -5,12 +5,14 @@ namespace mlopen {
 void OCLKernelInvoke::run() const
 {
 	cl_event ev;
-	/* way to run OCL group larger than 256 */
+	/* way to run OCL group larger than 256 
+	 * hack to ensure local_size == 0, just checking that the 1st dim is 0
+	 * may want to use a better solution*/
 	cl_int status = clEnqueueNDRangeKernel(queue, kernel.get(),
-		global_work_dim.size(),
+		work_dim,
 		((work_dim == 0) ? nullptr : global_work_offset.data()),
 		global_work_dim.data(),
-		((work_dim == 0) ? nullptr : local_work_dim.data()),
+		((local_work_dim[0] == 0) ? nullptr : local_work_dim.data()),
 		0, nullptr, callback ? &ev : nullptr);
 
 	clFlush(queue);
@@ -27,7 +29,7 @@ void OCLKernelInvoke::run() const
 
 OCLKernelInvoke OCLKernel::Invoke(cl_command_queue q, std::function<void(cl_event&)> callback)
 {
-	OCLKernelInvoke result{q, kernel, ldims.size(), {}, {}, {}, callback};
+	OCLKernelInvoke result{q, kernel, gdims.size(), {}, {}, {}, callback};
 	std::copy(gdims.begin(), gdims.end(), result.global_work_dim.begin());
 	std::copy(ldims.begin(), ldims.end(), result.local_work_dim.begin());
 	return result;
