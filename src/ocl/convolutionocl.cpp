9c1db8d6b428d49b0de8c0bb6525b16ecacf75a1
Mayank Daga <mayank.daga@amd.com>
2016-11-27 23:05:29 -0600

    added gemm() to wrw

--- src/ocl/convolutionocl.cpp
+++ src/ocl/convolutionocl.cpp
@@ -346,8 +346,8 @@ void ConvolutionDescriptor::FindConvBwdWeightsAlgorithm(Handle& handle,
 	int M = in_c * wei_h * wei_w;
 	int N = wei_n;
 	int K = out_h * out_w;
-	float alpha = 1.;
-	float beta = 1.;
+	float alpha = 1.0;
+	float beta = 0;
 	bool tA = true;
 	bool tB = false;
 	bool tC = false;
@@ -355,56 +355,32 @@ void ConvolutionDescriptor::FindConvBwdWeightsAlgorithm(Handle& handle,
 	unsigned int ldb = K;
 	unsigned int ldc = M;
 
-//	for(int i = 0; i < in_n; i++) {
-		size_t in_offset = 0 * in_c * in_h * in_w;
-		Im2ColGPU(handle, x, in_offset, in_c, in_h, in_w, wei_h, wei_w, out_h, out_w, pad_h, pad_w, v, u, workSpace);
-//./driver/MLOpenDriver conv  -s 1 -V 0 -t 1 -F 1 -W 700 -H 161 -c 1 -n 4 -k 32 -y 5  -x 20 -p 0 -q 0 -u 2 -v 2
+	size_t in_offset = 0;
+	Im2ColGPU(handle, x, in_offset, in_c, in_h, in_w, wei_h, wei_w, out_h, out_w, pad_h, pad_w, v, u, workSpace);
 
-		//bool                          isColMajor, bool tA, bool tB, bool tC, unsigned lda, unsigned ldb, unsigned ldc, unsigned m, unsigned n, unsigned k, unsigned a_offset, unsigned b_offset, unsigned c_offset
-		tinygemm::TinyGemmGeometry tgg(    true,       tA,      tB,       tC,      lda,          ldb,              ldc,            M,           N,          K,           0, 0, 0);
-		tinygemm::TinyGemmSolution soln = tinygemm::find(.001, handle.GetStream(), workSpace, dy, dw, false, 'f', tgg, alpha, beta, true, "" );
-		
-		std::string program_name = soln.main_kernel;
-		std::string kernel_name = soln.main_kernel_function_name;
-		std::string network_config = tgg.get_string(); //soln.statistics.benchmarked_geometry
-		
-		auto main_kernel_worksize_params =  soln.get_main_kernel_worksize_params(M, N);
-		//for (auto & qwe : main_kernel_worksize_params){
-			//std::cout << "|" << qwe.first << "|" << "  " << qwe.second << std::endl;
-		//}
-			 
-		size_t local_work_size = main_kernel_worksize_params.at("local_work_size");
-		size_t global_work_size = main_kernel_worksize_params.at("global_work_size");
-		
-		std::vector<size_t> vld (1, local_work_size);
-		std::vector<size_t> vgd (1, global_work_size);
-		
-		auto k = handle.GetKernel("mlopenConvolutionBwdWeightsAlgoGEMM",
+	//bool isColMajor, bool tA, bool tB, bool tC, unsigned lda, unsigned ldb, unsigned ldc, unsigned m, unsigned n, unsigned k, unsigned a_offset, unsigned b_offset, unsigned c_offset
+	tinygemm::TinyGemmGeometry tgg( true, tA, tB, tC, lda, ldb, ldc, M, N, K, 0, 0, 0);
+	tinygemm::TinyGemmSolution soln = tinygemm::find(5, handle.GetStream(), workSpace, dy, dw, false, 'f', tgg, alpha, beta, false, "" );
+
+	std::string program_name = soln.main_kernel;
+	std::string kernel_name = soln.main_kernel_function_name;
+	std::string network_config = tgg.get_networkconfig_string();
+
+	auto main_kernel_worksize_params =  soln.get_main_kernel_worksize_params(M, N);
+
+	size_t local_work_size = main_kernel_worksize_params.at("local_work_size");
+	size_t global_work_size = main_kernel_worksize_params.at("global_work_size");
+
+	std::vector<size_t> vld (1, local_work_size);
+	std::vector<size_t> vgd (1, global_work_size);
+
+	handle.GetKernel("mlopenConvolutionBwdWeightsAlgoGEMM",
 			network_config,
 			program_name,
 			kernel_name,
 			vld,
 			vgd,
 			"");
-			k(dw, workSpace, dy, alpha, beta, lda, ldb, ldc, M, N, K, 0,0,0); 
-
-
-  //__global TFLOAT       *          c,
-  //__global const TFLOAT * restrict a,
-  //__global const TFLOAT * restrict b,
-  //const TFLOAT alpha,
-  //const TFLOAT beta,
-  //const unsigned lda,
-  //const unsigned ldb,
-  //const unsigned ldc,
-  //const unsigned m,
-  //const unsigned n,
-  //const unsigned k,
-  //const unsigned a_offset,
-  //const unsigned b_offset,
-  //const unsigned c_offset  
-
-
 }
 
 // BackwardWeightsAlgorithm()
@@ -414,7 +390,7 @@ void ConvolutionDescriptor::ConvolutionBackwardWeights(Handle& handle,
 		const cl_mem					dy,
 		const TensorDescriptor&			xDesc,
 		const cl_mem					x,
-		mlopenConvBwdWeightsAlgorithm_t	/* algo */,
+		mlopenConvBwdWeightsAlgorithm_t	algo,
 		const void						* /*beta*/,
 		const TensorDescriptor&			dwDesc,
 		cl_mem							dw, 
@@ -446,11 +422,40 @@ void ConvolutionDescriptor::ConvolutionBackwardWeights(Handle& handle,
 	int out_h, out_w;
 	std::tie(std::ignore, std::ignore, out_h, out_w) = tie4(dyDesc.GetLengths());
 
-	for(int i = 0; i < in_n; i++) {
-		size_t in_offset = i * in_c * in_h * in_w;
-		Im2ColGPU(handle, x, in_offset, in_c, in_h, in_w, wei_h, wei_w, out_h, out_w, pad_h, pad_w, v, u, workSpace);
+	int M = in_c * wei_h * wei_w;
+	int N = wei_n;
+	int K = out_h * out_w;
+	float alpha = 1.0;
+	float beta = 0;
+	bool tA = true;
+	bool tB = false;
+	bool tC = false;
+	unsigned int lda = K;
+	unsigned int ldb = K;
+	unsigned int ldc = M;
 
+	tinygemm::TinyGemmGeometry tgg( true, tA, tB, tC, lda, ldb, ldc, M, N, K, 0, 0, 0);
+	std::string network_config = tgg.get_networkconfig_string();
+
+	std::string algorithm_name;
+	switch(algo) {
+		case mlopenConvolutionBwdWeightsAlgoGEMM:
+			algorithm_name = "mlopenConvolutionBwdWeightsAlgoGEMM";
+			break;
+//		case mlopenConvolutionBwdWeightsAlgoDirect:
+//			algorithm_name = "mlopenConvolutionFwdAlgoDirect";
+//			break;
 	}
+
+	int i = 0;
+//	for(int i = 0; i < in_n; i++) {
+		size_t in_offset = i * in_c * in_h * in_w;
+		if(wei_h != 1 && wei_w != 1)
+			Im2ColGPU(handle, x, in_offset, in_c, in_h, in_w, wei_h, wei_w, out_h, out_w, pad_h, pad_w, v, u, workSpace);
+	
+		// TODO: set offsets
+		handle.GetKernel(algorithm_name, network_config)(dw, workSpace, dy, alpha, beta, lda, ldb, ldc, M, N, K, 0,0,0);
+//	}
 
 }
 
