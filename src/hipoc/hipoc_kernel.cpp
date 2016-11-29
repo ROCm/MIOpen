79ee4f1ae08b2197784b0446f91cb1b62cf44989
Paul <pfultz2@yahoo.com>
2016-11-29 16:04:51 -0600

    Properly get the hip function

--- src/hipoc/hipoc_kernel.cpp
+++ src/hipoc/hipoc_kernel.cpp
@@ -1,5 +1,6 @@
 
 #include <mlopen/hipoc_kernel.hpp>
+#include <mlopen/errors.hpp>
 
 namespace mlopen {
 
@@ -11,7 +12,11 @@ void HIPOCKernel::run(void* args, std::size_t size) const
         HIP_LAUNCH_PARAM_END
     };
 
+    std::string kernel_module = "&__OpenCL_" + name + "_kernel";
+
     hipFunction_t fun;
+    if (hipSuccess != hipModuleGetFunction(&fun, program.module.get(), kernel_module.c_str()))
+        MLOPEN_THROW("Failed to get function");
     hipModuleLaunchKernel(fun, gdims[0], gdims[1], gdims[2], ldims[0], ldims[1], ldims[2], 0, 0, nullptr, (void**)&config);
 }
 }
