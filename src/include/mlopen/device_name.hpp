d447ec95293e9b6c30371d6134fa7eeb711cc506
Mayank Daga <mayank.daga@amd.com>
2017-01-27 13:25:57 -0600

    tabs --> whitespaces
    [ROCm/MIOpen commit: 210307ad7bac5d11a7392e356ff5ceb267c82377]

--- src/include/mlopen/device_name.hpp
+++ src/include/mlopen/device_name.hpp
@@ -5,23 +5,23 @@
 #include <map>
 
 namespace mlopen {
-	
+    
 std::string inline GetDeviceNameFromMap(std::string &name){
 
-	static std::map<std::string, std::string> device_name_map = {
-																	{"Fiji", "Fiji"},	
-																	{"gfx802", "Fiji"},	
-																	{"gfx803", "Fiji"},	
-																	{"gfx804", "Fiji"},	
-																};
+    static std::map<std::string, std::string> device_name_map = {
+                                                                    {"Fiji", "Fiji"},   
+                                                                    {"gfx802", "Fiji"}, 
+                                                                    {"gfx803", "Fiji"}, 
+                                                                    {"gfx804", "Fiji"}, 
+                                                                };
 
-	auto device_name_iterator = device_name_map.find(name);
-	if(device_name_iterator != device_name_map.end()) {
-		return device_name_iterator->second;
-	}
-	else {
-		return name;
-	}
+    auto device_name_iterator = device_name_map.find(name);
+    if(device_name_iterator != device_name_map.end()) {
+        return device_name_iterator->second;
+    }
+    else {
+        return name;
+    }
 }
 
 } // namespace mlopen
