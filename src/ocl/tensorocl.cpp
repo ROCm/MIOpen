cdc4831896ccfa956b437b114ceb2e4a2f88e674
Mayank Daga <mayank.daga@amd.com>
2017-02-14 23:20:07 -0600

    generic add_tensor
    [ROCm/MIOpen commit: c2e14c8762395d09f8809c276f4ebaab4ae60684]

--- src/ocl/tensorocl.cpp
+++ src/ocl/tensorocl.cpp
@@ -70,21 +70,15 @@ mlopenStatus_t AddTensor(Handle&              handle,
 
     for(auto i = 0; i < a_lens.size(); i++) {
         if(a_lens[i] != 1 && a_lens[i] != c_lens[i]) {
-            MLOPEN_THROW("ATensor dim != 1 && ATensor dim != CTensor dim: " + i);
+            MLOPEN_THROW("ATensor dim != 1 && ATensor dim != CTensor dim: " + std::to_string(i));
         }
     }
 
     auto first_not_one = std::find_if(a_lens.rbegin(), a_lens.rend(), [](int i){ return i != 1; });
     auto d = std::distance(a_lens.begin(), first_not_one.base());
 
-    int num_wg = *first_not_one;
-    int work_per_wg = std::accumulate(a_lens.begin(), a_lens.begin() + (d-1), 1, std::multiplies<int>()) *
-                    std::accumulate(c_lens.begin() + d, c_lens.end(), 1, std::multiplies<int>());
-
-    int n_not_ones = std::count_if(a_lens.begin(), a_lens.end(), [](int i){ return i != 1; });
-
-    int a_n, a_c, a_h, a_w;
-    std::tie(a_n, a_c, a_h, a_w) = tie4(aTensorDesc.GetLengths());
+    int num_wg = std::accumulate(a_lens.begin(), a_lens.begin() + d, 1, std::multiplies<int>());
+    int work_per_wg = std::accumulate(c_lens.begin() + d, c_lens.end(), 1, std::multiplies<int>());
 
     int c_n, c_c, c_h, c_w;
     std::tie(c_n, c_c, c_h, c_w) = tie4(cTensorDesc.GetLengths());
@@ -95,8 +89,7 @@ mlopenStatus_t AddTensor(Handle&              handle,
 	const std::vector<size_t> vld(1, 256);
 	const std::vector<size_t> vgd(1, num_wg*256);
 
-    std::string parms = " -DFIRST_N=" + std::to_string(d-1)
-                    + " -DN_NOT_ONES=" + std::to_string(n_not_ones);
+    std::string parms = " -DFIRST_N=" + std::to_string(d-1);
 
     handle.GetKernel(kernel_name,
             "placeholder",
@@ -104,7 +97,7 @@ mlopenStatus_t AddTensor(Handle&              handle,
             kernel_name,
             vld,
             vgd,
-            parms) (ATensor, a_n, a_c, a_h, a_w, CTensor, c_n, c_c, c_h, c_w, n_not_ones, work_per_wg);
+            parms) (ATensor, CTensor, c_n, c_c, c_h, c_w, work_per_wg);
 
     return mlopenStatusSuccess;
 }
