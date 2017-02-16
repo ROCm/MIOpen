0c1d6bcee63bf96f13da55c19379eeac832f4ea7
Mayank Daga <mayank.daga@amd.com>
2017-02-16 17:56:47 -0600

    updated device_name map

--- src/include/mlopen/device_name.hpp
+++ src/include/mlopen/device_name.hpp
@@ -9,10 +9,20 @@ namespace mlopen {
 std::string inline GetDeviceNameFromMap(std::string &name){
 
     static std::map<std::string, std::string> device_name_map = {
-                                                                    {"Fiji", "Fiji"},   
-                                                                    {"gfx802", "Fiji"}, 
-                                                                    {"gfx803", "Fiji"}, 
-                                                                    {"gfx804", "Fiji"}, 
+                                                                    {"Ellesmere", "gfx803"},
+                                                                    {"Baffin", "gfx803"},
+                                                                    {"RacerX", "gfx803"},
+                                                                    {"Polaris10", "gfx803"},
+                                                                    {"Polaris11", "gfx803"},
+                                                                    {"Tonga", "gfx803"},
+                                                                    {"Fiji", "gfx803"},   
+                                                                    {"gfx800", "gfx803"}, 
+                                                                    {"gfx802", "gfx803"}, 
+                                                                    {"gfx803", "gfx803"}, 
+                                                                    {"gfx804", "gfx803"}, 
+                                                                    {"Vega10", "gfx900"}, 
+                                                                    {"gfx900", "gfx900"}, 
+                                                                    {"gfx901", "gfx900"}, 
                                                                 };
 
     auto device_name_iterator = device_name_map.find(name);
