5848296efbf66b6f98bd1056df838a4f3ec174f8
Mayank Daga <mayank.daga@amd.com>
2017-02-16 17:57:09 -0600

    updated db-file to devname_cu
    [ROCm/MIOpen commit: 1f2197aece9bc7aca30f1851c12667d529fad8b6]

--- driver/conv_driver.hpp
+++ driver/conv_driver.hpp
@@ -354,7 +354,7 @@ int ConvDriver<T>::RunForwardGPU() {
 			weightTensor,
 			wei_dev->GetMem(),
 			convDesc,
-			mlopenConvolutionFwdAlgoGEMM,
+			mlopenConvolutionFwdAlgoDirect,
 			&beta,
 			outputTensor,
 			out_dev->GetMem(),
