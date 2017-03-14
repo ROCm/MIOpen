72cb288f9a39bd0a6ab5052e9ee362038e5c63b3
Radha Giduthuri <rgiduthuri@gmail.com>
2017-03-14 14:19:27 -0700

    conv5x10uv2fwd: updated amd_kernel_code_t, meta-data, and few fixes
    [ROCm/MIOpen commit: 7c0669ccceda5631d1c7debd2f89bc6bd1f18328]

--- src/Kernels/conv5x10uv2fwd.s
+++ src/Kernels/conv5x10uv2fwd.s
@@ -1,5 +1,10 @@
-.hsa_code_object_version 2, 1
-.hsa_code_object_isa 8, 0, 3, "AMD", "AMDGPU"
+/*
+ * Convolution Kernel for 5x10 stride=2 pad=0 
+ *
+ */
+
+.hsa_code_object_version 2,1
+.hsa_code_object_isa     8,0,3,"AMD","AMDGPU"
 
 ///////////////////////////////////////////////////
 // ******* configuration and resource allocation
@@ -27,24 +32,24 @@
 .set LDS_SIZE    ,4*(64*inp_u-1+(wei_w-1))*(4*inp_v-1+(wei_h-1)) // = 5984 bytes
 // ******* SGPR allocation
 // For used during initialization
-.set sreg_karg   ,   0   // [2]
-.set sreg_group_0,   2   // [1]
-.set sreg_group_1,   3   // [1]
-.set sreg_group_2,   4   // [1]
-.set sreg_tmp0   ,   5   // [1]
-.set sreg_tmp1   ,   6   // [1]
-.set sreg_iinc   ,   7   // [1]
-.set sreg_winc   ,   8   // [1]
-.set sreg_inp_addr, 10   // [2]
-.set sreg_wei_addr, 12   // [2]
-.set sreg_out_addr, 14   // [2]
+.set sreg_karg   ,   4   // [2]
+.set sreg_group_0,   6   // [1]
+.set sreg_group_1,   7   // [1]
+.set sreg_group_2,   8   // [1]
+.set sreg_tmp0   ,   9   // [1]
+.set sreg_tmp1   ,  10   // [1]
+.set sreg_tmp2   ,  11   // [1]
+.set sreg_iinc   ,  12   // [1]
+.set sreg_winc   ,  13   // [1]
+.set sreg_inp_addr, 14   // [2]
+.set sreg_out_addr, 16   // [2]
 // For use during core-loop and later
-.set sreg_wval    ,  0   // [50]
-.set sreg_wadr    , 50   // [2]
-.set sreg_k       , 52   // [1]
-.set sreg_c       , 53   // [1]
-.set sreg_dy      , 54   // [1]
-.set SGPR_COUNT   , 55   // COUNT
+.set sreg_wval    ,  8   // [50]
+.set sreg_wei_addr, 58   // [2]
+.set sreg_k       , 60   // [1]
+.set sreg_c       , 61   // [1]
+.set sreg_dy      , 62   // [1]
+.set SGPR_COUNT   , 64   // COUNT
 // ******* VGPR allocation
 // For used during initialization
 .set vreg_local_0 ,  0   // [1]
@@ -54,11 +59,9 @@
 .set vreg_tmp1    ,  4   // [1]
 .set vreg_tmp2    ,  5   // [1]
 .set vreg_tmp3    ,  6   // [1]
-.set vreg_dx      ,  7   // [1]
-.set vreg_iinc0   ,  8   // [1]
-.set vreg_iinc1   ,  9   // [1]
-.set vreg_winc    , 10   // [1]
-.set vreg_oinc    , 11   // [1]
+.set vreg_iinc0   ,  7   // [1]
+.set vreg_iinc1   ,  8   // [1]
+.set vreg_oinc    ,  9   // [1]
 // For use during core-loop and later
 .set vreg_ival    ,  0   // [8]
 .set vreg_oval    ,  8   // [4]
@@ -70,7 +73,7 @@
 .set vreg_inp_dsrd0,20   // [1]
 .set vreg_inp_dsrd1,21   // [1]
 .set vreg_inp_dsrd2,22   // [1]
-.set vreg_inp_dsrd3,23   // [1]
+.set vreg_dx      , 23   // [1]
 .set VGPR_COUNT    ,24   // COUNT
 // ******* derived constants
 .set inp_stride_y,(inp_w * 4)
@@ -105,32 +108,68 @@
 conv5x10uv2fwd:
 
 	.amd_kernel_code_t
-	   enable_sgpr_private_segment_buffer = 0
-	   enable_sgpr_dispatch_ptr = 0
-	   enable_sgpr_queue_ptr = 0
-	   enable_sgpr_kernarg_segment_ptr = 1
-	   enable_sgpr_dispatch_id = 0
-	   enable_sgpr_flat_scratch_init = 0
-	   enable_sgpr_private_segment_size = 0
-	   enable_sgpr_grid_workgroup_count_x = 0
-	   enable_sgpr_grid_workgroup_count_y = 0
-	   enable_sgpr_grid_workgroup_count_z = 0
-	   enable_sgpr_workgroup_id_x = 1
-	   enable_sgpr_workgroup_id_y = 1
-	   enable_sgpr_workgroup_id_z = 1
-	   enable_sgpr_workgroup_info = 0
-	   enable_sgpr_private_segment_wave_byte_offset = 0
-	   enable_vgpr_workitem_id = 1
-	   is_ptr64 = 1
-	   float_mode = 192
-	   workgroup_group_segment_byte_size = LDS_SIZE
-	   wavefront_sgpr_count = SGPR_COUNT
-	   workitem_vgpr_count = VGPR_COUNT
-	   granulated_lds_size = (LDS_SIZE + 31)/32
-	   granulated_wavefront_sgpr_count = (SGPR_COUNT-1)/8
-	   granulated_workitem_vgpr_count = (VGPR_COUNT-1)/8
-	   kernarg_segment_byte_size = 64
-	   user_sgpr_count = 8 // TODO: ???
+		amd_code_version_major = 1
+		amd_code_version_minor = 0
+		amd_machine_kind = 1
+		amd_machine_version_major = 8
+		amd_machine_version_minor = 0
+		amd_machine_version_stepping = 3
+		kernel_code_entry_byte_offset = 256
+		kernel_code_prefetch_byte_size = 0
+		max_scratch_backing_memory_byte_size = 0
+		granulated_workitem_vgpr_count = (VGPR_COUNT+3)/4
+		granulated_wavefront_sgpr_count = (SGPR_COUNT+7)/8
+		priority = 0
+		float_mode = 192
+		priv = 0
+		enable_dx10_clamp = 1
+		debug_mode = 0
+		enable_ieee_mode = 1
+		enable_sgpr_private_segment_wave_byte_offset = 0
+		user_sgpr_count = 6
+		enable_sgpr_workgroup_id_x = 1
+		enable_sgpr_workgroup_id_y = 1
+		enable_sgpr_workgroup_id_z = 1
+		enable_sgpr_workgroup_info = 0
+		enable_vgpr_workitem_id = 1
+		enable_exception_msb = 0
+		granulated_lds_size = 12
+		enable_exception = 0
+		enable_sgpr_private_segment_buffer = 1
+		enable_sgpr_dispatch_ptr = 0
+		enable_sgpr_queue_ptr = 0
+		enable_sgpr_kernarg_segment_ptr = 1
+		enable_sgpr_dispatch_id = 0
+		enable_sgpr_flat_scratch_init = 0
+		enable_sgpr_private_segment_size = 0
+		enable_sgpr_grid_workgroup_count_x = 0
+		enable_sgpr_grid_workgroup_count_y = 0
+		enable_sgpr_grid_workgroup_count_z = 0
+		enable_ordered_append_gds = 0
+		private_element_size = 1
+		is_ptr64 = 1
+		is_dynamic_callstack = 0
+		is_debug_enabled = 0
+		is_xnack_enabled = 0
+		workitem_private_segment_byte_size = 0
+		workgroup_group_segment_byte_size = 5984
+		gds_segment_byte_size = 0
+		kernarg_segment_byte_size = 64
+		workgroup_fbarrier_count = 0
+		wavefront_sgpr_count = SGPR_COUNT
+		workitem_vgpr_count = VGPR_COUNT
+		reserved_vgpr_first = 0
+		reserved_vgpr_count = 0
+		reserved_sgpr_first = 0
+		reserved_sgpr_count = 0
+		debug_wavefront_private_segment_offset_sgpr = 0
+		debug_private_segment_buffer_sgpr = 0
+		kernarg_segment_alignment = 4
+		group_segment_alignment = 4
+		private_segment_alignment = 4
+		wavefront_size = 6
+		call_convention = 0
+		runtime_loader_kernel_symbol = 0
 	.end_amd_kernel_code_t
 
 	//////////////////////////////////////////////////////////////////////////////
@@ -196,7 +235,7 @@ conv5x10uv2fwd:
 	s_add_u32  s[sreg_tmp0], s[sreg_tmp0], s[sreg_tmp1]
 	s_mul_i32  s[sreg_tmp1], s[sreg_dy], out_stride_y
 	s_add_u32  s[sreg_tmp0], s[sreg_tmp0], s[sreg_tmp1]
-	v_mad_u32_u24 v[vreg_oinc], v[vreg_dx], 4, s[sreg_tmp1]
+	v_mad_u32_u24 v[vreg_oinc], v[vreg_dx], 4, s[sreg_tmp0]
 	// compute: vreg_iinc0 = group_id(2) * inp_stride_n + dy * inp_v * inp_stride_y +
 	//                       group_id(0) * 64 * 4 * inp_u +
 	//                       local_id(0) * 8 + local_id(1) * inp_stride_y
@@ -208,17 +247,16 @@ conv5x10uv2fwd:
 	//                     ? vreg_inp_dswr0 + 8 * 136 * 4
 	//                     : (local_id(0) >> 2) * 136 * 4 + 128 * 4 + (local_id(0) & 3) * 8
 	//          vreg_inp_dsrd0 = local_id(0) * 8
-	//          vreg_inp_dsrd1 = vreg_inp_dsrd0 + 2 * 136 * 4
 	s_mul_i32     s[sreg_iinc], s[sreg_group_2], inp_stride_n
 	s_mul_i32     s[sreg_tmp0], s[sreg_dy], inp_stride_y * inp_v
 	s_add_u32     s[sreg_iinc], s[sreg_iinc], s[sreg_tmp0]
 	s_mul_i32     s[sreg_tmp0], s[sreg_group_0], 4 * 64 * inp_u
 	s_add_u32     s[sreg_iinc], s[sreg_iinc], s[sreg_tmp0]
-	s_movk_i32    s[sreg_tmp0], 0+(inp_stride_y/8)
-	s_movk_i32    s[sreg_tmp1], 136*4/8
-	v_mad_u32_u24 v[vreg_iinc0], v[vreg_local_1], s[sreg_tmp0], v[vreg_local_0]
-	v_mad_u32_u24 v[vreg_inp_dswr0], v[vreg_local_1], s[sreg_tmp1], v[vreg_local_0]
-	v_lshlrev_b32 v[vreg_iinc0], 3, v[vreg_iinc0]
+	s_movk_i32    s[sreg_tmp2], 0+inp_stride_y
+	s_movk_i32    s[sreg_tmp1], 136*4
+	v_lshlrev_b32 v[vreg_tmp0], 3, v[vreg_local_0]
+	v_mad_u32_u24 v[vreg_iinc0], v[vreg_local_1], s[sreg_tmp2], v[vreg_tmp0]
+	v_mad_u32_u24 v[vreg_inp_dswr0], v[vreg_local_1], s[sreg_tmp1], v[vreg_tmp0]
 	v_add_u32     v[vreg_iinc0], vcc, s[sreg_iinc], v[vreg_iinc0]
 	v_lshlrev_b32 v[vreg_inp_dswr0], 3, v[vreg_inp_dswr0]
 	v_add_u32     v[vreg_iinc1], vcc, 8 * inp_stride_y, v[vreg_iinc0]
@@ -236,18 +274,21 @@ conv5x10uv2fwd:
 	v_cndmask_b32 v[vreg_iinc1], v[vreg_tmp2], v[vreg_iinc1], vcc
 	v_cndmask_b32 v[vreg_inp_dswr1], v[vreg_tmp3], v[vreg_inp_dswr1], vcc
 	v_lshlrev_b32 v[vreg_inp_dsrd0], 3, v[vreg_local_0]
-	v_add_u32     v[vreg_inp_dsrd1], vcc, 2 * 136 * 4, v[vreg_inp_dsrd0]
 	// wait for load completion
 	s_waitcnt lgkmcnt(0)
 	// update address registers
+	v_mov_b32 v[vreg_out_addr], s[sreg_out_addr]
+	v_mov_b32 v[vreg_out_addr+1], s[sreg_out_addr+1]
+	v_mov_b32 v[vreg_inp_addr1], s[sreg_inp_addr]
+	v_mov_b32 v[vreg_inp_addr1+1], s[sreg_inp_addr+1]
 	s_add_u32  s[sreg_wei_addr], s[sreg_wei_addr], s[sreg_winc]
 	s_addc_u32 s[sreg_wei_addr+1], s[sreg_wei_addr+1], 0
-	v_add_u32  v[vreg_out_addr], vcc, s[sreg_out_addr], v[vreg_oinc]
-	v_addc_u32 v[vreg_out_addr+1], vcc, s[sreg_out_addr+1], 0, vcc
-	v_add_u32  v[vreg_inp_addr0], vcc, s[sreg_inp_addr], v[vreg_iinc0]
-	v_addc_u32 v[vreg_inp_addr0+1], vcc, s[sreg_inp_addr+1], 0, vcc
-	v_add_u32  v[vreg_inp_addr1], vcc, s[sreg_inp_addr], v[vreg_iinc1]
-	v_addc_u32 v[vreg_inp_addr1+1], vcc, s[sreg_inp_addr+1], 0, vcc
+	v_add_u32  v[vreg_out_addr], vcc, v[vreg_out_addr], v[vreg_oinc]
+	v_addc_u32 v[vreg_out_addr+1], vcc, v[vreg_out_addr+1], 0, vcc
+	v_add_u32  v[vreg_inp_addr0], vcc, v[vreg_inp_addr1], v[vreg_iinc0]
+	v_addc_u32 v[vreg_inp_addr0+1], vcc, v[vreg_inp_addr1+1], 0, vcc
+	v_add_u32  v[vreg_inp_addr1], vcc, v[vreg_inp_addr1], v[vreg_iinc1]
+	v_addc_u32 v[vreg_inp_addr1+1], vcc, v[vreg_inp_addr1+1], 0, vcc
 	// initialize output values and channel count
 	s_movk_i32 s[sreg_c], 0+wei_c
 	v_mov_b32 v[vreg_oval], 0
@@ -268,7 +309,6 @@ conv5x10uv2fwd:
 	//  - v[vreg_dswr0]
 	//  - v[vreg_dswr1]
 	//  - v[vreg_dsrd0]
-	//  - v[vreg_dsrd1]
 	//  - v[vreg_out_addr:vreg_out_addr+1]
 	//  - v[vreg_oval:vreg_oval+3]
 	// temporary registers used inside this loop:
@@ -299,8 +339,8 @@ skip_load0:
 	ds_write_b64 v[vreg_inp_dswr1], v[vreg_ival+2:vreg_ival+3]
 skip_load1:
 	ds_write_b64 v[vreg_inp_dswr0], v[vreg_ival+0:vreg_ival+1]
+	v_add_u32 v[vreg_inp_dsrd1], vcc, 1 * 136 * 4, v[vreg_inp_dsrd0]
 	v_add_u32 v[vreg_inp_dsrd2], vcc, 4 * 136 * 4, v[vreg_inp_dsrd0]
-	v_add_u32 v[vreg_inp_dsrd3], vcc, 4 * 136 * 4, v[vreg_inp_dsrd1]
 	s_waitcnt lgkmcnt(0)
 	s_barrier
 
@@ -442,10 +482,11 @@ skip_load1:
 	v_mac_f32 v[vreg_oval+2], v[vreg_ival+5], s[sreg_wval+1*10+3]
 	v_mac_f32 v[vreg_oval+2], v[vreg_ival+6], s[sreg_wval+1*10+4]
 	v_mac_f32 v[vreg_oval+2], v[vreg_ival+7], s[sreg_wval+1*10+5]
+	v_add_u32 v[vreg_inp_dsrd1], vcc, 5 * 136 * 4, v[vreg_inp_dsrd0]
 	ds_read2_b32 v[vreg_ival+0:vreg_ival+1], v[vreg_inp_dsrd2] offset0:1*136+6 offset1:1*136+7
 	ds_read2_b32 v[vreg_ival+2:vreg_ival+3], v[vreg_inp_dsrd2] offset0:1*136+8 offset1:1*136+9
-	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd3] offset0:0*136+0 offset1:0*136+1
-	ds_read2_b32 v[vreg_ival+6:vreg_ival+7], v[vreg_inp_dsrd3] offset0:0*136+2 offset1:0*136+3
+	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd1] offset0:0*136+0 offset1:0*136+1
+	ds_read2_b32 v[vreg_ival+6:vreg_ival+7], v[vreg_inp_dsrd1] offset0:0*136+2 offset1:0*136+3
 	s_waitcnt lgkmcnt(0)
 	v_mac_f32 v[vreg_oval+1], v[vreg_ival+0], s[sreg_wval+3*10+6]
 	v_mac_f32 v[vreg_oval+1], v[vreg_ival+1], s[sreg_wval+3*10+7]
@@ -467,10 +508,10 @@ skip_load1:
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+5], s[sreg_wval+0*10+1]
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+6], s[sreg_wval+0*10+2]
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+7], s[sreg_wval+0*10+3]
-	ds_read2_b32 v[vreg_ival+0:vreg_ival+1], v[vreg_inp_dsrd3] offset0:0*136+4 offset1:0*136+5
-	ds_read2_b32 v[vreg_ival+2:vreg_ival+3], v[vreg_inp_dsrd3] offset0:0*136+6 offset1:0*136+7
-	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd3] offset0:0*136+8 offset1:0*136+9
-	ds_read2_b32 v[vreg_ival+6:vreg_ival+7], v[vreg_inp_dsrd3] offset0:1*136+0 offset1:1*136+1
+	ds_read2_b32 v[vreg_ival+0:vreg_ival+1], v[vreg_inp_dsrd1] offset0:0*136+4 offset1:0*136+5
+	ds_read2_b32 v[vreg_ival+2:vreg_ival+3], v[vreg_inp_dsrd1] offset0:0*136+6 offset1:0*136+7
+	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd1] offset0:0*136+8 offset1:0*136+9
+	ds_read2_b32 v[vreg_ival+6:vreg_ival+7], v[vreg_inp_dsrd1] offset0:1*136+0 offset1:1*136+1
 	s_waitcnt lgkmcnt(0)
 	v_mac_f32 v[vreg_oval+1], v[vreg_ival+0], s[sreg_wval+4*10+4]
 	v_mac_f32 v[vreg_oval+1], v[vreg_ival+1], s[sreg_wval+4*10+5]
@@ -494,12 +535,12 @@ skip_load1:
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+5], s[sreg_wval+0*10+9]
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+6], s[sreg_wval+1*10+0]
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+7], s[sreg_wval+1*10+1]
-	ds_read2_b32 v[vreg_ival+0:vreg_ival+1], v[vreg_inp_dsrd3] offset0:1*136+2 offset1:1*136+3
-	ds_read2_b32 v[vreg_ival+2:vreg_ival+3], v[vreg_inp_dsrd3] offset0:1*136+4 offset1:1*136+5
-	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd3] offset0:1*136+6 offset1:1*136+7
-	ds_read2_b32 v[vreg_ival+6:vreg_ival+7], v[vreg_inp_dsrd3] offset0:1*136+8 offset1:1*136+9
+	ds_read2_b32 v[vreg_ival+0:vreg_ival+1], v[vreg_inp_dsrd1] offset0:1*136+2 offset1:1*136+3
+	ds_read2_b32 v[vreg_ival+2:vreg_ival+3], v[vreg_inp_dsrd1] offset0:1*136+4 offset1:1*136+5
+	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd1] offset0:1*136+6 offset1:1*136+7
+	ds_read2_b32 v[vreg_ival+6:vreg_ival+7], v[vreg_inp_dsrd1] offset0:1*136+8 offset1:1*136+9
 	v_add_u32 v[vreg_inp_dsrd2], vcc, 8 * 136 * 4, v[vreg_inp_dsrd0]
-	v_add_u32 v[vreg_inp_dsrd3], vcc, 8 * 136 * 4, v[vreg_inp_dsrd1]
+	v_add_u32 v[vreg_inp_dsrd1], vcc, 9 * 136 * 4, v[vreg_inp_dsrd0]
 	s_waitcnt lgkmcnt(0)
 	v_mac_f32 v[vreg_oval+2], v[vreg_ival+0], s[sreg_wval+3*10+2]
 	v_mac_f32 v[vreg_oval+2], v[vreg_ival+1], s[sreg_wval+3*10+3]
@@ -555,8 +596,8 @@ skip_load1:
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+7], s[sreg_wval+3*10+5]
 	ds_read2_b32 v[vreg_ival+0:vreg_ival+1], v[vreg_inp_dsrd2] offset0:1*136+6 offset1:1*136+7
 	ds_read2_b32 v[vreg_ival+2:vreg_ival+3], v[vreg_inp_dsrd2] offset0:1*136+8 offset1:1*136+9
-	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd3] offset0:0*136+0 offset1:0*136+1
-	ds_read2_b32 v[vreg_ival+6:vreg_ival+7], v[vreg_inp_dsrd3] offset0:0*136+2 offset1:0*136+3
+	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd1] offset0:0*136+0 offset1:0*136+1
+	ds_read2_b32 v[vreg_ival+6:vreg_ival+7], v[vreg_inp_dsrd1] offset0:0*136+2 offset1:0*136+3
 	s_waitcnt lgkmcnt(0)
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+0], s[sreg_wval+3*10+6]
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+1], s[sreg_wval+3*10+7]
@@ -566,9 +607,9 @@ skip_load1:
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+5], s[sreg_wval+4*10+1]
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+6], s[sreg_wval+4*10+2]
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+7], s[sreg_wval+4*10+3]
-	ds_read2_b32 v[vreg_ival+0:vreg_ival+1], v[vreg_inp_dsrd3] offset0:0*136+4 offset1:0*136+5
-	ds_read2_b32 v[vreg_ival+2:vreg_ival+3], v[vreg_inp_dsrd3] offset0:0*136+6 offset1:0*136+7
-	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd3] offset0:0*136+8 offset1:0*136+9
+	ds_read2_b32 v[vreg_ival+0:vreg_ival+1], v[vreg_inp_dsrd1] offset0:0*136+4 offset1:0*136+5
+	ds_read2_b32 v[vreg_ival+2:vreg_ival+3], v[vreg_inp_dsrd1] offset0:0*136+6 offset1:0*136+7
+	ds_read2_b32 v[vreg_ival+4:vreg_ival+5], v[vreg_inp_dsrd1] offset0:0*136+8 offset1:0*136+9
 	s_waitcnt lgkmcnt(0)
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+0], s[sreg_wval+4*10+4]
 	v_mac_f32 v[vreg_oval+3], v[vreg_ival+1], s[sreg_wval+4*10+5]
@@ -584,11 +625,11 @@ skip_load1:
 	//////////////////////////////////////////////////////////////////////////////
 	// write output values
 	//  - do bound checks before writing
-	//  - use s[sreg_wadr:sreg_wadr+1] as temporary registers
+	//  - use s[sreg_wei_addr:sreg_wei_addr+1] as temporary registers
 	s_cmpk_ge_u32 s[sreg_k], 0+wei_k
 	s_cbranch_scc1 skip_write
-	v_cmp_gt_u32 vcc, 0+inp_w, v[vreg_dx]
-	s_and_saveexec_b64 s[sreg_wadr:sreg_wadr+1], vcc
+	v_cmp_gt_u32 vcc, 0+out_w, v[vreg_dx]
+	s_and_saveexec_b64 s[sreg_wei_addr:sreg_wei_addr+1], vcc
 	flat_store_dword v[vreg_out_addr:vreg_out_addr+1], v[vreg_oval]
 	s_cmpk_ge_u32 s[sreg_dy], 0+out_h-1
 	s_cbranch_scc1 skip_write
@@ -607,9 +648,6 @@ skip_load1:
 	flat_store_dword v[vreg_out_addr:vreg_out_addr+1], v[vreg_oval+3]
 skip_write:
 	s_endpgm
-
-end_of_text:
-	.size conv5x10uv2fwd, end_of_text - conv5x10uv2fwd
 
 ///////////////////////////////////////////////////
 // ******* meta-data section of the kernels
