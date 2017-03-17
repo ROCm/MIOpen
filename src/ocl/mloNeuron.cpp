afbc88a04c091094b85ac8e79077914c63160fcc
AMD\alyashev <Alexnader.Lyashevsky@amd.com>
2017-03-16 17:37:09 -0700

    all availblae bwd activation has been activated but not verified
    [ROCm/MIOpen commit: 098e6398e7c2358addc7f946bb60432cadd51bf7]

--- src/ocl/mloNeuron.cpp
+++ src/ocl/mloNeuron.cpp
@@ -22,23 +22,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 int mlo_construct_neuron::mloConstruct()
 {
 	int ret = 0;
-
-
-	if (isForwardDirection())
-	{
-
-		ret = mloConstructFwd();
-	}
-	else
-	{
-		ret = mloConstructBwd();
-	}
-	return(ret);
-} 
-
-int mlo_construct_neuron::mloConstructFwd()
-{
-	int ret = 0;
 	_hw_wave_sz = 64;
 
 	int data_len = (!_in_data_type.compare("FP32") ? 4 : 8);
@@ -55,7 +38,7 @@ int mlo_construct_neuron::mloConstructFwd()
 
 
 
-	_grp_tile0 = std::min((int)((glbl_wk + _hw_wave_sz-1)/ _hw_wave_sz)*_hw_wave_sz, (int)256);
+	_grp_tile0 = std::min((int)((glbl_wk + _hw_wave_sz - 1) / _hw_wave_sz)*_hw_wave_sz, (int)256);
 	_grp_tile1 = 1;
 
 	_comp_options =
@@ -63,7 +46,7 @@ int mlo_construct_neuron::mloConstructFwd()
 		+ std::string(" -DMLO_NRN_GROUP_SZ1=") + std::to_string(static_cast<long long>(_grp_tile1))
 		+ std::string(" -DMLO_NRN_OP_ID=") + std::to_string(static_cast<long long>(_neuron_type))
 		+ std::string(" -DMLO_N_PIXS_OFF=") + std::to_string(static_cast<long long>(N_PIXS_OFF))
-		+ std::string(" -DMLO_MAP_SIZE_ALIGNED=") + std::to_string(static_cast<long long>(map_size_aligned))
+		+ std::string(" -DMLO_MAP_SZ_ALIGNED=") + std::to_string(static_cast<long long>(map_size_aligned))
 		+ std::string(" -DMLO_READ_UNIT=") + std::to_string(static_cast<long long>(read_unit))
 		+ getGeneralCompOptions()
 		;
@@ -80,8 +63,15 @@ int mlo_construct_neuron::mloConstructFwd()
 
 
 	_kernel_file = "MLOpenNeuron.cl";
-	_kernel_name = (isForwardDirection())? "MLOpenNeuron4" : "MLOpenNeuron4_Bwd";
+	_kernel_name = (isForwardDirection()) ? "MLOpenNeuronFwd" : "MLOpenNeuronBwd";
 
+
+	return(ret);
+} 
+
+int mlo_construct_neuron::mloConstructFwd()
+{
+	int ret = 0;
 	return(ret);
 }
 
@@ -89,43 +79,5 @@ int mlo_construct_neuron::mloConstructFwd()
 int mlo_construct_neuron::mloConstructBwd()
 {
 	int ret = 0;
-
-#if 1
-	ret = mloConstructFwd();
-#else
-	int data_len = (!_in_data_type.compare("FP32") ? 4 : 8);
-
-	size_t size = (_bot_sz / data_len);
-	if (((size / 4) * 4) != size)
-	{
-		printf("Error: buffer size is not multipel of 4.\n");
-	//	ret = -1;
-	//	return(ret);
-	}
-
-	size_t glbl_wk = size / 4;
-
-	_grp_tile0 = 256;
-	_grp_tile1 = 1;
-
-	_comp_options =
-		std::string(" -DMLO_NRN_GROUP_SZ0=") + std::to_string(static_cast<long long>(_grp_tile0))
-		+ std::string(" -DMLO_NRN_GROUP_SZ1=") + std::to_string(static_cast<long long>(_grp_tile1))
-		+ std::string(" -DMLO_NRN_OP_ID=") + std::to_string(static_cast<long long>(_neuron_type))
-		+ getGeneralCompOptions()
-		;
-
-	_l_wk.clear();
-	_l_wk.push_back(_grp_tile0);
-	_l_wk.push_back(_grp_tile1);
-	_l_wk.push_back(1);
-
-	_g_wk.clear();
-	_g_wk.push_back(glbl_wk);
-	_g_wk.push_back(1);
-	_g_wk.push_back(1);
-	_kernel_file = "MLOpenNeuron.cl";
-	_kernel_name = "MLOpenNeuron4_Bwd";
-#endif
 	return(ret);
 }
