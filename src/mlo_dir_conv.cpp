fa3b4d43f302ec5d4b73ae02df15b3956a4b5dff
mayank daga <mayank.daga@amd.com>
2017-03-21 16:31:25 -0500

    Revert "added 7x7 winograd check"
    This reverts commit f5706e98753deafe9cc8633e988a45bd00876a9d.
    
    
    [ROCm/MIOpen commit: 42561284c2690b791b4b8756cfba20a23145f3ae]

--- src/mlo_dir_conv.cpp
+++ src/mlo_dir_conv.cpp
@@ -569,9 +569,7 @@ bool mlo_construct_direct2D::mloIsCorrectBinaryWinograd3x3Fwd() const
 
 bool mlo_construct_direct2D::mloIsFastBinaryWinograd3x3Fwd() const
 {
-
-    return !(_in_width == 7 && _in_height == 7);
-//	return true;
+	return true;
 }
 
 int mlo_construct_direct2D::mloConstructBinaryWinograd3x3Fwd(bool is_metadata_v10)
