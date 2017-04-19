f389aefe9dae9e5ef16c41cab9bef25c15074887
mayank daga <mayank.daga@amd.com>
2017-04-19 14:41:34 -0500

    fixed global grid for tinygemm-beta

--- src/gemm_geometry.cpp
+++ src/gemm_geometry.cpp
@@ -69,7 +69,7 @@ void GemmGeometry::FindSolution(float time,
         EnableBetaKernel(true, beta_kernel_worksize_params);
 
         vld[0] = local_work_size;
-        vgd[1] = global_work_size;
+        vgd[0] = global_work_size;
 
         handle.GetKernel(algorithm_name+"_beta",
                 "placeholder", // TODO: hack for now because the kernel is not cached if config is ""
