a8af54d6a9ad8c0375c8a709d228a8b49c485ffb
AMD\alyashev <Alexnader.Lyashevsky@amd.com>
2017-04-20 17:18:40 -0700

    new cleaner and simplier .bat files for windows
    [ROCm/MIOpen commit: 1732f98314944ee1f46d2d96957902254d56cb5a]

--- src/ocl/perf_cases_CIFAR10.bat
+++ src/ocl/perf_cases_CIFAR10.bat
@@ -1,19 +1,14 @@
-set B=%1
-set S=%2
-set V=%3
-set T=%4
-cd ../../build
-PATH=.\src\Debug;%PATH%
+rem arguments:
+rem D - MiOpenDriver location
+set D=%1
 echo "CIFAR-10"
 echo "conv1 100x3x32x32x32x5x5"
-.\driver\Debug\MIOpenDriver.exe conv -i 1 -n %B% -W 32 -H 32 -c 3 -k 32 -x 5 -y 5 -p 2 -q 2 -F 1 -t %T% -s %S% -V %V%
+%D%\MIOpenDriver.exe conv -i 1 -n 100 -W 32 -H 32 -c 3 -k 32 -x 5 -y 5 -p 2 -q 2 -F 0 -V 1 -t 1 -s 0 
 echo "pool1 100x32x32x32x32x3x3"
-.\driver\Debug\MIOpenDriver.exe pool -t %T% -i 1 -n %B%  -V %V% -W  32 -H 32 -c 32 -k 32 -x 3 -y 3 -p 1 -q 1 -u 2 -v 2 -F 1
+%D%\MIOpenDriver.exe pool -i 1 -n 100 -W  32 -H 32 -c 32 -k 32 -x 3 -y 3 -p 1 -q 1 -u 2 -v 2 -F 0 -V 1 -t 1
 echo "conv2 100x32x16x16x32x5x5"
-.\driver\Debug\MIOpenDriver.exe conv -i 1 -n %B% -W 16 -H 16 -c 32 -k 32 -x 5 -y 5 -p 2 -q 2 -F 1 -t %T% -s %S% -V %V%
+%D%\MIOpenDriver.exe conv -i 1 -n 100 -W 16 -H 16 -c 32 -k 32 -x 5 -y 5 -p 2 -q 2 -F 0 -V 1 -t 1 -s 0
 echo "pool2 100x32x16x16x3x3"
-.\driver\Debug\MIOpenDriver.exe pool -t %T% -i 1 -n %B%  -V %V% -W  32 -H 32 -c 32 -k 32 -x 3 -y 3 -p 1 -q 1 -u 2 -v 2 -F 1
+%D%\MIOpenDriver.exe pool -i 1 -n 100  -W  32 -H 32 -c 32 -k 32 -x 3 -y 3 -p 1 -q 1 -u 2 -v 2 -F 0 -V 1 -t 1
 echo "conv3 100x32x8x8x64x5x5"
-.\driver\Debug\MIOpenDriver.exe conv -i 1 -n %B% -W 8 -H 8 -c 32 -k 64 -x 5 -y 5 -p 2 -q 2 -F 1 -t %T% -s %S% -V %V%
-:end
-cd ../src/OCL
+%D%\MIOpenDriver.exe conv -i 1 -n 100 -W 8 -H 8 -c 32 -k 64 -x 5 -y 5 -p 2 -q 2 -F 0 -V 1 -t 1 -s 0
