ff654b787b793f67561a56b8095627d99d381315
Daniel Lowell <daniel.lowell@amd.com>
2017-04-28 11:48:04 -0500

    Initial commit for this branch. Adding in everything from the batch-norm branch.
    [ROCm/MIOpen commit: ac1ee36ac178e267cd37867c9e808966ba5420c6]

---
+++ src/include/miopen/batch_norm.hpp
@@ -0,0 +1,84 @@
+#ifndef GUARD_MIOPEN_BATCHNORMALIZATION_HPP_
+#define GUARD_MIOPEN_BATCHNORMALIZATION_HPP_
+
+#include <chrono>
+#include <cmath>
+#include <miopen.h>
+#include <miopen/handle.hpp>
+#include <miopen/tensor.hpp>
+#include <miopen/common.hpp>
+#include <miopen/mlo_internal.hpp>
+
+#define MIOPEN_BN_CPP_DEBUG     0
+#define MIO_BN_OCL_SEQ_TIMING   0
+#define MIO_BN_STATIC_WGSIZE    256
+
+#define MIO_BN_TIME_EVERYTHING  0
+
+namespace miopen {
+
+
+void BatchNormForwardInference(
+		Handle&			handle,
+		miopenBatchNormMode_t	bn_mode,
+		const void		*alpha,
+		const void              *beta,
+		const TensorDescriptor&	xDesc,
+		ConstData_t		x,
+		const TensorDescriptor& yDesc,
+		Data_t                  y,
+		const TensorDescriptor& bnScaleBiasMeanVarDesc,
+		ConstData_t		bnScale,
+		ConstData_t		bnBias,
+		ConstData_t     	estimatedMean,
+		ConstData_t		estimatedVariance,
+		double			epsilon);
+
+
+
+void BatchNormForwardTraining(
+		Handle& 		handle,
+		miopenBatchNormMode_t	bn_mode,
+		const void 		*alpha, /* these don't seem to be used in conv */
+		const void 		*beta,
+		const TensorDescriptor& xDesc,
+		ConstData_t		x,
+		const TensorDescriptor& yDesc,
+		Data_t			y,
+		const TensorDescriptor& bnScaleBiasMeanVarDesc,
+		ConstData_t		bnScale,
+		ConstData_t		bnBias,
+		double 			expAvgFactor,
+		Data_t			resultRunningMean,
+		Data_t			resultRunningVariance,
+		double 			epsilon,
+		Data_t 			resultSaveMean,
+		Data_t			resultSaveInvVariance);
+
+
+
+void BatchNormBackward(
+		Handle&			handle,
+		miopenBatchNormMode_t	bn_mode,
+		const void		*alphaDataDiff,
+		const void		*betaDataDiff,
+		const void		*alphaParamDiff,
+		const void		*betaParamDiff,
+		const TensorDescriptor&	xDesc,
+		ConstData_t		x,
+		const TensorDescriptor&	dyDesc,
+		ConstData_t		dy,
+		const TensorDescriptor&	dxDesc,
+		Data_t			dx,
+		const TensorDescriptor&	bnScaleBiasDiffDesc,
+		ConstData_t		bnScale,
+		Data_t			resultBnScaleDiff,
+		Data_t			resultBnBiasDiff,
+		double			epsilon,
+		ConstData_t		savedMean,
+		ConstData_t		savedInvVariance);
+
+}  // namespace miopen
+
+
+#endif // GUARD_MIOPEN_BATCHNORMALIZATION_HPP_
