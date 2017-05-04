46822f4d7b9a316c608c47adde0857e87a4ea553
Daniel Lowell <daniel.lowell@amd.com>
2017-05-04 14:00:16 -0500

    Added C macro
    [ROCm/MIOpen commit: 94549362155f31376f89a953cecc0e89b065ee1b]

--- src/ocl/batchnormocl.cpp
+++ src/ocl/batchnormocl.cpp
@@ -476,6 +476,7 @@ void BatchNormForwardInference(
             program_name += "Spatial.cl";
             kernel_name += "Spatial";
             parms += "-DMIO_BN_N="+std::to_string(n);
+            parms += " -DMIO_BN_C="+std::to_string(c);
             parms += " -DMIO_BN_HW="+std::to_string(in_cstride);
             parms += " -DMIO_BN_NHW="+std::to_string(n*h*w);
             parms += " -DMIO_BN_CHW="+std::to_string(in_nstride);
@@ -662,6 +663,7 @@ void BatchNormBackward(
             program_name += "Spatial.cl";
             kernel_name += "Spatial";
             parms += "-DMIO_BN_N="+std::to_string(n);
+            parms += " -DMIO_BN_C="+std::to_string(c);
             parms += " -DMIO_BN_HW="+std::to_string(in_cstride);
             parms += " -DMIO_BN_NHW="+std::to_string(n*h*w);
             parms += " -DMIO_BN_CHW="+std::to_string(in_nstride);
