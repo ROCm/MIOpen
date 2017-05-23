40caf54ddceec4f774808a566d53622d5782f9a1
Paul <pfultz2@yahoo.com>
2017-05-23 18:53:43 -0500

    Cache environment variables

--- src/logger.cpp
+++ src/logger.cpp
@@ -4,9 +4,11 @@
 
 namespace miopen {
 
+MIOPEN_DECLARE_ENV_VAR(MIOPEN_ENABLE_LOGGING)
+
 bool IsLogging()
 {
-    return miopen::IsEnvvarValueEnabled("MIOPEN_ENABLE_LOGGING");
+    return miopen::IsEnabled(MIOPEN_ENABLE_LOGGING{});
 }
 
 } // namespace miopen
