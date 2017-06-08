5d5b689b70b8fdfd11db7682923011c8b4a8ae93
Vasilii Filippov <vasja.filippov94@outlook.com>
2017-06-08 18:33:12 +0300

    Applied clang-format-3.8
    Now mloConstruct returns cause of last construction failure if no construction has succeeded.
    
    Fixed mloMultiStep.
    
    Fixed build
    
    Made implementations to return status code too.
    Now it's working as before.
    
    Added search parameters struct to mlo_construct_...
    Removed all fields that were duplicating ones from struct.
    
    Fixed missing compiler options.
    
    Finished reworking bwd.
    
    Implemented two backward algorithms as implementation descriptions.
    Added some parameters to implementation usage description.
    
    Refactored winograd.
    Began refactoring backward.
    
    Fixed warnings and moved algorithm implementation class to its hpp file.
    
    Splitted mlo_dir_conv.cpp forward convolution algorithm search implementation to several classes.
    
    
    [ROCm/MIOpen commit: 765f409f2e08cf98af0e7d2902632ec2297dbf98]

---
+++ src/implementations/conv_ocl_dir2D3x3.cpp
@@ -0,0 +1,169 @@
+#include "miopen/algorithm_implementations.hpp"
+#include "miopen/handle.hpp"
+
+namespace miopen {
+bool ConvOclDirectFwd3x3::IsCorrect(const ImplementationSearchParameters& params) const
+{
+    return (params.kernel_size0 == 3 && params.kernel_size1 == 3 && params.pad1 == 1 &&
+            params.pad0 == 1 && params.kernel_stride0 == 1 && params.kernel_stride1 == 1 &&
+            params.forward) &&
+           (params.out_width == 512 || params.out_width == 64 || params.out_width == 128 ||
+            params.out_width == 256);
+}
+
+ImplementationUsageDescription
+ConvOclDirectFwd3x3::PrepareForUsage(const ImplementationSearchParameters& params,
+                                     const ExaustiveSearchResult& /*exaustive_search_result*/) const
+{
+    // size_t localMemSize = params.stream.GetLocalMemorySize();
+
+    ImplementationUsageDescription result;
+    auto hw_wave_sz = 64;
+    // auto dev_local_mem_sz = localMemSize; // in bytes
+    int n_waves = 4;
+
+    int wei_cstride = params.kernel_size0 * params.kernel_size1;
+    int wei_bstride = params.n_inputs * wei_cstride;
+
+    result.out_pix_tile0   = 4;
+    result.out_pix_tile1   = 2;
+    result.n_stacks        = 1;
+    result.n_out_pix_tiles = 4;
+    int read_unit          = result.out_pix_tile0;
+    //	std::string READ_TYPE = (read_unit == 1) ? "_FLOAT" : "_FLOAT" +
+    // std::to_string(static_cast<long long>(read_unit));
+    // MD: read_unit is never == 1
+    std::string READ_TYPE = "_FLOAT" + std::to_string(static_cast<long long>(read_unit));
+
+    int GRP_SZ = hw_wave_sz * n_waves;
+
+    int ALU_EXTENT_X     = (params.out_width + read_unit - 1) / read_unit;
+    auto LG2ALU_EXTENT_X = static_cast<int>(std::ceil(std::log(ALU_EXTENT_X) / std::log(2)));
+    int ALU_EXTENT_Y     = (GRP_SZ >> LG2ALU_EXTENT_X);
+    auto LG2ALU_EXTENT_Y = static_cast<int>(std::ceil(std::log(ALU_EXTENT_Y) / std::log(2)));
+
+    // the wave is logical is a unit of shareing weights in SGPRs
+    // it cannot be less than HW_WAVE_SIZE = 64
+    // it cannot be larger than the group size.
+
+    int LG2_WAVE_SZ0    = std::ceil(std::log(ALU_EXTENT_X) / std::log(2));
+    int logical_wave_sz = std::max(1, ALU_EXTENT_X / hw_wave_sz) * hw_wave_sz;
+    if(logical_wave_sz > GRP_SZ)
+    {
+        printf("Conv3x3 conf error\n");
+        return ImplementationUsageDescription(static_cast<miopenStatus_t>(-1));
+    }
+    int logical_n_waves = std::max(1, GRP_SZ / logical_wave_sz);
+    int LG2_WAVE_SZ     = std::ceil(std::log(logical_wave_sz) / std::log(2));
+    int WAVE_SZ1        = (logical_wave_sz >> LG2_WAVE_SZ0);
+    int lg2_n_waves     = std::ceil(std::log(logical_n_waves) / std::log(2));
+    int N_WAVES_MASK    = (1 << lg2_n_waves) - 1;
+
+    int OUT_EXTENT1 = result.out_pix_tile1 * WAVE_SZ1;
+    int OUT_EXTENT0 = (result.out_pix_tile0 << LG2_WAVE_SZ0);
+
+    int total_out_maps = result.n_out_pix_tiles * logical_n_waves;
+
+    // number of batches inside wk_item
+    result.n_stacks = std::min(params.batch_sz, result.n_stacks);
+
+    int N_HEIGHT_EXTENTS = (params.out_height + OUT_EXTENT1 - 1) / OUT_EXTENT1;
+    int N_WIDTH_EXTENTS  = (params.out_width + OUT_EXTENT0 - 1) / OUT_EXTENT0;
+    int N_GROUPS_PER_MAP = N_HEIGHT_EXTENTS * N_WIDTH_EXTENTS;
+
+    result.grp_tile0 = GRP_SZ;
+    result.grp_tile1 = 1;
+    int grp_tile2    = 1;
+    // auto in_tile0 = OUT_EXTENT0;
+    // auto in_tile1 = OUT_EXTENT1;
+    auto n_in_data_tiles = 1;
+
+    //	_gen_comp_options += std::string(" -limit-vector-registers=64 ");
+
+    KernelUsageDescription construction_parameters;
+
+    construction_parameters.comp_options =
+        std::string(" -DMLO_DIR_FORWARD=") +
+        std::to_string(static_cast<long long>(params.forward ? 1 : 0)) +
+        std::string(" -DMLO_GRP_SZ=") + std::to_string(static_cast<long long>(GRP_SZ)) +
+        std::string(" -DMLO_GRP_SZ0=") + std::to_string(static_cast<long long>(result.grp_tile0)) +
+        std::string(" -DMLO_GRP_SZ1=") + std::to_string(static_cast<long long>(result.grp_tile1)) +
+        std::string(" -DMLO_GRP_SZ2=") + std::to_string(static_cast<long long>(grp_tile2)) +
+        std::string(" -DMLO_FILTER_SIZE0=") +
+        std::to_string(static_cast<long long>(params.kernel_size0)) +
+        std::string(" -DMLO_FILTER_SIZE1=") +
+        std::to_string(static_cast<long long>(params.kernel_size1)) +
+        std::string(" -DMLO_FILTER_PAD0=") + std::to_string(static_cast<long long>(params.pad0)) +
+        std::string(" -DMLO_FILTER_PAD1=") + std::to_string(static_cast<long long>(params.pad1)) +
+        std::string(" -DMLO_N_OUTPUTS=") +
+        std::to_string(static_cast<long long>(params.n_outputs)) + std::string(" -DMLO_N_INPUTS=") +
+        std::to_string(static_cast<long long>(params.n_inputs)) + std::string(" -DMLO_BATCH_SZ=") +
+        std::to_string(static_cast<long long>(params.batch_sz)) +
+        std::string(" -DMLO_OUT_BATCH_STRIDE=") +
+        std::to_string(static_cast<long long>(params.out_batch_stride)) +
+        std::string(" -DMLO_OUT_CHANNEL_STRIDE=") +
+        std::to_string(static_cast<long long>(params.out_channel_stride)) +
+        std::string(" -DMLO_OUT_STRIDE=") +
+        std::to_string(static_cast<long long>(params.out_stride)) +
+        std::string(" -DMLO_IN_BATCH_STRIDE=") +
+        std::to_string(static_cast<long long>(params.in_batch_stride)) +
+        std::string(" -DMLO_IN_CHANNEL_STRIDE=") +
+        std::to_string(static_cast<long long>(params.in_channel_stride)) +
+        std::string(" -DMLO_IN_STRIDE=") +
+        std::to_string(static_cast<long long>(params.in_stride)) +
+        std::string(" -DMLO_WEI_BATCH_STRIDE=") +
+        std::to_string(static_cast<long long>(wei_bstride)) +
+        std::string(" -DMLO_WEI_CHANNEL_STRIDE=") +
+        std::to_string(static_cast<long long>(wei_cstride)) + std::string(" -DMLO_IN_WIDTH=") +
+        std::to_string(static_cast<long long>(params.in_width)) + std::string(" -DMLO_IN_HEIGHT=") +
+        std::to_string(static_cast<long long>(params.in_height)) +
+        std::string(" -DMLO_N_LCL_BATCHS=") +
+        std::to_string(static_cast<long long>(result.n_stacks)) // # of diff stacks (part of batch).
+        + std::string(" -DMLO_N_LCL_OUT_MAPS=") +
+        std::to_string(static_cast<long long>(
+            result.n_out_pix_tiles)) // # output pixel tiles per wk-item (ALU)
+        + std::string(" -DMLO_N_LCL_IN_MAPS=") +
+        std::to_string(
+            static_cast<long long>(n_in_data_tiles)) // total # of blocks of different inputs in LDS
+        + std::string(" -DMLO_OUT_TILE0=") +
+        std::to_string(
+            static_cast<long long>(result.out_pix_tile0)) // size of ouptput tile per wk-item (ALU))
+        + std::string(" -DMLO_OUT_TILE1=") +
+        std::to_string(static_cast<long long>(result.out_pix_tile1)) //
+        + std::string(" -DMLO_ALU_EXTENT_X=") +
+        std::to_string(static_cast<long long>(ALU_EXTENT_X)) +
+        std::string(" -DMLO_LG2ALU_EXTENT_X=") +
+        std::to_string(static_cast<long long>(LG2ALU_EXTENT_X)) +
+        std::string(" -DMLO_ALU_EXTENT_Y=") + std::to_string(static_cast<long long>(ALU_EXTENT_Y)) +
+        std::string(" -DMLO_LG2ALU_EXTENT_Y=") +
+        std::to_string(static_cast<long long>(LG2ALU_EXTENT_Y)) +
+        std::string(" -DMLO_OUT_EXTENT1=") + std::to_string(static_cast<long long>(OUT_EXTENT1)) +
+        std::string(" -DMLO_OUT_EXTENT0=") + std::to_string(static_cast<long long>(OUT_EXTENT0)) +
+        std::string(" -DMLO_N_WAVES=") + std::to_string(static_cast<long long>(logical_n_waves)) +
+        std::string(" -DMLO_N_WAVES_MASK=") + std::to_string(static_cast<long long>(N_WAVES_MASK)) +
+        std::string(" -DMLO_LG2_WAVE_SZ=") + std::to_string(static_cast<long long>(LG2_WAVE_SZ)) +
+        std::string(" -DMLO_LG2_WAVE_SZ0=") + std::to_string(static_cast<long long>(LG2_WAVE_SZ0)) +
+        std::string(" -DMLO_READ_TYPE=") + READ_TYPE + std::string(" -DMLO_READ_UNIT=") +
+        std::to_string(static_cast<long long>(read_unit)) + std::string(" -DMLO_CONV_BIAS=") +
+        std::to_string(static_cast<long long>(params.bias)) + params.general_compile_options;
+
+    construction_parameters.l_wk.push_back(result.grp_tile0);
+    construction_parameters.l_wk.push_back(result.grp_tile1);
+    construction_parameters.l_wk.push_back(grp_tile2);
+
+    size_t gbl_wk0 = N_GROUPS_PER_MAP;
+
+    size_t gbl_wk1 = (params.n_outputs + total_out_maps - 1) / total_out_maps;
+    size_t gbl_wk2 = (params.batch_sz + result.n_stacks - 1) / result.n_stacks;
+
+    construction_parameters.g_wk.push_back(gbl_wk0 * result.grp_tile0);
+    construction_parameters.g_wk.push_back(gbl_wk1);
+    construction_parameters.g_wk.push_back(gbl_wk2);
+
+    construction_parameters.kernel_file = "MIOpenConvD3x3.cl";
+    construction_parameters.kernel_name = "MIOpenCvD3x3_WSR0";
+
+    result.construction_params.push_back(construction_parameters);
+    return result;
+}
+}
---
+++ src/include/miopen/algorithm_implementations.hpp
@@ -0,0 +1,251 @@
+#ifndef KERNEL_TRAITS_HPP_
+#define KERNEL_TRAITS_HPP_
+
+#include <vector>
+#include <memory>
+#include "miopen/mlo_internal.hpp"
+#include "miopen/miopen.h"
+
+namespace miopen {
+
+class KernelUsageDescription
+{
+    public:
+    std::string comp_options;
+    std::vector<size_t> l_wk;
+    std::vector<size_t> g_wk;
+    std::string kernel_file;
+    std::string kernel_name;
+};
+
+class ImplementationUsageDescription
+{
+    public:
+    std::vector<KernelUsageDescription> construction_params;
+    miopenStatus_t status;
+    int passes;
+
+    size_t workspce_sz;
+    int grp_tile1;
+    int grp_tile0;
+    int in_tile1;
+    int in_tile0;
+    int out_pix_tile1;
+    int out_pix_tile0;
+    int n_out_pix_tiles;
+    int n_in_data_tiles;
+    int n_stacks;
+
+    ImplementationUsageDescription(miopenStatus_t status_ = miopenStatusSuccess, int passes_ = 1)
+        : status(status_), passes(passes_)
+    {
+    }
+
+    inline bool Succeeded() const { return status == miopenStatusSuccess; }
+};
+
+class ExaustiveSearchResult
+{
+};
+
+class Direct2DfwdExaustiveSearchResult : public ExaustiveSearchResult
+{
+    public:
+    int grp_tile1;
+    int grp_tile0;
+    int in_tile1;
+    int in_tile0;
+    int out_pix_tile1;
+    int out_pix_tile0;
+    int n_out_pix_tiles;
+    int n_in_data_tiles;
+    int n_stacks;
+
+    inline void FillImplementationUsageDescription(ImplementationUsageDescription& iud) const
+    {
+        iud.grp_tile0       = grp_tile0;
+        iud.grp_tile1       = grp_tile1;
+        iud.in_tile0        = in_tile0;
+        iud.in_tile1        = in_tile1;
+        iud.out_pix_tile0   = out_pix_tile0;
+        iud.out_pix_tile1   = out_pix_tile1;
+        iud.n_out_pix_tiles = n_out_pix_tiles;
+        iud.n_in_data_tiles = n_in_data_tiles;
+        iud.n_stacks        = n_stacks;
+    }
+};
+
+class AlgotithmImplementationDescription
+{
+    public:
+    virtual std::shared_ptr<ExaustiveSearchResult>
+    PrepareExaustiveSearchResult(const ImplementationSearchParameters& /*params*/) const
+    {
+        return nullptr;
+    }
+    virtual bool IsCorrect(const ImplementationSearchParameters& /*params*/) const { return true; }
+    virtual bool IsFast(const ImplementationSearchParameters& /*params*/) const { return true; }
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const = 0;
+};
+
+class ConvAsm3x3U : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual bool IsFast(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvAsm5x10u2v2f1 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvAsm5x10u2v2b1 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvAsm7x7c3h224w224k64u2v2p3q3f1 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclDirectFwd11x11 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclDirectFwdGen : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclDirectFwd3x3 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclDirectFwdLegacyExaustiveSearch : public AlgotithmImplementationDescription
+{
+    public:
+    virtual std::shared_ptr<ExaustiveSearchResult>
+    PrepareExaustiveSearchResult(const ImplementationSearchParameters& params) const override;
+
+    private:
+    void SearchDirect2D(const ImplementationSearchParameters& params,
+                        Direct2DfwdExaustiveSearchResult& result) const;
+
+    int MeasuredLoop(miopen::Handle* profile_h,
+                     Data_t bot_ocl_buf,
+                     Data_t top_ocl_buf,
+                     Data_t wei_ocl_buf,
+                     Data_t bias_ocl_buf,
+                     double& processing_time,
+                     const ImplementationSearchParameters& params) const;
+
+    static std::vector<AlgotithmImplementationDescription*> _kernels_to_measure;
+};
+
+class ConvOclDirectFwd : public ConvOclDirectFwdLegacyExaustiveSearch
+{
+    public:
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclDirectFwd1x1 : public ConvOclDirectFwdLegacyExaustiveSearch
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclDirectFwdC : public ConvOclDirectFwdLegacyExaustiveSearch
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvBinWinograd3x3F : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvAsmBwdWrW3x3 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual bool IsFast(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclBwdWrW2 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclBwdWrW53 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+
+class ConvOclBwdWrW1x1 : public AlgotithmImplementationDescription
+{
+    public:
+    virtual bool IsCorrect(const ImplementationSearchParameters& params) const override;
+    virtual ImplementationUsageDescription
+    PrepareForUsage(const ImplementationSearchParameters& params,
+                    const ExaustiveSearchResult& exaustive_search_result) const override;
+};
+}
+
+#endif // !KERNEL_TRAITS_HPP
