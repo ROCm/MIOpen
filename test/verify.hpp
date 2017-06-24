28eae14a0cd76f23ad4ea53e2c2b84d99ecf53f6
Paul <pfultz2@yahoo.com>
2017-06-23 19:11:26 -0500

    Fix clang tidy warnings

--- test/verify.hpp
+++ test/verify.hpp
@@ -7,15 +7,13 @@
 #include <iostream>
 #include <functional>
 #include <miopen/returns.hpp>
+#include <miopen/float_equal.hpp>
 
 namespace miopen {
 
 // Compute the value of a range
 template<class R>
 using range_value = typename std::decay<decltype(*std::declval<R>().begin())>::type;
-
-template<class... Ts>
-using common_type = typename std::common_type<Ts...>::type;
 
 struct sum_fn
 {
@@ -54,28 +52,6 @@ T as(T, U x)
 {
     return x;
 }
-
-struct float_equal_fn
-{
-    template<class T>
-    static bool apply(T x, T y)
-    {
-        return 
-            std::isfinite(x) and 
-            std::isfinite(y) and
-            std::nextafter(x, std::numeric_limits<T>::lowest()) <= y and 
-            std::nextafter(x, std::numeric_limits<T>::max()) >= y;
-    }
-
-    template<class T, class U>
-    bool operator()(T x, U y) const
-    {
-        return float_equal_fn::apply<common_type<T, U>>(x, y);
-    }
-
-};
-
-static constexpr float_equal_fn float_equal{};
 
 struct compare_mag_fn
 {
