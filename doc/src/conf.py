1b1715249b2571b353829af9012126de9bbe095b
Daniel Lowell <daniel.lowell@amd.com>
2017-06-26 16:13:57 -0500

    Added in pip requirement ReadTheDoc theme. Modfied top level README.md text to have more information about dependencies for doxygen and sphinx Added ./doc level README.md with external link and more info about pip

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -86,13 +86,18 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'agogo'
-#html_theme = 'sphinx_rtd_theme'
-#html_theme_options = {
-#    'collapse_navigation': True,
-#    'display_version': False,
-#    'sticky_navigation': True,
-#}
+
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': True,
+}
+
+html_title = "MIOpen version 1.0 AMD's deep learning library"
+
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
