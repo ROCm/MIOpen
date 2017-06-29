2922e31044f2d6eab04e074b295f5b3fd77e8635
Paul <pfultz2@yahoo.com>
2017-06-29 14:38:22 -0500

    Remove tabs from cmake files
    [ROCm/MIOpen commit: 959c4193526c52d61e46e4c41a9ae66ec750058e]

--- cmake/FindOpenCL.cmake
+++ cmake/FindOpenCL.cmake
@@ -24,51 +24,51 @@
 # 
 ################################################################################
 find_path(OPENCL_INCLUDE_DIRS
-	NAMES OpenCL/cl.h CL/cl.h
-	HINTS
-	${OPENCL_ROOT}/include
-	$ENV{AMDAPPSDKROOT}/include
-	$ENV{CUDA_PATH}/include
-	PATHS
-	/usr/include
-	/usr/local/include
-	/usr/local/cuda/include
-	/opt/cuda/include
-	/opt/rocm/opencl/include
-	DOC "OpenCL header file path"
-	)
+    NAMES OpenCL/cl.h CL/cl.h
+    HINTS
+    ${OPENCL_ROOT}/include
+    $ENV{AMDAPPSDKROOT}/include
+    $ENV{CUDA_PATH}/include
+    PATHS
+    /usr/include
+    /usr/local/include
+    /usr/local/cuda/include
+    /opt/cuda/include
+    /opt/rocm/opencl/include
+    DOC "OpenCL header file path"
+    )
 mark_as_advanced( OPENCL_INCLUDE_DIRS )
 
 if("${CMAKE_SIZEOF_VOID_P}" EQUAL "8")
-	find_library( OPENCL_LIBRARIES
-		NAMES OpenCL
-		HINTS
-		${OPENCL_ROOT}/lib
-		$ENV{AMDAPPSDKROOT}/lib
-		$ENV{CUDA_PATH}/lib
-		DOC "OpenCL dynamic library path"
-		PATH_SUFFIXES x86_64 x64 x86_64/sdk
-		PATHS
-		/usr/lib
-		/usr/local/cuda/lib
-		/opt/cuda/lib
-		/opt/rocm/opencl/lib
-		)
+    find_library( OPENCL_LIBRARIES
+        NAMES OpenCL
+        HINTS
+        ${OPENCL_ROOT}/lib
+        $ENV{AMDAPPSDKROOT}/lib
+        $ENV{CUDA_PATH}/lib
+        DOC "OpenCL dynamic library path"
+        PATH_SUFFIXES x86_64 x64 x86_64/sdk
+        PATHS
+        /usr/lib
+        /usr/local/cuda/lib
+        /opt/cuda/lib
+        /opt/rocm/opencl/lib
+        )
 else( )
-	find_library( OPENCL_LIBRARIES
-		NAMES OpenCL
-		HINTS
-		${OPENCL_ROOT}/lib
-		$ENV{AMDAPPSDKROOT}/lib
-		$ENV{CUDA_PATH}/lib
-		DOC "OpenCL dynamic library path"
-		PATH_SUFFIXES x86 Win32
+    find_library( OPENCL_LIBRARIES
+        NAMES OpenCL
+        HINTS
+        ${OPENCL_ROOT}/lib
+        $ENV{AMDAPPSDKROOT}/lib
+        $ENV{CUDA_PATH}/lib
+        DOC "OpenCL dynamic library path"
+        PATH_SUFFIXES x86 Win32
 
-		PATHS
-		/usr/lib
-		/usr/local/cuda/lib
-		/opt/cuda/lib
-		)
+        PATHS
+        /usr/lib
+        /usr/local/cuda/lib
+        /opt/cuda/lib
+        )
 endif( )
 mark_as_advanced( OPENCL_LIBRARIES )
 
@@ -80,5 +80,5 @@ set(OpenCL_LIBRARIES ${OPENCL_LIBRARIES} CACHE INTERNAL "")
 set(OpenCL_INCLUDE_DIRS ${OPENCL_INCLUDE_DIRS} CACHE INTERNAL "")
 
 if( NOT OPENCL_FOUND )
-	message( STATUS "FindOpenCL looked for libraries named: OpenCL" )
+    message( STATUS "FindOpenCL looked for libraries named: OpenCL" )
 endif()
