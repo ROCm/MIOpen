662691bcdf0f0baee57481792931e2ded61cf8bd
Daniel Lowell <dlowell@tengu.amd.com>
2017-07-13 17:09:28 -0500

    Activating DPP kernels for spatial batchnorm. Excluding gpfx803 from DPP kernels for variants 2 and 4 of bwd spatial pass.
    [ROCm/MIOpen commit: 4bab0c4685d1b0fee2de14f20ddf8fe9fa0ce7f0]

--- test/bn_spatial_test.cpp
+++ test/bn_spatial_test.cpp
@@ -1072,7 +1072,6 @@ struct batch_norm_spatial_driver : test_driver
 
     void run()
     {
-        return;
         int n, c, h, w;
 
         std::tie(n, c, h, w) = miopen::tie4(input.desc.GetLengths());
