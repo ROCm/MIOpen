8433380d1b96d7fc98f44df8fc147d6d706469eb
mayank daga <mayank.daga@amd.com>
2017-07-15 15:58:39 -0500

    fixed tidy

--- src/convolution.cpp
+++ src/convolution.cpp
@@ -210,11 +210,8 @@ bool ConvolutionDescriptor::IsBwdWeightsDirectSupported(const TensorDescriptor& 
 {
     int _kernel_size0, _kernel_size1;
     std::tie(std::ignore, std::ignore, _kernel_size0, _kernel_size1) = tie4(wDesc.GetLengths());
-    
-    if(_kernel_size0 == 1 && _kernel_size1 == 1 && u != 1 && v != 1)
-        return false;
 
-    return true;
+    return !(_kernel_size0 == 1 && _kernel_size1 == 1 && u != 1 && v != 1);
 }
 
 size_t ConvolutionDescriptor::ForwardGetWorkSpaceSize(Handle& handle,
