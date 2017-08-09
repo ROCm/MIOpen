ae97ec33814388ec9204be68e1119b5718c8920b
Paul <pfultz2@yahoo.com>
2017-08-09 10:42:09 -0500

    Formatting

--- src/include/miopen/binary_cache.hpp
+++ src/include/miopen/binary_cache.hpp
@@ -6,7 +6,10 @@
 namespace miopen {
 
 std::string LoadBinary(const std::string& name, const std::string& args, bool is_kernel_str);
-void SaveBinary(const std::string& binary_path, const std::string& name, const std::string& args, bool is_kernel_str);
+void SaveBinary(const std::string& binary_path,
+                const std::string& name,
+                const std::string& args,
+                bool is_kernel_str);
 
 } // namespace miopen
 
