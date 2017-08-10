a4fcadfec63b4e173198d5603761918135085e91
Paul <pfultz2@yahoo.com>
2017-08-10 18:48:43 -0500

    Refactor tmp dir to a seperate file

--- src/hipoc/hipoc_program.cpp
+++ src/hipoc/hipoc_program.cpp
@@ -29,6 +29,7 @@
 #include <miopen/kernel.hpp>
 #include <miopen/kernel_warnings.hpp>
 #include <miopen/stringutils.hpp>
+#include <miopen/tmp_dir.hpp>
 
 #include <sstream>
 
@@ -36,51 +37,7 @@
 
 namespace miopen {
 
-std::string quote(std::string s) { return '"' + s + '"'; }
-
-void system_cmd(std::string cmd)
-{
-// std::cout << cmd << std::endl;
-// We shouldn't call system commands
-#ifdef MIOPEN_USE_CLANG_TIDY
-    (void)cmd;
-#else
-    if(std::system(cmd.c_str()) != 0)
-        MIOPEN_THROW("Can't execute " + cmd);
-#endif
-}
-
-struct tmp_dir
-{
-    std::string name;
-    tmp_dir(std::string prefix)
-    {
-        std::string s = "/tmp/miopen-" + std::move(prefix) + "-XXXXXX";
-        std::vector<char> t(s.begin(), s.end());
-        t.push_back(0);
-        name = mkdtemp(t.data());
-    }
-
-    void execute(std::string exe, std::string args)
-    {
-        std::string cd  = "cd " + this->name + "; ";
-        std::string cmd = cd + exe + " " + args; // + " > /dev/null";
-        system_cmd(cmd);
-    }
-
-    std::string path(std::string f) { return name + '/' + f; }
-
-    ~tmp_dir()
-    {
-        if(!name.empty())
-        {
-            std::string cmd = "rm -rf " + name;
-            system_cmd(cmd);
-        }
-    }
-};
-
-void WriteFile(const std::string& content, const std::string& name)
+void WriteFile(const std::string& content, const boost::filesystem::path& name)
 {
     // std::cerr << "Write file: " << name << std::endl;
     HIPOCProgram::FilePtr f{std::fopen(name.c_str(), "w")};
@@ -92,10 +49,10 @@ hipModulePtr CreateModule(const std::string& program_name, std::string params, b
 {
     std::string filename =
         is_kernel_str ? "tinygemm.cl" : program_name; // jn : don't know what this is
-    tmp_dir dir{filename};
-    std::string bin_file   = dir.path(filename) + ".bin";
-    std::string hsaco_file = dir.path(filename) + ".o";
-    std::string obj_file   = dir.path(filename) + ".obj";
+    TmpDir dir{filename};
+    auto bin_file   = dir.path / (filename + ".bin");
+    auto hsaco_file = dir.path / (filename + ".o");
+    auto obj_file   = dir.path / (filename + ".obj");
 
     std::string src = is_kernel_str ? program_name : GetKernelSrc(program_name);
     if(!is_kernel_str && miopen::EndsWith(program_name, ".so"))
@@ -110,7 +67,7 @@ hipModulePtr CreateModule(const std::string& program_name, std::string params, b
     else
     {
 
-        WriteFile(src, dir.path(filename));
+        WriteFile(src, dir.path / filename);
 
 #if MIOPEN_BUILD_DEV
         params += " -Werror" + KernelWarningsString();
@@ -120,11 +77,11 @@ hipModulePtr CreateModule(const std::string& program_name, std::string params, b
 #else
         params += " -Wno-everything";
 #endif
-        dir.execute(HIP_OC_COMPILER, params + " " + filename + " -o " + hsaco_file);
+        dir.Execute(HIP_OC_COMPILER, params + " " + filename + " -o " + hsaco_file.string());
     }
 
     hipModule_t raw_m;
-    auto status = hipModuleLoad(&raw_m, hsaco_file.c_str());
+    auto status = hipModuleLoad(&raw_m, hsaco_file.string().c_str());
     hipModulePtr m{raw_m};
     if(status != hipSuccess)
         MIOPEN_THROW_HIP_STATUS(status, "Failed creating module");
