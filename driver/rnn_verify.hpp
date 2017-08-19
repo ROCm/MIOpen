ec8fcb21ea240ebb7d136fe8186de8294c85273b
Zhou <jingz@amd.com>
2017-08-19 16:31:31 -0700

    bwd weight
    [ROCm/MIOpen commit: d48359976d1d2831c47e9a16eebcf92060825c5c]

--- driver/rnn_verify.hpp
+++ driver/rnn_verify.hpp
@@ -517,8 +517,8 @@ void RunRNNBackwardWeightCPUVerify(std::vector<T>& in,
 	int in_h, // input data length
 	int seqLength, // Number of iterations to unroll over
 	bool bidirection, // whether using bidirectional net
-	int hy_d, // 1 by numlayer (number of stacks of hidden layers) for unidirection, 2 by numlayer for bidirection
 	bool biased, // whether using bias
+	int hy_d, // 1 by numlayer (number of stacks of hidden layers) for unidirection, 2 by numlayer for bidirection
 	int hy_n, // equal to input batch size in_n[0]
 	int hy_h, // hidden state number
 //	std::vector<int>& out_n, // equals in_n
