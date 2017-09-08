29942e9adafc7b76f9fc4360a9a322530cdae16c
Daniel Lowell <daniel.lowell@amd.com>
2017-09-08 12:22:07 -0500

    Added a bunch of API interfaces. This needs a lot more work, but this is my first pass.
    [ROCm/MIOpen commit: 694ff93e083661fcf8beb037ed77cf56bde0d4d3]

--- src/include/miopen/rnn.hpp
+++ src/include/miopen/rnn.hpp
@@ -25,19 +25,19 @@ struct PerfField
 struct RNNDescriptor : miopenRNNDescriptor {
 	
 	RNNDescriptor();
-    RNNDescriptor(int seqLength = 1, int layer = 1, miopenRNNDirectionMode_t bidir = 0);
-    RNNDescriptor(miopenRNNMode_t p_mode, int seqLength = 1, int layer = 1, miopenRNNDirectionMode_t bidir = 0);
-    RNNDescriptor(int hsize,
-                    int nlayers = 1,
-                    miopenRNNInputMode_t inputMode, 
-                    miopenRNNDirectionMode_t dirMode, 
-                    miopenRNNMode_t rnnMode, 
-                    miopenRNNAlgo_t algoMode,
-                    miopenDataType_t dataType);
+    RNNDescriptor(int sLength, int layer, miopenRNNDirectionMode_t bidir = miopenRNNunidirection);
+    RNNDescriptor(miopenRNNMode_t p_mode, int sLength, int layer, miopenRNNDirectionMode_t bidir);
+    RNNDescriptor(int hsz, 
+                    int layers, 
+                    miopenRNNInputMode_t inMode, 
+                    miopenRNNDirectionMode_t bidir,
+                    miopenRNNMode_t rmode,
+                    miopenRNNAlgo_t amode,
+                    miopenDataType_t dType = miopenFloat);
     
     
     
-
+    int hsize;
     int seqLength;
     int nlayers;
     
@@ -45,16 +45,100 @@ struct RNNDescriptor : miopenRNNDescriptor {
     miopenRNNDirectionMode_t dirMode;
     miopenRNNAlgo_t          algoMode;
     miopenRNNInputMode_t     inputMode;
+    miopenDataType_t         dataType;
+    /*
+        //TODO (dlowell)  this requires array_view for the array of tensor descriptors
+     
+    size_t GetWorkspaceSize(Handle& handle,
+                                const int seqLength,
+                                const TensorDescriptor& *xDesc) const;
+    size_t GetReserveSize(Handle& handle,
+                                const int seqLength,
+                                const TensorDescriptor *xDesc) const;  
+     */
+    size_t GetParamsSize(Handle& handle,
+                        const TensorDescriptor& xDesc,
+                        miopenDataType_t dtype) const;
+   
+    void GetLayerParam(Handle& handle,
+                        const TensorDescriptor& xDesc,
+                        const TensorDescriptor& wDesc,
+                        ConstData_t w,
+                        const int layerID,
+                        const TensorDescriptor& paramDesc,
+                        Data_t **layerParam) const;
     
-    void GetWorkspaceSize() const;
-    void GetReserveSize() const;  
-    void GetParamsSize() const;
-    void GetLayerParam() const;
-    void GetLayerBias() const;
-    void ForwardInferRNNCell() const;
-    void ForwardTrainRNNCell() const;
-    void BackwardDataRNNCell() const;
-    void BackwardWeightsRNNCell() const;
+    void GetLayerBias(Handle& handle,
+                        const TensorDescriptor& xDesc,
+                        const TensorDescriptor& wDesc,
+                        ConstData_t w,
+                        const int layerID,
+                        const TensorDescriptor& biasDesc,
+                        Data_t **layerBias) const;
+    
+    void ForwardInferRNNCell(Handle& handle,
+                            const TensorDescriptor& xDesc,
+                            ConstData_t x,
+                            const TensorDescriptor& hxDesc,
+                            ConstData_t hx,
+                            const TensorDescriptor& wDesc,
+                            ConstData_t w,
+                            const TensorDescriptor& hyDesc,
+                            ConstData_t hy,
+                            const TensorDescriptor& yDesc,
+                            Data_t y,
+                            Data_t workSpace,
+                            size_t workSpaceSize) const;
+    
+    void ForwardTrainRNNCell(Handle& handle,
+                            const TensorDescriptor& xDesc,
+                            ConstData_t x,
+                            const TensorDescriptor& hxDesc,
+                            ConstData_t hx,
+                            const TensorDescriptor& wDesc,
+                            ConstData_t w,
+                            const TensorDescriptor& yDesc,
+                            Data_t y,
+                            const TensorDescriptor& hyDesc,
+                            Data_t hy,
+                            Data_t workSpace,
+                            size_t workSpaceSize,
+                            Data_t reserveSpace,
+                            size_t reserveSpaceSize) const;
+    
+    void BackwardDataRNNCell(Handle& handle,
+                            const TensorDescriptor& yDesc,
+                            ConstData_t y,
+                            const TensorDescriptor& dyDesc,
+                            ConstData_t dy,
+                            const TensorDescriptor& dhyDesc,
+                            ConstData_t dhy,
+                            const TensorDescriptor& wDesc,
+                            ConstData_t w,
+                            const TensorDescriptor& hxDesc,
+                            ConstData_t hx,
+                            const TensorDescriptor& dxDesc,
+                            Data_t dx,
+                            const TensorDescriptor& dhxDesc,
+                            Data_t dhx,
+                            Data_t workSpace,
+                            size_t workSpaceSize,
+                            ConstData_t reserveSpace,
+                            size_t reserveSpaceSize) const;
+    
+    void BackwardWeightsRNNCell(Handle& handle,
+                            const TensorDescriptor& xDesc,
+                            ConstData_t x,
+                            const TensorDescriptor& hxDesc,
+                            ConstData_t hx,
+                            const TensorDescriptor& yDesc,
+                            ConstData_t y,
+                            const TensorDescriptor& dwDesc,
+                            Data_t dw,
+                            ConstData_t workSpace,
+                            size_t workSpaceSize,
+                            ConstData_t reserveSpace,
+                            size_t reserveSpaceSize) const;
 	
 };
 
