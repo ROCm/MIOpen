3772b71612c603c89be1bec11838342660ecc359
Daniel Lowell <daniel.lowell@amd.com>
2017-09-19 17:32:22 -0500

    Finished 5-1d tensor ops. Code cleanup needed.
    [ROCm/MIOpen commit: d1919c164c9c127e9fa4efee26872b9bef4c6352]

--- src/ocl/tensorocl.cpp
+++ src/ocl/tensorocl.cpp
@@ -29,6 +29,8 @@
 #include <miopen/tensor_ops.hpp>
 #include <numeric>
 
+#define MIO_TENSOROCL_DEBUG 0
+
 namespace miopen {
 
 void SetTensor(Handle& handle, const TensorDescriptor& yDesc, Data_t y, const void* alpha)
@@ -143,7 +145,8 @@ void OpTensor(Handle& handle,
               Data_t CTensor)
 {
                 
-    printf("\n\nSTARTING\n\n.");
+    
+
     if(ATensor == nullptr || BTensor == nullptr || CTensor == nullptr)
     {
         MIOPEN_THROW(miopenStatusBadParm);
@@ -162,6 +165,12 @@ void OpTensor(Handle& handle,
     auto b_lens = bTensorDesc.GetLengths();
     auto c_lens = cTensorDesc.GetLengths();
     auto dims   = c_lens.size();
+    
+    if(c_lens.size() > 5)
+    {
+        MIOPEN_THROW("Tensor dimension larger than 5: " +
+                     std::to_string(c_lens.size()));
+    }
     
     if(b_lens.size() != c_lens.size())
     {
@@ -184,20 +193,16 @@ void OpTensor(Handle& handle,
     //int work_per_wg = std::accumulate(c_lens.begin(), c_lens.end(), 1, std::multiplies<int>());
     printf("work_per_wg0: %d, d: %ld\n", work_per_wg, d);
     
-
-    int c_n = 0;
-    int c_c = 0;
-    int c_d = 0;
-    int c_h = 0;
-    int c_w = 0;
-    
     int b_n = 0;
     int b_c = 0;
     int b_d = 0;
     int b_h = 0;
     int b_w = 0;
-    
+    int b_nstride = 0;
+    int b_cstride = 0;
+    int b_dstride = 0;
     auto blens = bTensorDesc.GetLengths();
+    auto bstrides =bTensorDesc.GetStrides();
     auto bsize = blens.size();
 
     if(bsize == 5){
@@ -206,7 +211,13 @@ void OpTensor(Handle& handle,
         b_d = blens[2];
         b_h = blens[3];
         b_w = blens[4];
+        b_nstride = bstrides[0];
+        b_cstride = bstrides[1];
+        b_dstride = bstrides[2];
+#if(MIO_TENSOROCL_DEBUG==1)
         printf("blens[0,1,2,3,4]: %lu, %lu, %lu, %lu, %lu\n", blens[0], blens[1], blens[2], blens[3], blens[4]);
+        printf("bstride[0,1,2]: %lu, %lu, %lu\n", bstrides[0], bstrides[1], bstrides[2]);        
+#endif
     }
     else if(bsize == 4)
     {
@@ -214,28 +225,53 @@ void OpTensor(Handle& handle,
         b_c = blens[1];
         b_h = blens[2];
         b_w = blens[3];
+        
+        b_nstride = bstrides[0];
+        b_cstride = bstrides[1];
+#if(MIO_TENSOROCL_DEBUG==1)
         printf("blens[0,1,2,3]: %lu, %lu, %lu, %lu\n", blens[0], blens[1], blens[2], blens[3]);
+        printf("bstride[0,1]: %lu, %lu\n", bstrides[0], bstrides[1]);        
+#endif
     }
     else if(bsize == 3)
     {
         b_n = blens[0];
         b_c = blens[1];
         b_h = blens[2];
+        b_nstride = bstrides[0];
+#if(MIO_TENSOROCL_DEBUG==1)
         printf("blens[0,1,2]: %lu, %lu, %lu\n", blens[0], blens[1], blens[2]);
+        printf("bstride[0]: %lu\n", bstrides[0]);
+#endif
     }
     else if(bsize == 2)
     {
         b_n = blens[0];
         b_c = blens[1];
+        b_nstride = bstrides[0];
+#if(MIO_TENSOROCL_DEBUG==1)
         printf("blens[0,1]: %lu, %lu\n", blens[0], blens[1]);
+        printf("bstride[0]: %lu\n", bstrides[0]);
+#endif
     }
     else if(bsize == 1)
     {
         b_n = blens[0];
+#if(MIO_TENSOROCL_DEBUG==1)
         printf("blens[0]: %lu\n", blens[0]);
+#endif
     }
 
+    int c_n = 0;
+    int c_c = 0;
+    int c_d = 0;
+    int c_h = 0;
+    int c_w = 0;
+    int c_dstride = 0;
+    int c_nstride = 0;
+    int c_cstride = 0;
     auto clens = cTensorDesc.GetLengths();
+    auto cstrides = cTensorDesc.GetStrides();
     auto csize = clens.size();
     if(csize == 5){
         c_n = clens[0];
@@ -243,87 +279,56 @@ void OpTensor(Handle& handle,
         c_d = clens[2];
         c_h = clens[3];
         c_w = clens[4];
+        c_nstride = cstrides[0];
+        c_cstride = cstrides[1];
+        c_dstride = cstrides[2];
+#if(MIO_TENSOROCL_DEBUG==1)
         printf("clens[0,1,2,3,4]: %lu, %lu, %lu, %lu, %lu\n", clens[0], clens[1], clens[2], clens[3], clens[4]);
-
+        printf("cstride[0,1,2,3,4]: %lu, %lu, %lu\n", cstrides[0], cstrides[1], cstrides[2]);
+#endif
     }
     else if(csize == 4)
     {
         c_n = clens[0];
         c_c = clens[1];
         c_h = clens[2];
-        c_w = clens[3];
-        printf("clens[0,1,2,3]: %lu, %lu, %lu, %lu\n", clens[0], clens[1], clens[2], clens[3]);
-
+        c_w = clens[3];       
+        c_nstride = cstrides[0];
+        c_cstride = cstrides[1];
+#if(MIO_TENSOROCL_DEBUG==1)
+        printf("clens[0,1,2,3]: %lu, %lu, %lu, %lu\n", clens[0], clens[1], clens[2], clens[3]);        
+        printf("cstride[0,1]: %lu, %lu\n", cstrides[0], cstrides[1]);
+#endif
     }
     else if(csize == 3)
     {
         c_n = clens[0];
         c_c = clens[1];
         c_h = clens[2];
+        c_nstride = cstrides[0];
+#if(MIO_TENSOROCL_DEBUG==1)
         printf("clens[0,1,2]: %lu, %lu, %lu\n", clens[0], clens[1], clens[2]);
+        printf("cstride[0]: %lu\n", cstrides[0]);
+#endif
     }
     else if(csize == 2)
     {
         c_n = clens[0];
         c_c = clens[1];
-        printf("clens[0,1,2]: %lu, %lu\n", clens[0], clens[1]);
+        c_nstride = cstrides[0];
+#if(MIO_TENSOROCL_DEBUG==1)
+        printf("clens[0,1]: %lu, %lu\n", clens[0], clens[1]);
+        printf("cstride[0]: %lu\n", cstrides[0]);
+#endif
     }
     else if(csize == 1)
     {
         c_n = clens[0];
-        printf("clens[0,1,2]: %lu\n", clens[0]);
+#if(MIO_TENSOROCL_DEBUG==1)
+        printf("clens[0]: %lu\n", clens[0]);
+#endif
     }
 
-    int c_dstride = 0;
-    int c_nstride = 0;
-    int c_cstride = 0;
-
-    //std::tie(c_nstride, c_cstride, std::ignore, std::ignore) = tien<4>(cTensorDesc.GetStrides());
-    auto cstrides = cTensorDesc.GetStrides();
-    if(csize == 5){
-        c_nstride = cstrides[0];
-        c_cstride = cstrides[1];
-        c_dstride = cstrides[2];
-        printf("cstride[0,1,2,3,4]: %lu, %lu, %lu\n", cstrides[0], cstrides[1], cstrides[2]);
-    }
-    else if(csize == 4)
-    {
-        c_nstride = cstrides[0];
-        c_cstride = cstrides[1];
-        printf("cstride[0,1]: %lu, %lu\n", cstrides[0], cstrides[1]);
-    }
-    else if(csize == 3 || csize == 2)
-    {
-        c_nstride = cstrides[0];
-        printf("cstride[0,1,2,3,4]: %lu\n", cstrides[0]);
-    }
-    
-    int b_nstride = 0;
-    int b_cstride = 0;
-    int b_dstride = 0;
-    
-    auto bstrides =bTensorDesc.GetStrides();
-    if(bsize == 5){
-        b_nstride = bstrides[0];
-        b_cstride = bstrides[1];
-        b_dstride = bstrides[2];
-        printf("bstride[0,1,2]: %lu, %lu, %lu\n", bstrides[0], bstrides[1], bstrides[2]);
-    }
-    else if(bsize == 4)
-    {
-        b_nstride = bstrides[0];
-        b_cstride = bstrides[1];
-        printf("bstride[0,1]: %lu, %lu\n", bstrides[0], bstrides[1]);
-    }
-    else if(bsize == 3 || bsize == 2)
-    {
-        b_nstride = bstrides[0];
-        printf("bstride[0]: %lu\n", bstrides[0]);
-    }
-    //printf("bstride[0,1,2]: %d, %d, %d\n", bstrides[0], bstrides[1], bstrides[2]);
-    //printf("bstride[0,1,2,3,4]: %lu, %lu, %lu, %lu, %lu\n", bstrides[0], bstrides[1], bstrides[2], bstrides[3], bstrides[4]);
-    //std::tie(b_nstride, b_cstride, std::ignore, std::ignore) = tien<4>(bTensorDesc.GetStrides());
-   // printf("D is %d\n", d);
     
     unsigned int bitmap = 0;
     // update bitmap for first_not_one
@@ -341,20 +346,18 @@ void OpTensor(Handle& handle,
     // Is the no. of work-groups and the work for each wg balanced?
     auto fwd_conv_bias = bitmap == (1 << 2) ? 1 : 0;
     auto incr_wg       = 0;
-    if(fwd_conv_bias == 1 && num_wg < 640 && work_per_wg > 256)
+    //This block gives off indexing for 5d tensors, skipping
+    if(fwd_conv_bias == 1 && dims < 5 && num_wg < 640 && work_per_wg > 256)
     { // 640 workgroups of size 256 needed to completely fill the GPU
         work_per_wg /= c_n;
         num_wg *= c_n;
         incr_wg = 1;
     }
-    
-    printf("work_per_wg2: %d, num_wg2: %d\n", work_per_wg, num_wg);
     size_t local_threads = 256;
 
     // Does the bitmap contain leading ones, i.e. 1,1,1,0 or 1,1,0,0
     // or 1,1,1,1 or 1,0,0,0
-   // bool leading_ones = IsBitmapLeadingOnes(bitmap, 4, (d - 2)); // DLOWELL: not quite working
-    bool leading_ones = IsBitmapLeadingOnes(bitmap, dims, (d - 2)); // DLOWELL: not quite working
+    bool leading_ones = IsBitmapLeadingOnes(bitmap, dims, (d - 2)); //DLOWELL
     if(leading_ones && work_per_wg < 64)
     {
         local_threads = 64;
