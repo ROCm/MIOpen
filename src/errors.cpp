2572959c02bf3bb1749a90e5c7c069523d721ae4
Paul <pfultz2@yahoo.com>
2017-09-24 15:55:13 -0500

    Make what() inline to workaround HIP

--- src/errors.cpp
+++ src/errors.cpp
@@ -27,6 +27,6 @@
 
 namespace miopen {
 
-const char* Exception::what() const noexcept { return message.c_str(); }
+// const char* Exception::what() const noexcept { return message.c_str(); }
 
 } // namespace miopen
