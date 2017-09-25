51580c84f38fbdd331a6320c4732457a6aaaf07a
Ben Sander <ben.sander@amd.com>
2017-09-25 20:56:22 +0000

    Incorporate review and tidy feedback
    [ROCm/MIOpen commit: 291b69935da9a13b9ea96620e719ab4758148ca4]

--- src/include/miopen/check_numerics.hpp
+++ src/include/miopen/check_numerics.hpp
@@ -2,8 +2,8 @@
 #include <miopen/util.hpp>
 
 namespace miopen {
-    int CheckNumericsEnabled(int bitmask=-1);
+    int CheckNumericsEnabled(int bitMask=-1);
 
-    bool checkNumericsInput(Handle &handle, const TensorDescriptor &dDesc, ConstData_t d);
-    bool checkNumericsOutput(Handle &handle, const TensorDescriptor &dDesc, Data_t d);
-};
+    bool checkNumericsInput(Handle &handle, const TensorDescriptor &dDesc, ConstData_t data);
+    bool checkNumericsOutput(Handle &handle, const TensorDescriptor &dDesc, Data_t data);
+} // namespace miopen
