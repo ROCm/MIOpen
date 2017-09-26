4ed0f6276c1d3eb2bdeb679941b9228fa4654ba3
Daniel Lowell <daniel.lowell@amd.com>
2017-09-25 19:57:04 -0500

    Added recursive CPU code to combine all dimensions supported. Refactored OCL interface code to reduce lines. Removed OP macros in place of function calls.

--- test/tensor_ops.cpp
+++ test/tensor_ops.cpp
@@ -76,161 +76,49 @@ struct verify_tensor_ops : tensor_ops_base<T>
         b = pb(dims);
     }
 
+    T add_elem(T a, T b) { return a + b; }
+
+    void tensor_for_loop(const tensor<T>& a,
+                         const tensor<T>& b,
+                         tensor<T>& c,
+                         const std::vector<size_t>& a_dims,
+                         const std::vector<size_t>& b_dims,
+                         int coffset,
+                         int boffset,
+                         int dim)
+    {
+
+        int cstride = c.desc.GetStrides()[dim];
+        int bstride = b.desc.GetStrides()[dim];
+
+        for(int idx = 0; idx < a_dims[dim]; idx++)
+        {
+            size_t acindex = coffset + cstride * idx;
+            size_t bindex  = (b_dims[dim] == a_dims[dim]) ? boffset + bstride * idx : boffset;
+
+            if(bindex < b.desc.GetElementSize())
+                c[acindex] = add_elem(a[acindex], b[bindex]);
+            if(dim < (a_dims.size() - 1))
+            {
+
+                tensor_for_loop(a, b, c, a_dims, b_dims, acindex, bindex, dim + 1);
+            }
+        }
+        return;
+    }
+
     tensor<T> cpu()
     {
         c = a;
         std::fill(c.begin(), c.end(), 0);
-        const std::vector<size_t>& a_dims = a.desc.GetLengths();
         std::fill(c.begin(), c.end(), 0);
-        int c_n, c_c, c_d, c_h, c_w;
-        int b_n, b_c, b_d, b_h, b_w;
+        auto clens    = c.desc.GetLengths();
+        auto blens    = b.desc.GetLengths();
+        auto bstrides = b.desc.GetStrides();
+        auto cstrides = c.desc.GetStrides();
 
-        auto dims = a_dims.size();
-        switch(dims)
-        {
-        case 5:
-            std::tie(c_n, c_c, c_d, c_h, c_w) = miopen::tien<5>(c.desc.GetLengths());
-            std::tie(b_n, b_c, b_d, b_h, b_w) = miopen::tien<5>(b.desc.GetLengths());
-            for(int n = 0; n < c_n; n++)
-            {
-                c(n, 0, 0, 0, 0) = (b_n == c_n) ? a(n, 0, 0, 0, 0) + b(n, 0, 0, 0, 0)
-                                                : a(n, 0, 0, 0, 0) + b(0, 0, 0, 0, 0);
-                for(int x = 0; x < c_c; x++)
-                {
-                    c(n, x, 0, 0, 0) = (b_c == c_c)
-                                           ? a(n, x, 0, 0, 0) + b((b_n == c_n ? n : 0), x, 0, 0, 0)
-                                           : a(n, x, 0, 0, 0) + b((b_n == c_n ? n : 0), 0, 0, 0, 0);
+        tensor_for_loop(a, b, c, clens, blens, 0, 0, 0);
 
-                    for(int d = 0; d < c_d; d++)
-                    {
-                        c(n, x, d, 0, 0) =
-                            (b_d == c_d)
-                                ? a(n, x, d, 0, 0) +
-                                      b((b_n == c_n ? n : 0), (b_c == c_c ? x : 0), d, 0, 0)
-                                : a(n, x, d, 0, 0) +
-                                      b((b_n == c_n ? n : 0), (b_c == c_c ? x : 0), 0, 0, 0);
-                        for(int h = 0; h < c_h; h++)
-                        {
-                            c(n, x, d, h, 0) = (b_h == c_h)
-                                                   ? a(n, x, d, h, 0) + b((b_n == c_n ? n : 0),
-                                                                          (b_c == c_c ? x : 0),
-                                                                          (b_d == c_d ? d : 0),
-                                                                          h,
-                                                                          0)
-                                                   : a(n, x, d, h, 0) + b((b_n == c_n ? n : 0),
-                                                                          (b_c == c_c ? x : 0),
-                                                                          (b_d == c_d ? d : 0),
-                                                                          0,
-                                                                          0);
-
-                            for(int w = 0; w < c_w; w++)
-                            {
-                                c(n, x, d, h, w) = (b_w == c_w)
-                                                       ? a(n, x, d, h, w) + b((b_n == c_n ? n : 0),
-                                                                              (b_c == c_c ? x : 0),
-                                                                              (b_d == c_d ? d : 0),
-                                                                              (b_h == c_h ? h : 0),
-                                                                              w)
-                                                       : a(n, x, d, h, w) + b((b_n == c_n ? n : 0),
-                                                                              (b_c == c_c ? x : 0),
-                                                                              (b_d == c_d ? d : 0),
-                                                                              (b_h == c_h ? h : 0),
-                                                                              0);
-                            }
-                        }
-                    }
-                }
-            }
-            break;
-
-        case 4:
-
-            std::tie(c_n, c_c, c_h, c_w) = miopen::tien<4>(c.desc.GetLengths());
-            std::tie(b_n, b_c, b_h, b_w) = miopen::tien<4>(b.desc.GetLengths());
-            for(int n = 0; n < c_n; n++)
-            {
-                c(n, 0, 0, 0) =
-                    (b_n == c_n) ? a(n, 0, 0, 0) + b(n, 0, 0, 0) : a(n, 0, 0, 0) + b(0, 0, 0, 0);
-                for(int x = 0; x < c_c; x++)
-                {
-                    c(n, x, 0, 0) = (b_c == c_c) ? a(n, x, 0, 0) + b((b_n == c_n ? n : 0), x, 0, 0)
-                                                 : a(n, x, 0, 0) + b((b_n == c_n ? n : 0), 0, 0, 0);
-
-                    for(int h = 0; h < c_h; h++)
-                    {
-                        c(n, x, h, 0) =
-                            (b_h == c_h)
-                                ? a(n, x, h, 0) +
-                                      b((b_n == c_n ? n : 0), (b_c == c_c ? x : 0), h, 0)
-                                : a(n, x, h, 0) +
-                                      b((b_n == c_n ? n : 0), (b_c == c_c ? x : 0), 0, 0);
-
-                        for(int w = 0; w < c_w; w++)
-                        {
-                            c(n, x, h, w) = (b_w == c_w)
-                                                ? a(n, x, h, w) + b((b_n == c_n ? n : 0),
-                                                                    (b_c == c_c ? x : 0),
-                                                                    (b_h == c_h ? h : 0),
-                                                                    w)
-                                                : a(n, x, h, w) + b((b_n == c_n ? n : 0),
-                                                                    (b_c == c_c ? x : 0),
-                                                                    (b_h == c_h ? h : 0),
-                                                                    0);
-                        }
-                    }
-                }
-            }
-            break;
-
-        case 3:
-
-            std::tie(c_n, c_c, c_h) = miopen::tien<3>(c.desc.GetLengths());
-            std::tie(b_n, b_c, b_h) = miopen::tien<3>(b.desc.GetLengths());
-            for(int n = 0; n < c_n; n++)
-            {
-                c(n, 0, 0) = (b_n == c_n) ? a(n, 0, 0) + b(n, 0, 0) : a(n, 0, 0) + b(0, 0, 0);
-                for(int x = 0; x < c_c; x++)
-                {
-                    c(n, x, 0) = (b_c == c_c) ? a(n, x, 0) + b((b_n == c_n ? n : 0), x, 0)
-                                              : a(n, x, 0) + b((b_n == c_n ? n : 0), 0, 0);
-                    for(int h = 0; h < c_h; h++)
-                    {
-                        c(n, x, h) =
-                            (b_h == c_h)
-                                ? a(n, x, h) + b((b_n == c_n ? n : 0), (b_c == c_c ? x : 0), h)
-                                : a(n, x, h) + b((b_n == c_n ? n : 0), (b_c == c_c ? x : 0), 0);
-                    }
-                }
-            }
-            break;
-
-        case 2:
-            std::tie(c_n, c_c) = miopen::tien<2>(c.desc.GetLengths());
-            std::tie(b_n, b_c) = miopen::tien<2>(b.desc.GetLengths());
-            for(int n = 0; n < c_n; n++)
-            {
-                c(n, 0) = (b_n == c_n) ? a(n, 0) + b(n, 0) : a(n, 0) + b(0, 0);
-                for(int x = 0; x < c_c; x++)
-                {
-                    c(n, x) = (b_c == c_c) ? a(n, x) + b((b_n == c_n ? n : 0), x)
-                                           : a(n, x) + b((b_n == c_n ? n : 0), 0);
-                }
-            }
-            break;
-
-        case 1:
-            std::tie(c_n) = miopen::tien<1>(c.desc.GetLengths());
-            std::tie(b_n) = miopen::tien<1>(b.desc.GetLengths());
-            for(int n = 0; n < c_n; n++)
-            {
-                c(n) = (b_n == c_n) ? a(n) + b(n) : a(n) + b(0);
-            }
-            break;
-
-        default:; // TODO:  some exception here
-        }
-
-// tensor_for_loop(a, b, c, a_dims, b_dims, 0);
 #if(MIO_OPS_DEBUG)
         for(int i = 0; i < c.desc.GetElementSize(); i++)
         {
@@ -295,6 +183,10 @@ struct tensor_ops_driver : test_driver
     {
         add(a, "a", generate_tensor(get_tensor_a(), {11, 7, 13, 13}));
         add(b, "b", generate_tensor(get_tensor_b(), {1, 7, 1, 1}));
+        //     add(a, "a", generate_tensor(get_tensor_a(), {2, 2}));
+        //     add(b, "b", generate_tensor(get_tensor_b(), {1, 2}));
+        // add(a, "a", generate_tensor(get_tensor_a(), {2}));
+        // add(b, "b", generate_tensor(get_tensor_b(), {2}));
     }
 
     std::set<std::vector<int>> get_tensor_a()
