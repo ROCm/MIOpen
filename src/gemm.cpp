25be777418c71ae3d3c45ffdc9274b3b83aaeb4b
Zhou <jingz@amd.com>
2017-10-03 15:09:14 -0700

    use rungemm........

--- src/gemm.cpp
+++ src/gemm.cpp
@@ -223,6 +223,33 @@ GemmGeometry GetGemmGeometry(std::string algorithm_name, std::string network_con
     }
 }
 
+GemmGeometry CreateGemmGeometryRNN(const int M,
+	const int N,
+	const int K,
+	const float alpha,
+	const float beta,
+	bool tA,
+	bool tB,
+	bool tC,
+	const int lda,
+	const int ldb,
+	const int ldc,
+	bool isDataColMajor,
+	std::string& network_config)
+{
+	// GEMM
+	MIOpenGEMM::Geometry tgg{};
+	GemmGeometry gg;
+	(void)isDataColMajor;
+
+		tgg = MIOpenGEMM::Geometry(false, tA, tB, tC, lda, ldb, ldc, M, N, K, 0, 'f');
+		gg = GemmGeometry{ "miopenRNNAlgoGEMM", alpha, beta, tgg };
+	
+	network_config = tgg.get_networkconfig_string();
+	return gg;
+}
+
+/*
 GemmGeometry CreateGemmGeometryRNNfwdfull(const int m,
 	const int n,
 	const int k,
@@ -336,5 +363,5 @@ GemmGeometry CreateGemmGeometryRNNbwddatafull(const int m,
 	network_config = tgg.get_networkconfig_string();
 	return gg;
 }
-
+*/
 } // namespace miopen
