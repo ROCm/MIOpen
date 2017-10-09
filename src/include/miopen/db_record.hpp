ec5af4f956bc5dafafccf096a38386809af3f045
Artem Tamazov <artem.tamazov@gmail.com>
2017-10-10 01:03:25 +0300

    Perf Db: tidy fixes.

--- src/include/miopen/db_record.hpp
+++ src/include/miopen/db_record.hpp
@@ -161,7 +161,7 @@ class DbRecord
     bool ParseContents(const std::string& contents);
     bool SaveValues(const std::string& id, const std::string& values);
 #if MIOPEN_PERFDB_CONV_LEGACY_SUPPORT
-    bool LoadValues(const std::string& id, std::string& values, ContentFormat& values_format);
+    bool LoadValues(const std::string& id, std::string& values, ContentFormat& content_format);
 #else
     bool LoadValues(const std::string& id, std::string& values);
 #endif
