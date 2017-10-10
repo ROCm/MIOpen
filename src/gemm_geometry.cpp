ceb7329279f68e24ff12e5b7bd6700e4e59b72f6
Artem Tamazov <artem.tamazov@gmail.com>
2017-10-10 14:30:02 +0300

    Perf Db: Fix hip build w/MIOpenGEMM
    [ROCm/MIOpen commit: 439fbffa4f95d2704fb97e1401d9530260caf71d]

--- src/gemm_geometry.cpp
+++ src/gemm_geometry.cpp
@@ -89,7 +89,7 @@ void GemmGeometry::FindSolution(
     (void)tgg;
     (void)alpha;
     (void)beta;
-    MIOpenGEMM::Solver soln = MIOpenGEMM::get_default(tgg);
+    MIOpenGEMM::Solution soln = MIOpenGEMM::get_default(tgg);
 #endif
 
     // jn : the main kernel is at the back of the solution vector
