180cbd369ddb3ac4c7acbaaa8e138f9daf1527e4
alyashev <alexander.lyashevsky@amd.com>
2017-10-19 16:19:51 -0700

    formatting new fixes
    [ROCm/MIOpen commit: 9a4601c17fe2659308ec3c699ff7f63d206f83a2]

--- src/solver/conv_ocl_dir2D_bwdWrW_53.cpp
+++ src/solver/conv_ocl_dir2D_bwdWrW_53.cpp
@@ -39,10 +39,10 @@ ConvSolution ConvOclBwdWrW53::GetSolution(const ConvolutionContext& params,
 {
     ConvSolution result;
 
-	if (params.kernel_stride1 > 1 || params.kernel_stride0 > 1)
-	{
-		return ConvSolution(static_cast<miopenStatus_t>(-1));
-	}
+    if(params.kernel_stride1 > 1 || params.kernel_stride0 > 1)
+    {
+        return ConvSolution(static_cast<miopenStatus_t>(-1));
+    }
     size_t localMemSize = 64 * 1024;
 
     const auto hw_wave_sz       = 64;
