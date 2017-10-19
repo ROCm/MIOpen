85cae9cbbc83b9dfd47d2040b6ac6e1486eee30d
Peter Y. Yeh <pyeh@amd.com>
2017-10-18 17:14:23 -0700

    Implement Tensorflow padding mode
    Signed-off-by: Peter Y. Yeh <pyeh@amd.com>
    
    
    [ROCm/MIOpen commit: 16e996047b91b977f68c36e7fca3d19d43ab24bb]

--- src/convolution.cpp
+++ src/convolution.cpp
@@ -124,15 +124,23 @@ ConvolutionDescriptor::GetForwardOutputDim(const TensorDescriptor& inputTensorDe
             1, u * (input_h - 1) + 1 + dilation_h * (filter_h - 1.0) - 2 * pad_h);
         output_w = std::max<std::ptrdiff_t>(
             1, v * (input_w - 1) + 1 + dilation_w * (filter_w - 1.0) - 2 * pad_w);
-    }
-    else
+    } else if (mode == miopenConvolution)
     {
         output_c = filter_k;
         output_h = std::max<std::ptrdiff_t>(
             1, (input_h - (1 + dilation_h * (filter_h - 1)) + 2 * pad_h) / u + 1);
         output_w = std::max<std::ptrdiff_t>(
             1, (input_w - (1 + dilation_w * (filter_w - 1)) + 2 * pad_w) / v + 1);
-    }
+    } else if (mode == miopenTensorflowSame) {
+        output_c = input_c;
+        output_h = std::ceil(static_cast<double>(input_h) / u);
+        output_w = std::ceil(static_cast<double>(input_w) / v);
+    } else if (mode == miopenTensorflowValid) {
+        output_c = input_c;
+        output_h = std::ceil(static_cast<double>(input_h - filter_h + 1) / u);
+        output_w = std::ceil(static_cast<double>(input_w - filter_w + 1) / v);
+    } else
+        MIOPEN_THROW(miopenStatusInvalidValue, "Invalid convolution mode!");
 
     return std::make_tuple(input_n, output_c, output_h, output_w);
 
