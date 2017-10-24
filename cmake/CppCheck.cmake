33bd7a99dfe7ee176bd3a5078b7f798444b77f89
Paul <pfultz2@yahoo.com>
2017-10-23 19:39:53 -0500

    Add cppcheck module
    [ROCm/MIOpen commit: 7076b482f30737134d9df5bd417176802198a6d1]

---
+++ cmake/CppCheck.cmake
@@ -0,0 +1,84 @@
+################################################################################
+# 
+# MIT License
+# 
+# Copyright (c) 2017 Advanced Micro Devices, Inc.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+################################################################################
+
+include(CMakeParseArguments)
+include(ProcessorCount)
+
+find_program(CPPCHECK_EXE 
+    NAMES 
+        cppcheck
+    PATHS
+        /opt/rocm/bin
+)
+
+ProcessorCount(CPPCHECK_JOBS)
+
+macro(enable_cppcheck)
+    set(options)
+    set(oneValueArgs)
+    set(multiValueArgs CHECKS SUPPRESS DEFINE INCLUDE SOURCES)
+
+    cmake_parse_arguments(PARSE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    string(REPLACE ";" "," CPPCHECK_CHECKS "${PARSE_CHECKS}")
+    string(REPLACE ";" "\n" CPPCHECK_SUPPRESS "${PARSE_SUPPRESS}")
+    file(WRITE ${CMAKE_BINARY_DIR}/cppcheck-supressions "${CPPCHECK_SUPPRESS}")
+    set(CPPCHECK_DEFINES)
+    foreach(DEF ${PARSE_DEFINE})
+        list(APPEND CPPCHECK_DEFINES -D ${DEF})
+    endforeach()
+
+    set(CPPCHECK_INCLUDES)
+    foreach(INC ${PARSE_INCLUDE})
+        list(APPEND CPPCHECK_INCLUDES -I ${INC})
+    endforeach()
+
+    if(PARSE_SUPPRESS)
+        set(CPPCHECK_SUPPRESS_FLAG "--suppressions-list=${CMAKE_BINARY_DIR}/cppcheck-supressions")
+    else()
+        set(CPPCHECK_SUPPRESS_FLAG)
+    endif()
+
+    set(CPPCHECK_COMMAND 
+        ${CPPCHECK_EXE}
+        -q
+        --template '{file}:{line}: {severity} ({id}): {message}'
+        -j ${CPPCHECK_JOBS}
+        ${CPPCHECK_DEFINES}
+        ${CPPCHECK_INCLUDES}
+        "--enable=${CPPCHECK_CHECKS}"
+        "${CPPCHECK_SUPPRESS_FLAG}"
+        "--project=${CMAKE_BINARY_DIR}/compile_commands.json"
+        ${PARSE_SOURCES}
+    )
+
+    add_custom_target(cppcheck
+        COMMAND ${CPPCHECK_COMMAND}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMENT "cppcheck: Running cppcheck..."
+    )
+endmacro()
+
+
