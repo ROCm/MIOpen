2c5fda1206dd2215a110219b5ef6dd2e6dd22aef
Jing Zhang <zhang@jing-desktop.amd.com>
2017-11-07 15:41:59 -0600

    formatting codes

--- src/ocl/tensorocl.cpp
+++ src/ocl/tensorocl.cpp
@@ -149,8 +149,7 @@ void OpTensor(Handle& handle,
         MIOPEN_THROW(miopenStatusBadParm);
     }
 
-
-    //if(aTensorDesc != cTensorDesc)
+    // if(aTensorDesc != cTensorDesc)
     if(aTensorDesc.GetElementSize() != cTensorDesc.GetElementSize())
     {
         MIOPEN_THROW("A and C Tensors do not match");
