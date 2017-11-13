cf17c301b5a4f94efb0974dce0273d2356cc09cc
Daniel Lowell <daniel.lowell@amd.com>
2017-11-13 16:07:18 -0600

    Finishing fwd rnn ctest.

--- driver/rnn_driver.hpp
+++ driver/rnn_driver.hpp
@@ -733,7 +733,6 @@ int RNNDriver<T>::RunForwardCPU()
     if(mode == miopenRNNRELU || mode == miopenRNNTANH)
     {
         printf("verify rnn fwd \n");
-
         RunRNNForwardGEMMCPUVerify(in,
                                    wei,
                                    hy_host,
