8fa7b5e740d98dcbfe74be3085ca199fe42c6dac
AMD\alyashev <alexander.lyashevsky@amd.com>
2017-11-15 09:58:24 -0800

    fixed 1x1 and all

--- src/include/miopen/serializable.hpp
+++ src/include/miopen/serializable.hpp
@@ -27,23 +27,11 @@
 #ifndef GUARD_MLOPEN_SERIALIZABLE_HPP
 #define GUARD_MLOPEN_SERIALIZABLE_HPP
 
+#include <iso646.h>
 #include <miopen/config.h>
 #include <iostream>
 #include <sstream>
 #include <string>
-#if !defined(and) && (!defined(__cplusplus) || defined(_MSC_EXTENSIONS))
-#define and	&&
-#define and_eq	&=
-#define bitand	&
-#define bitor	|
-#define compl	~
-#define not	!
-#define not_eq	!=
-#define or		||
-#define or_eq	|=
-#define xor	^
-#define xor_eq	^=
-#endif /* !defined(__cplusplus) || defined(_MSC_EXTENSIONS) */
 
 namespace miopen {
 namespace solver {
