e25233ae877a3f15fcbc29e4b29f8549a1edfcff
Sam Kolton <sam.kolton@amd.com>
2017-11-27 17:28:35 +0300

    Link to librt on UNIX. Needed by Boost.Interprocess

--- test/perfdb.cpp
+++ test/perfdb.cpp
@@ -1,28 +1,28 @@
 /*******************************************************************************
-*
-* MIT License
-*
-* Copyright (c) 2017 Advanced Micro Devices, Inc.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-*******************************************************************************/
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
 
 #include <cassert>
 #include <cstdlib>
@@ -143,12 +143,12 @@ class DbTest
 
 class DbFindTest : public DbTest
 {
-public:
+    public:
     inline void Run()
     {
         std::ostringstream ss_vals;
         ss_vals << key().x << ',' << key().y << '=' << id1() << ':' << value1().x << ','
-            << value1().y << ';' << id0() << ':' << value0().x << ',' << value0().y;
+                << value1().y << ';' << id0() << ':' << value0().x << ',' << value0().y;
 
         std::ofstream(temp_file_path()) << ss_vals.str() << std::endl;
 
@@ -174,7 +174,7 @@ public:
 
 class DbStoreTest : public DbTest
 {
-public:
+    public:
     inline void Run()
     {
         (void)std::ofstream(temp_file_path());
@@ -211,7 +211,7 @@ public:
 
 class DbUpdateTest : public DbTest
 {
-public:
+    public:
     inline void Run()
     {
         (void)std::ofstream(temp_file_path());
@@ -261,7 +261,7 @@ public:
 
 class DbRemoveTest : public DbTest
 {
-public:
+    public:
     inline void Run()
     {
         (void)std::ofstream(temp_file_path());
