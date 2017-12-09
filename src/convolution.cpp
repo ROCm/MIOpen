0396205a34246f7a71d81df6ea9a40a6e4bfb68a
AMD\alyashev <alexander.lyashevsky@amd.com>
2017-12-09 11:29:51 -0800

    wider range

--- src/convolution.cpp
+++ src/convolution.cpp
@@ -253,12 +253,9 @@ bool ConvolutionDescriptor::IsBwdWeightsDirectSupported(const TensorDescriptor& 
           pad_w == 0));
 
     bool workarounds =
-
         (((_kernel_size0 == 1 && _kernel_size1 == 1 && ((c & 0xF) > 0 || (k & 0xF) > 0))) ||      
-         (_kernel_size0 == 3 && _kernel_size1 == 3 && (pad_h > 1 || pad_w > 1 || u > 2 || v > 2)) ||
+         (_kernel_size0 == 3 && _kernel_size1 == 3 && ( u > 2 || v > 2)) ||
          (_kernel_size0 % 2 == 0 && _kernel_size1 % 2 == 0));
-
-
 
     return (supported_filters && !workarounds);
 }
@@ -280,11 +277,10 @@ bool ConvolutionDescriptor::IsDirectSupported(const TensorDescriptor& wDesc) con
           pad_w == 0));
 
     bool workarounds =
-        ((_kernel_size0 == 3 && _kernel_size1 == 3 && (pad_h > 1 || pad_w > 1 || u > 1 || v > 1)) ||
+        ( (_kernel_size0 == 3 && _kernel_size1 == 3 && (u > 2 || v > 2)) ||
          (_kernel_size0 == 1 && _kernel_size1 == 1 && (pad_h > 0 || pad_w > 0)) ||
          (_kernel_size0 % 2 == 0 && _kernel_size1 % 2 == 0)); 
   
-
     return (supported_filters && !workarounds);
 }
 
