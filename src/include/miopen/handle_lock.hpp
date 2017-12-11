a07bcb740025b954885a07c31766c8af7091a6cd
Paul <pfultz2@yahoo.com>
2017-12-11 17:20:23 -0600

    Sync gpu verification

--- src/include/miopen/handle_lock.hpp
+++ src/include/miopen/handle_lock.hpp
@@ -59,29 +59,53 @@ inline boost::filesystem::path get_handle_lock_path(const char* name)
     return p;
 }
 
-using handle_mutex = boost::interprocess::file_lock;
+struct handle_mutex
+{
+    std::recursive_timed_mutex m;
+    boost::interprocess::file_lock flock;
+
+    handle_mutex(const char * name)
+    : flock(name)
+    {}
+
+    bool try_lock()
+    {
+        return std::try_lock(m, flock);
+    }
+
+    void lock()
+    {
+        std::lock(m, flock);
+    }
+
+    template<class Duration>
+    bool try_lock_for(Duration d)
+    {
+        return m.try_lock_for(d) && flock.timed_lock(boost::posix_time::second_clock::universal_time() +
+                    boost::posix_time::milliseconds(std::chrono::duration_cast<std::chrono::milliseconds>(d).count()));
+
+    }
+
+    template<class Point>
+    bool try_lock_until(Point p)
+    {
+        return m.try_lock_for(p - std::chrono::system_clock::now());
+
+    }
+
+    void unlock()
+    {
+        flock.unlock();
+        m.unlock();
+    }
+};
+
+
 template <class T>
-inline handle_mutex& get_handle_mutex(T)
+inline std::unique_lock<handle_mutex> get_handle_lock(T, int timeout = 60)
 {
     static handle_mutex m{get_handle_lock_path(T::value()).c_str()};
-    return m;
-}
-
-using handle_lock = std::unique_lock<handle_mutex>;
-template <class T>
-inline handle_lock get_handle_lock(T, int timeout = 120)
-{
-    auto& m = get_handle_mutex(T{});
-    if(m.timed_lock(boost::posix_time::second_clock::universal_time() +
-                    boost::posix_time::seconds(timeout)))
-    {
-        return {m, std::adopt_lock_t{}};
-    }
-    else
-    {
-        m.unlock();
-        MIOPEN_THROW("GPU is stalled");
-    }
+    return {m, std::chrono::seconds{timeout}};
 }
 
 } // namespace miopen
