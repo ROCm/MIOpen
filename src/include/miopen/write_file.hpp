c5b9e0cd5a05823b674330e830a85f7d0df0cf72
AMD\alyashev <alexander.lyashevsky@amd.com>
2017-12-21 16:46:11 -0800

    minor develo error

--- src/include/miopen/write_file.hpp
+++ src/include/miopen/write_file.hpp
@@ -12,7 +12,7 @@ using FilePtr = MIOPEN_MANAGE_PTR(FILE*, std::fclose);
 inline void WriteFile(const std::string& content, const boost::filesystem::path& name)
 {
     // std::cerr << "Write file: " << name << std::endl;
-    FilePtr f{std::fopen(name.c_str(), "w")};
+    FilePtr f{std::fopen((const char*)name.c_str(), "w")};
     if(std::fwrite(content.c_str(), 1, content.size(), f.get()) != content.size())
         MIOPEN_THROW("Failed to write to src file");
 }
