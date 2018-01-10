b3ea064f7d2364ecc132f7724723e8069a446674
Paul <pfultz2@yahoo.com>
2018-01-10 16:44:01 -0600

    Formatting
    [ROCm/MIOpen commit: 6688d23ff5077ae98023bf2a73768c5d6dfa66d4]

--- test/tensor_holder.hpp
+++ test/tensor_holder.hpp
@@ -92,9 +92,7 @@ struct tensor
     miopen::TensorDescriptor desc;
     std::vector<T> data;
 
-    tensor() 
-    : desc(miopen_type<T>{}, {})
-    {}
+    tensor() : desc(miopen_type<T>{}, {}) {}
 
     template <class X>
     tensor(const std::vector<X>& dims)
