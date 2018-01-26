adcf58924159abd09490031ad8ddc66f9ac1c6ce
Paul <pfultz2@yahoo.com>
2018-01-25 22:01:16 -0600

    Add parameter to pack tensor
    [ROCm/MIOpen commit: 4fba26d051affe2505eb928d085ebe63d8eb690c]

--- test/tensor_ops.cpp
+++ test/tensor_ops.cpp
@@ -261,6 +261,7 @@ struct tensor_ops_driver : test_driver
     std::vector<int> tensorlens_b;
     std::vector<int> offsets;
     std::vector<float> alphabeta;
+    bool packed = false;
 
     std::vector<std::vector<int>> get_sub_tensor_a()
     {
@@ -308,10 +309,11 @@ struct tensor_ops_driver : test_driver
         std::vector<std::vector<int>> get_offsets     = {{64, 32, 16}, {32, 16, 32}, {32, 16, 32}};
         std::vector<std::vector<float>> get_alphabeta = {{1, 1, 0}, {-1, 1, 1}, {1.0, 0.5, 0.3}};
 
-        add(tensorlens_ac, "tensorlens_ac", generate_data(get_sub_tensor_a()));
-        add(tensorlens_b, "tensorlens_b", generate_data(get_sub_tensor_b()));
+        add(tensorlens_ac, "a", generate_data(get_sub_tensor_a()));
+        add(tensorlens_b, "b", generate_data(get_sub_tensor_b()));
         add(offsets, "offsets", generate_data(get_offsets));
-        add(alphabeta, "alphabeta", generate_data(get_alphabeta));
+        add(alphabeta, "alpha-beta", generate_data(get_alphabeta));
+        add(packed, "packed", generate_data({false, true}));
     }
 
     tensor<T> get_subtensors(tensor<T>& super_tensor, std::vector<int>& lens)
@@ -329,9 +331,9 @@ struct tensor_ops_driver : test_driver
     {
         if(tensorlens_ac.size() == tensorlens_b.size())
         {
-            tensor<T> aTensor = get_subtensors(super_a, tensorlens_ac);
-            tensor<T> bTensor = get_subtensors(super_b, tensorlens_b);
-            tensor<T> cTensor = get_subtensors(super_c, tensorlens_ac);
+            tensor<T> aTensor = packed ? tensorlens_ac : get_subtensors(super_a, tensorlens_ac);
+            tensor<T> bTensor = packed ? tensorlens_b : get_subtensors(super_b, tensorlens_b);
+            tensor<T> cTensor = packed ? tensorlens_ac : get_subtensors(super_c, tensorlens_ac);
 
             verify(verify_tensor_ops<T>{
                 aTensor, bTensor, cTensor, offsets, alphabeta[0], alphabeta[1], alphabeta[2]});
