9558f83d29bd9ad8ec28045c16423b2e5cb36446
Paul <pfultz2@yahoo.com>
2018-02-09 09:45:28 -0600

    Add workaround for gcc 5

--- src/ocl/convolutionocl.cpp
+++ src/ocl/convolutionocl.cpp
@@ -1782,7 +1782,8 @@ void ConvolutionDescriptor::FindConvBwdWeightsAlgorithm(Handle& handle,
                         }
                         else
                         {
-                            workspace_req = BackwardWeightsGetWorkSpaceSizeDirect(
+                            // this pointer needed here as a workaround in gcc 5
+                            workspace_req = this->BackwardWeightsGetWorkSpaceSizeDirect(
                                 handle, dyDesc, xDesc, dwDesc);
 
                             if(workSpace != nullptr && workSpaceSize >= workspace_req)
