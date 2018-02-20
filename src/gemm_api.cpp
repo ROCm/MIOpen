d6272d553b1fde721c02c29ee70df1e3f3bb788b
Paul <pfultz2@yahoo.com>
2018-02-20 13:50:34 -0600

    Formatting
    [ROCm/MIOpen commit: 89ca908561bcd0b429d1c72c3ecb5216a884ecba]

--- src/gemm_api.cpp
+++ src/gemm_api.cpp
@@ -35,11 +35,11 @@ using rocblas_handle_ptr = MIOPEN_MANAGE_PTR(rocblas_handle, rocblas_destroy_han
 
 rocblas_handle_ptr create_rocblas_handle_ptr(miopen::Handle& h)
 {
-  rocblas_handle x = nullptr;
-  rocblas_create_handle(&x);
-  auto result = rocblas_handle_ptr{x};
-  rocblas_set_stream(result.get(), h.GetStream());
-  return result;
+    rocblas_handle x = nullptr;
+    rocblas_create_handle(&x);
+    auto result = rocblas_handle_ptr{x};
+    rocblas_set_stream(result.get(), h.GetStream());
+    return result;
 }
 #endif
 
@@ -61,13 +61,26 @@ miopenStatus_t miopenGemm(miopenHandle_t handle,
                           int find)
 {
 #if MIOPEN_USE_ROCBLAS
-  (void)isDataColMajor;
-  (void)find;
-  static rocblas_handle_ptr rhandle = create_rocblas_handle_ptr(miopen::deref(handle));
-  float alpha_local = *static_cast<const float*>(alpha);
-  float beta_local = *static_cast<const float*>(beta);
-  rocblas_sgemm(rhandle.get(), transA ? rocblas_operation_transpose : rocblas_operation_none, transB ? rocblas_operation_transpose : rocblas_operation_none, M, N, K, &alpha_local, static_cast<const float*>(A), lda, static_cast<const float*>(B), ldb, &beta_local, static_cast<float*>(C), ldc);
-  return miopenStatusSuccess;
+    (void)isDataColMajor;
+    (void)find;
+    static rocblas_handle_ptr rhandle = create_rocblas_handle_ptr(miopen::deref(handle));
+    float alpha_local                 = *static_cast<const float*>(alpha);
+    float beta_local                  = *static_cast<const float*>(beta);
+    rocblas_sgemm(rhandle.get(),
+                  transA ? rocblas_operation_transpose : rocblas_operation_none,
+                  transB ? rocblas_operation_transpose : rocblas_operation_none,
+                  M,
+                  N,
+                  K,
+                  &alpha_local,
+                  static_cast<const float*>(A),
+                  lda,
+                  static_cast<const float*>(B),
+                  ldb,
+                  &beta_local,
+                  static_cast<float*>(C),
+                  ldc);
+    return miopenStatusSuccess;
 #else
 
     // JN make column major
