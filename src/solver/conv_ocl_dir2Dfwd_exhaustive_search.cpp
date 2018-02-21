f9910a8da988e1768f6a00a7ba892146c27ddcf1
alyashev <alexander.lyashevsky@amd.com>
2018-02-20 17:28:57 -0800

    remove gen C convolution
    [ROCm/MIOpen commit: c49df194fa591452c21dec077e3adfafcd90bb4f]

--- src/solver/conv_ocl_dir2Dfwd_exhaustive_search.cpp
+++ src/solver/conv_ocl_dir2Dfwd_exhaustive_search.cpp
@@ -663,15 +663,14 @@ ConvOclDirectFwdLegacyExhaustiveSearch::Search(const ConvolutionContext& params)
                                     }
 
                                     const auto ret =
-                                        MeasureLoop<ConvOclDirectFwd>(
-                                            &profile_h,
-                                            bot_ocl_buf.get(),
-                                            top_ocl_buf.get(),
-                                            wei_ocl_buf.get(),
-                                            bias_ocl_buf.get(),
-                                            processing_time,
-                                            params,
-                                            result);
+                                        MeasureLoop<ConvOclDirectFwd>(&profile_h,
+                                                                      bot_ocl_buf.get(),
+                                                                      top_ocl_buf.get(),
+                                                                      wei_ocl_buf.get(),
+                                                                      bias_ocl_buf.get(),
+                                                                      processing_time,
+                                                                      params,
+                                                                      result);
 
                                     runs_left--;
                                     runs_left = (runs_left < 0) ? 0 : runs_left;
