995b56015b3ffaa5cb793df5342d767af36d303e
Paul <pfultz2@yahoo.com>
2018-03-19 18:39:32 -0500

    Formatting

--- src/temp_file.cpp
+++ src/temp_file.cpp
@@ -8,7 +8,8 @@
 namespace miopen {
 TempFile::TempFile(const std::string& path_template) : name(path_template), dir("tmp")
 {
-    if(!std::ofstream{this->Path(), std::ios_base::out|std::ios_base::in|std::ios_base::trunc}.good())
+    if(!std::ofstream{this->Path(), std::ios_base::out | std::ios_base::in | std::ios_base::trunc}
+            .good())
     {
         MIOPEN_THROW("Failed to create temp file: " + this->Path());
     }
