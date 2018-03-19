7295aa42191ddda2a9d2a662b9195987c0d8a4ba
AMD\alyashev <alexander.lyashevsky@amd.com>
2018-03-19 10:05:31 -0700

    restore old one

--- src/temp_file.cpp
+++ src/temp_file.cpp
@@ -1,50 +1,11 @@
 #include <miopen/temp_file.hpp>
 #include <miopen/errors.hpp>
 
-#ifndef WIN32
+
 #include <unistd.h>
-#else
-#include <Windows.h>
-#include <fcntl.h>
-#include <io.h>
-#define _O_EXCL 0x0400
-#define O_EXCL _O_EXCL
-
-static int mkstemp(char* tmpl)
-{
-
-    static LARGE_INTEGER string_value;
-    LARGE_INTEGER random_time;
-    int fd;
-    int save_errno = errno;
-
-    QueryPerformanceCounter(&random_time);
-
-    string_value.HighPart += random_time.HighPart;
-    string_value.LowPart += random_time.LowPart ^ GetCurrentThreadId();
-
-    std::string nm = std::string(tmpl) + std::string("-") + std::to_string(string_value.HighPart) +
-                     std::string("-") + std::to_string(string_value.LowPart);
-
-    fd = open(nm.c_str(), O_RDWR | O_CREAT | O_EXCL, _S_IREAD | _S_IWRITE);
-    if(fd >= 0)
-    {
-        errno = save_errno;
-        return fd;
-    }
-    else if(errno != EEXIST)
-        return -1;
-}
-#endif
-
 namespace miopen {
 TempFile::TempFile(const std::string& path_template)
-#ifdef WIN32
-    : _path((GetTempDirectoryPath().path / (path_template)).string())
-#else
     : _path((GetTempDirectoryPath().path / (path_template + "-XXXXXX")).string())
-#endif
-
 {
     _fd = mkstemp(&_path[0]);
     if(_fd == -1)
