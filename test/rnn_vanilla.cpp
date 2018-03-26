91febb1c4844fc189ef2b931cbd606b61a60ccb5
Daniel Lowell <daniel.lowell@amd.com>
2018-03-25 20:27:40 -0500

    Formatting

--- test/rnn_vanilla.cpp
+++ test/rnn_vanilla.cpp
@@ -45,7 +45,7 @@
 #include <utility>
 #include <cfloat>
 
-#define MIO_RNN_TEST_DEBUG 0
+#define MIO_RNN_TEST_DEBUG 1
 #define MIO_RNN_TIME_EVERYTHING 0
 
 /**********************************************
