a22e78b30574dc583d2e66e98fbad68362ced89a
Paul <pfultz2@yahoo.com>
2018-03-27 10:51:43 -0500

    Suppress output in catch

--- src/include/miopen/errors.hpp
+++ src/include/miopen/errors.hpp
@@ -65,7 +65,7 @@ std::string HIPErrorMessage(int error, const std::string& msg = "");
 
 // TODO(paul): Debug builds should leave the exception uncaught
 template <class F>
-miopenStatus_t try_(F f)
+miopenStatus_t try_(F f, bool output=true)
 {
     try
     {
@@ -73,12 +73,12 @@ miopenStatus_t try_(F f)
     }
     catch(const Exception& ex)
     {
-        std::cerr << "MIOpen Error: " << ex.what() << std::endl;
+        if (output) std::cerr << "MIOpen Error: " << ex.what() << std::endl;
         return ex.status;
     }
     catch(const std::exception& ex)
     {
-        std::cerr << "MIOpen Error: " << ex.what() << std::endl;
+        if (output) std::cerr << "MIOpen Error: " << ex.what() << std::endl;
         return miopenStatusUnknownError;
     }
     catch(...)
