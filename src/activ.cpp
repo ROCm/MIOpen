7751bff9704dfd2e1b16996c3de006330769cc27
Chao Liu <lc.roy86@gmail.com>
2018-04-10 15:25:46 -0500

    activation more name changes

--- src/activ.cpp
+++ src/activ.cpp
@@ -64,7 +64,7 @@ std::ostream& operator<<(std::ostream& stream, const ActivationDescriptor& x)
                     miopenActivationPOWER,
                     miopenActivationCLIPPEDRELU,
                     miopenActivationLEAKYRELU,
-                    miopenActivationELU,
+                    miopenActivationELU
                     )
         << ", ";
     LogRange(stream, x.parms, ", ") << ", ";
