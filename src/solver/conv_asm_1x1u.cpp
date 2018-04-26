26994a5a37cf09e5bc9eee2a084aae99a5951897
Jing Zhang <jing.zhang3@amd.com>
2018-04-26 22:55:22 +0000

    merged search tweak

--- src/solver/conv_asm_1x1u.cpp
+++ src/solver/conv_asm_1x1u.cpp
@@ -574,7 +574,11 @@ int ConvAsm1x1U::RunAndMeasureSolution(miopen::Handle& profile_h,
 
 PerformanceConfigConvAsm1x1U ConvAsm1x1U::Search(const ConvolutionContext& context) const
 {
-    return GenericSearch(*this, context);
+    return GenericSearch(*this,
+                         context,
+                         UseSubsample(context) || UseUpsample(context)
+                             ? SearchTweak::Skip2xSubsample
+                             : SearchTweak::None);
 }
 
 } // namespace solver
