f85e044bba71dd4bd8fb858809d2c3ddb4472f1d
Vasja Filippov <vasja.filippov94@outlook.com>
2018-05-04 11:16:12 +0300

    Removed warning for unreadablility of user db file.

--- src/include/miopen/db.hpp
+++ src/include/miopen/db.hpp
@@ -50,7 +50,7 @@ std::string LockFilePath(const boost::filesystem::path& filename_);
 class Db
 {
     public:
-    Db(const std::string& filename_);
+    Db(const std::string& filename_, bool should_warn_if_unreadable_ = true);
 
     /// Searches db for provided key and returns found record or none if key not found in database
     boost::optional<DbRecord> FindRecord(const std::string& key);
@@ -138,6 +138,7 @@ class Db
     private:
     std::string filename;
     LockFile& lock_file;
+    bool should_warn_if_unreadable;
 
     boost::optional<DbRecord> FindRecordUnsafe(const std::string& key, RecordPositions* pos);
     bool FlushUnsafe(const DbRecord& record, const RecordPositions* pos);
@@ -157,7 +158,7 @@ class MultiFileDb
 {
     public:
     MultiFileDb(const std::string& installed_path, const std::string& user_path)
-        : _installed(installed_path), _user(user_path)
+        : _installed(installed_path), _user(user_path, false)
     {
     }
 
