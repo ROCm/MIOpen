c08128b40a92303b1d0083bd4abc20bac5b56866
Ashish <ashish.farmer@amd.com>
2018-05-16 10:00:29 -0700

    Included batch size in scaling factor

--- test/conv_bias.cpp
+++ test/conv_bias.cpp
@@ -101,11 +101,10 @@ struct conv_bias_driver : test_driver
     {
         auto mode      = cmode_lookup[miopen::ToUpper(conv_mode)];
         tensor<T> bias = {1, output.desc.GetLengths()[1], 1, 1};
-        int out_h, out_w;
-        std::tie(std::ignore, std::ignore, out_h, out_w) =
-            miopen::tien<4>(output.desc.GetLengths());
+        int out_n, out_h, out_w;
+        std::tie(out_n, std::ignore, out_h, out_w) = miopen::tien<4>(output.desc.GetLengths());
         output.par_for_each([&](int n, int c, int h, int w) {
-            output(n, c, h, w) = T(output(n, c, h, w) * 65535.0f / (4 * out_h * out_w));
+            output(n, c, h, w) = T(output(n, c, h, w) * 65535.0f / (4 * out_n * out_h * out_w));
         });
         verify(verify_backwards_bias<T>{output, bias, mode});
     }
