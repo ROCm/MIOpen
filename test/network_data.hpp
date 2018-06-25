824356d9e1a489798f53c963ce5925f304b3ff04
Jing Zhang <jing.zhang3@amd.com>
2018-06-25 16:44:05 -0500

    added biasTensor=<1, 1, 1, 1> case into ctest

--- test/network_data.hpp
+++ test/network_data.hpp
@@ -50,6 +50,7 @@ inline std::set<std::vector<int>> get_inputs(int n = MIOPEN_TEST_DEFAULT_BATCH_S
     // clang-format off
     return 
     {
+        { pick_batch_size(100, n), 1,    32,  32},
         { pick_batch_size(100, n), 19,   1024,2048},
         { pick_batch_size(100, n), 3,    32,  32  },
         { pick_batch_size(100, n), 32,   16,  16  },
