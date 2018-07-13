2bfd505cf3d1085290e0cd2d8619c97fce2e93f5
Jehandad Khan <jehandad@vt.edu>
2018-07-13 17:08:11 -0500

    Minor bug fixes
    [ROCm/MIOpen commit: 905193553f07ab74a93990609729d8eceeb5fd26]

--- src/ocl/fusionopconvocl.cpp
+++ src/ocl/fusionopconvocl.cpp
@@ -204,6 +204,7 @@ miopenStatus_t ConvForwardOpDescriptor::GetNetworkConfig(std::string& network_co
     network_config += conv_config;
     return miopenStatusSuccess;
 }
+#if 0
 bool ConvForwardOpDescriptor::isASMApplicable(Handle& handle)
 {
     auto ki = GetKernelInfo(handle);
@@ -241,11 +242,14 @@ solver::KernelInfo& ConvForwardOpDescriptor::GetKernelInfo(Handle& handle)
     }
     return kernel_info;
 }
+#endif
 
 // DLOWELL: This implementation is a hack since we are hardcoding in a way to get around ASM
 solver::KernelInfo& ConvForwardOpDescriptor::GetKernelInfo(Handle& handle,
                                                            std::string algorithm_name)
 {
+    if(kernel_info_valid)
+        return kernel_info;
 
     if(algorithm_name == "miopenConvDirectBatchNormBiasActiv")
     { // May need an extra check of FP16 versus FP32
@@ -255,38 +259,37 @@ solver::KernelInfo& ConvForwardOpDescriptor::GetKernelInfo(Handle& handle,
         construct_params.mloCopyTo(params);
         // params.general_compile_options += compile_config;
         kernel_info = solver::CBAFusionGetSolution(params);
+        kernel_info_valid = true;
         // printf("Compiler options: %s\n", kernel_info.comp_options.c_str());
         // kernel_info = solver::CBAFusionGetSolution(compile_config);
     }
     else
     { // DLOWELL: Is this valid if there is dType is FP16?
-        if(!kernel_info_valid)
+        
+        // In the absence of the config tree
+        mlo_construct_direct2D_fusion construct_params = ConstructParams(handle);
+        ConvolutionContext params;
+        construct_params.mloCopyTo(params);
+        const auto solution = FindFirstSolution(construct_params);
+        auto k_file         = solution.construction_params[0].kernel_file;
+        // TODO: There is redundant code hidden in both the branches below
+        if(k_file[k_file.length() - 1] == 's') // is an asm kernel
         {
-            // In the absence of the config tree
-            mlo_construct_direct2D_fusion construct_params = ConstructParams(handle);
-            ConvolutionContext params;
-            construct_params.mloCopyTo(params);
-            const auto solution = FindFirstSolution(construct_params);
-            auto k_file         = solution.construction_params[0].kernel_file;
-            // TODO: There is redundant code hidden in both the branches below
-            if(k_file[k_file.length() - 1] == 's') // is an asm kernel
-            {
-                solver::KernelInfo ki;
-                ki.comp_options =
-                    solution.construction_params[0].comp_options + " -Wa,-defsym,fusion_mode=1";
-                ki.l_wk        = solution.construction_params[0].l_wk;
-                ki.g_wk        = solution.construction_params[0].g_wk;
-                ki.kernel_file = "conv1x1u_bias_activ.s";
-                ki.kernel_name = "gcnAsmConv1x1U";
-                kernel_info    = ki;
-            }
-            else
-            { // May need an extra check of FP16 versus FP32
-                params.general_compile_options += " -DMIOPEN_USE_FP32=1 -DMIOPEN_USE_FP16=0";
-                kernel_info = solver::CBAFusionGetSolution(params);
-            }
-            kernel_info_valid = true;
+            solver::KernelInfo ki;
+            ki.comp_options =
+                solution.construction_params[0].comp_options + " -Wa,-defsym,fusion_mode=1";
+            ki.l_wk        = solution.construction_params[0].l_wk;
+            ki.g_wk        = solution.construction_params[0].g_wk;
+            ki.kernel_file = "conv1x1u_bias_activ.s";
+            ki.kernel_name = "gcnAsmConv1x1U";
+            kernel_info    = ki;
         }
+        else
+        { // May need an extra check of FP16 versus FP32
+            params.general_compile_options += " -DMIOPEN_USE_FP32=1 -DMIOPEN_USE_FP16=0";
+            kernel_info = solver::CBAFusionGetSolution(params);
+        }
+        kernel_info_valid = true;
     }
     conv_compiler_options += kernel_info.comp_options;
     return kernel_info;
