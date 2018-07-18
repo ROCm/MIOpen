23abc010b6e6c30ef86c0449af5556ff0d80e464
hchevanne <herve.chevanne@amd.com>
2018-07-18 11:11:23 +0200

    - Build errors fixes when gemm is disabled 	+ driver/gemm_driver.hpp 	+ driver/main.cpp 	+ src/ocl/convolutionocl.cpp
    - Batch normalization fixes to support fp16:
    	+ driver/bn_driver.hpp
    	+ driver/main.cpp
    	+ driver/miopen_BatchNormHost.hpp
    
    - fp16 generic bug fix
    	+ src/include/miopen/mlo_internal.hpp

--- driver/gemm_driver.hpp
+++ driver/gemm_driver.hpp
@@ -26,6 +26,7 @@
 #ifndef GUARD_MIOPEN_GEMM_DRIVER_HPP
 #define GUARD_MIOPEN_GEMM_DRIVER_HPP
 
+#if MIOPEN_USE_MIOPENGEMM == 1
 #include "InputFlags.hpp"
 #include "driver.hpp"
 #include <algorithm>
@@ -420,4 +421,5 @@ int GemmDriver<T>::VerifyBackward()
     return 0;
 }
 
+#endif // MIOPEN_USE_MIOPENGEMM
 #endif // GUARD_MIOPEN_GEMM_DRIVER_HPP
