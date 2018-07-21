660d7ec7a9ddbb9b32706ba709769e6b3e242a4e
Jehandad Khan <jehandad@vt.edu>
2018-07-21 17:31:08 -0500

    Added solver for fused asm kernel
    [ROCm/MIOpen commit: c342038888df24b826afc5f05828e9501a4a95a1]

--- src/mlo_dir_conv.cpp
+++ src/mlo_dir_conv.cpp
@@ -74,6 +74,19 @@ std::vector<miopen::solver::ConvSolution> mlo_construct_direct2D::FindAllSolutio
 }
 
 // TODO: Temporary addition!
+
+miopen::solver::ConvSolution mlo_construct_direct2D_fusion::FindSolution()
+{
+    return miopen::solver::SearchForSolution<
+    miopen::solver::ConvActivAsm1x1U, /* These need to be pruned*/
+    miopen::solver::ConvOclDirectFwd11x11,
+    miopen::solver::ConvOclDirectFwdGen,
+    miopen::solver::ConvOclDirectFwd3x3,
+    miopen::solver::ConvOclDirectFwd1x1,
+    miopen::solver::ConvOclDirectFwd
+    >(_search_params, this->GetDb());
+}
+
 miopen::solver::ConvSolution mlo_construct_direct2D::FindSolution()
 {
     // clang-format off
