c1e66f81fc36960d535ac2279ceb5313d45c3c24
Vasja Filippov <vasja.filippov94@outlook.com>
2018-08-07 15:37:03 +0300

    Added local network config field to find db.

--- src/include/miopen/perf_field.hpp
+++ src/include/miopen/perf_field.hpp
@@ -1,28 +1,28 @@
 /*******************************************************************************
-*
-* MIT License
-*
-* Copyright (c) 2017 Advanced Micro Devices, Inc.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-*******************************************************************************/
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
 #ifndef GUARD_MIOPEN_PERF_FIELD_HPP_
 #define GUARD_MIOPEN_PERF_FIELD_HPP_
 
@@ -46,11 +46,15 @@ struct FindDbData : solver::Serializable<FindDbData>
     std::string solver_id;
     float time;
     std::size_t workspace;
+    std::string network_config;
 
-    FindDbData() : solver_id("<unknown>"), time(-1), workspace(-1) {}
+    FindDbData() : solver_id("<unknown>"), network_config("<unknown>"), time(-1), workspace(-1) {}
 
-    FindDbData(const std::string& solver_id_, float time_, std::size_t workspace_)
-        : solver_id(solver_id_), time(time_), workspace(workspace_)
+    FindDbData(const std::string& solver_id_,
+               float time_,
+               std::size_t workspace_,
+               const std::string& network_config_)
+        : solver_id(solver_id_), time(time_), workspace(workspace_), network_config(network_config_)
     {
     }
 
@@ -60,6 +64,7 @@ struct FindDbData : solver::Serializable<FindDbData>
         f(self.solver_id, "solver_id");
         f(self.time, "time");
         f(self.workspace, "workspace");
+        f(self.network_config, "network_config");
     }
 };
 
