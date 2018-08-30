3246fbdd58ee71257e193a74b174f372618136f1
Jehandad Khan <jahandad@gmail.com>
2018-08-30 09:30:46 -0500

    Changed c tensor pointers to c++ tensor objects

--- test/mdgraph.cpp
+++ test/mdgraph.cpp
@@ -28,16 +28,17 @@
 #include <miopen/manage_ptr.hpp>
 #include <miopen/fusion_plan.hpp>
 
+#include "get_handle.hpp"
 #include "test.hpp"
 
-void BNAlgTest(miopenHandle_t handle,
-               std::vector<int> inputs,
+void BNAlgTest(std::vector<int> inputs,
                miopenBatchNormMode_t bnmode,
                std::string& pgm,
                std::string& krn,
                std::string& alg)
 {
     MIOPEN_LOG_I("*********************************************************");
+    auto&& handle = get_handle();
     miopen::TensorDescriptor inputTensor;
     miopen::TensorDescriptor scaleTensor;
     miopenFusionOpDescriptor_t bNormOp = nullptr;
@@ -49,13 +50,12 @@ void BNAlgTest(miopenHandle_t handle,
 
     miopenCreateOpBatchNormInference(&fp, &bNormOp, bnmode, &scaleTensor);
 
-    pgm = fp.GetProgramName(*(static_cast<miopen::Handle*>(handle)));
+    pgm = fp.GetProgramName(handle);
     krn = fp.GetKernelName();
     alg = fp.GetAlgorithmName();
 }
 
-void ConvAlgTest(miopenHandle_t handle,
-                 std::vector<int> inputs,
+void ConvAlgTest(std::vector<int> inputs,
                  std::vector<int> conv_filter,
                  std::vector<int> conv_desc,
                  std::string& pgm,
@@ -63,8 +63,9 @@ void ConvAlgTest(miopenHandle_t handle,
                  std::string& alg)
 {
     MIOPEN_LOG_I("*********************************************************");
+    auto&& handle = get_handle();
     miopen::TensorDescriptor inputTensor;
-    miopenTensorDescriptor_t convFilter{};
+    miopen::TensorDescriptor convFilter;
     miopenConvolutionDescriptor_t convDesc{};
     miopenFusionOpDescriptor_t convoOp;
 
@@ -72,8 +73,7 @@ void ConvAlgTest(miopenHandle_t handle,
     STATUS(miopenSet4dTensorDescriptor(
         &inputTensor, miopenFloat, inputs[0], inputs[1], inputs[2], inputs[3]));
     // convolution descriptor
-    STATUS(miopenCreateTensorDescriptor(&convFilter));
-    STATUS(miopenSet4dTensorDescriptor(convFilter,
+    STATUS(miopenSet4dTensorDescriptor(&convFilter,
                                        miopenFloat,
                                        conv_filter[0], // outputs k
                                        conv_filter[1], // inputs c
@@ -92,53 +92,38 @@ void ConvAlgTest(miopenHandle_t handle,
 
     miopen::FusionPlanDescriptor fp(miopenVerticalFusion, inputTensor);
 
-    STATUS(miopenCreateOpConvForward(&fp, &convoOp, convDesc, convFilter));
-    pgm = fp.GetProgramName(*(static_cast<miopen::Handle*>(handle)));
+    STATUS(miopenCreateOpConvForward(&fp, &convoOp, convDesc, &convFilter));
+    pgm = fp.GetProgramName(handle);
     krn = fp.GetKernelName();
     alg = fp.GetAlgorithmName();
 
     // Cleanup
-    miopenDestroyTensorDescriptor(convFilter);
     miopenDestroyConvolutionDescriptor(convDesc);
 }
 
 int main()
 {
-    // Create the handle
-    miopenHandle_t handle{};
-#if MIOPEN_BACKEND_OPENCL
-    cl_command_queue q{};
-#endif
-
-    miopenCreate(&handle);
-#if MIOPEN_BACKEND_OPENCL
-    miopenGetStream(handle, &q);
-#endif
     std::string pgm_name;
     std::string krn_name;
     std::string alg_name;
 
     // Winograd because c, x and y satisfy criteria
-    ConvAlgTest(
-        handle, {100, 32, 8, 8}, {64, 32, 3, 3}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
+    ConvAlgTest({100, 32, 8, 8}, {64, 32, 3, 3}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
     EXPECT(krn_name == "sp3AsmConvRxSU_CBA");
     EXPECT(alg_name == "miopenConvolutionWinogradBiasActiv");
 
     // c is odd so winograd not supported
-    ConvAlgTest(
-        handle, {100, 31, 8, 8}, {64, 31, 3, 3}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
+    ConvAlgTest({100, 31, 8, 8}, {64, 31, 3, 3}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
     EXPECT(krn_name != "sp3AsmConvRxSU_CBA");
     EXPECT(alg_name != "miopenConvolutionWinogradBiasActiv");
 
     // c is less than 18 so winograd not supported
-    ConvAlgTest(
-        handle, {100, 15, 8, 8}, {64, 15, 3, 3}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
+    ConvAlgTest({100, 15, 8, 8}, {64, 15, 3, 3}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
     EXPECT(krn_name != "sp3AsmConvRxSU_CBA");
     EXPECT(alg_name != "miopenConvolutionWinogradBiasActiv");
 
     // the asm kernel is the fastest for 1x1
-    ConvAlgTest(
-        handle, {100, 32, 8, 8}, {64, 32, 1, 1}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
+    ConvAlgTest({100, 32, 8, 8}, {64, 32, 1, 1}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
     EXPECT(pgm_name == "conv1x1u_bias_activ.s");
     EXPECT(krn_name == "gcnAsmConv1x1U");
     EXPECT(alg_name == "miopenConvolutionDirectBiasActivAsm");
@@ -146,27 +131,20 @@ int main()
     // only the opencl kernels supports other odd sizes
     for(auto idx : {5, 7, 9, 11})
     {
-        ConvAlgTest(handle,
-                    {100, 32, 8, 8},
-                    {64, 32, idx, idx},
-                    {0, 0, 1, 1, 1, 1},
-                    pgm_name,
-                    krn_name,
-                    alg_name);
+        ConvAlgTest(
+            {100, 32, 8, 8}, {64, 32, idx, idx}, {0, 0, 1, 1, 1, 1}, pgm_name, krn_name, alg_name);
         EXPECT(pgm_name == "MIOpenConvDirBatchNormActiv.cl");
         EXPECT(krn_name == "MIOpenConvUniBatchNormActiv");
         EXPECT(alg_name == "miopenConvolutionDirectBiasActiv");
     }
 
-    BNAlgTest(handle, {100, 32, 8, 8}, miopenBNSpatial, pgm_name, krn_name, alg_name);
+    BNAlgTest({100, 32, 8, 8}, miopenBNSpatial, pgm_name, krn_name, alg_name);
     EXPECT(pgm_name == "MIOpenBatchNormActivInfer.cl");
     EXPECT(krn_name == "MIOpenBatchNormActivInferSpatialEst");
     EXPECT(alg_name == "MIOpenBatchNormActivInferSpatialEst");
 
-    BNAlgTest(handle, {100, 32, 8, 8}, miopenBNPerActivation, pgm_name, krn_name, alg_name);
+    BNAlgTest({100, 32, 8, 8}, miopenBNPerActivation, pgm_name, krn_name, alg_name);
     EXPECT(pgm_name == "MIOpenBatchNormActivInfer.cl");
     EXPECT(krn_name == "MIOpenBatchNormActivInferPerActEst");
     EXPECT(alg_name == "MIOpenBatchNormActivInferPerActEst");
-
-    miopenDestroy(handle);
 }
