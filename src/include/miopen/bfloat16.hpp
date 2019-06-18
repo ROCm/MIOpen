38eccb458ec5e344f98708eb6a92425769cf52e1
Tejash Shah <tejashshah@gmail.com>
2019-06-18 12:24:47 -0700

    Add rounding to nearest even support for float to bfloat16 cast.  (#1822)
    * Added rounding to nearest even support for float to bfloat16 cast. Enabled it by default. Aligns with rocblas behavior. Added cmake flag to turn on truncation rounding scheme for bfloat16
    * Address code review comments and preserver signalling nan during truncation mode of bfloat16
    * Restore treating int32 datatype as valid for int8 test cases
    * Reordered condition checks in bfloat16 operations and addedmore comments

--- src/include/miopen/bfloat16.hpp
+++ src/include/miopen/bfloat16.hpp
@@ -27,6 +27,7 @@
 #define BFLOAT16_H_
 #include <boost/operators.hpp>
 #include <iostream>
+#include <miopen/config.h>
 
 class bfloat16 : boost::totally_ordered<bfloat16, boost::arithmetic<bfloat16>>
 {
@@ -41,7 +42,50 @@ class bfloat16 : boost::totally_ordered<bfloat16, boost::arithmetic<bfloat16>>
         } bits_st;
 
         bits_st.float_st = rhs;
-        data_            = bits_st.bf16_st >> 16;
+
+        // BF16 round and NaN preservation code matches
+        // https://github.com/ROCmSoftwarePlatform/rocBLAS/blob/develop/library/include/rocblas_bfloat16.h
+        if((~bits_st.bf16_st & 0x7f800000) == 0) // Inf or NaN
+        {
+            // When all of the exponent bits are 1, the value is Inf or NaN.
+            // Inf is indicated by a zero mantissa. NaN is indicated by any nonzero
+            // mantissa bit. Quiet NaN is indicated by the most significant mantissa
+            // bit being 1. Signaling NaN is indicated by the most significant
+            // mantissa bit being 0 but some other bit(s) being 1. If any of the
+            // lower 16 bits of the mantissa are 1, we set the least significant bit
+            // of the bfloat16 mantissa, in order to preserve signaling NaN in case
+            // the bloat16's mantissa bits are all 0.
+            if((bits_st.bf16_st & 0xffff) != 0)
+            {
+                bits_st.bf16_st |= 0x10000; // Preserve signaling NaN
+            }
+        }
+        else
+        {
+#if MIOPEN_USE_RNE_BFLOAT16 == 1
+            // When the exponent bits are not all 1s, then the value is zero, normal,
+            // or subnormal. We round the bfloat16 mantissa up by adding 0x7FFF, plus
+            // 1 if the least significant bit of the bfloat16 mantissa is 1 (odd).
+            // This causes the bfloat16's mantissa to be incremented by 1 if the 16
+            // least significant bits of the float mantissa are greater than 0x8000,
+            // or if they are equal to 0x8000 and the least significant bit of the
+            // bfloat16 mantissa is 1 (odd). This causes it to be rounded to even when
+            // the lower 16 bits are exactly 0x8000. If the bfloat16 mantissa already
+            // has the value 0x7f, then incrementing it causes it to become 0x00 and
+            // the exponent is incremented by one, which is the next higher FP value
+            // to the unrounded bfloat16 value. When the bfloat16 value is subnormal
+            // with an exponent of 0x00 and a mantissa of 0x7F, it may be rounded up
+            // to a normal value with an exponent of 0x01 and a mantissa of 0x00.
+            // When the bfloat16 value has an exponent of 0xFE and a mantissa of 0x7F,
+            // incrementing it causes it to become an exponent of 0xFF and a mantissa
+            // of 0x00, which is Inf, the next higher value to the unrounded value.
+            bits_st.bf16_st +=
+                (0x7fff + ((bits_st.bf16_st >> 16) & 1)); // Round to nearest, round to even
+#else                                                     // truncation
+// do nothing
+#endif
+        }
+        data_ = bits_st.bf16_st >> 16;
     }
     operator float() const
     {
--- src/kernels/bfloat16.h
+++ src/kernels/bfloat16.h
@@ -25,6 +25,7 @@
  *******************************************************************************/
 typedef union
 {
+    uint u32;
     ushort2 ushortx2;
     float f32;
 } cvt_bf16_fp32_t;
@@ -40,5 +41,47 @@ ushort float_to_bfloat16(float src_val)
 {
     cvt_bf16_fp32_t target_val;
     target_val.f32 = src_val;
-    return target_val.ushortx2.hi; // Truncation rounding
+    // BF16 round and NaN preservation code matches
+    // https://github.com/ROCmSoftwarePlatform/rocBLAS/blob/develop/library/include/rocblas_bfloat16.h
+    if((~target_val.u32 & 0x7f800000) == 0) // Inf or NaN
+    {
+        // When all of the exponent bits are 1, the value is Inf or NaN.
+        // Inf is indicated by a zero mantissa. NaN is indicated by any nonzero
+        // mantissa bit. Quiet NaN is indicated by the most significant mantissa
+        // bit being 1. Signaling NaN is indicated by the most significant
+        // mantissa bit being 0 but some other bit(s) being 1. If any of the
+        // lower 16 bits of the mantissa are 1, we set the least significant bit
+        // of the bfloat16 mantissa, in order to preserve signaling NaN in case
+        // the bloat16's mantissa bits are all 0.
+        if((target_val.u32 & 0xffff) != 0)
+        {
+            target_val.u32 |= 0x10000; // Preserve signaling NaN
+        }
+    }
+    else
+    {
+#ifdef MIOPEN_USE_RNE_BFLOAT16
+        // When the exponent bits are not all 1s, then the value is zero, normal,
+        // or subnormal. We round the bfloat16 mantissa up by adding 0x7FFF, plus
+        // 1 if the least significant bit of the bfloat16 mantissa is 1 (odd).
+        // This causes the bfloat16's mantissa to be incremented by 1 if the 16
+        // least significant bits of the float mantissa are greater than 0x8000,
+        // or if they are equal to 0x8000 and the least significant bit of the
+        // bfloat16 mantissa is 1 (odd). This causes it to be rounded to even when
+        // the lower 16 bits are exactly 0x8000. If the bfloat16 mantissa already
+        // has the value 0x7f, then incrementing it causes it to become 0x00 and
+        // the exponent is incremented by one, which is the next higher FP value
+        // to the unrounded bfloat16 value. When the bfloat16 value is subnormal
+        // with an exponent of 0x00 and a mantissa of 0x7F, it may be rounded up
+        // to a normal value with an exponent of 0x01 and a mantissa of 0x00.
+        // When the bfloat16 value has an exponent of 0xFE and a mantissa of 0x7F,
+        // incrementing it causes it to become an exponent of 0xFF and a mantissa
+        // of 0x00, which is Inf, the next higher value to the unrounded value.
+        target_val.u32 +=
+            (0x7fff + (target_val.ushortx2.hi & 1)); // Round to nearest, round to even
+#else                                                // Truncation rounding
+// do nothing
+#endif
+    }
+    return target_val.ushortx2.hi;
 }
