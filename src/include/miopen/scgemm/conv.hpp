879fa39d4f195b83a30a855e5fc935bfa182e57a
Vin Huang <39558462+vin-huang@users.noreply.github.com>
2019-07-29 00:45:35 +0800

    Static Compiled GEMM integration (#1764)
    * Static Compiled GEMM Utils
    
    *Used to generate the required arguments for operating SCGemm Kernels.
    -scgemm.hpp is the interface of the SCGEMM
    -Generate tensorshap for input/output/weight
    -Generate aux data (such as indexes) that required by SCGemm Kernels.
    
    *Using MIOPEN_USE_SCGEMM to enble/disable SCGEMM at compile times.
    
    Co-Authored-by: Xiuyu Li <Xiuyu.Li@amd.com>
    *SCGEMM API releated codes will place at folder src/scgemm
    
    * SCGEMM Utils Fix warnings found by cppcheck
    
    * SCGEMM Utils get op and routine type by kernel name
    
    * SCGEMM Utils remove definition and use enum type
    
    *use enum type to instead int when using switch case.
    *remove the definition of FP32 in scgemm.hpp
    
    * SCGEMM Utils refining the way to get routine type by kernel name
    
    * SCGEMM Utils modify typo at macro.hpp
    
    * SCGEMM Utils Enable OpenCL aux kernels.
    
    * SCGEMM Utils changing the type of kernel name to std::string
    *remove strop.cpp/strop.h
    *changing the type of kernel name to std::string
    
    * SCGEMM utils include vector for avoid compiler error
    
    * SCGEMM Utils use std containers instead of pointer arrays.
    
    * SCGEMM Utils comment out functions for compiled gemm/fconv kernel argument but left the code here for future reference
    
    * SCGEMM Utils comment out the code instead of using the preprocessor.
    
    * SCGEMM Utils change the type of is_relu to bool
    
    * SCGEMM Utils add comment for span function
    Authored-by: Xiuyu Li <Xiuyu.Li@amd.com>
    
    * SCGEMM Utils Merge relative function into one file and remove unused files.
    *remove scgemm_tensor,gmap,amap,span
    *move macro to tensorshape.hpp
    
    * SCGEMM Utils use struct: tensorshap_t directly instead of encode/decode a uint64_t
    
    * SCGEMM Utils use unsorted_map::find to instead unsorted_map::get, used to error handling when cannot find associate routine by kernel_name.
    
    * SCGEMM Utils move the definition of scgemm_kernel_args_t to scgemme.hpp for reference.
    *scgemm_kernel_args_t is the return tye of scgemm_compiled_fgemm/fconv_args.
    
    * SCGEMM Utils force to use _om kernels for gemm cases
    
    * SCGEMM Utils fix clang-tidy error bugprone-branch-clone

---
+++ src/include/miopen/scgemm/conv.hpp
@@ -0,0 +1,65 @@
+﻿/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+#ifndef GUARD_MIOPEN_SCGEMM_CONV_HPP_
+#define GUARD_MIOPEN_SCGEMM_CONV_HPP_
+
+#include <miopen/scgemm/scgemm_op.hpp>
+#include <miopen/scgemm/tensorshape.hpp>
+#include <miopen/scgemm/scgemm.hpp>
+#include <string>
+
+namespace miopen {
+namespace scgemm {
+bool is_fconv(const std::string& kernel_name);
+
+scgemm_conv_routine_t get_fconv_routine(const std::string& kernel_name);
+
+int generate_fconv_aux(void* auxbuf,
+                       const group_prop_t* gprop,
+                       const std::vector<uint32_t>& strides,
+                       const std::vector<uint32_t>& dilations,
+                       uint32_t ntidx,
+                       uint32_t nb_amap,
+                       uint32_t aozero);
+
+scgemm_fconv_params create_fconv_params(std::string& kernel_name,
+                                        std::vector<uint32_t>& grids,
+                                        std::vector<uint32_t>& blocks,
+                                        scgemm_conv_routine_t routine,
+                                        const group_prop_t* gprop,
+                                        uint32_t mask,
+                                        uint32_t ng);
+
+/*
+/// This fucintion is used to present how to fill in the kernel arguments for a
+/// Static Compiled GEMM - Forwared Convolution Kernel.
+/// Left the code here for reference.
+scgemm_kernel_args_t compiled_fconv_args(
+    size_t&, const void*, const void*, const void*, void*, void*, scgemm_fconv_params*, float);
+*/
+} // namespace scgemm
+} // namespace miopen
+#endif
