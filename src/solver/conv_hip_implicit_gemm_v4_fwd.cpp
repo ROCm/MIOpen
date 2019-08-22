26d21f83ad222701ff4c69a51307380d1cd2d81a
zjing14 <zhangjing14@gmail.com>
2019-08-22 06:59:46 -0500

    implicitgemm_stride_fix(1): check out_of_bound (#2012)

--- src/solver/conv_hip_implicit_gemm_v4_fwd.cpp
+++ src/solver/conv_hip_implicit_gemm_v4_fwd.cpp
@@ -42,10 +42,16 @@ bool ConvHipImplicitGemmV4Fwd::IsApplicable(const ConvolutionContext& ctx) const
         ctx.IsFp16() ? ((ctx.n_inputs * ctx.kernel_size_h * ctx.kernel_size_w) % 32 == 0)
                      : ((ctx.n_inputs * ctx.kernel_size_h * ctx.kernel_size_w) % 8 == 0);
 
+    // padding support required for out_of_bound configs
+    bool no_out_of_bound = (ctx.in_width >= ((ctx.kernel_size_w - 1) * ctx.kernel_dilation_w + 1) +
+                                                (ctx.out_width - 1) * ctx.kernel_stride_w) &&
+                           (ctx.in_height >= ((ctx.kernel_size_h - 1) * ctx.kernel_dilation_h + 1) +
+                                                 (ctx.out_height - 1) * ctx.kernel_stride_h);
+
     return ctx.Is2d() && isTypeSupported && ctx.direction.IsForward() && ctx.pad_h == 0 &&
            ctx.pad_w == 0 && ctx.group_counts == 1 && ctx.batch_sz % 8 == 0 &&
            (ctx.batch_sz * ctx.out_height * ctx.out_width) % 64 == 0 && isNumInputsInMultiple &&
-           ctx.n_outputs % 16 == 0;
+           ctx.n_outputs % 16 == 0 && no_out_of_bound;
 }
 
 /// \todo move to separate header and use in other solvers.
