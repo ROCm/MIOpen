ed8f93c855d24bd24de06c933ab55a56c17ecb5b
Artem Tamazov <artem.tamazov@gmail.com>
2019-11-20 22:15:15 +0300

    CO v3 support in Winograd kernels  (#2241)
    * cov3-asm-3x3(01) Add new ROCm MD/CO values. Enforce via env var (detection TBD)
    * cov3-asm-3x3(02) Enable CO v3 in ConvAsm3x3U
    - Update ConvAsm3x3U solver. EXPERIMENTAL_COv3 -> ROCM_METADATA_VERSION == 5.
    - Improve asm options logging.
    - Update + some refactors in the gcnAsmConv3x3U kernel source.
    - GCN assembly utils: Do not insert "-no-code-object-v3" when assembling for CO v3. Some refactors.
    * cov3-asm-3x3(03) Rework rocm_meta_version to a class. Simplify MD version checks.
    * cov3-asm-3x3(05) Some fixes and env.vars.
    - MIOPEN_DEBUG_AMD_ROCM_METADATA_PREFER_NEWER: prefer COv3 when both v3 and v2 are supported by both ROCm and Asm Solver.
    - MIOPEN_DEBUG_AMD_OPENCL_ENFORCE_COV3: unconditionally enforce COv3 for OpenCL kernels.
    * cov3-asm-3x3(07) Fix BN kernels & solver- rocm version, formatting, whitespace.
    * cov3-winograd(01) CO v3 for 3x3 Fwd/Bwd fp32 Winograd kernels. Not yet enabled in Solver.
    * cov3-winograd(04) Log caught exceptions
    * cov3-winograd(05) Enable Co v3 in ConvBinWinograd3x3U
    * cov3-winograd(06) Implemented in ConvBinWinogradRxS, fp32 fwd/bwd cases. Some fixes.
    * cov3-winograd(07) Implemented in ConvBinWinogradRxS, fp16 fwd/bwd cases (v13_3_12, V14_3_3)
    * cov3-winograd(08) Fix MD sgpr count (VCC and XNACK) in 3x3 and 9_0_15
    * cov3-winograd(10) MIOPEN_DEBUG_AMD_WINOGRAD_RXS_FWD_BWD
    * cov3-winograd(11) Update env.var documentation
    * cov3-winograd(12) ConvBinWinogradRxS (fp32, WrW) and ConvBinWinogradRxSf3x2 (fp32 fwd/bwd) -- v16_5_0
    * cov3-winograd(13) ConvBinWinogradRxSFused -- v9_2_7*
    * cov3-winograd(14) Clean up error messages (terminate if ROCM_METADATA_VERSION is not defined).
    
    [ROCm/MIOpen commit: 5d12b1054161ca1b9c5d176d8c4e75793d03336d]

--- src/solver/conv_winoRxS_f3x2.cpp
+++ src/solver/conv_winoRxS_f3x2.cpp
@@ -24,9 +24,10 @@
  *
  *******************************************************************************/
 
-#include "miopen/solver.hpp"
-#include "miopen/env.hpp"
-#include "miopen/stringutils.hpp"
+#include <miopen/solver.hpp>
+#include <miopen/env.hpp>
+#include <miopen/stringutils.hpp>
+#include <miopen/kernel_build_params.hpp>
 
 MIOPEN_DECLARE_ENV_VAR(MIOPEN_DEBUG_AMD_WINOGRAD_RXS_F3X2)
 
@@ -210,7 +211,7 @@ bool ConvBinWinogradRxSf3x2::IsApplicable(const ConvolutionContext& params) cons
         return false;
     if(!params.use_asm_kernels)
         return false;
-    if(!params.rmv.IsV2())
+    if(!params.rmv.IsV2orV3())
         return false;
 
     const auto name = params.GetStream().GetDeviceName();
@@ -257,6 +258,11 @@ ConvSolution ConvBinWinogradRxSf3x2::GetSolution(const ConvolutionContext& param
     kernel.l_wk.push_back(512);
     kernel.l_wk.push_back(1);
     kernel.l_wk.push_back(1);
+
+    KernelBuildParameters options{
+        {"ROCM_METADATA_VERSION", params.rmv.UseV3() ? 5 : 4},
+    };
+    kernel.comp_options = options.GenerateFor(kbp::GcnAsm{});
 
     kernel.kernel_name = "miopenSp3AsmConvRxSf3x2";
     kernel.kernel_file = "Conv_Winograd_v16_5_0_stride1.s";
