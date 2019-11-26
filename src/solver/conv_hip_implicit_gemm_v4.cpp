adc1afccd195f2aa7bcabef6172ed106fda2f0ae
Tejash Shah <tejashshah@gmail.com>
2019-11-26 14:56:03 -0800

    add bfp16 fp16 support in padding implicit gemm kernels (non xdlops) and enable tuning for all datatypes (#2213)
    * Enable padding support in fp16/bfp16
    * Add fp16/bfp16 support in implicit gemm padding kernels
    * Checkpoint for fp16/bfp16 as the kernel compiles but still have accuracy issues. It doesn't use buffer load/store
    * Resolve functional accuracy issue with paddiding non-xdlops kernels for fp16/bfp16
    * Resolve clang format issue
    * Resolve clang format 3.8 issues. Apparently, clang format 3.9 on ubuntu18.04 has different style
    * Change fp16 packing algorithm
    * Clang-format 3.9 formatting resolved
    * Separate out fp16/bfp16 and fp32 impls and add tuning support in non-xdlops padding kernels
    * Restored channel packing order to working one
    * clang-format3.8 style issues resolved
    * Settle code review comments
    * fp32 padding compilation error

--- src/solver/conv_hip_implicit_gemm_v4.cpp
+++ src/solver/conv_hip_implicit_gemm_v4.cpp
@@ -38,376 +38,6 @@
 namespace miopen {
 namespace solver {
 
-bool PerformanceImplicitGemm::operator==(const PerformanceImplicitGemm& other) const
-{
-    // clang-format off
-    return BPerBlock == other.BPerBlock
-        && KPerBlock == other.KPerBlock
-        && EPerBlock == other.EPerBlock
-        && GemmNRepeat == other.GemmNRepeat
-        && GemmMPerThreadSubC == other.GemmMPerThreadSubC
-        && GemmNPerThreadSubC == other.GemmNPerThreadSubC
-        && GemmMLevel0Cluster == other.GemmMLevel0Cluster
-        && GemmNLevel0Cluster == other.GemmNLevel0Cluster
-        && GemmMLevel1Cluster == other.GemmMLevel1Cluster
-        && GemmNLevel1Cluster == other.GemmNLevel1Cluster
-        && InBlockCopyClusterLengths_E == other.InBlockCopyClusterLengths_E
-        && InBlockCopyClusterLengths_B == other.InBlockCopyClusterLengths_B
-        && InBlockCopyClusterLengths_N1 == other.InBlockCopyClusterLengths_N1
-        && InBlockCopyClusterLengths_N2 == other.InBlockCopyClusterLengths_N2
-        && WeiBlockCopyClusterLengths_E == other.WeiBlockCopyClusterLengths_E
-        && WeiBlockCopyClusterLengths_K == other.WeiBlockCopyClusterLengths_K
-        && use_spare_set == other.use_spare_set;
-    // clang-format on
-}
-
-bool PerformanceImplicitGemm::IsValid(const ConvolutionContext& ctx) const
-{
-    int N = ctx.batch_sz;
-    int K = ctx.n_outputs;
-    int C = ctx.n_inputs;
-
-    int Ho = ImgHeight(ctx);
-    int Wo = ImgWidth(ctx);
-
-    int Y = ctx.kernel_size_h;
-    int X = ctx.kernel_size_w;
-
-    if(ctx.direction.IsBackwardWrW())
-    {
-        N  = ctx.n_outputs; // swapped
-        K  = ctx.n_inputs;  // swapped
-        C  = ctx.batch_sz;  // swapped
-        Ho = ctx.kernel_size_h;
-        Wo = ctx.kernel_size_w;
-        Y  = ctx.in_height; // swapped
-        X  = ctx.in_width;  // swapped
-    }
-
-    const int N1 = GemmNRepeat;
-    const int N2 = GemmNPerThreadSubC;
-    if(N % (N1 * N2) != 0)
-        return false; // wrong! cannot divice N evenly among thread
-
-    const int N0 = N / (N1 * N2);
-
-    const int B = N0 * Ho * Wo;
-
-    const auto nonVectorizedC = C / GetEPackLength(ctx, false);
-    const auto E              = nonVectorizedC * Y * X;
-
-    if(!(EPerBlock % InBlockCopyClusterLengths_E == 0 &&
-         EPerBlock % WeiBlockCopyClusterLengths_E == 0 &&
-         BPerBlock % InBlockCopyClusterLengths_B == 0 &&
-         KPerBlock % WeiBlockCopyClusterLengths_K == 0 && N1 % InBlockCopyClusterLengths_N1 == 0 &&
-         N2 % InBlockCopyClusterLengths_N2 == 0))
-        return false;
-
-    if(ctx.direction.IsBackwardWrW())
-    {
-        if(!((X * Y) % (EPerBlock / WeiBlockCopyClusterLengths_E) == 0))
-            return false;
-    }
-    // divide block work by [K, B]
-    if(!(K % KPerBlock == 0 && B % BPerBlock == 0 && E % (2 * EPerBlock) == 0))
-        return false; // wrong! cannot divice N evenly among thread
-
-    if((N1 * N2 * BPerBlock) % (GemmNPerThreadSubC * GemmNLevel0Cluster * GemmNLevel1Cluster) != 0)
-        return false;
-
-    if(ctx.IsFp16() && GemmNPerThreadSubC != GemmMPerThreadSubC)
-        return false;
-
-    // sanity check
-    if((KPerBlock % (GemmMPerThreadSubC * GemmMLevel0Cluster * GemmMLevel1Cluster)) != 0)
-        return false;
-
-    if(GemmNRepeat !=
-       (N1 * N2 * BPerBlock) / (GemmNPerThreadSubC * GemmNLevel0Cluster * GemmNLevel1Cluster))
-        return false;
-
-    const int ThreadPerLevel1Cluster =
-        GemmMLevel0Cluster * GemmNLevel0Cluster * GemmMLevel1Cluster * GemmNLevel1Cluster;
-
-    const int block_size = ThreadPerLevel1Cluster;
-
-    if(block_size < 64 || block_size > 512)
-        return false;
-
-    if(block_size !=
-       InBlockCopyClusterLengths_E * InBlockCopyClusterLengths_N1 * InBlockCopyClusterLengths_B *
-           InBlockCopyClusterLengths_N2)
-        return false;
-
-    if(block_size != WeiBlockCopyClusterLengths_K * WeiBlockCopyClusterLengths_E)
-        return false;
-
-    const int GemmMRepeat =
-        KPerBlock / (GemmMPerThreadSubC * GemmMLevel0Cluster * GemmMLevel1Cluster);
-
-    if(!(GemmMRepeat == 2 && GemmNRepeat == 2))
-        return false;
-
-    const int InBlockCopySubLengths_E = EPerBlock / InBlockCopyClusterLengths_E;
-    const int InBlockCopySubLengths_B = BPerBlock / InBlockCopyClusterLengths_B;
-
-    return (InBlockCopySubLengths_E == 1 && InBlockCopySubLengths_B == 1);
-}
-
-void PerformanceImplicitGemm::EuristicInit(const ConvolutionContext& config)
-{
-    // default
-    {
-        BPerBlock = 16;
-        KPerBlock = 128;
-        EPerBlock = 8;
-
-        GemmNRepeat = 2;
-
-        GemmMPerThreadSubC = 4;
-        GemmNPerThreadSubC = 4;
-
-        GemmMLevel0Cluster = 4;
-        GemmNLevel0Cluster = 4;
-        GemmMLevel1Cluster = 4;
-        GemmNLevel1Cluster = 4;
-
-        InBlockCopyClusterLengths_E  = 8;
-        InBlockCopyClusterLengths_N1 = 2;
-        InBlockCopyClusterLengths_B  = 16;
-        InBlockCopyClusterLengths_N2 = 1;
-
-        WeiBlockCopyClusterLengths_E = 2;
-        WeiBlockCopyClusterLengths_K = 128;
-    }
-
-    if(!IsValid(config))
-    {
-        BPerBlock = 8;
-        KPerBlock = 128;
-        EPerBlock = 8;
-
-        GemmMLevel0Cluster = 4;
-        GemmNLevel0Cluster = 4;
-        GemmMLevel1Cluster = 4;
-        GemmNLevel1Cluster = 2;
-
-        InBlockCopyClusterLengths_E  = 8;
-        InBlockCopyClusterLengths_N1 = 1;
-        InBlockCopyClusterLengths_B  = 8;
-        InBlockCopyClusterLengths_N2 = 2;
-
-        WeiBlockCopyClusterLengths_E = 2;
-        WeiBlockCopyClusterLengths_K = 64;
-    }
-
-    if(!IsValid(config))
-    {
-        BPerBlock = 8;
-        KPerBlock = 64;
-        EPerBlock = 8;
-
-        GemmMLevel0Cluster = 4;
-        GemmNLevel0Cluster = 2;
-        GemmMLevel1Cluster = 2;
-        GemmNLevel1Cluster = 4;
-
-        InBlockCopyClusterLengths_E  = 8;
-        InBlockCopyClusterLengths_N1 = 1;
-        InBlockCopyClusterLengths_B  = 8;
-        InBlockCopyClusterLengths_N2 = 1;
-
-        WeiBlockCopyClusterLengths_E = 4;
-        WeiBlockCopyClusterLengths_K = 16;
-    }
-
-    if(!IsValid(config))
-    {
-        BPerBlock = 16;
-        KPerBlock = 32;
-        EPerBlock = 4;
-
-        GemmMLevel0Cluster = 1;
-        GemmNLevel0Cluster = 4;
-        GemmMLevel1Cluster = 4;
-        GemmNLevel1Cluster = 4;
-
-        InBlockCopyClusterLengths_E  = 4;
-        InBlockCopyClusterLengths_N1 = 1;
-        InBlockCopyClusterLengths_B  = 16;
-        InBlockCopyClusterLengths_N2 = 1;
-    }
-
-    if(!IsValid(config))
-    {
-        BPerBlock = 16;
-        KPerBlock = 16;
-        EPerBlock = 4;
-
-        GemmMPerThreadSubC = 2;
-        GemmNPerThreadSubC = 2;
-
-        GemmMLevel0Cluster = 2;
-        GemmNLevel0Cluster = 4;
-        GemmMLevel1Cluster = 2;
-        GemmNLevel1Cluster = 4;
-    }
-
-    if(!IsValid(config))
-    {
-        MIOPEN_LOG_E("All attempts failed");
-        assert(false);
-    }
-    MIOPEN_LOG_I(ToString());
-}
-
-bool PerformanceImplicitGemm::IsValidValue() const
-{
-    // clang-format off
-    return IsTwoPower<8,16>(BPerBlock)
-        && IsTwoPower<16,128>(KPerBlock)
-        && IsTwoPower<4,16>(EPerBlock)
-        && GemmNRepeat == 2
-        && IsTwoPower<2,4>(GemmMPerThreadSubC)
-        && IsTwoPower<2,4>(GemmNPerThreadSubC)
-        && IsTwoPower<1,4>(GemmMLevel0Cluster)
-        && IsTwoPower<1,4>(GemmNLevel0Cluster)
-        && IsTwoPower<1,4>(GemmMLevel1Cluster)
-        && IsTwoPower<1,4>(GemmNLevel1Cluster)
-        && IsTwoPower<4,16>(InBlockCopyClusterLengths_E)
-        && IsTwoPower<8,16>(InBlockCopyClusterLengths_B)
-        && IsTwoPower<1,2>(InBlockCopyClusterLengths_N1)
-        && IsTwoPower<1,4>(InBlockCopyClusterLengths_N2)
-        && IsTwoPower<1,4>(WeiBlockCopyClusterLengths_E)
-        && IsTwoPower<16,128>(WeiBlockCopyClusterLengths_K); // clang-format on
-}
-
-bool PerformanceImplicitGemm::SetNextValue()
-{
-    // GemmNRepeat = 2 cosntant
-    do
-    {
-        // BPerBlock == 16 constant for no-spare
-        // GemmNLevel0Cluster = 4 constant for no-spare
-        // GemmNLevel1Cluster = 4 constant for no-spare
-        // InBlockCopyClusterLengths_B = 16 constant for no-spare
-        if(!use_spare_set)
-        {
-            if(!NextTwoPower<2, 4>(GemmMLevel0Cluster))
-                break;
-            if(!NextTwoPower<2, 4>(GemmMLevel1Cluster))
-                break;
-        }
-        else
-        {
-            if(!NextTwoPower<8, 16>(BPerBlock))
-                break;
-            if(!NextTwoPower<1, 4>(GemmNLevel0Cluster))
-                break;
-            if(!NextTwoPower<1, 4>(GemmNLevel1Cluster))
-                break;
-            if(!NextTwoPower<8, 16>(InBlockCopyClusterLengths_B))
-                break;
-            if(!NextTwoPower<1, 4>(GemmMLevel0Cluster))
-                break;
-            if(!NextTwoPower<1, 4>(GemmMLevel1Cluster))
-                break;
-        }
-
-        if(!NextTwoPower<1, 4>(WeiBlockCopyClusterLengths_E))
-            break;
-        if(!NextTwoPower<16, 128>(WeiBlockCopyClusterLengths_K))
-            break;
-        if(!NextTwoPower<2, 4>(GemmMPerThreadSubC))
-            break;
-        if(!NextTwoPower<2, 4>(GemmNPerThreadSubC))
-            break;
-        if(!NextTwoPower<16, 128>(KPerBlock))
-            break;
-        if(!NextTwoPower<4, 16>(EPerBlock))
-            break;
-        if(!NextTwoPower<4, 16>(InBlockCopyClusterLengths_E))
-            break;
-        if(!NextTwoPower<1, 2>(InBlockCopyClusterLengths_N1))
-            break;
-        if(!NextTwoPower<1, 2>(InBlockCopyClusterLengths_N2))
-            break;
-        return false;
-    } while(false);
-
-    return true;
-}
-
-std::string PerformanceImplicitGemm::ToString() const
-{
-    std::ostringstream ss;
-    Serialize(ss);
-    return ss.str();
-}
-
-PerformanceImplicitGemm::PerformanceImplicitGemm(bool spare)
-{
-    BPerBlock = spare ? 8 : 16; // constant for no-spare
-    KPerBlock = 16;
-    EPerBlock = 4;
-
-    GemmNRepeat = 2; // constant for all
-
-    GemmMPerThreadSubC = 2;
-    GemmNPerThreadSubC = 2;
-
-    GemmMLevel0Cluster = spare ? 1 : 2;
-    GemmNLevel0Cluster = spare ? 1 : 4; // constant for no-spare
-    GemmMLevel1Cluster = spare ? 1 : 2;
-    GemmNLevel1Cluster = spare ? 1 : 4; // constant for no-spare
-
-    InBlockCopyClusterLengths_E  = 4;
-    InBlockCopyClusterLengths_N1 = 1;
-    InBlockCopyClusterLengths_B  = spare ? 8 : 16; // constant for no-spare
-    InBlockCopyClusterLengths_N2 = 1;
-
-    WeiBlockCopyClusterLengths_E = 1;
-    WeiBlockCopyClusterLengths_K = 16;
-    use_spare_set                = spare;
-}
-
-PerformanceImplicitGemm::PerformanceImplicitGemm(int BPerBlock_,
-                                                 int KPerBlock_,
-                                                 int EPerBlock_,
-                                                 int GemmNRepeat_,
-                                                 int GemmMPerThreadSubC_,
-                                                 int GemmNPerThreadSubC_,
-                                                 int GemmMLevel0Cluster_,
-                                                 int GemmNLevel0Cluster_,
-                                                 int GemmMLevel1Cluster_,
-                                                 int GemmNLevel1Cluster_,
-                                                 int InBlockCopyClusterLengths_E_,
-                                                 int InBlockCopyClusterLengths_B_,
-                                                 int InBlockCopyClusterLengths_N1_,
-                                                 int InBlockCopyClusterLengths_N2_,
-                                                 int WeiBlockCopyClusterLengths_E_,
-                                                 int WeiBlockCopyClusterLengths_K_,
-                                                 bool use_spare_set_)
-    : BPerBlock(BPerBlock_),
-      KPerBlock(KPerBlock_),
-      EPerBlock(EPerBlock_),
-      GemmNRepeat(GemmNRepeat_),
-      GemmMPerThreadSubC(GemmMPerThreadSubC_),
-      GemmNPerThreadSubC(GemmNPerThreadSubC_),
-      GemmMLevel0Cluster(GemmMLevel0Cluster_),
-      GemmNLevel0Cluster(GemmNLevel0Cluster_),
-      GemmMLevel1Cluster(GemmMLevel1Cluster_),
-      GemmNLevel1Cluster(GemmNLevel1Cluster_),
-      InBlockCopyClusterLengths_E(InBlockCopyClusterLengths_E_),
-      InBlockCopyClusterLengths_B(InBlockCopyClusterLengths_B_),
-      InBlockCopyClusterLengths_N1(InBlockCopyClusterLengths_N1_),
-      InBlockCopyClusterLengths_N2(InBlockCopyClusterLengths_N2_),
-      WeiBlockCopyClusterLengths_E(WeiBlockCopyClusterLengths_E_),
-      WeiBlockCopyClusterLengths_K(WeiBlockCopyClusterLengths_K_),
-      use_spare_set(use_spare_set_)
-{
-}
-
 bool ConvHipImplicitGemmV4_1x1::IsApplicable(const ConvolutionContext& ctx) const
 {
     if(!ctx.Is2d())
@@ -421,6 +51,18 @@ bool ConvHipImplicitGemmV4_1x1::IsApplicable(const ConvolutionContext& ctx) cons
 
 bool ConvHipImplicitGemmV4Fwd::IsApplicable(const ConvolutionContext& ctx) const
 {
+    if(!ctx.direction.IsForward())
+        return false;
+
+    if(!ctx.Is2d())
+        return false;
+
+    if(!ctx.IsFp32() && !ctx.IsFp16() && !ctx.IsBfp16())
+        return false;
+
+    if(ctx.group_counts != 1)
+        return false;
+
 #if WORKAROUND_ISSUE_2174_2222_2224_2243
     if(miopen::HipGetHccVersion() >= external_tool_version_t{2, 6, 0})
     {
@@ -428,8 +70,6 @@ bool ConvHipImplicitGemmV4Fwd::IsApplicable(const ConvolutionContext& ctx) const
             return false;
     }
 #endif
-
-    bool isTypeSupported = ctx.IsFp32() || ctx.IsFp16() || ctx.IsBfp16();
 
     // channels is divided by epack to pack 2/4 fp16/bfp16
     if(ctx.n_inputs % GetEPackLength(ctx, false) != 0)
@@ -439,9 +79,9 @@ bool ConvHipImplicitGemmV4Fwd::IsApplicable(const ConvolutionContext& ctx) const
     // operation
     // For bfp16/fp16, when E=c*x*y % 16 == 0, 2 channels are accumulated through dot2 operation
     // For fp32, when E=c*x*y % 8 == 0, no dot2 operation exist.
-    bool isEInMultiple = (ctx.IsFp16() || ctx.IsBfp16())
-                             ? ((ctx.n_inputs * ctx.kernel_size_h * ctx.kernel_size_w) % 16 == 0)
-                             : ((ctx.n_inputs * ctx.kernel_size_h * ctx.kernel_size_w) % 8 == 0);
+    const int MultipleOf = (ctx.IsFp16() || ctx.IsBfp16()) ? 16 : 8;
+    if((ctx.n_inputs * ctx.kernel_size_h * ctx.kernel_size_w) % MultipleOf != 0)
+        return false;
 
     // padding support required for out_of_bound configs
     bool no_out_of_bound = (ctx.in_width >= ((ctx.kernel_size_w - 1) * ctx.kernel_dilation_w + 1) +
@@ -449,10 +89,9 @@ bool ConvHipImplicitGemmV4Fwd::IsApplicable(const ConvolutionContext& ctx) const
                            (ctx.in_height >= ((ctx.kernel_size_h - 1) * ctx.kernel_dilation_h + 1) +
                                                  (ctx.out_height - 1) * ctx.kernel_stride_h);
 
-    return ctx.Is2d() && isTypeSupported && ctx.direction.IsForward() && ctx.pad_h == 0 &&
-           ctx.pad_w == 0 && ctx.group_counts == 1 && ctx.batch_sz % 8 == 0 &&
-           (ctx.batch_sz * ctx.out_height * ctx.out_width) % 64 == 0 && isEInMultiple &&
-           ctx.n_outputs % 16 == 0 && no_out_of_bound;
+    return ctx.pad_h == 0 && ctx.pad_w == 0 && ctx.batch_sz % 8 == 0 &&
+           (ctx.batch_sz * ctx.out_height * ctx.out_width) % 64 == 0 && ctx.n_outputs % 16 == 0 &&
+           no_out_of_bound;
 }
 
 bool ConvHipImplicitGemmV4WrW::IsApplicable(const ConvolutionContext& ctx) const
@@ -464,6 +103,9 @@ bool ConvHipImplicitGemmV4WrW::IsApplicable(const ConvolutionContext& ctx) const
         return false;
 
     if(!(ctx.IsFp32() || ctx.IsFp16() || ctx.IsBfp16()))
+        return false;
+
+    if(ctx.group_counts != 1)
         return false;
 
 #if WORKAROUND_ISSUE_2174_2222_2224_2243
@@ -478,12 +120,16 @@ bool ConvHipImplicitGemmV4WrW::IsApplicable(const ConvolutionContext& ctx) const
     if(ctx.batch_sz % GetEPackLength(ctx, false) != 0)
         return false;
 
-    bool isEInMultiple = (ctx.IsFp16() || ctx.IsBfp16())
-                             ? ((ctx.batch_sz * ctx.in_height * ctx.in_width) % 16 == 0)
-                             : ((ctx.batch_sz * ctx.in_height * ctx.in_width) % 8 == 0);
+    // For fp16, when E=c*x*y % 32 == 0, 4 channels are accumulated through dot4 (2 * dot2)
+    // operation
+    // For bfp16/fp16, when E=c*x*y % 16 == 0, 2 channels are accumulated through dot2 operation
+    // For fp32, when E=c*x*y % 8 == 0, no dot2 operation exist.
+    const int MultipleOf = (ctx.IsFp16() || ctx.IsBfp16()) ? 16 : 8;
+    if((ctx.batch_sz * ctx.in_height * ctx.in_width) % MultipleOf != 0)
+        return false;
 
-    return ctx.pad_h == 0 && ctx.pad_w == 0 && ctx.group_counts == 1 && ctx.n_outputs % 8 == 0 &&
-           (ctx.n_outputs * ctx.kernel_size_h * ctx.kernel_size_w) % 64 == 0 && isEInMultiple &&
+    return ctx.pad_h == 0 && ctx.pad_w == 0 && ctx.n_outputs % 8 == 0 &&
+           (ctx.n_outputs * ctx.kernel_size_h * ctx.kernel_size_w) % 64 == 0 &&
            ctx.n_inputs % 16 == 0;
 }
 
