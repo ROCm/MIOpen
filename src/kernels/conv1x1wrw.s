6ce4c3fcab25c02b0e93023818727ba5579184ed
Artem Tamazov <artem.tamazov@gmail.com>
2019-11-26 21:38:16 +0300

    CO v3 - fix SGPR count etc (#2264)
    * cov3-fix-sgpr-count-etc(01) Fixed in conv3x3.s. Introduced and used "rocm_version.inc".
    
    * cov3-fix-sgpr-count-etc(03) conv3x3.s: fix total sgpr count issues and simplify code.
    
    * cov3-fix-sgpr-count-etc(04) conv1x1u.s: Fix sgpr reservation. conv3x3.s: Fix comment.
    
    * cov3-fix-sgpr-count-etc(05) conv1x1u_stride2.s: Fix sgpr reservation, simplify code.
    
    * cov3-fix-sgpr-count-etc(06) conv1x1wrw.s: Fix sgpr reservation, simplify code.
    
    * cov3-fix-sgpr-count-etc(07) conv1x1u_bias_activ.s: Fix sgpr reservation, simplify code.

--- src/kernels/conv1x1wrw.s
+++ src/kernels/conv1x1wrw.s
@@ -37,6 +37,7 @@
 .amdgpu_hsa_kernel miopenGcnAsmConv1x1WrW
 .endif
 
+.include "rocm_version.inc"
 .include "gpr_alloc.inc"
 .include "common.inc"
 .include "inst_wrappers.inc"
@@ -305,6 +306,7 @@ part2_offset = part1_chunks * input_w_stride * active_lanes_in_part1_chunks
 .SGPR_ALLOC wave_id // wave_id in group
 
 .SGPR_RESERVE_XNACK
+.SGPR_RESERVE_VCC
 
 .VGPR_ALLOC_FROM 0
 .VGPR_ALLOC tid
@@ -1149,7 +1151,7 @@ workgroup_size_x = n_per_group * 64
         .amdhsa_system_sgpr_workgroup_id_y 1
         .amdhsa_system_sgpr_workgroup_id_z 1
         .amdhsa_system_vgpr_workitem_id 1
-        .amdhsa_next_free_sgpr .AUTO_SGPR_COUNT
+        .amdhsa_next_free_sgpr __amdhsa_next_free_sgpr
         .amdhsa_next_free_vgpr .AUTO_VGPR_COUNT
         .amdhsa_group_segment_fixed_size .AUTO_LDS_BYTE_SIZE
         .amdhsa_dx10_clamp 0
@@ -1158,9 +1160,12 @@ workgroup_size_x = n_per_group * 64
         .amdhsa_float_round_mode_16_64 0
         .amdhsa_float_denorm_mode_32 0
         .amdhsa_float_denorm_mode_16_64 3
-        .amdhsa_reserve_flat_scratch 0
+        .amdhsa_reserve_flat_scratch __sgpr_reserve_flatscr
+        .amdhsa_reserve_xnack_mask __sgpr_reserve_xnack
+        .amdhsa_reserve_vcc __sgpr_reserve_vcc
 .end_amdhsa_kernel
 
+.altmacro
 .macro METADATA sc, vc, wg_x, lds_size, kernarg_size
 .amdgpu_metadata
 ---
@@ -1197,6 +1202,7 @@ amdhsa.kernels:
 .endm // METADATA
 
 .elseif ROCM_METADATA_VERSION == 4
+.altmacro
 .macro METADATA sc, vc, wg_x, lds_size, kernarg_size
     .amd_amdgpu_hsa_metadata
     { Version: [ 1, 0 ],
@@ -1223,16 +1229,7 @@ amdhsa.kernels:
     }
     .end_amd_amdgpu_hsa_metadata
 .endm
-
-.else
-.error "Unsupported ROCM_METADATA_VERSION"
-.end
 .endif
 
-.altmacro
-.macro METADATA_WRAPPER sc, vc, wg_x, lds_size, kernarg_size
-    METADATA %\sc, %\vc, %\wg_x, %\lds_size, %\kernarg_size
-.endm
-
-METADATA_WRAPPER .AUTO_SGPR_COUNT, .AUTO_VGPR_COUNT, workgroup_size_x, .AUTO_LDS_BYTE_SIZE, KERNEL_ARGUMENTS_SIZE
+METADATA %.AUTO_SGPR_COUNT, %.AUTO_VGPR_COUNT, %workgroup_size_x, %.AUTO_LDS_BYTE_SIZE, %KERNEL_ARGUMENTS_SIZE
 
