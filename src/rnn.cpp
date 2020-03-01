2a4225b71a9cfdd0e47798e62cbf3e8490d1fd44
Jing Zhou <jingz@amd.com>
2020-02-29 19:29:53 -0800

    rnn driver with dropout feature verification

--- src/rnn.cpp
+++ src/rnn.cpp
@@ -281,17 +281,18 @@ std::vector<int> RNNDescriptor::pTensorLengthsCalculation(const TensorDescriptor
 
 RNNDescriptor::RNNDescriptor()
 {
-    nLayers                = 1;
-    hsize                  = 0;
-    nHiddenTensorsPerLayer = 0;
-    rnnMode                = miopenRNNTANH;
-    dirMode                = miopenRNNunidirection;
-    biasMode               = miopenRNNNoBias;
-    algoMode               = miopenRNNdefault;
-    inputMode              = miopenRNNlinear;
-    dataType               = miopenFloat;
-    typeSize               = 4;
-    workspaceScale         = 1;
+    nLayers                     = 1;
+    hsize                       = 0;
+    nHiddenTensorsPerLayer      = 0;
+    rnnMode                     = miopenRNNTANH;
+    dirMode                     = miopenRNNunidirection;
+    biasMode                    = miopenRNNNoBias;
+    algoMode                    = miopenRNNdefault;
+    inputMode                   = miopenRNNlinear;
+    dataType                    = miopenFloat;
+    typeSize                    = 4;
+    workspaceScale              = 1;
+    miopen::deref(&dropoutDesc) = new miopen::DropoutDescriptor();
 }
 
 RNNDescriptor::RNNDescriptor(int hsz,
@@ -330,14 +331,15 @@ RNNDescriptor::RNNDescriptor(int hsz,
         typeSize = 4;
     }
 
-    hsize     = hsz;
-    nLayers   = layers;
-    inputMode = inMode;
-    dirMode   = bidir;
-    rnnMode   = rmode;
-    algoMode  = amode;
-    biasMode  = bmode;
-    dataType  = dType;
+    hsize                       = hsz;
+    nLayers                     = layers;
+    inputMode                   = inMode;
+    dirMode                     = bidir;
+    rnnMode                     = rmode;
+    algoMode                    = amode;
+    biasMode                    = bmode;
+    dataType                    = dType;
+    miopen::deref(&dropoutDesc) = new miopen::DropoutDescriptor();
 
     switch(rmode)
     {
