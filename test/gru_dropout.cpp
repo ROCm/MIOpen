ffab992f6b477f859fed47f02ff71d5956dd5662
Jing Zhou <jingz@amd.com>
2020-03-03 17:36:44 -0800

    refactor gru ctest with dropout pattern test enabled

--- test/gru_dropout.cpp
+++ test/gru_dropout.cpp
@@ -27,55 +27,37 @@
 #include "gru_common.hpp"
 
 template <class T>
-struct gru_driver : gru_basic_driver<T>
+struct gru_dropout_driver : gru_basic_driver<T>
 {
-    gru_driver() : gru_basic_driver<T>()
+    gru_dropout_driver() : gru_basic_driver<T>()
     {
         std::vector<int> modes(2, 0);
         modes[1] = 1;
         std::vector<int> defaultBS(1);
 
-        this->add(this->batchSize, "batch-size", this->generate_data(get_gru_batchSize(), {17}));
-        this->add(this->seqLength, "seq-len", this->generate_data(get_gru_seq_len(), {2}));
-        this->add(this->inVecLen, "vector-len", this->generate_data(get_gru_vector_len()));
-        this->add(this->hiddenSize, "hidden-size", this->generate_data(get_gru_hidden_size()));
-        this->add(this->numLayers, "num-layers", this->generate_data(get_gru_num_layers()));
+        this->add(this->batchSize, "batch-size", this->generate_data({17}));
+        this->add(this->seqLength, "seq-len", this->generate_data({23}));
+        this->add(this->inVecLen, "vector-len", this->generate_data({13}));
+        this->add(this->hiddenSize, "hidden-size", this->generate_data({67}));
+        this->add(this->numLayers, "num-layers", this->generate_data({3}));
         this->add(this->nohx, "no-hx", this->flag());
         this->add(this->nodhy, "no-dhy", this->flag());
         this->add(this->nohy, "no-hy", this->flag());
         this->add(this->nodhx, "no-dhx", this->flag());
-        this->add(this->flatBatchFill, "flat-batch-fill", this->flag());
-        this->add(this->useDropout, "use-dropout", this->generate_data({0}));
+        this->add(this->flatBatchFill, "flat-batch-fill", this->generate_data({false, true}));
+        this->add(this->useDropout, "use-dropout", this->generate_data({1}));
 
 #if(MIO_GRU_TEST_DEBUG == 3)
         this->biasMode  = 0;
         this->dirMode   = 1;
         this->inputMode = 0;
 #else
-        this->add(this->inputMode, "in-mode", this->generate_data(modes));
-        this->add(this->biasMode, "bias-mode", this->generate_data(modes));
+        this->add(this->inputMode, "in-mode", this->generate_data({0}));
+        this->add(this->biasMode, "bias-mode", this->generate_data({0}));
         this->add(this->dirMode, "dir-mode", this->generate_data(modes));
 #endif
-        this->add(
-            this->batchSeq,
-            "batch-seq",
-            this->lazy_generate_data(
-                [=] { return generate_batchSeq(this->batchSize, this->seqLength); }, defaultBS));
+        this->add(this->batchSeq, "batch-seq", this->generate_data(defaultBS));
     }
 };
 
-int main(int argc, const char* argv[])
-{
-#if(MIO_RNN_TIME_EVERYTHING > 0)
-    auto t_start = std::chrono::high_resolution_clock::now();
-#endif
-    test_drive<gru_driver>(argc, argv);
-
-#if(MIO_RNN_TIME_EVERYTHING > 0)
-    auto t_end = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Wall clock: GRU test pass time: "
-              << std::chrono::duration<double>(t_end - t_start).count() << " seconds." << std::endl;
-#endif
-    exit(0);
-}
+int main(int argc, const char* argv[]) { test_drive<gru_dropout_driver>(argc, argv); }
