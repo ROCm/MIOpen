d473fea22f750b42698c7554b68b1df231067641
Murali Nadhimandalam <muralin.rama@gmail.com>
2020-04-01 13:44:45 -0700

    Bfloat changes

--- src/kernels/composable_kernel/include/utility/amd_buffer_addressing.hpp
+++ src/kernels/composable_kernel/include/utility/amd_buffer_addressing.hpp
@@ -41,11 +41,24 @@ __device__ half __llvm_amdgcn_buffer_load_f16(int32x4_t rsrc,
                                               bool glc,
                                               bool slc) __asm("llvm.amdgcn.buffer.load.f16");
 
+__device__ ushort __llvm_amdgcn_buffer_load_bf16(int32x4_t rsrc,
+                                                 index_t vindex,
+                                                 index_t offset,
+                                                 bool glc,
+                                                 bool slc) __asm("llvm.amdgcn.buffer.load.f16");
+
 __device__ half2_t __llvm_amdgcn_buffer_loadx2_f16(int32x4_t rsrc,
                                                    index_t vindex,
                                                    index_t offset,
                                                    bool glc,
                                                    bool slc) __asm("llvm.amdgcn.buffer.load.v2f16");
+
+__device__ ushort2_t
+__llvm_amdgcn_buffer_loadx2_bf16(int32x4_t rsrc,
+                                 index_t vindex,
+                                 index_t offset,
+                                 bool glc,
+                                 bool slc) __asm("llvm.amdgcn.buffer.load.v2f16");
 
 __device__ half4_t __llvm_amdgcn_buffer_loadx4_f16(int32x4_t rsrc,
                                                    index_t vindex,
@@ -53,6 +66,12 @@ __device__ half4_t __llvm_amdgcn_buffer_loadx4_f16(int32x4_t rsrc,
                                                    bool glc,
                                                    bool slc) __asm("llvm.amdgcn.buffer.load.v4f16");
 
+__device__ ushort4_t
+__llvm_amdgcn_buffer_loadx4_bf16(int32x4_t rsrc,
+                                 index_t vindex,
+                                 index_t offset,
+                                 bool glc,
+                                 bool slc) __asm("llvm.amdgcn.buffer.load.v4f16");
 __device__ void __llvm_amdgcn_buffer_store(float vdata,
                                            int32x4_t rsrc,
                                            index_t vindex,
@@ -94,6 +113,27 @@ __device__ void __llvm_amdgcn_buffer_storex4_f16(half4_t vdata,
                                                  index_t offset,
                                                  bool glc,
                                                  bool slc) __asm("llvm.amdgcn.buffer.store.v4f16");
+
+__device__ void __llvm_amdgcn_buffer_store_bf16(ushort vdata,
+                                                int32x4_t rsrc,
+                                                index_t vindex,
+                                                index_t offset,
+                                                bool glc,
+                                                bool slc) __asm("llvm.amdgcn.buffer.store.f16");
+
+__device__ void __llvm_amdgcn_buffer_storex2_bf16(ushort2_t vdata,
+                                                  int32x4_t rsrc,
+                                                  index_t vindex,
+                                                  index_t offset,
+                                                  bool glc,
+                                                  bool slc) __asm("llvm.amdgcn.buffer.store.v2f16");
+
+__device__ void __llvm_amdgcn_buffer_storex4_bf16(ushort4_t vdata,
+                                                  int32x4_t rsrc,
+                                                  index_t vindex,
+                                                  index_t offset,
+                                                  bool glc,
+                                                  bool slc) __asm("llvm.amdgcn.buffer.store.v4f16");
 
 __device__ void
 __llvm_amdgcn_buffer_atomic_add(float vdata,
@@ -196,6 +236,40 @@ __device__ half amd_intrinsic_buffer_load<half, 1>(const half* p_src_block,
     return dst;
 }
 template <>
+__device__ ushort amd_intrinsic_buffer_load<ushort, 1>(const ushort* p_src_block,
+                                                       index_t src_thread_data_offset,
+                                                       index_t src_const_data_offset)
+{
+    ushort dst;
+
+    index_t src_thread_addr_offset = src_thread_data_offset * sizeof(ushort);
+    index_t src_const_addr_offset  = src_const_data_offset * sizeof(ushort);
+
+    BufferLoadStoreDwordConfig<ushort> src_block_config;
+
+    // fill in byte 0 - 1
+    src_block_config.address[0] = const_cast<ushort*>(p_src_block);
+    // fill in byte 2
+    src_block_config.range[2] = -1;
+    // fill in byte 3
+    src_block_config.range[3] = 0x00027000;
+
+#if CK_USE_AMD_BUFFER_ADDRESSING_INTRINSIC
+    dst = __llvm_amdgcn_buffer_load_bf16(
+        src_block_config.data, 0, src_thread_addr_offset + src_const_addr_offset, false, false);
+#else
+    asm volatile(
+        "\n \
+    buffer_load_ushort %0, %1, %2, %3 offen offset:0 \n \
+    s_waitcnt 0 \n \
+    "
+        : "=v"(dst)
+        : "v"(src_thread_addr_offset), "s"(src_block_config.data), "s"(src_const_addr_offset));
+#endif
+
+    return dst;
+}
+template <>
 __device__ float2_t amd_intrinsic_buffer_load<float, 2>(const float* p_src_block,
                                                         index_t src_thread_data_offset,
                                                         index_t src_const_data_offset)
@@ -251,6 +325,40 @@ __device__ half2_t amd_intrinsic_buffer_load<half, 2>(const half* p_src_block,
 
 #if CK_USE_AMD_BUFFER_ADDRESSING_INTRINSIC
     dst_out = __llvm_amdgcn_buffer_loadx2_f16(
+        src_block_config.data, 0, src_thread_addr_offset + src_const_addr_offset, false, false);
+#else
+    asm volatile(
+        "\n \
+    buffer_load_dword %0, %1, %2, %3 offen offset:0 \n \
+    s_waitcnt 0 \n \
+    "
+        : "=v"(dst_out)
+        : "v"(src_thread_addr_offset), "s"(src_block_config.data), "s"(src_const_addr_offset));
+#endif
+
+    return dst_out;
+}
+template <>
+__device__ ushort2_t amd_intrinsic_buffer_load<ushort, 2>(const ushort* p_src_block,
+                                                          index_t src_thread_data_offset,
+                                                          index_t src_const_data_offset)
+{
+    ushort2_t dst_out;
+
+    index_t src_thread_addr_offset = src_thread_data_offset * sizeof(ushort);
+    index_t src_const_addr_offset  = src_const_data_offset * sizeof(ushort);
+
+    BufferLoadStoreDwordConfig<ushort> src_block_config;
+
+    // fill in byte 0 - 1
+    src_block_config.address[0] = const_cast<ushort*>(p_src_block);
+    // fill in byte 2
+    src_block_config.range[2] = -1;
+    // fill in byte 3
+    src_block_config.range[3] = 0x00027000;
+
+#if CK_USE_AMD_BUFFER_ADDRESSING_INTRINSIC
+    dst_out = __llvm_amdgcn_buffer_loadx2_bf16(
         src_block_config.data, 0, src_thread_addr_offset + src_const_addr_offset, false, false);
 #else
     asm volatile(
@@ -335,6 +443,41 @@ __device__ half4_t amd_intrinsic_buffer_load<half, 4>(const half* p_src_block,
 }
 
 template <>
+__device__ ushort4_t amd_intrinsic_buffer_load<ushort, 4>(const ushort* p_src_block,
+                                                          index_t src_thread_data_offset,
+                                                          index_t src_const_data_offset)
+{
+    ushort4_t dst_out;
+
+    index_t src_thread_addr_offset = src_thread_data_offset * sizeof(ushort);
+    index_t src_const_addr_offset  = src_const_data_offset * sizeof(ushort);
+
+    BufferLoadStoreDwordConfig<ushort> src_block_config;
+
+    // fill in byte 0 - 1
+    src_block_config.address[0] = const_cast<ushort*>(p_src_block);
+    // fill in byte 2
+    src_block_config.range[2] = -1;
+    // fill in byte 3
+    src_block_config.range[3] = 0x00027000;
+
+#if CK_USE_AMD_BUFFER_ADDRESSING_INTRINSIC
+    dst_out = __llvm_amdgcn_buffer_loadx4_bf16(
+        src_block_config.data, 0, src_thread_addr_offset + src_const_addr_offset, false, false);
+#else
+    asm volatile(
+        "\n \
+    buffer_load_dwordx2 %0, %1, %2, %3 offen offset:0 \n \
+    s_waitcnt 0 \n \
+    "
+        : "=v"(dst_out)
+        : "v"(src_thread_addr_offset), "s"(src_block_config.data), "s"(src_const_addr_offset));
+#endif
+
+    return dst_out;
+}
+
+template <>
 __device__ void amd_intrinsic_buffer_store<float, 1>(const float* p_src,
                                                      float* p_dst_block,
                                                      index_t dst_thread_data_offset,
@@ -391,6 +534,43 @@ __device__ void amd_intrinsic_buffer_store<half, 1>(const half* p_src,
 
 #if CK_USE_AMD_BUFFER_ADDRESSING_INTRINSIC
     __llvm_amdgcn_buffer_store_f16(*p_src,
+                                   dst_block_config.data,
+                                   0,
+                                   dst_thread_addr_offset + dst_const_addr_offset,
+                                   false,
+                                   false);
+#else
+    asm volatile("\n \
+    buffer_store_short %1, %2, %0, %3 offen offset:0 \n \
+    "
+                 :
+                 : "s"(dst_block_config.data),
+                   "v"(*p_src),
+                   "v"(dst_thread_addr_offset),
+                   "s"(dst_const_addr_offset));
+#endif
+}
+
+template <>
+__device__ void amd_intrinsic_buffer_store<ushort, 1>(const ushort* p_src,
+                                                      ushort* p_dst_block,
+                                                      index_t dst_thread_data_offset,
+                                                      index_t dst_const_data_offset)
+{
+    index_t dst_thread_addr_offset = dst_thread_data_offset * sizeof(ushort);
+    index_t dst_const_addr_offset  = dst_const_data_offset * sizeof(ushort);
+
+    BufferLoadStoreDwordConfig<ushort> dst_block_config;
+
+    // fill in byte 0 - 1
+    dst_block_config.address[0] = p_dst_block;
+    // fill in byte 2
+    dst_block_config.range[2] = -1;
+    // fill in byte 3
+    dst_block_config.range[3] = 0x00027000;
+
+#if CK_USE_AMD_BUFFER_ADDRESSING_INTRINSIC
+    __llvm_amdgcn_buffer_store_f16(*reinterpret_cast<const half*>(p_src),
                                    dst_block_config.data,
                                    0,
                                    dst_thread_addr_offset + dst_const_addr_offset,
@@ -483,6 +663,42 @@ __device__ void amd_intrinsic_buffer_store<half, 2>(const half* p_src,
 }
 
 template <>
+__device__ void amd_intrinsic_buffer_store<ushort, 2>(const ushort* p_src,
+                                                      ushort* p_dst_block,
+                                                      index_t dst_thread_data_offset,
+                                                      index_t dst_const_data_offset)
+{
+    index_t dst_thread_addr_offset = dst_thread_data_offset * sizeof(ushort);
+    index_t dst_const_addr_offset  = dst_const_data_offset * sizeof(ushort);
+
+    BufferLoadStoreDwordConfig<ushort> dst_block_config;
+
+    // fill in byte 0 - 1
+    dst_block_config.address[0] = p_dst_block;
+    // fill in byte 2
+    dst_block_config.range[2] = -1;
+    // fill in byte 3
+    dst_block_config.range[3] = 0x00027000;
+
+#if CK_USE_AMD_BUFFER_ADDRESSING_INTRINSIC
+    __llvm_amdgcn_buffer_storex2_f16(*reinterpret_cast<const half2_t*>(p_src),
+                                     dst_block_config.data,
+                                     0,
+                                     dst_thread_addr_offset + dst_const_addr_offset,
+                                     false,
+                                     false);
+#else
+    asm volatile("\n \
+    buffer_store_dword %1, %2, %0, %3 offen offset:0 \n \
+    "
+                 :
+                 : "s"(dst_block_config.data),
+                   "v"(*reinterpret_cast<const ushort2_t*>(p_src)),
+                   "v"(dst_thread_addr_offset),
+                   "s"(dst_const_addr_offset));
+#endif
+}
+template <>
 __device__ void amd_intrinsic_buffer_store<float, 4>(const float* p_src,
                                                      float* p_dst_block,
                                                      index_t dst_thread_data_offset,
@@ -551,6 +767,43 @@ __device__ void amd_intrinsic_buffer_store<half, 4>(const half* p_src,
                  :
                  : "s"(dst_block_config.data),
                    "v"(*reinterpret_cast<const half4_t*>(p_src)),
+                   "v"(dst_thread_addr_offset),
+                   "s"(dst_const_addr_offset));
+#endif
+}
+
+template <>
+__device__ void amd_intrinsic_buffer_store<ushort, 4>(const ushort* p_src,
+                                                      ushort* p_dst_block,
+                                                      index_t dst_thread_data_offset,
+                                                      index_t dst_const_data_offset)
+{
+    index_t dst_thread_addr_offset = dst_thread_data_offset * sizeof(ushort);
+    index_t dst_const_addr_offset  = dst_const_data_offset * sizeof(ushort);
+
+    BufferLoadStoreDwordConfig<ushort> dst_block_config;
+
+    // fill in byte 0 - 1
+    dst_block_config.address[0] = p_dst_block;
+    // fill in byte 2
+    dst_block_config.range[2] = -1;
+    // fill in byte 3
+    dst_block_config.range[3] = 0x00027000;
+
+#if CK_USE_AMD_BUFFER_ADDRESSING_INTRINSIC
+    __llvm_amdgcn_buffer_storex4_f16(*reinterpret_cast<const half4_t*>(p_src),
+                                     dst_block_config.data,
+                                     0,
+                                     dst_thread_addr_offset + dst_const_addr_offset,
+                                     false,
+                                     false);
+#else
+    asm volatile("\n \
+    buffer_store_dwordx2 %1, %2, %0, %3 offen offset:0 \n \
+    "
+                 :
+                 : "s"(dst_block_config.data),
+                   "v"(*reinterpret_cast<const ushort4_t*>(p_src)),
                    "v"(dst_thread_addr_offset),
                    "s"(dst_const_addr_offset));
 #endif
