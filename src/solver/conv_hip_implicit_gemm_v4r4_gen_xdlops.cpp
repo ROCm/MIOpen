f17f5513d8eec341b2ba56415f96ccac2bdf8f57
Tejash Shah <tejashshah@gmail.com>
2020-04-17 13:32:04 -0500

    improve fp16/bfp16 fwd performance by changing epack extraction logic to have vectorized read for odd size filters for matrix a i.e. weight tensor

--- src/solver/conv_hip_implicit_gemm_v4r4_gen_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_v4r4_gen_xdlops.cpp
@@ -153,16 +153,10 @@ static inline ConvSolution GetSolutionBase(const ConvolutionContext& ctx,
     {
         if(ctx.group_counts > 1)
         {
-            // For fp16 group cases, E = C/EPack * Y * X
-            // Since C/EPack are not in contiguous memory along with Y*X, vector length
-            // needs to be in multiple of Y*X
-            MIOPEN_LOG_I("mul " << (C / config.EPACKSize) * Y * X << " ABlockCopySubLengths_GemmK "
-                                << ABlockCopySubLengths_GemmK);
+            // For bfp16/fp16 group fwd cases, E = C/EPack * Y * X, where C/EPack are in 
+	    // continuous memory with Y*X because EPack is extracted from 
             ABlockCopySrcDataPerRead_GemmK =
-                (((C / config.EPACKSize) * Y * X) % ABlockCopySubLengths_GemmK) != 0
-                    ? 1
-                    : GetReadWriteVectorSize(ABlockCopySubLengths_GemmK);
-            MIOPEN_LOG_I("gemmk " << ABlockCopySrcDataPerRead_GemmK);
+                (((C / config.EPACKSize) * Y * X) % ABlockCopySubLengths_GemmK) != 0 ? 1 : GetReadWriteVectorSize(ABlockCopySubLengths_GemmK);
         }
         else
         {
