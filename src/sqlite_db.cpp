06c21a3099cc2eead556735c93c7b51c71f5e146
JD <Jehandad.Khan@amd.com>
2020-05-05 08:14:43 -0500

    Moving SQLite calls to cpp files (#195)
    * added impl classes
    * finished move semantics
    * move methods out of impl, rename result type, fix test compile errors
    
    [ROCm/MIOpen commit: 087a030afc09343fde2a8991d8ab4093324cadb7]

--- src/sqlite_db.cpp
+++ src/sqlite_db.cpp
@@ -50,6 +50,207 @@
 
 namespace miopen {
 
+class SQLite::impl
+{
+    struct SQLiteCloser
+    {
+        void operator()(sqlite3* ptr)
+        {
+            std::string filename_(sqlite3_db_filename(ptr, "main"));
+            SQLite::Retry([&]() { return sqlite3_close(ptr); }, filename_);
+        }
+    };
+
+    public:
+    impl(const std::string& filename_, bool is_system)
+    {
+        sqlite3* ptr_tmp;
+        int rc = 0;
+        if(is_system)
+            rc = sqlite3_open_v2(filename_.c_str(), &ptr_tmp, SQLITE_OPEN_READONLY, nullptr);
+        else
+            rc = sqlite3_open_v2(
+                filename_.c_str(), &ptr_tmp, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nullptr);
+        ptrDb   = sqlite3_ptr{ptr_tmp};
+        isValid = (rc == 0);
+    }
+
+    using sqlite3_ptr = std::unique_ptr<sqlite3, SQLiteCloser>;
+    sqlite3_ptr ptrDb = nullptr;
+    bool isValid;
+};
+
+static int find_callback(void* _res, int argc, char** argv, char** azColName)
+{
+    SQLite::result_type* res = static_cast<SQLite::result_type*>(_res);
+    std::unordered_map<std::string, std::string> record;
+    for(auto i               = 0; i < argc; i++)
+        record[azColName[i]] = (argv[i] != nullptr) ? argv[i] : "NULL";
+    if(res != nullptr)
+        res->push_back(record);
+    return 0;
+}
+
+SQLite::SQLite() : pImpl(nullptr) {}
+SQLite::~SQLite()                 = default;
+SQLite::SQLite(SQLite&&) noexcept = default;
+SQLite& SQLite::operator=(SQLite&&) noexcept = default;
+SQLite::result_type SQLite::Exec(const std::string& query) const
+{
+    SQLite::result_type res;
+    MIOPEN_LOG_T(std::this_thread::get_id() << ":" << query);
+    {
+        auto rc = Retry([&]() {
+            return sqlite3_exec(pImpl->ptrDb.get(),
+                                query.c_str(),
+                                find_callback,
+                                static_cast<void*>(&res),
+                                nullptr);
+        });
+        if(rc != SQLITE_OK)
+        {
+            MIOPEN_LOG_I2(query);
+            MIOPEN_THROW(miopenStatusInternalError, ErrorMessage());
+        }
+    }
+    return res;
+}
+
+int SQLite::Retry(std::function<int()> f, std::string filename)
+{
+    auto timeout_end = std::chrono::high_resolution_clock::now() +
+                       std::chrono::seconds(30); // TODO: make configurable
+    auto tries = 0;
+    while(true)
+    {
+        int rc = f();
+        if(rc == SQLITE_BUSY)
+        {
+            MIOPEN_LOG_I2("Database" + filename + "  busy, retrying ...");
+            ++tries;
+            if(tries > 50)
+                std::this_thread::sleep_for(std::chrono::microseconds(100));
+            else
+                std::this_thread::yield();
+        }
+        else
+            return rc;
+        if(std::chrono::high_resolution_clock::now() > timeout_end)
+            MIOPEN_THROW("Timeout while waiting for Database: " + filename);
+    }
+}
+
+int SQLite::Retry(std::function<int()> f) const
+{
+    std::string filename(sqlite3_db_filename(pImpl->ptrDb.get(), "main"));
+    return SQLite::Retry(f, filename);
+}
+
+int SQLite::Changes() const { return sqlite3_changes(pImpl->ptrDb.get()); }
+
+std::string SQLite::ErrorMessage() const
+{
+    std::string errMsg = "Internal error while accessing SQLite database: ";
+    return errMsg + sqlite3_errmsg(pImpl->ptrDb.get());
+}
+bool SQLite::Valid() const { return pImpl->isValid; }
+
+class SQLite::Statement::impl
+{
+    using sqlite3_stmt_ptr = MIOPEN_MANAGE_PTR(sqlite3_stmt*, sqlite3_finalize);
+    sqlite3_stmt_ptr Prepare(const SQLite& sql, const std::string& query)
+    {
+        sqlite3_stmt* ptr = nullptr;
+        MIOPEN_LOG_I2(query);
+        auto rc =
+            sqlite3_prepare_v2(sql.pImpl->ptrDb.get(), query.c_str(), query.size(), &ptr, nullptr);
+        if(rc != SQLITE_OK)
+        {
+            std::string err_msg = "SQLite prepare error: ";
+            MIOPEN_THROW(miopenStatusInternalError, err_msg + sql.ErrorMessage());
+        }
+        return sqlite3_stmt_ptr{ptr};
+    }
+
+    public:
+    impl(const SQLite& sql, const std::string& query) { ptrStmt = Prepare(sql, query); }
+    impl(const SQLite& sql, const std::string& query, const std::vector<std::string>& vals)
+    {
+        ptrStmt = Prepare(sql, query);
+        int cnt = 1;
+        for(auto& kinder : vals)
+        {
+            auto rc = sqlite3_bind_text(
+                ptrStmt.get(), cnt++, kinder.data(), kinder.size(), SQLITE_TRANSIENT); // NOLINT
+            if(rc != SQLITE_OK)
+                MIOPEN_THROW(miopenStatusInternalError, sql.ErrorMessage());
+        }
+        MIOPEN_LOG_I2("[" << JoinStrings(vals, ",") << "]");
+    }
+
+    sqlite3_stmt_ptr ptrStmt = nullptr;
+};
+
+SQLite::SQLite(const std::string& filename_, bool is_system)
+    : pImpl{std::make_unique<impl>(filename_, is_system)}
+{
+}
+
+SQLite::Statement::Statement(const SQLite& sql, const std::string& query)
+    : pImpl{std::make_unique<impl>(sql, query)}
+{
+}
+SQLite::Statement::Statement(const SQLite& sql,
+                             const std::string& query,
+                             const std::vector<std::string>& vals)
+    : pImpl{std::make_unique<impl>(sql, query, vals)}
+{
+}
+SQLite::Statement::~Statement() = default;
+SQLite::Statement::Statement() : pImpl{nullptr} {}
+SQLite::Statement::Statement(Statement&&) noexcept = default;
+SQLite::Statement& SQLite::Statement::operator=(Statement&&) noexcept = default;
+int SQLite::Statement::Step(const SQLite& sql)
+{
+    return sql.Retry([&]() { return sqlite3_step(pImpl->ptrStmt.get()); });
+}
+std::string SQLite::Statement::ColumnText(int idx)
+{
+    size_t bytes = sqlite3_column_bytes(pImpl->ptrStmt.get(), idx);
+    return std::string{
+        reinterpret_cast<const char*>(sqlite3_column_text(pImpl->ptrStmt.get(), idx)), bytes};
+}
+
+std::string SQLite::Statement::ColumnBlob(int idx)
+{
+    auto ptr = sqlite3_column_blob(pImpl->ptrStmt.get(), idx);
+    auto sz  = sqlite3_column_bytes(pImpl->ptrStmt.get(), idx);
+    return std::string{reinterpret_cast<const char*>(ptr), static_cast<size_t>(sz)};
+}
+int64_t SQLite::Statement::ColumnInt64(int idx)
+{
+    return sqlite3_column_int64(pImpl->ptrStmt.get(), idx);
+}
+
+int SQLite::Statement::BindText(int idx, const std::string& txt)
+{
+    sqlite3_bind_text(
+        pImpl->ptrStmt.get(), idx, txt.data(), txt.size(), SQLITE_TRANSIENT); // NOLINT
+    return 0;
+}
+int SQLite::Statement::BindBlob(int idx, const std::string& blob)
+{
+    sqlite3_bind_blob(
+        pImpl->ptrStmt.get(), idx, blob.data(), blob.size(), SQLITE_TRANSIENT); // NOLINT
+    return 0;
+}
+
+int SQLite::Statement::BindInt64(int idx, const int64_t num)
+{
+    sqlite3_bind_int64(pImpl->ptrStmt.get(), idx, num);
+    return 0;
+}
+
 SQLitePerfDb::SQLitePerfDb(const std::string& filename_,
                            bool is_system,
                            const std::string& arch_,
@@ -70,7 +271,7 @@ SQLitePerfDb::SQLitePerfDb(const std::string& filename_,
     prob_desc.weights_data_type = miopenFloat;
     if(!is_system)
     {
-        SQLRes_t res;
+        SQLite::result_type res;
         const std::string create_config = prob_desc.CreateQuery();
         // clang-format off
         const std::string create_perfdb_sql =
@@ -97,14 +298,13 @@ SQLitePerfDb::SQLitePerfDb(const std::string& filename_,
                   "type = 'table' AND "
                   "(name = 'config' OR name = 'perf_db');";
             // clang-format on
-            SQLExec(check_tables, res);
+            res = sql.Exec(check_tables);
         }
         if(res.empty())
         {
             const auto lock = exclusive_lock(lock_file, GetLockTimeout());
             MIOPEN_VALIDATE_LOCK(lock);
-            if(!SQLExec(create_config + create_perfdb_sql))
-                MIOPEN_THROW(miopenStatusInternalError);
+            sql.Exec(create_config + create_perfdb_sql);
             MIOPEN_LOG_I2("Database created successfully");
         }
     }
