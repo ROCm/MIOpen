a85078b8834f0432827a4af3c1da94a749a3602c
zjing14 <jizhan@amd.com>
2020-05-07 21:50:28 -0500

    Refactored igemm kernel and solver for xdlops wrw fp32 (#152)
    * add new v4r4_gen_wrw solver and kernel
    
    * format
    
    * add heuristic in search
    
    * clean code
    
    * excluded from the original solver
    
    * Change the max workgroup size to 256.
    
    * Fixed IsApplicable
    
    * fixed format
    
    * fixed error msg
    
    * fixed dirt marco
    
    * Update solver.hpp
    
    [ROCm/MIOpen commit: b25e2e8120cecb5c3ce24303963a30362b85708d]

--- src/mlo_dir_conv.cpp
+++ src/mlo_dir_conv.cpp
@@ -153,7 +153,8 @@ static auto GetWindogradSolvers()
 
 static auto GetImplicitGemmWrWSolvers()
 {
-    return miopen::solver::SolverContainer<miopen::solver::ConvHipImplicitGemmV4R4WrWXdlops,
+    return miopen::solver::SolverContainer<miopen::solver::ConvHipImplicitGemmV4R4GenXdlopsWrWFp32,
+                                           miopen::solver::ConvHipImplicitGemmV4R4WrWXdlops,
                                            miopen::solver::ConvHipImplicitGemmV4R4GenWrWXdlops,
                                            miopen::solver::ConvHipImplicitGemmV4WrW,
                                            miopen::solver::ConvHipImplicitGemmV4R1WrW>{};
