e60354c2192ff8722579cece8042f92b450ca20e
Jehandad Khan <jahandad@gmail.com>
2020-05-13 15:36:29 -0500

    replace underscore in package name with a hyphen

--- utils/install_precompiled_kernels.sh
+++ utils/install_precompiled_kernels.sh
@@ -12,13 +12,15 @@ else
     echo "rocminfo is not installed, please install the rocminfo package"
     return -1
 fi
-arches=$($ROCMINFO | grep -e ' gfx' -e 'Compute Unit:' | awk '/Name/{ arch= $2} /Compute Unit:/ {if(arch != "") { all_arches[(arch "_" $3)] }} END { for (a in all_arches) { print a}  }')
+arches=$($ROCMINFO | grep -e ' gfx' -e 'Compute Unit:' | awk '/Name/{ arch= $2} /Compute Unit:/ {if(arch != "") { all_arches[(arch "-" $3)] }} END { for (a in all_arches) { print a}  }')
 
 while IFS= read -r line ; 
 do 
     if [ -f /etc/redhat-release ]; then
+          echo sudo yum -y install "miopenkernels-$line"
           sudo yum -y install "miopenkernels-$line"
     elif [ -f /etc/lsb-release ]; then
+          echo sudo apt install -y "miopenkernels-$line"
           sudo apt install -y "miopenkernels-$line"
     else
         echo "Unknown distribution"
