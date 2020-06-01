272a87b7ff5bf5ecc73623046f71f9760a78f101
Jehandad Khan <jahandad@gmail.com>
2020-06-01 12:52:27 -0500

    make script installable, fix sudo

--- utils/install_precompiled_kernels.sh
+++ utils/install_precompiled_kernels.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ROCMINFO=$(which rocminfo)
+SUDO=$(which sudo)
 if [ -z "$ROCMINFO" ];
 then
     ROCMINFO=/opt/rocm/bin/rocminfo;
@@ -18,11 +19,11 @@ while IFS= read -r line ;
 do 
     if [ -f /etc/redhat-release ]; then
           echo sudo yum -y install "miopenkernels-$line"
-          sudo yum -y install "miopenkernels-$line"
+          $SUDO yum -y install --nogpgcheck "miopenkernels-$line"
     elif [ -f /etc/lsb-release ]; then
           echo sudo apt install -y "miopenkernels-$line"
-          sudo apt update
-          sudo apt install -y "miopenkernels-$line"
+          $SUDO apt update
+          $SUDO apt install -y "miopenkernels-$line"
     else
         echo "Unknown distribution"
         echo "Please install the miopenkernels-$line package using an appropriate package manager"
