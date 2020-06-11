6a12f0fd30b2b1417ec9361944c019ece7f9bbd9
Vasilii Filippov <vasja.filippov94@outlook.com>
2020-06-11 22:38:21 +0300

    Removed redundant SetTensor

--- src/solver/conv_hip_implicit_gemm_v4r4_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_v4r4_xdlops.cpp
@@ -221,24 +221,7 @@ static inline ConvSolution GetSolutionBase(const ConvolutionContext& ctx,
             return [=](const Handle& handle, const boost::any& primitve_params) {
                 const auto invoke_params = boost::any_cast<conv::WrWInvokeParams>(primitve_params);
                 const auto& tensors      = invoke_params.tensors;
-                float zero               = 0.f;
-                auto elapsed             = 0.f;
-
-                if(tensors.dwDesc.GetType() != miopenHalf &&
-                   tensors.dwDesc.GetType() != miopenBFloat16)
-                {
-                    SetTensor(handle, tensors.dwDesc, tensors.dw, &zero);
-                    if(handle.IsProfilingEnabled())
-                        elapsed += handle.GetKernelTime();
-                }
-
                 handle.Run(kernels[0])(tensors.x, tensors.dy, tensors.dw);
-                if(handle.IsProfilingEnabled())
-                {
-                    elapsed += handle.GetKernelTime();
-                    handle.ResetKernelTime();
-                    handle.AccumKernelTime(elapsed);
-                }
             };
         };
     }
