056785efd6323a5016018e0b3af63b5d763c21f0
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2020-06-15 11:19:05 +0000

    clang-format ... /composable_kernel/include/utility/in_memory_operation.hpp

--- src/kernels/composable_kernel/include/utility/in_memory_operation.hpp
+++ src/kernels/composable_kernel/include/utility/in_memory_operation.hpp
@@ -52,34 +52,31 @@ __device__ void set_data(const T* p_src, index_t src_offset, T* p_dst, index_t d
         //     p_dst[dst_offset+i] = p_src[src_offset+i];
         *reinterpret_cast<vector_t*>(&p_dst[dst_offset]) =
             *reinterpret_cast<const vector_t*>(&p_src[src_offset]);
-    })
-        .Else([&](auto) {
-            // TODO: use static_if::ElseIf, instead of nested static_if
-            static_if<SrcAddressSpace == AddressSpace::Global &&
-                      DstAddressSpace == AddressSpace::Vgpr>{}([&](auto) {
-                // buffer_load requires:
-                //   1) p_src must be in global memory space, d_dst must be vgpr
-                //   2) p_src to be a block-invariant pointer.
+    }).Else([&](auto) {
+        // TODO: use static_if::ElseIf, instead of nested static_if
+        static_if<SrcAddressSpace == AddressSpace::Global &&
+                  DstAddressSpace == AddressSpace::Vgpr>{}([&](auto) {
+            // buffer_load requires:
+            //   1) p_src must be in global memory space, d_dst must be vgpr
+            //   2) p_src to be a block-invariant pointer.
+            // It is user's responsibility to make sure that is true.
+            *reinterpret_cast<vector_t*>(&p_dst[dst_offset]) =
+                amd_intrinsic_buffer_load<T, DataPerAccess>(p_src, src_offset, 0);
+        }).Else([&](auto) {
+            static_if<SrcAddressSpace == AddressSpace::Vgpr &&
+                      DstAddressSpace == AddressSpace::Global>{}([&](auto) {
+                // buffer_store requires:
+                //   1) p_src must be in vgpr space, d_dst must be global memory
+                //   2) p_dst to be a block-invariant pointer.
                 // It is user's responsibility to make sure that is true.
+                amd_intrinsic_buffer_store<T, DataPerAccess>(
+                    &(p_src[src_offset]), p_dst, dst_offset, 0);
+            }).Else([&](auto) {
                 *reinterpret_cast<vector_t*>(&p_dst[dst_offset]) =
-                    amd_intrinsic_buffer_load<T, DataPerAccess>(p_src, src_offset, 0);
-            })
-                .Else([&](auto) {
-                    static_if<SrcAddressSpace == AddressSpace::Vgpr &&
-                              DstAddressSpace == AddressSpace::Global>{}([&](auto) {
-                        // buffer_store requires:
-                        //   1) p_src must be in vgpr space, d_dst must be global memory
-                        //   2) p_dst to be a block-invariant pointer.
-                        // It is user's responsibility to make sure that is true.
-                        amd_intrinsic_buffer_store<T, DataPerAccess>(
-                            &(p_src[src_offset]), p_dst, dst_offset, 0);
-                    })
-                        .Else([&](auto) {
-                            *reinterpret_cast<vector_t*>(&p_dst[dst_offset]) =
-                                *reinterpret_cast<const vector_t*>(&p_src[src_offset]);
-                        });
-                });
+                    *reinterpret_cast<const vector_t*>(&p_src[src_offset]);
+            });
         });
+    });
 #else
     *reinterpret_cast<vector_t*>(&p_dst[dst_offset]) =
         *reinterpret_cast<const vector_t*>(&p_src[src_offset]);
@@ -92,21 +89,20 @@ template <typename T,
           AddressSpace DstAddressSpace>
 __device__ void atomic_add_data(const T* p_src, index_t src_offset, T* p_dst, index_t dst_offset)
 {
-    static_if<SrcAddressSpace == AddressSpace::Vgpr && DstAddressSpace == AddressSpace::Global>{}(
-        [&](auto) {
+    static_if<SrcAddressSpace == AddressSpace::Vgpr &&
+              DstAddressSpace == AddressSpace::Global>{}([&](auto) {
 #if CK_USE_AMD_BUFFER_ATOMIC_ADD
-            amd_intrinsic_buffer_atomic_add<T, DataPerAccess>(
-                &(p_src[src_offset]), p_dst, dst_offset, 0);
+        amd_intrinsic_buffer_atomic_add<T, DataPerAccess>(
+            &(p_src[src_offset]), p_dst, dst_offset, 0);
 #else
-            using vector_t = typename vector_type<T, DataPerAccess>::MemoryType;
+        using vector_t = typename vector_type<T, DataPerAccess>::MemoryType;
 
-            atomic_add_impl(reinterpret_cast<vector_t*>(&p_dst[dst_offset]),
-                            *reinterpret_cast<const vector_t*>(&p_src[src_offset]));
+        atomic_add_impl(reinterpret_cast<vector_t*>(&p_dst[dst_offset]),
+                        *reinterpret_cast<const vector_t*>(&p_src[src_offset]));
 #endif
-        })
-        .Else([&](auto fwd) {
-            static_assert(fwd(false), "atomic_add doesn't support this memory space");
-        });
+    }).Else([&](auto fwd) {
+        static_assert(fwd(false), "atomic_add doesn't support this memory space");
+    });
 }
 
 template <typename T,
