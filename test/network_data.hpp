97cf2d8f238ebcbe4e505d50406604c261cb88ab
Murali N <33875998+muralinr@users.noreply.github.com>
2020-06-23 09:55:23 -0700

    Batchnorm backward changes for small filter sizes (#249)
    * Batchnorm backward for small filter sizes
    * Added new unit test case for n>768 and in_cstride>63 config
    * Modifying test vector
    
    [ROCm/MIOpen commit: ab90b0e415d58c18b578ad6bad97f36151392a47]

--- test/network_data.hpp
+++ test/network_data.hpp
@@ -436,7 +436,9 @@ get_bn_spatial_inputs(int n = MIOPEN_TEST_DEFAULT_BATCH_SIZE_FACTOR)
         { pick_batch_size(32, n),  256,  28,  28  },
         { pick_batch_size(32, n),  3,    224, 224 },
         { pick_batch_size(32, n),  480,  128, 256 },
-        { pick_batch_size(32, n),  528,  64,  128 }
+        { pick_batch_size(32, n),  528,  64,  128 },
+        { pick_batch_size(770, n),  1,  8,  8 },
+        { pick_batch_size(770, n),  1024,  1,  1 }
     };
     // clang-format on
 }
