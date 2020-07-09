ad5ac00a1a3ac8381195f50731a4941d2a12f394
XiuYuLi <295553381@qq.com>
2020-07-09 20:15:04 +0800

    Update cellfft.hpp

--- src/include/miopen/conv/invokers/cellfft.hpp
+++ src/include/miopen/conv/invokers/cellfft.hpp
@@ -30,13 +30,11 @@
 #include <miopen/invoker.hpp>
 #include "../../../../cellfft/include/cellfft_param.hpp"
 
-namespace miopen
-{
-namespace conv
-{
+namespace miopen{
+namespace conv{
 InvokerFactory MakeCellfftInvokerFactory( const cellfft::cellfft_param_t&, float );
 } // namespace conv
 } // namespace miopen
+#endif
 // clang-format on
 
-#endif
