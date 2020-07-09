04414463cbd754b24dc6aea7e74e061e7d7383ed
Tejash Shah <tejashshah@gmail.com>
2020-07-09 12:04:26 -0500

    Reenable all solvers

--- src/mlo_dir_conv.cpp
+++ src/mlo_dir_conv.cpp
@@ -136,13 +136,13 @@ static auto GetImplicitGemmSolvers()
                                            miopen::solver::ConvHipImplicitGemmV4R4GenFwdXdlops,
                                            miopen::solver::ConvHipImplicitGemmV4R4FwdXdlops,
                                            miopen::solver::ConvHipImplicitGemmBwdDataV1R1Xdlops,
-                                           // miopen::solver::ConvHipImplicitGemmBwdDataV4R1Xdlops,
+                                           miopen::solver::ConvHipImplicitGemmBwdDataV4R1Xdlops,
                                            miopen::solver::ConvHipImplicitGemmV4_1x1,
                                            miopen::solver::ConvHipImplicitGemmV4Fwd,
                                            miopen::solver::ConvHipImplicitGemmV4R1Fwd,
                                            miopen::solver::ConvHipImplicitGemmV4R4Fwd,
-                                           // miopen::solver::ConvHipImplicitGemmBwdDataV1R1,
-                                           // miopen::solver::ConvHipImplicitGemmBwdDataV4R1,
+                                           miopen::solver::ConvHipImplicitGemmBwdDataV1R1,
+                                           miopen::solver::ConvHipImplicitGemmBwdDataV4R1,
                                            miopen::solver::ConvAsmImplicitGemmV4R1DynamicFwd_1x1,
                                            miopen::solver::ConvAsmImplicitGemmV4R1DynamicFwd>{};
 }
