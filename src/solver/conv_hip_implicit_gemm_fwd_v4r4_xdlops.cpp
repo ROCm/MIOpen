16b25f14f88d74bd6fd325b913e5c467bfb7d61c
Jing Zhang <jizhan@amd.com>
2020-07-11 02:34:06 +0000

    [skip ci] remove debug info

--- src/solver/conv_hip_implicit_gemm_fwd_v4r4_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_fwd_v4r4_xdlops.cpp
@@ -907,8 +907,6 @@ int ConvHipImplicitGemmForwardV4R4Xdlops::RunAndMeasureSolution(const miopen::Ha
 
 bool ConvHipImplicitGemmForwardV4R4Xdlops::IsApplicable(const ConvolutionContext& ctx) const
 {
-    MIOPEN_LOG_I("skip_solutions = "
-                 << ctx.skip_solutions_that_take_long_time_to_build_and_have_narrow_coverage);
     if(ctx.skip_solutions_that_take_long_time_to_build_and_have_narrow_coverage)
         return false;
 
