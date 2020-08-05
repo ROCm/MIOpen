e285bba4af187abc2d8115703f011fa684e30e1d
Shaojie WANG <wsjmessi@163.com>
2020-08-06 00:34:16 +0800

    dynamic igemm wrw (#317)
    * split v4r1 wrw kernel
    * runnable v4r1 wrw code
    * atomic add version of v4r1 igemm wrw fp32
    * draft commit to dynamic igemm wrw
    * correct wrw v4r1 invoker
    * use other shell path for 18 server
    * support odd size image
    * add solver code for dynamic wrw; not runnable yet
    * adding dynamic wrw igemm path 07-16
    * write result code for igemm dynamic wrw with reduction
    * remove v4r1 atomic add solver cpp in CMakeLists.txt
    * update gemmk groups computation
    * add small(32x32x4) block size gemm for igemm dynamic v4r1 wrw
    * remove tmp_gemmk_groups' initiation in GetImplicitGemmWrwV4R1DynamicGemmkGroups
    * remove tmp_gemmk_groups' init in invoker
    * remove usage of log2f
    * add solver code to support 32x32x4 block gemm size
    * init for workspace in igemm dynamic wrw
    * remove non-used var init
    * use COMPOSABLE_KERNEL_DYNAMIC_ASM_SOURCE and COMPOSABLE_KERNEL_DYNAMIC_CPP_SOURCE in src/CMakeList.txt
    Co-authored-by: shaojiewang <shaowang@DC-SMC-24.amd.com>
    Co-authored-by: shaojie wang <shaowang@dc-smc-18.amd.com>
    
    [ROCm/MIOpen commit: f094f46c3a88638a94277243eb2991007f207fa8]

--- src/mlo_dir_conv.cpp
+++ src/mlo_dir_conv.cpp
@@ -157,7 +157,8 @@ static auto GetImplicitGemmWrWSolvers()
     return miopen::solver::SolverContainer<miopen::solver::ConvHipImplicitGemmV4R4GenXdlopsWrWFp32,
                                            miopen::solver::ConvHipImplicitGemmV4R4GenWrWXdlops,
                                            miopen::solver::ConvHipImplicitGemmV4R1WrW,
-                                           miopen::solver::ConvHipImplicitGemmV4R4WrW>{};
+                                           miopen::solver::ConvHipImplicitGemmV4R4WrW,
+                                           miopen::solver::ConvAsmImplicitGemmV4R1DynamicWrw>{};
 }
 
 static auto GetWindogradWrWSolvers()
