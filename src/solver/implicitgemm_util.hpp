62ac77f7e7bdc70a03ac3aa2a4e057d804385afc
Jing Zhang <jizhan@amd.com>
2020-08-17 18:04:36 +0000

    workaround SWDEV_247082

--- src/solver/implicitgemm_util.hpp
+++ src/solver/implicitgemm_util.hpp
@@ -23,6 +23,7 @@ MIOPEN_DECLARE_ENV_VAR(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_BLOCK_SYNC_LDS_WITHOUT_SY
 // LLVM xdlops instrinsic will do unnecessey VGRP <--> AGPR movement, and result in
 // register spill, for bfloat16 datatype, when doing wave-wise GEMM larger than 64x64
 #define WORKAROUND_SWDEV_240356 1
+#define WORKAROUND_SWDEV_247082 1
 
 namespace miopen {
 
@@ -514,6 +515,11 @@ static inline bool IsValidBlockwiseGemmXdlops(const ConvolutionContext& ctx,
                                               const int GemmNPerWave,
                                               const int GemmKPack)
 {
+#if WORKAROUND_SWDEV_247082
+    if(GemmKPerBlock == 8)
+        return false;
+#endif
+
     // check k
     if(ctx.IsFp16() && GemmKPack % 4 != 0)
         return false;
