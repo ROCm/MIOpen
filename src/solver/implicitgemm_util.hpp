6e13d1d75ee15e1cfbea926a649cc3be2af8ca7e
Chao Liu <lc.roy86@gmail.com>
2020-08-17 15:18:25 -0500

    disable faulty -amdgpu-enable-global-sgpr-addr compiler option; remove workaround for SWDEV-239555

--- src/solver/implicitgemm_util.hpp
+++ src/solver/implicitgemm_util.hpp
@@ -18,8 +18,6 @@ MIOPEN_DECLARE_ENV_VAR(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_BLOCK_SYNC_LDS_WITHOUT_SY
 #define WORKAROUND_SWDEV_229564 1
 // workaround for buffer load/store fp16/bfp16 intrinsic bug
 #define WORKAROUND_SWDEV_231101 1
-// workaround compiler bug: GPU memory access fault when there is padding in fp16/bfp16 case
-#define WORKAROUND_SWDEV_239555 1
 // LLVM xdlops instrinsic will do unnecessey VGRP <--> AGPR movement, and result in
 // register spill, for bfloat16 datatype, when doing wave-wise GEMM larger than 64x64
 #define WORKAROUND_SWDEV_240356 1
