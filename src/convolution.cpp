426bf4bf7f8908f151dd25e4d18ee355f029d5cf
XiuYuLi <295553381@qq.com>
2020-08-27 14:26:02 +0800

    clang-format

--- src/convolution.cpp
+++ src/convolution.cpp
@@ -582,10 +582,8 @@ ConvolutionDescriptor::BackwardDataGetWorkSpaceSize(Handle& handle,
     size_t workspace_size_gemm = 0;
 
 #if MIOPEN_USE_GEMM
-    size_t tmp_max_workspace = std::max({direct_workspace,
-                                         implicit_gemm_workspace,
-                                         workspace_size_winograd,
-                                         cellfft_workspace});
+    size_t tmp_max_workspace = std::max(
+        {direct_workspace, implicit_gemm_workspace, workspace_size_winograd, cellfft_workspace});
     if(!miopen::IsDisabled(MIOPEN_DEBUG_CONV_GEMM{}))
     {
         workspace_size_gemm = BackwardDataGetWorkSpaceSizeGEMM(wDesc, dyDesc);
