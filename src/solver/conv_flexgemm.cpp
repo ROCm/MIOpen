b1cff37a923e57b87fcab1a612a9f1a8aa8b3eae
XiuYuLi <295553381@qq.com>
2020-09-26 14:27:32 +0800

    Changed file struct and var names form result of reviewing

--- src/solver/conv_flexgemm.cpp
+++ src/solver/conv_flexgemm.cpp
@@ -23,19 +23,341 @@
  * SOFTWARE.
  *
  *******************************************************************************/
-#include <sstream>
-#include <miopen/config.h>
-#include <miopen/kernel_info.hpp>
 #include <miopen/gcn_asm_utils.hpp>
 #include <miopen/solver.hpp>
 #include <miopen/env.hpp>
 #include <miopen/kernel_build_params.hpp>
-#include <miopen/conv/tensors.hpp>
+#include <miopen/conv/data_invoke_params.hpp>
+#include <miopen/tensor.hpp>
+#include <miopen/idiv.hpp>
 #include <boost/any.hpp>
-#include <miopen/conv/invokers/flexgemm.hpp>
 
 namespace miopen {
+struct param_ufconv_t
+{
+    magic_t amag;
+    magic_t cmag;
+    uint32_t id;
+    uint32_t ng;
+    uint32_t m;
+    uint32_t n;
+    uint32_t k;
+    uint32_t dimx;
+    uint32_t ntidx;
+    uint32_t dir;
+};
+struct param_conv_t
+{
+    size_t s_pad;
+    size_t s_idx;
+    size_t s_perm;
+    uint32_t id;
+    uint32_t ng;
+    uint32_t bs;
+    uint32_t sd;
+    uint32_t pad;
+    uint32_t pnx;
+    uint32_t pny;
+    uint32_t anx;
+    uint32_t any;
+    uint32_t bnx;
+    uint32_t bny;
+    uint32_t cnx;
+    uint32_t cny;
+    uint32_t lda;
+    uint32_t ldc;
+    uint32_t inc;
+    uint32_t ags;
+    uint32_t m;
+    uint32_t n;
+    uint32_t k;
+    uint32_t ntidx;
+    uint32_t dir;
+};
+
+static inline uint32_t choose_routine_ufconv(uint32_t m, uint32_t n, uint32_t k, int dir)
+{
+    uint32_t r    = (n + 31) >> 5;
+    uint32_t s    = (n + 15) >> 4;
+    uint32_t mode = ((m & 1) ^ 1) + ((m & 3) == 0 ? 1 : 0);
+    uint32_t id   = 1 + ((r & 3) == 0 ? ((k & 15) == 0 ? 2 : 1) : ((r & 1) ^ 1));
+    if((s & 1) != 0 && n <= 112)
+        id = 0;
+    if((dir != 0) && (id != 0))
+    {
+        id = (n & 3) != 0 ? ((n & 1) != 0 ? 1 : 2) : id;
+    }
+    return ((mode << 16) | id);
+}
+static inline uint32_t choose_routine_fconv(uint32_t n, uint32_t k)
+{
+    uint32_t r  = (n + 31) >> 5;
+    uint32_t s  = (n + 15) >> 4;
+    uint32_t id = 1 + ((r & 3) == 0 ? ((k & 15) == 0 ? 2 : 1) : ((r & 1) ^ 1));
+    return ((k & 7) != 0 ? 4 : (((s & 1) != 0) && (n <= 112) ? 0 : id));
+}
+static inline uint32_t choose_routine_bconv(uint32_t n)
+{
+    uint32_t r  = (n + 31) >> 5;
+    uint32_t s  = (n + 15) >> 4;
+    uint32_t id = 1 + ((r & 3) == 0 ? 2 : ((r & 1) ^ 1));
+    return (((s & 1) != 0) && (n <= 112) ? 0 : id);
+}
+static size_t get_auxbuf_size(const param_conv_t& p) { return (p.s_pad + p.s_perm + p.s_idx); }
+
+static void lk_ufconv(const Handle& handle,
+                      const Kernel& kern,
+                      const param_ufconv_t& p,
+                      void* c,
+                      const void* a,
+                      const void* b,
+                      float alpha)
+{
+    handle.Run(kern)(a, b, p.ng, p.m, p.n, p.k, p.amag, p.cmag, c, alpha, p.dimx);
+}
+static void lk_padding2d(
+    const Handle& handle, const Kernel& kern, const param_conv_t& p, void* dst, const void* src)
+{
+    handle.Run(kern)(dst, src, p.anx, p.any, p.pad, p.ng * p.inc, p.lda);
+}
+static void lk_perm2d(
+    const Handle& handle, const Kernel& kern, const param_conv_t& p, void* dst, const void* src)
+{
+    uint32_t bnn   = p.bnx * p.bny;
+    uint32_t lda   = p.n * bnn;
+    uint32_t align = p.id != 3 ? 7 : 15;
+    handle.Run(kern)(dst, src, lda, (p.n + 3) & ~3, p.k, p.n, bnn, (p.k + align) & ~align);
+}
+static void lk_genidx2d(const Handle& handle, const Kernel& kern, const param_conv_t& p, void* dst)
+{
+    uint32_t npx = p.pnx * p.pny;
+    uint32_t inc = p.ng * p.inc;
+    uint32_t onc = p.ng * p.n;
+    uint32_t ldx = npx * (p.pad == 0 ? inc : 1);
+    handle.Run(kern)(
+        dst, p.ntidx, p.pnx, p.sd, ldx, onc, p.m, p.cnx, p.cny, p.bnx, p.bny, p.lda, p.k);
+}
+static void lk_conv(const Handle& handle,
+                    const Kernel& kern,
+                    const param_conv_t& p,
+                    void* c,
+                    const void* a,
+                    const void* b,
+                    const void* idx,
+                    float alpha)
+{
+    const uint8_t* relo = static_cast<const uint8_t*>(idx) + (p.ntidx << 3);
+    uint32_t n          = (p.pad != 0 ? 0x80000000 : 0) | p.n;
+    if(p.dir == 0)
+    {
+        handle.Run(kern)(idx, relo, n, p.k, p.ags, alpha, a, b, c, p.m, p.ldc);
+    }
+    else
+    {
+        uint32_t align = p.id != 3 ? 7 : 15;
+        uint32_t k     = (p.k + align) & ~align;
+        handle.Run(kern)(idx, relo, (p.n + 3) & ~3, n, k, p.ags, a, b, c, alpha, p.m, p.ldc);
+    }
+}
+
+namespace conv {
+InvokerFactory MakeFlexgemmInvokerFactory(const param_ufconv_t& p, float alpha)
+{
+    return [=](const std::vector<Kernel>& kernels) {
+        return [=](const Handle& handle, const boost::any& prim_params) {
+            const auto& tensors = boost::any_cast<DataInvokeParams>(prim_params).tensors;
+            lk_ufconv(handle, kernels[0], p, tensors.out, tensors.in, tensors.w, alpha);
+            if(handle.IsProfilingEnabled())
+            {
+                float elapsed = handle.GetKernelTime();
+                handle.ResetKernelTime();
+                handle.AccumKernelTime(elapsed);
+            }
+        };
+    };
+}
+InvokerFactory MakeFlexgemmInvokerFactory(const param_conv_t& p, float alpha)
+{
+    return [=](const std::vector<Kernel>& kernels) {
+        return [=](const Handle& handle, const boost::any& prim_params) {
+            const auto& params   = boost::any_cast<DataInvokeParams>(prim_params);
+            const size_t auxsize = get_auxbuf_size(p);
+            if(params.workSpace == nullptr || params.workSpaceSize < auxsize)
+                MIOPEN_THROW("Workspace is not enough for flexgemm");
+            const auto& tensors = params.tensors;
+            float elapsed       = 0.f;
+            uint8_t* a          = reinterpret_cast<uint8_t*>(params.workSpace);
+            uint8_t* b          = a + p.s_pad;
+            uint8_t* idx        = b + p.s_perm;
+            const void* src     = p.pad != 0 ? static_cast<const void*>(a) : tensors.in;
+            const void* fil     = p.dir != 0 ? static_cast<const void*>(b) : tensors.w;
+            int ikern           = 0;
+            if(p.pad != 0)
+            {
+                lk_padding2d(handle, kernels[ikern++], p, a, tensors.in);
+                if(handle.IsProfilingEnabled())
+                {
+                    elapsed += handle.GetKernelTime();
+                }
+            }
+            if(p.dir != 0)
+            {
+                lk_perm2d(handle, kernels[ikern++], p, b, tensors.w);
+                if(handle.IsProfilingEnabled())
+                {
+                    elapsed += handle.GetKernelTime();
+                }
+            }
+            lk_genidx2d(handle, kernels[ikern++], p, idx);
+            if(handle.IsProfilingEnabled())
+            {
+                elapsed += handle.GetKernelTime();
+            }
+
+            lk_conv(handle, kernels[ikern], p, tensors.out, src, fil, idx, alpha);
+            if(handle.IsProfilingEnabled())
+            {
+                elapsed += handle.GetKernelTime();
+                handle.ResetKernelTime();
+                handle.AccumKernelTime(elapsed);
+            }
+        };
+    };
+}
+} // namespace conv
+
 namespace solver {
+static size_t get_auxbuf_size(const ConvolutionContext& ctx)
+{
+    uint32_t pu  = ctx.pad_w;
+    uint32_t pv  = ctx.pad_h;
+    uint32_t ng  = ctx.group_counts;
+    uint32_t bs  = ctx.batch_sz;
+    uint32_t inc = ctx.n_inputs / ng;
+    uint32_t anx = ctx.in_width;
+    uint32_t any = ctx.in_height;
+    uint32_t bnx = ctx.kernel_size_w;
+    uint32_t bny = ctx.kernel_size_h;
+    uint32_t cnx = ctx.out_width;
+    uint32_t cny = ctx.out_height;
+    if(!ctx.direction.IsForward())
+    {
+        pu = bnx - pu - 1;
+        pv = bny - pv - 1;
+    }
+    uint32_t pnx   = anx + (pu << 1);
+    uint32_t pny   = any + (pv << 1);
+    uint32_t k     = bnx * bny * inc;
+    uint32_t n     = ctx.n_outputs / ng;
+    uint32_t ldc   = cnx * cny;
+    uint32_t m     = ldc * bs;
+    uint32_t fid   = choose_routine_fconv(n, k);
+    uint32_t bid   = choose_routine_bconv(n);
+    uint32_t id    = ctx.direction.IsForward() ? fid : bid;
+    uint32_t temp  = id == 0 ? 127 : 255;
+    uint32_t ntidx = (m + temp) & ~temp;
+    uint32_t lda   = pnx * pny;
+    if((pu | pv) != 0)
+    {
+        lda *= bs;
+        if(lda > 1024)
+        {
+            temp = (lda + 63) >> 6;
+            lda  = (temp + (1 ^ (temp & 1))) << 6;
+        }
+    }
+    temp          = id != 3 ? 7 : 15;
+    size_t ags    = lda * inc;
+    size_t s_pad  = static_cast<size_t>(ng << 2) * ags;
+    size_t s_perm = static_cast<size_t>(ng << 2) * ((k + temp) & ~temp) * ((n + 3) & ~3);
+    size_t s_idx  = (ntidx << 3) + (((k + 15) & ~15) << 2) + 128;
+    s_pad         = (pu | pv) == 0 ? 0 : s_pad;
+    s_perm        = ctx.direction.IsForward() ? 0 : s_perm;
+    return (s_pad + s_perm + s_idx);
+}
+
+static void build_params_ufconv(param_ufconv_t& p, const ConvolutionContext& ctx)
+{
+    p.ng               = ctx.group_counts;
+    p.m                = ctx.out_width * ctx.out_height;
+    p.n                = ctx.n_outputs / p.ng;
+    p.k                = ctx.n_inputs / p.ng;
+    p.dir              = ctx.direction.IsForward() ? 0 : 1;
+    p.id               = choose_routine_ufconv(p.m, p.n, p.k, p.dir);
+    uint32_t tile      = p.id & 0xffff;
+    uint32_t mode      = p.id >> 16;
+    uint32_t sx        = (0x024 >> (mode << 1)) & 0x3;
+    uint32_t sy        = (0xc00 >> (tile * 3 + mode)) & 0x1;
+    uint32_t alignment = (tile > 0) && (tile < 3) ? 255 : 127;
+    p.dimx             = p.m * ctx.batch_sz;
+    p.ntidx            = (p.dimx + alignment) & (~alignment);
+    p.amag             = idiv_magic(p.ntidx >> sx, p.m >> sx);
+    if(sx != sy)
+    {
+        p.cmag = idiv_magic(p.ntidx >> sy, p.m >> sy);
+    }
+}
+
+static void build_params_conv(param_conv_t& p, const ConvolutionContext& ctx)
+{
+    uint32_t pu = ctx.pad_w;
+    uint32_t pv = ctx.pad_h;
+    uint32_t su = ctx.kernel_stride_w;
+    uint32_t sv = ctx.kernel_stride_h;
+    uint32_t du = ctx.kernel_dilation_w;
+    uint32_t dv = ctx.kernel_dilation_h;
+    p.dir       = ctx.direction.IsForward() ? 0 : 1;
+    p.ng        = ctx.group_counts;
+    p.bs        = ctx.batch_sz;
+    p.inc       = ctx.n_inputs / p.ng;
+    p.anx       = ctx.in_width;
+    p.any       = ctx.in_height;
+    p.bnx       = ctx.kernel_size_w;
+    p.bny       = ctx.kernel_size_h;
+    p.cnx       = ctx.out_width;
+    p.cny       = ctx.out_height;
+    if(p.dir != 0)
+    {
+        pu = p.bnx - pu - 1;
+        pv = p.bny - pv - 1;
+    }
+    p.pnx = p.anx + (pu << 1);
+    p.pny = p.any + (pv << 1);
+    p.pad = (pv << 24) | (pv << 16) | (pu << 8) | pu;
+    p.sd  = (dv << 18) | (du << 12) | (sv << 6) | su;
+    p.ldc = p.cnx * p.cny;
+    p.m   = p.ldc * p.bs;
+    p.n   = ctx.n_outputs / p.ng;
+    p.k   = p.bnx * p.bny * p.inc;
+    if(p.dir == 0)
+    {
+        p.id = choose_routine_fconv(p.n, p.k);
+    }
+    else
+    {
+        p.id = choose_routine_bconv(p.n);
+    }
+    uint32_t temp = p.id == 0 ? 127 : 255;
+    p.ntidx       = (p.m + temp) & ~temp;
+    p.lda         = p.pnx * p.pny;
+    if(p.pad != 0)
+    {
+        p.lda *= p.bs;
+        if(p.lda > 1024)
+        {
+            temp  = (p.lda + 63) >> 6;
+            p.lda = (temp + (1 ^ (temp & 1))) << 6;
+        }
+    }
+    temp     = p.id != 3 ? 7 : 15;
+    p.ags    = p.lda * p.inc;
+    p.s_pad  = static_cast<size_t>(p.ng << 2) * p.ags;
+    p.s_perm = static_cast<size_t>(p.ng << 2) * ((p.k + temp) & ~temp) * ((p.n + 3) & ~3);
+    p.s_idx  = (p.ntidx << 3) + (((p.k + 15) & ~15) << 2) + 128;
+    p.s_pad  = p.pad == 0 ? 0 : p.s_pad;
+    p.s_perm = p.dir == 0 ? 0 : p.s_perm;
+}
+
 static void fill_kernels_info(ConvSolution& sol,
                               const ConvolutionContext& ctx,
                               const param_ufconv_t& p,
@@ -206,7 +528,7 @@ ConvSolution ConvFlexgemm::GetSolution(const ConvolutionContext& ctx) const
     {
         param_conv_t params{};
         build_params_conv(params, ctx);
-        sol.workspce_sz = params.spad + params.sperm + params.sidx;
+        sol.workspce_sz = params.s_pad + params.s_perm + params.s_idx;
         fill_kernels_info(sol, ctx, params, fname, 0);
         sol.invoker_factory = conv::MakeFlexgemmInvokerFactory(params, 1.f);
     }
