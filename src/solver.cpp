5d2196b2ce0fbc9da20b5a0b7191fbc6f5928881
Artem Tamazov <artem.tamazov@gmail.com>
2020-09-30 22:05:27 +0300

    Formatting

--- src/solver.cpp
+++ src/solver.cpp
@@ -340,7 +340,7 @@ inline SolverRegistrar::SolverRegistrar(IdRegistryData& registry)
         registry, ++id, ConvMPBidirectWinograd<5, 3>{}, miopenConvolutionAlgoWinograd);
     RegisterWithSolver(
         registry, ++id, ConvMPBidirectWinograd<6, 3>{}, miopenConvolutionAlgoWinograd);
-  
+
     RegisterWithSolver(registry,
                        ++id,
                        ConvAsmImplicitGemmGTCDynamicWrwXdlops{},
