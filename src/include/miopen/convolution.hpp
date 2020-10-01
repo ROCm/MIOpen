c7f48506723e4b8a6f282e55f65099dcb4fa0b9c
Vasilii Filippov <vasja.filippov94@outlook.com>
2020-10-01 17:11:36 +0300

    format

--- src/include/miopen/convolution.hpp
+++ src/include/miopen/convolution.hpp
@@ -115,8 +115,7 @@ struct ConvolutionDescriptor : miopenConvolutionDescriptor
     std::size_t
     ForwardBackwardDataGetWorkSpaceSizeDirect(const miopen::ConvolutionContext& ctx) const;
 
-    std::size_t
-    ForwardBackwardDataGetWorkSpaceSizeFFT(const miopen::ConvolutionContext& ctx) const;
+    std::size_t ForwardBackwardDataGetWorkSpaceSizeFFT(const miopen::ConvolutionContext& ctx) const;
 
     std::size_t
     ForwardBackwardGetWorkSpaceSizeImplicitGemm(const miopen::ConvolutionContext& ctx) const;
