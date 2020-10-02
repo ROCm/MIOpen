d443852fde95a141d631d12fb33c8e5586497a2b
ltqin <letao.qin@amd.com>
2020-10-02 20:27:54 +0800

    implicit gemm backward weight with xdlops and atomic add 2 (#472)
    * wrw atomic
    * fix function name bug and mask other wrw
    * remove -mllvm -amdgpu-enable-global-sgpr-addr , otherwise dump for 3X3 filter
    * remove mask other wrw
    * remove static assert groupg==1
    * move to first of wrw in GetImplicitGemmWrWSolvers function
    * remove GemmKBlocks from tuning parameters,just caculate
    * remove WORKAROUND_SWDEV_240356 and WORKAROUND_SWDEV_229564
    * recover enable global sgpr addr
    * set GemmkPack range: 1~8
    * add enable global sgpr addr
    * skip_solutions_that_take_long_time_to_build_and_have_narrow_coverage
    * add code to check value of data_per_thread_copy_gemmk
    * remove flag -mllvm -amdgpu-enable-global-sgpr-addr
    * change reading order(B matrix)
    Co-authored-by: root <root@dc-smc-18.amd.com>
    Co-authored-by: Chao Liu <lc.roy86@gmail.com>
    Co-authored-by: Artem Tamazov <artem.tamazov@gmail.com>

--- src/solver.cpp
+++ src/solver.cpp
@@ -340,10 +340,14 @@ inline SolverRegistrar::SolverRegistrar(IdRegistryData& registry)
         registry, ++id, ConvMPBidirectWinograd<5, 3>{}, miopenConvolutionAlgoWinograd);
     RegisterWithSolver(
         registry, ++id, ConvMPBidirectWinograd<6, 3>{}, miopenConvolutionAlgoWinograd);
+
     RegisterWithSolver(registry,
                        ++id,
                        ConvAsmImplicitGemmGTCDynamicWrwXdlops{},
                        miopenConvolutionAlgoImplicitGEMM);
+
+    RegisterWithSolver(
+        registry, ++id, ConvHipImplicitGemmWrwV4R4Xdlops{}, miopenConvolutionAlgoImplicitGEMM);
 }
 
 } // namespace solver
