5e791ddff44e56dc94da8746080b0d9c71a3eee1
Zhuoran Yin <zhuoryin@amd.com>
2020-10-13 13:06:45 -0500

    [igemm] Adding flexible layout support (#343)
    * Adding flexible layout support
    * Modified tensor_api and descriptor for layout updates
    * Updated conv_driver to set tensor layouts
    * Updated problem_description to avoid key collision
    * Enabled implicit_gemm to get layouts from tensors
    * Synced changes between two problem_description
    * Update function name and add check for the set layout API
    * Make tensor layouts default to all capitalized letters
    * Removing redundant layout lines
    * Removed the SetTensorDescriptorLayout() API
    * Use strides to represent tensor layout
    * Revert to "NCHW" layout string to all of input/output/filter tensors
    * Get rid of asserts on weights layout
    * Adding compatibility layout to building network_config
    * Use GetLayout() as the lightweight getter and ComputeLayout() to be
      called in constructor
    * Replaced weights_layout check with IsLayoutDefault()
    * Adding comment explaining copy construct

--- src/solver/conv_asm_5x10u2v2b1.cpp
+++ src/solver/conv_asm_5x10u2v2b1.cpp
@@ -59,7 +59,10 @@ bool ConvAsm5x10u2v2b1::IsApplicable(const ConvolutionContext& params) const
     {
         return false;
     }
-    assert(params.weights_layout.length() == 0); // _weights_layout is not supported yet.
+    if(!params.IsLayoutDefault())
+    {
+        return false;
+    }
 
     // Min image + padding shall be not smaller than filter matrix.
     const int min_out_width  = 138;
