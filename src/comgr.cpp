7c5172c41093cc99d96c823fa52655716efc7dda
zjing14 <jizhan@amd.com>
2020-10-16 15:21:52 -0500

    Fix "Workaround for SWDEV-255735" (PR 505) (#515)
    * fixed marco
    * AmdgcnAssembleQuiet(): removed xnack option as useless. Added xnack option to comgr::BuildAsm().
    * Revert changes in inst_wrappers.inc.
    Co-authored-by: Artem Tamazov <artem.tamazov@gmail.com>

--- src/comgr.cpp
+++ src/comgr.cpp
@@ -30,6 +30,8 @@
 #include <miopen/algorithm.hpp>
 #include <miopen/env.hpp>
 #include <miopen/errors.hpp>
+#include <miopen/hip_build_utils.hpp>
+#include <miopen/gcn_asm_utils.hpp>
 #include <miopen/kernel.hpp>
 #include <miopen/logger.hpp>
 #include <miopen/stringutils.hpp>
@@ -858,6 +860,10 @@ void BuildAsm(const std::string& name,
         SetIsaName(action, device);
         action.SetLogging(true);
         auto optAsm = miopen::SplitSpaceSeparated(options);
+#if WORKAROUND_SWDEV_255735
+        if(miopen::HipCompilerVersion() >= miopen::external_tool_version_t{3, 8, 20403})
+            optAsm.push_back("-mno-xnack");
+#endif
         compiler::lc::gcnasm::RemoveOptionsUnwanted(optAsm);
         action.SetOptionList(optAsm);
 
