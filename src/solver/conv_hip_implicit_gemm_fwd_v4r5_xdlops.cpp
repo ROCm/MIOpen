4e0ae8ab4ce45058f23a0dda28a0ef1bc9aa7e0d
zjing14 <jizhan@amd.com>
2020-10-27 12:09:40 -0500

    Fixed iGemm v4r5 tuning (#547)
    * Add check of N in tensor contraction

--- src/solver/conv_hip_implicit_gemm_fwd_v4r5_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_fwd_v4r5_xdlops.cpp
@@ -617,6 +617,16 @@ bool PerformanceImplicitGemmForwardV4R5Xdlops::IsReallyValid(const ConvolutionCo
 
     bool valid = false;
 
+    // check tensor contraction
+    {
+        const std::size_t n = ConvolutionContextInterpreter::GetBatchN(ctx);
+        const auto NWaves   = GemmNPerBlock / GemmNPerWave;
+        // check if N can be divided by NWaves, as N will be unmerged into N1
+        // and NWaves for tensor contraction
+        if(n % NWaves != 0)
+            return false;
+    }
+
     // check blockwise GEMM size
     {
         int gemm_m       = -1;
