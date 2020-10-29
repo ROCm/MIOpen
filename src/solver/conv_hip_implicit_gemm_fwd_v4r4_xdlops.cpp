1e86c4cac38f2c83dd050659104240cf52224798
asleepzzz <hanwen.chang@amd.com>
2020-10-30 00:02:40 +0800

    [igemm]fix issue 427(don't drop small kpack*kperblock for all cases) (#531)

--- src/solver/conv_hip_implicit_gemm_fwd_v4r4_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_fwd_v4r4_xdlops.cpp
@@ -749,17 +749,22 @@ bool PerformanceImplicitGemmForwardV4R4Xdlops::IsFastToBeUsedForTuning(
     // GemmKPerBlock*GemmKPack should not be too small, otherwise read performance of A matrix would
     // be bad
     {
+        int gemm_ktotal = 0;
+
+        std::tie(std::ignore, std::ignore, std::ignore, gemm_ktotal) =
+            ConvHipImplicitGemmForwardV4R4Xdlops::CalculateGemmSize(ctx);
+
         if(ctx.IsFp32())
         {
             if(GemmKPack > 4)
                 return false;
 
-            if(GemmKPerBlock * GemmKPack < 8)
+            if(GemmKPerBlock * GemmKPack < 8 && gemm_ktotal % 8 == 0)
                 return false;
         }
         else if(ctx.IsFp16() || ctx.IsBfp16())
         {
-            if(GemmKPerBlock * GemmKPack < 16)
+            if(GemmKPerBlock * GemmKPack < 16 && gemm_ktotal % 16 == 0)
                 return false;
         }
     }
