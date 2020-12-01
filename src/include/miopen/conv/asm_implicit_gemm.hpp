4a3dc9d0154a7af0e047ba7b05e97770c45e7723
carlushuang <carlus.huang@amd.com>
2020-12-01 10:47:40 +0800

    [dynamic-igemm] odd image support in asm igemm xdlops (#552)
    * post review for pr441-enable asm igemm generic tensor contraction for backward_data
    * add sperate asm_implicit_gemm.hpp to include asm_igemm common parts and fix for bwd solver part
    * 1. move asm_implicit_gemm.hpp to src/include/miopen/conv/; 2. use alias in wrw file
    * modification for code quality for gtc dynamic igemm solver
    * remove outdated comment for FindImplicitGemmGtcDynamicFwdKernel
    * make function in TunableImplicitGemmGTCDynamic_t to const type
    * add DIV/0 protection for GetBlockSize in TunableImplicitGemmGTCDynamic_t
    * add odd image size support.
    * add bwd tunable check
    * fix logic for wrw
    * fix a bug in isApplicable
    * fix bug in condition check of c. add more comments
    Co-authored-by: jane-zxy <xiaoyan.zhou@amd.com>
    Co-authored-by: shaojiewang <wsjmessi@163.com>
    Co-authored-by: Qianfeng Zhang <Qianfeng.Zhang@amd.com>

--- src/include/miopen/conv/asm_implicit_gemm.hpp
+++ src/include/miopen/conv/asm_implicit_gemm.hpp
@@ -44,6 +44,7 @@ struct TunableImplicitGemmGTCDynamic_t
 
     int wave_tile_m;
     int wave_tile_n;
+    int wave_tile_k;
     int wave_step_m;
     int wave_step_n;
     int wave_repeat_m;
@@ -69,10 +70,10 @@ struct TunableImplicitGemmGTCDynamic_t
         std::ostringstream kernel_name;
         kernel_name << "igemm_" << direction << "_gtcx_nchw_" << precision << "_bx" << nxb << "_ex"
                     << nxe << "_bt" << gemm_m_per_block << "x" << gemm_n_per_block << "x"
-                    << gemm_k_per_block << "_wt" << wave_tile_m << "x" << wave_tile_n << "_ws"
-                    << wave_step_m << "x" << wave_step_n << "_wr" << wave_repeat_m << "x"
-                    << wave_repeat_n << "_ta" << tensor_a_thread_lengths[0] << "x"
-                    << tensor_a_thread_lengths[1] << "x" << tensor_a_thread_lengths[2] << "x"
+                    << gemm_k_per_block << "_wt" << wave_tile_m << "x" << wave_tile_n << "x"
+                    << wave_tile_k << "_ws" << wave_step_m << "x" << wave_step_n << "_wr"
+                    << wave_repeat_m << "x" << wave_repeat_n << "_ta" << tensor_a_thread_lengths[0]
+                    << "x" << tensor_a_thread_lengths[1] << "x" << tensor_a_thread_lengths[2] << "x"
                     << tensor_a_thread_lengths[3] << "_" << tensor_a_cluster_lengths[0] << "x"
                     << tensor_a_cluster_lengths[1] << "x" << tensor_a_cluster_lengths[2] << "x"
                     << tensor_a_cluster_lengths[3] << "_tb" << tensor_b_thread_lengths[0] << "x"
