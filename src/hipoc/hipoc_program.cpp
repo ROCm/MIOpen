f3b88f81487e644c556dbb834c20856a8329a6ad
Artem Tamazov <artem.tamazov@gmail.com>
2020-12-30 00:52:13 +0300

    targetid-fixes(21) OCL builds (HIP be): Update CO version selection option to match new syntax & value

--- src/hipoc/hipoc_program.cpp
+++ src/hipoc/hipoc_program.cpp
@@ -78,7 +78,7 @@ int DetectCodeObjectOptionSyntax()
 
     if(syntax == 0)
     {
-        if(HipCompilerVersion() >= external_tool_version_t{4, 0, 999999}) /// \ todo
+        if(HipCompilerVersion() >= external_tool_version_t{4, 0, 20482})
             return 4;
         else
             return 1;
@@ -99,7 +99,7 @@ int DetectCodeObjectVersion()
 
     if(co_version == 0)
     {
-        if(HipCompilerVersion() >= external_tool_version_t{4, 0, 999999}) /// \ todo
+        if(HipCompilerVersion() >= external_tool_version_t{4, 0, 20482})
             return 4;
         else if(HipCompilerVersion() >= external_tool_version_t{3, 0, -1})
             return 3;
