161882988925bfc34dea0492720df2d03e9c6c64
Chao Liu <chao.liu2@amd.com>
2021-01-11 10:03:46 -0600

    fix issue 650 for develop (#671)
    * fix issue 650
    * change return type of buffer atomic back to void
    
    [ROCm/MIOpen commit: 38a23bbf0a7531a9554a21e4ebba1f8e5a5d2573]

--- src/kernels/composable_kernel/include/utility/amd_buffer_addressing.hpp
+++ src/kernels/composable_kernel/include/utility/amd_buffer_addressing.hpp
@@ -139,8 +139,8 @@ __llvm_amdgcn_buffer_store_bf16x4(ushort4_t vdata,
                                   bool slc) __asm("llvm.amdgcn.buffer.store.v4bf16");
 
 #if CK_USE_AMD_BUFFER_ATOMIC_FADD
-#if CK_HIP_VERSION_FLAT >= 3010020405
-// starting ROCm-3.10, the return type becomes float
+#if CK_HIP_VERSION_FLAT >= 9999999999
+// It is planned to change return type from void to float in some future version
 __device__ float
 #else
 __device__ void
