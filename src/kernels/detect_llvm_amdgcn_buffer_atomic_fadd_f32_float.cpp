e3c5f8101112a601a95ff170b41ed877a0f8e0f3
Artem Tamazov <artem.tamazov@gmail.com>
2021-01-23 01:37:56 +0300

    detect-hip-intrinsic(08) Fix detection kernel

--- src/kernels/detect_llvm_amdgcn_buffer_atomic_fadd_f32_float.cpp
+++ src/kernels/detect_llvm_amdgcn_buffer_atomic_fadd_f32_float.cpp
@@ -1,6 +1,28 @@
-#ifndef MIOPEN_DONT_USE_HIP_RUNTIME_HEADERS
-#include "hip/hip_runtime.h"
-#endif
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2021 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
 
 typedef int32_t int32x4_t __attribute__((ext_vector_type(4)));
 
@@ -14,8 +36,5 @@ __llvm_amdgcn_buffer_atomic_add_f32(float vdata,
 extern "C" __global__ void test_llvm_amdgcn_buffer_atomic_fadd_f32_float(float* p_global)
 {
     int32x4_t buffer_resource{0};
-
-    float tmp = __llvm_amdgcn_buffer_atomic_add_f32(*p_global, buffer_resource, 0, 0, false);
-
-    p_global[0] = tmp;
+    (void) __llvm_amdgcn_buffer_atomic_add_f32(*p_global, buffer_resource, 0, 0, false);
 }
