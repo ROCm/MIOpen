22c2872c5eb4726f88bdd672ae57e7de795d1658
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2021-01-28 07:46:50 +0000

    Integration of fp16 implicit-gemm bwd dynamic asm kernel for gfx908

--- src/include/miopen/conv/invokers/impl_gemm_dynamic.hpp
+++ src/include/miopen/conv/invokers/impl_gemm_dynamic.hpp
@@ -29,11 +29,74 @@
 #include <miopen/invoker.hpp>
 #include <miopen/kernel.hpp>
 #include <miopen/conv/context.hpp>
+#include <miopen/numeric.hpp>
+#include <miopen/conv/asm_implicit_gemm.hpp>
 
 #include <vector>
 
 namespace miopen {
 namespace conv {
+
+struct _magic_div_u32_
+{
+    uint32_t magic;
+    uint8_t shift;
+};
+
+using magic_div_u32_t = _magic_div_u32_;
+
+/*
+*
+* numer / denom = quotient, reminder
+*
+* use magic number to do integer division of uint32 (acctually INT32_MAX, the 31 bit divisoin)
+* most algorithm to compute uint32 need branching if cover all 32 bit of uint32.
+* since we compute the magic number on host side, implement the division in gpu side, it is better
+* not use branching
+* hence add more restriction to numer and denom, to be 1 bit less. hence need less-or-equal than
+* INT32_MAX
+*
+* magic_div_u32_gen() compute from input arg d, to get a magic and a shift.
+* to use the value, below is a example host-side code to do this
+*
+* // host side version
+* static inline uint32_t magic_div_mulhi_u32(uint32_t x, uint32_t y) {
+*     uint64_t xl = x, yl = y;
+*     uint64_t rl = xl * yl;
+*     return (uint32_t)(rl >> 32);
+* }
+* uint32_t magic_div_u32_do(uint32_t numer, const struct magic_div_u32_t *denom) {
+*     uint32_t tmp = magic_div_mulhi_u32(denom->magic, numer);
+*     return (tmp + numer) >> denom->shift;
+* }
+*
+*/
+static inline magic_div_u32_t magic_div_u32_gen(uint32_t d)
+{
+    assert(d >= 1 && d <= INT32_MAX);
+    uint8_t shift;
+    for(shift = 0; shift < 32; shift++)
+        if((1U << shift) >= d)
+            break;
+
+    uint64_t one   = 1;
+    uint64_t magic = ((one << 32) * ((one << shift) - d)) / d + 1;
+    assert(magic <= 0xffffffffUL);
+
+    magic_div_u32_t result;
+    result.magic = magic;
+    result.shift = shift;
+    return result;
+}
+
+static inline uint32_t magic_div_u32_pack_shift(uint8_t s0, uint8_t s1, uint8_t s2, uint8_t s3)
+{
+    uint32_t shift_0 = static_cast<uint32_t>(s0);
+    uint32_t shift_1 = static_cast<uint32_t>(s1);
+    uint32_t shift_2 = static_cast<uint32_t>(s2);
+    uint32_t shift_3 = static_cast<uint32_t>(s3);
+    return (shift_3 << 24) | (shift_2 << 16) | (shift_1 << 8) | shift_0;
+}
 
 float CallImplGemmDynamicForward(const miopen::Handle& handle,
                                  const ProblemDescription& conv_problem,
@@ -56,7 +119,19 @@ float CallImplGemmDynamicBackwardData(const miopen::Handle& handle,
 
 InvokerFactory MakeImplGemmDynamicForwardInvokerFactory(const ConvolutionContext& ctx);
 InvokerFactory MakeImplGemmDynamicForward1x1InvokerFactory(const ConvolutionContext& ctx);
-InvokerFactory MakeImplGemmDynamicBackwardDataInvokerFactory(const ConvolutionContext& ctx);
+
+template <typename T = int>
+InvokerFactory MakeImplGemmDynamicBackwardDataInvokerFactory(const ConvolutionContext& ctx,
+                                                             const T& cfg);
+
+template <>
+InvokerFactory MakeImplGemmDynamicBackwardDataInvokerFactory<int>(const ConvolutionContext& ctx,
+                                                                  const int& cfg);
+
+template <>
+InvokerFactory
+MakeImplGemmDynamicBackwardDataInvokerFactory<solver::TunableImplicitGemmGTCDynamic_t>(
+    const ConvolutionContext& ctx, const solver::TunableImplicitGemmGTCDynamic_t& cfg);
 
 } // namespace conv
 } // namespace miopen
