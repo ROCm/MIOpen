ea97d11dc4059deb0d05f6160365cbb489de615b
shaojiewang <wsjmessi@163.com>
2021-02-23 15:02:55 +0800

    use constexpr as type for const number

--- src/include/miopen/conv/invokers/impl_gemm_dynamic.hpp
+++ src/include/miopen/conv/invokers/impl_gemm_dynamic.hpp
@@ -82,7 +82,7 @@ static inline magic_div_u32_t magic_div_u32_gen(uint32_t d)
         if((1U << shift) >= d)
             break;
 
-    uint64_t one   = 1;
+    constexpr one   = 1;
     uint64_t magic = ((one << 32) * ((one << shift) - d)) / d + 1;
     assert(magic <= 0xffffffffUL);
 
