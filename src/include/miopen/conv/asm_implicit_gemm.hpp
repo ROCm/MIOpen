af7ea8ca31da711afe38ddb91a6a2b96fd844932
shaojiewang <wsjmessi@163.com>
2021-03-01 18:07:22 +0800

    1. use enum insteat of string in TunableImplicitGemmGTCDynamic_t; 2. make GetSolution and GetWorkspaceSize return same value

--- src/include/miopen/conv/asm_implicit_gemm.hpp
+++ src/include/miopen/conv/asm_implicit_gemm.hpp
@@ -34,7 +34,7 @@ namespace solver {
 struct TunableImplicitGemmGTCDynamic_t
 {
     std::string direction;
-    std::string precision;
+    miopenDataType_t precision;
     int nxb;
     int nxe;
 
@@ -68,7 +68,8 @@ struct TunableImplicitGemmGTCDynamic_t
     std::string GetKernelName() const
     {
         std::ostringstream kernel_name;
-        kernel_name << "igemm_" << direction << "_gtcx_nchw_" << precision << "_bx" << nxb << "_ex"
+        std::string kernel_precision = precision == miopenFloat ? "fp32" : "fp16";
+        kernel_name << "igemm_" << direction << "_gtcx_nchw_" << kernel_precision << "_bx" << nxb << "_ex"
                     << nxe << "_bt" << gemm_m_per_block << "x" << gemm_n_per_block << "x"
                     << gemm_k_per_block << "_wt" << wave_tile_m << "x" << wave_tile_n << "x"
                     << wave_tile_k << "_ws" << wave_step_m << "x" << wave_step_n << "_wr"
