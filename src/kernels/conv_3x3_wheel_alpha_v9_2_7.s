59643b8b00ba4d77b029086a0aa83117b243b947
Lapo Lapo <glarochk@amd.com>
2021-03-15 01:05:30 +0300

    Convert Winograd binaries to sources (#748)
    [ROCm/MIOpen commit: 62cfbf95fe05fb6d88bb6b543c671897782ed58b]

--- src/kernels/conv_3x3_wheel_alpha_v9_2_7.s
+++ src/kernels/conv_3x3_wheel_alpha_v9_2_7.s
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2019 Advanced Micro Devices, Inc.
+ * Copyright (c) 2021 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,11 @@
  * SOFTWARE.
  *
  *******************************************************************************/
+.include "rocm_version.inc"
 .include "conv_3x3_wheel_alpha_v9_2_7_prologue.inc"
-.include "conv_3x3_wheel_alpha_v9_2_7.inc"
+.if (.option.machine_version_major == 8)
+    .include "conv_3x3_wheel_alpha_v9_2_7_gfx8.inc"
+.elseif (.option.machine_version_major == 9)
+    .include "conv_3x3_wheel_alpha_v9_2_7_gfx9.inc"
+.endif
 .include "conv_3x3_wheel_alpha_v9_2_7_epilogue.inc"
