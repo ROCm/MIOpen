f52f1d0f89936a403c656988a375b99fa01b78fe
Artem Tamazov <artem.tamazov@gmail.com>
2021-04-06 01:24:33 +0300

    [COMGR][MI100] Fix passing the CK_AMD_BUFFER_ATOMIC_FADD_RETURNS_FLOAT macro (SWDEV-280049) (#844)
    [ROCm/MIOpen commit: b49c52eccab9c5c50bacb48a2ece769b72de0cfa]

--- src/comgr.cpp
+++ src/comgr.cpp
@@ -756,7 +756,7 @@ void BuildHip(const std::string& name,
                        (" -DHIP_PACKAGE_VERSION_FLAT=") + std::to_string(HIP_PACKAGE_VERSION_FLAT);
 #if ROCM_FEATURE_LLVM_AMDGCN_BUFFER_ATOMIC_FADD_F32_RETURNS_FLOAT
             if(miopen::solver::support_amd_buffer_atomic_fadd(target.Name()))
-                raw += "-DCK_AMD_BUFFER_ATOMIC_FADD_RETURNS_FLOAT=1";
+                raw += " -DCK_AMD_BUFFER_ATOMIC_FADD_RETURNS_FLOAT=1";
 #endif
             auto optCompile = miopen::SplitSpaceSeparated(raw, compiler::lc::GetOptionsNoSplit());
             compiler::lc::hip::RemoveCompilerOptionsUnwanted(optCompile);
@@ -772,7 +772,7 @@ void BuildHip(const std::string& name,
                        (" -DHIP_PACKAGE_VERSION_FLAT=") + std::to_string(HIP_PACKAGE_VERSION_FLAT);
 #if ROCM_FEATURE_LLVM_AMDGCN_BUFFER_ATOMIC_FADD_F32_RETURNS_FLOAT
             if(miopen::solver::support_amd_buffer_atomic_fadd(target.Name()))
-                raw += "-DCK_AMD_BUFFER_ATOMIC_FADD_RETURNS_FLOAT=1";
+                raw += " -DCK_AMD_BUFFER_ATOMIC_FADD_RETURNS_FLOAT=1";
 #endif
 #if COMGR_SUPPORTS_PCH
             if(compiler::lc::hip::IsPchEnabled())
