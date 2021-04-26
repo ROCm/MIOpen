0d20a692824648399dbae14fbdfb831dddcaf90b
Artem Tamazov <artem.tamazov@gmail.com>
2021-04-27 01:07:35 +0300

    [OCL][FP16] Fix: Disable GEMM/MIOpenGEMM. [Immediate Mode] Always use Naive Direct in Fallback. (#891)

--- src/include/miopen/solver.hpp
+++ src/include/miopen/solver.hpp
@@ -2103,11 +2103,9 @@ struct ConvDirectNaiveConvFwd : SolverBase<ConvolutionContext>
 {
     bool IsApplicable(const ConvolutionContext& ctx) const;
     bool IsDynamic() const { return true; }
-#if WORKAROUND_MIOPENGEMM_SINCE_ROCM41
     /// Use very small fixed value enough to backup GEMM for cases when
     /// GEMM is disabled due to MIOpenGemm or OCL compiler issues.
     float GetWti(const ConvolutionContext&) const { return 0.01; }
-#endif
     ConvSolution GetSolution(const ConvolutionContext& ctx) const;
 };
 
@@ -2115,11 +2113,9 @@ struct ConvDirectNaiveConvBwd : SolverBase<ConvolutionContext>
 {
     bool IsApplicable(const ConvolutionContext& ctx) const;
     bool IsDynamic() const { return true; }
-#if WORKAROUND_MIOPENGEMM_SINCE_ROCM41
     /// Use very small fixed value enough to backup GEMM for cases when
     /// GEMM is disabled due to MIOpenGemm or OCL compiler issues.
     float GetWti(const ConvolutionContext&) const { return 0.01; }
-#endif
     ConvSolution GetSolution(const ConvolutionContext& ctx) const;
 };
 
@@ -2127,11 +2123,9 @@ struct ConvDirectNaiveConvWrw : SolverBase<ConvolutionContext>
 {
     bool IsApplicable(const ConvolutionContext& ctx) const;
     bool IsDynamic() const { return true; }
-#if WORKAROUND_MIOPENGEMM_SINCE_ROCM41
     /// Use very small fixed value enough to backup GEMM for cases when
     /// GEMM is disabled due to MIOpenGemm or OCL compiler issues.
     float GetWti(const ConvolutionContext&) const { return 0.01; }
-#endif
     ConvSolution GetSolution(const ConvolutionContext& ctx) const;
 };
 
