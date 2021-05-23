e63e0983a18e591c890ebe75de0165c77d5d8765
Artem Tamazov <artem.tamazov@gmail.com>
2021-05-24 02:00:03 +0300

    Restrict workaround for SWDEV-286774 for FP16 only. Fix MIOPEN_DEBUG_FIND_ONLY_SOLVER in test_conv_igemm_dynamic* tests  (#949)
    * Restrict workaround for SWDEV-286774 for FP16 only.
    * [TESTS] Fix MIOPEN_DEBUG_FIND_ONLY_SOLVER in all test_conv_igemm_dynamic* tests (enforce Normal Find mode)

--- src/solver/conv_asm_implicit_gemm_gtc_bwd.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_bwd.cpp
@@ -963,11 +963,7 @@ static std::tuple<bool, // is suitable kernel found
 
 bool ConvAsmImplicitGemmGTCDynamicBwdXdlops::IsApplicable(const ConvolutionContext& ctx) const
 {
-#if WORKAROUND_SWDEV_286774
-    if(!miopen::IsEnabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_ASM_BWD_GTC_XDLOPS{}))
-#else
     if(miopen::IsDisabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_ASM_BWD_GTC_XDLOPS{}))
-#endif
         return false;
 
     const auto device_name = ctx.GetStream().GetDeviceName();
@@ -985,6 +981,11 @@ bool ConvAsmImplicitGemmGTCDynamicBwdXdlops::IsApplicable(const ConvolutionConte
 
     if(!ctx.IsFp32() && !ctx.IsFp16())
         return false;
+
+#if WORKAROUND_SWDEV_286774
+    if(ctx.IsFp16() && !miopen::IsEnabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_ASM_BWD_GTC_XDLOPS{}))
+        return false;
+#endif
 
     if(!ctx.rmv.IsV3())
         return false;
