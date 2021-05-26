51cfb0eeec04c7cf070a7446b1a1eddac6734faf
Qianfeng <qianfeng.zhang@amd.com>
2021-05-27 06:26:55 +0800

    Fix missing padding of the transformed 2d-src and 1d-dst tensors in Generic-Tensor Reduction (SWDEV-284291) (#953)
    * Fix missing padding needed for the transformed 2D-src and 1D-dst Tensors of Generic-Tensor Reduction
    * Remove the work-around previously added for rocm-3.7 in reduce_test.cpp
    * Refine the assignment of Reduction Method

--- src/kernels/composable_kernel/include/kernel_algorithm/reduction_kernel_simple_configurator.hpp
+++ src/kernels/composable_kernel/include/kernel_algorithm/reduction_kernel_simple_configurator.hpp
@@ -16,9 +16,9 @@ struct ReduceKernelSimpleConfigurator
     template <index_t invariantLength, index_t toReduceLength>
     __device__ static constexpr index_t GetGridSize(Number<invariantLength>, Number<toReduceLength>)
     {
-        if(toReduceLength < warpSize / 4) // let one thread to do each reduction
+        if(toReduceLength <= warpSize / 4) // let one thread to do each reduction
             return ((invariantLength + BlockSize - 1) / BlockSize);
-        else if(toReduceLength < BlockSize) // let one warp to do each reduction
+        else if(toReduceLength <= BlockSize) // let one warp to do each reduction
             return ((invariantLength + numWarpsPerBlock - 1) / numWarpsPerBlock);
         else
             return (invariantLength); // let one block to do each reduction
@@ -28,9 +28,9 @@ struct ReduceKernelSimpleConfigurator
     __device__ static constexpr ReductionMethod_t GetReductionMethod(Number<invariantLength>,
                                                                      Number<toReduceLength>)
     {
-        if(toReduceLength < warpSize / 4) // let one thread to do each reduction
+        if(toReduceLength <= warpSize / 4) // let one thread to do each reduction
             return (ReductionMethod_t::DirectThreadWise);
-        else if(toReduceLength < BlockSize) // let one warp to do each reduction
+        else if(toReduceLength <= BlockSize) // let one warp to do each reduction
             return (ReductionMethod_t::DirectWarpWise);
         else
             return (ReductionMethod_t::BlockWise);
