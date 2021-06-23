873c15d3855f33ef8ab8f4ab977fab78971fbdfe
carlushuang <carlus.huang@amd.com>
2021-06-24 07:40:01 +0800

    [NHWC] asm igemm xdlops fwd/bwd fp32/fp16 kernel (#958)
    * add NHWC fwd/bwd fp32/fp16 kernel
    * fix several bug in Herustic and Tuning
    * add missing kernel and update config list
    * remove useless config lists
    * fix fwd fp32 not valid config
    * fix isValid
    * fix bwd fp16 not proper IsApplicable
    * add ctest for nhwc asm kernels
    * reorg NextLinear
    * parse in opArgs as mutable, reserve place for in/wei/out pointer outside lambda
    * Assign OpKernelArg() to opArgs vector, set proper ctest flag

--- src/include/miopen/conv/asm_implicit_gemm.hpp
+++ src/include/miopen/conv/asm_implicit_gemm.hpp
@@ -27,6 +27,10 @@
 #define CK_ASM_IMPLICITGEMM_HPP_
 #include <string>
 #include <ostream>
+#include <tuple>
+#include <vector>
+#include <limits>
+
 namespace miopen {
 
 namespace solver {
@@ -88,6 +92,140 @@ struct TunableImplicitGemmGTCDynamic_t
         return kernel_name.str();
     }
 };
+
+// calculate log2_gemm_k_global_splits
+// with assumption that dimension_0, _1 will merge into a single dimension, and do split only along
+// dimension_0
+static inline size_t ComputeLog2GemmKGlobalSplitsWith2DMerge(size_t current_grid_size,
+                                                             size_t max_grid_size,
+                                                             size_t merge_dimension_0,
+                                                             size_t merge_dimensoin_1,
+                                                             size_t gemm_k_per_block,
+                                                             size_t max_log2_splits)
+{
+    size_t log2_gemm_k_global_splits = 0;
+    for(size_t gs = 0; gs < max_log2_splits; gs++)
+    {
+        if((current_grid_size << gs) > max_grid_size)
+            break;
+
+        if((merge_dimension_0 % (1 << gs)) != 0)
+        {
+            break;
+        }
+
+        if((merge_dimension_0 >> gs) * merge_dimensoin_1 % gemm_k_per_block != 0)
+        {
+            break;
+        }
+        log2_gemm_k_global_splits = gs;
+    }
+    return log2_gemm_k_global_splits;
+}
+
+static inline size_t
+ComputeMatrixPadSize(size_t col, size_t col_per_block, size_t row, size_t row_per_block)
+{
+    size_t col_padded = ((col + col_per_block - 1) / col_per_block) * col_per_block;
+    size_t row_padded = ((row + row_per_block - 1) / row_per_block) * row_per_block;
+    size_t col_extra  = col_padded - col;
+    size_t row_extra  = row_padded - row;
+
+    return col_extra * row + row_extra * col + col_extra * row_extra;
+}
+
+static inline std::tuple<int, int, int> // m_per_block, n_per_block, k_per_block
+    HeuristicInitMacroTileNoPadGemmK(size_t gemm_m,
+                                     size_t gemm_n,
+                                     size_t gemm_k,
+                                     const std::vector<std::tuple<int, int, int>>& tile_list)
+{
+    int m_per_block, n_per_block, k_per_block;
+    bool found = false;
+
+    // find exact divide
+    for(const auto& tile : tile_list)
+    {
+        int mpb, npb, kpb;
+        std::tie(mpb, npb, kpb) = tile;
+        if(gemm_m % mpb == 0 && gemm_n % npb == 0 && gemm_k % kpb == 0)
+        {
+            m_per_block = mpb;
+            n_per_block = npb;
+            k_per_block = kpb;
+            found       = true;
+            break;
+        }
+    }
+
+    if(!found)
+    {
+        size_t min_pad_pixel = std::numeric_limits<std::size_t>::max();
+        int mpb_pad          = 0;
+        int npb_pad          = 0;
+        // first try gemm_m, gemm_n padding
+        for(const auto& tile : tile_list)
+        {
+            int mpb, npb, kpb;
+            std::tie(mpb, npb, kpb) = tile;
+            if(gemm_k % kpb != 0)
+                continue;
+            size_t cur_pad_pixel = ComputeMatrixPadSize(gemm_m, mpb, gemm_k, kpb) +
+                                   ComputeMatrixPadSize(gemm_n, npb, gemm_k, kpb) +
+                                   ComputeMatrixPadSize(gemm_m, mpb, gemm_n, npb);
+            if(cur_pad_pixel < min_pad_pixel)
+            {
+                min_pad_pixel = cur_pad_pixel;
+                mpb_pad       = mpb;
+                npb_pad       = npb;
+            }
+        }
+
+        // second, we need find the max k_per_block among the same mpb/npb per block
+        for(const auto& tile : tile_list)
+        {
+            int mpb, npb, kpb;
+            std::tie(mpb, npb, kpb) = tile;
+            if(mpb == mpb_pad && npb == npb_pad)
+            {
+                if(gemm_k % kpb == 0)
+                {
+                    m_per_block = mpb;
+                    n_per_block = npb;
+                    k_per_block = kpb;
+                    found       = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if(found)
+        return std::make_tuple(m_per_block, n_per_block, k_per_block);
+    else
+        return std::make_tuple(0, 0, 0);
+}
+
+template <int L, int H>
+inline static bool IsLinear(const int v)
+{
+    static_assert(L <= H, "L <= H");
+    return L <= v && v <= H;
+}
+
+template <int L, int H>
+inline static bool NextLinear(int& v)
+{
+    assert((IsLinear<L, H>(v)));
+    if(H == v)
+    {
+        v = L;
+        return true;
+    }
+    ++v;
+    return false;
+}
+
 } // namespace solver
 } // namespace miopen
 #endif
---
+++ src/solver/conv_asm_implicit_gemm_gtc_perf_config.cpp
@@ -0,0 +1,261 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2021 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+#include <miopen/solver.hpp>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
+namespace miopen {
+namespace solver {
+
+PerformanceConfigAsmImplicitGemmGTC::PerformanceConfigAsmImplicitGemmGTC(
+    std::string dir,
+    std::string layout,
+    std::string prec,
+    int b,
+    int e,
+    int mpb,
+    int npb,
+    int kpb,
+    int wtm,
+    int wtn,
+    int wtk,
+    int wsm,
+    int wsn,
+    int wrm,
+    int wrn,
+    int mh,
+    int vs,
+    int gks,
+    int me,
+    int pta,
+    std::initializer_list<int> ta_t,
+    std::initializer_list<int> ta_c,
+    std::initializer_list<int> tb_t,
+    std::initializer_list<int> tb_c,
+    bool spare)
+    : direction(dir),
+      tensor_layout(layout),
+      precision(prec),
+      nxb(b),
+      nxe(e),
+
+      gemm_m_per_block(mpb),
+      gemm_n_per_block(npb),
+      gemm_k_per_block(kpb),
+
+      wave_tile_m(wtm),
+      wave_tile_n(wtn),
+      wave_tile_k(wtk),
+      wave_step_m(wsm),
+      wave_step_n(wsn),
+      wave_repeat_m(wrm),
+      wave_repeat_n(wrn),
+      multihead(mh),
+      vector_store(vs),
+      gemm_k_global_split(gks),
+      merge_e(me),
+      tensor_a_pass_through(pta),
+      tensor_a_thread_lengths(ta_t),
+      tensor_a_cluster_lengths(ta_c),
+      tensor_b_thread_lengths(tb_t),
+      tensor_b_cluster_lengths(tb_c),
+      use_spare_set(spare),
+      index(0)
+{
+}
+
+void PerformanceConfigAsmImplicitGemmGTC::HeuristicInit(const ConvolutionContext& ctx)
+{
+    // need override in child struct
+    (void)ctx;
+}
+bool PerformanceConfigAsmImplicitGemmGTC::SetNextValue()
+{
+    // need override in child struct
+    return false;
+}
+bool PerformanceConfigAsmImplicitGemmGTC::IsValidValue() const
+{
+    // need override in child struct
+    return false;
+}
+bool PerformanceConfigAsmImplicitGemmGTC::IsValid(const ConvolutionContext& ctx) const
+{
+    // need override in child struct
+    (void)ctx;
+    return false;
+}
+bool PerformanceConfigAsmImplicitGemmGTC::IsDefaultConstructed() const
+{
+    int default_lengths[4] = {1, 1, 1, 1};
+    // clang-format off
+    return direction == "fwd"
+        && tensor_layout == "nchw"
+        && precision == "fp32"
+        && nxb == 1
+        && nxe == 1
+        && gemm_m_per_block == 1
+        && gemm_n_per_block == 1
+        && gemm_k_per_block == 1
+        && wave_tile_m == 1
+        && wave_tile_n == 1
+        && wave_tile_k == 1
+        && wave_step_m == 1
+        && wave_step_n == 1
+        && wave_repeat_m == 1
+        && wave_repeat_n == 1
+        && multihead == 1
+        && vector_store == 1
+        && gemm_k_global_split == 1
+        && merge_e == 1
+        && tensor_a_pass_through == 1
+        && std::equal(std::begin(tensor_a_thread_lengths),  std::end(tensor_a_thread_lengths),  std::begin(default_lengths))
+        && std::equal(std::begin(tensor_a_cluster_lengths), std::end(tensor_a_cluster_lengths), std::begin(default_lengths))
+        && std::equal(std::begin(tensor_b_thread_lengths),  std::end(tensor_b_thread_lengths),  std::begin(default_lengths))
+        && std::equal(std::begin(tensor_b_cluster_lengths), std::end(tensor_b_cluster_lengths), std::begin(default_lengths))
+        && index == 0;
+    // clang-format on
+}
+
+bool PerformanceConfigAsmImplicitGemmGTC::
+operator==(const PerformanceConfigAsmImplicitGemmGTC& other) const
+{
+    // clang-format off
+    return direction == other.direction
+        && tensor_layout == other.tensor_layout
+        && precision == other.precision
+        && nxb == other.nxb
+        && nxe == other.nxe
+        && gemm_m_per_block == other.gemm_m_per_block
+        && gemm_n_per_block == other.gemm_n_per_block
+        && gemm_k_per_block == other.gemm_k_per_block
+        && wave_tile_m == other.wave_tile_m
+        && wave_tile_n == other.wave_tile_n
+        && wave_tile_k == other.wave_tile_k
+        && wave_step_m == other.wave_step_m
+        && wave_step_n == other.wave_step_n
+        && wave_repeat_m == other.wave_repeat_m
+        && wave_repeat_n == other.wave_repeat_n
+        && multihead == other.multihead
+        && vector_store == other.vector_store
+        && ((gemm_k_global_split == 0 && other.gemm_k_global_split == 0)
+            || (gemm_k_global_split > 0 && other.gemm_k_global_split > 0))
+        && merge_e == other.merge_e
+        && tensor_a_pass_through == other.tensor_a_pass_through
+        && std::equal(std::begin(tensor_a_thread_lengths),  std::end(tensor_a_thread_lengths),  std::begin(other.tensor_a_thread_lengths))
+        && std::equal(std::begin(tensor_a_cluster_lengths), std::end(tensor_a_cluster_lengths), std::begin(other.tensor_a_cluster_lengths))
+        && std::equal(std::begin(tensor_b_thread_lengths),  std::end(tensor_b_thread_lengths),  std::begin(other.tensor_b_thread_lengths))
+        && std::equal(std::begin(tensor_b_cluster_lengths), std::end(tensor_b_cluster_lengths), std::begin(other.tensor_b_cluster_lengths));
+    // clang-format on
+}
+void PerformanceConfigAsmImplicitGemmGTC::CopyParameters(
+    const PerformanceConfigAsmImplicitGemmGTC& other)
+{
+    // only copy parameters except spare/index, in case we break the search state
+    direction             = other.direction;
+    tensor_layout         = other.tensor_layout;
+    precision             = other.precision;
+    nxb                   = other.nxb;
+    nxe                   = other.nxe;
+    gemm_m_per_block      = other.gemm_m_per_block;
+    gemm_n_per_block      = other.gemm_n_per_block;
+    gemm_k_per_block      = other.gemm_k_per_block;
+    wave_tile_m           = other.wave_tile_m;
+    wave_tile_n           = other.wave_tile_n;
+    wave_tile_k           = other.wave_tile_k;
+    wave_step_m           = other.wave_step_m;
+    wave_step_n           = other.wave_step_n;
+    wave_repeat_m         = other.wave_repeat_m;
+    wave_repeat_n         = other.wave_repeat_n;
+    multihead             = other.multihead;
+    vector_store          = other.vector_store;
+    gemm_k_global_split   = other.gemm_k_global_split;
+    merge_e               = other.merge_e;
+    tensor_a_pass_through = other.tensor_a_pass_through;
+    std::copy(std::begin(other.tensor_a_thread_lengths),
+              std::end(other.tensor_a_thread_lengths),
+              std::begin(tensor_a_thread_lengths));
+    std::copy(std::begin(other.tensor_a_cluster_lengths),
+              std::end(other.tensor_a_cluster_lengths),
+              std::begin(tensor_a_cluster_lengths));
+    std::copy(std::begin(other.tensor_b_thread_lengths),
+              std::end(other.tensor_b_thread_lengths),
+              std::begin(tensor_b_thread_lengths));
+    std::copy(std::begin(other.tensor_b_cluster_lengths),
+              std::end(other.tensor_b_cluster_lengths),
+              std::begin(tensor_b_cluster_lengths));
+}
+
+std::string PerformanceConfigAsmImplicitGemmGTC::ToString() const
+{
+    std::ostringstream ss;
+    ss << ToKernelName();
+    if(gemm_k_global_split != 0)
+        ss << "[" << gemm_k_global_split << "]";
+    return ss.str();
+}
+std::string PerformanceConfigAsmImplicitGemmGTC::ToKernelName() const
+{
+    std::ostringstream kernel_name;
+    std::string kernel_precision = precision;
+    kernel_name << "igemm_" << direction << "_gtcx_" << tensor_layout << "_" << kernel_precision
+                << "_bx" << nxb << "_ex" << nxe << "_bt" << gemm_m_per_block << "x"
+                << gemm_n_per_block << "x" << gemm_k_per_block << "_wt" << wave_tile_m << "x"
+                << wave_tile_n << "x" << wave_tile_k << "_ws" << wave_step_m << "x" << wave_step_n
+                << "_wr" << wave_repeat_m << "x" << wave_repeat_n << "_ta"
+                << tensor_a_thread_lengths[0] << "x" << tensor_a_thread_lengths[1] << "x"
+                << tensor_a_thread_lengths[2] << "x" << tensor_a_thread_lengths[3] << "_"
+                << tensor_a_cluster_lengths[0] << "x" << tensor_a_cluster_lengths[1] << "x"
+                << tensor_a_cluster_lengths[2] << "x" << tensor_a_cluster_lengths[3] << "_tb"
+                << tensor_b_thread_lengths[0] << "x" << tensor_b_thread_lengths[1] << "x"
+                << tensor_b_thread_lengths[2] << "x" << tensor_b_thread_lengths[3] << "_"
+                << tensor_b_cluster_lengths[0] << "x" << tensor_b_cluster_lengths[1] << "x"
+                << tensor_b_cluster_lengths[2] << "x" << tensor_b_cluster_lengths[3];
+
+    if(tensor_a_pass_through != 0)
+        kernel_name << "_pta";
+    if(multihead != 0)
+        kernel_name << "_mh";
+    if(merge_e != 0)
+        kernel_name << "_me";
+    if(vector_store != 0)
+        kernel_name << "_vs" + std::to_string(vector_store);
+    if(gemm_k_global_split != 0)
+        kernel_name << "_gkgs";
+
+    return kernel_name.str();
+}
+int PerformanceConfigAsmImplicitGemmGTC::BlockSize() const
+{
+    return std::accumulate(std::begin(tensor_a_cluster_lengths),
+                           std::end(tensor_a_cluster_lengths),
+                           1,
+                           std::multiplies<int>());
+}
+
+} // namespace solver
+} // namespace miopen
