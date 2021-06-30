62fe9efbf5dee39fe9f2fdbb5084b171c0070d2f
carlushuang <carlus.huang@amd.com>
2021-07-01 07:04:09 +0800

    [HOTFIX][MI100][OCL] Remove unsed kernel arg for wrw fp32 NCHW (#1013)
    * Remove unused kernel arg for wrw fp32 xdlops NCHW
    * ConvAsmImplicitGemmGTCDynamicWrwXdlops: fix metadata in igemm_wrw_gtc_gfx908.s
    * Add regression tests
    * Re-target OpenCL smoke tests from Vega to gfx908. Enable regression test during Smoke test stages.
    
    Co-authored-by: Artem Tamazov <artem.tamazov@gmail.com>
    
    [ROCm/MIOpen commit: c812a33784b4081e0e733ece5632648dc4f8deb0]

--- src/solver/conv_asm_implicit_gemm_wrw_gtc_dynamic_xdlops.cpp
+++ src/solver/conv_asm_implicit_gemm_wrw_gtc_dynamic_xdlops.cpp
@@ -488,8 +488,11 @@ ComputeDynamicIGemmWrwKernelArgs(const conv::ProblemDescription& conv_problem,
     opArgs.emplace_back(y);
     opArgs.emplace_back(x);
     opArgs.emplace_back(log2_gemm_k_global_splits);
-    opArgs.emplace_back(group);
-    opArgs.emplace_back(ho_padded);
+    if(conv_problem.IsFp16())
+    {
+        opArgs.emplace_back(group);
+        opArgs.emplace_back(ho_padded);
+    }
 
     return opArgs;
 }
