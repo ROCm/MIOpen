95f34a34287e95b797100b1669ac5d1860ee5e79
yiqian1 <68618497+yiqian1@users.noreply.github.com>
2021-07-28 17:07:06 -0500

    [MLIR] Remove gemm size checks from ConvMlirIgemmBwd and ConvMlirIgemmBwdXdlops (#1016)
    [ROCm/MIOpen commit: ade84b5221b9100ac1f4286291927ea64ff838bd]

--- src/solver/conv_mlir_igemm_bwd_xdlops.cpp
+++ src/solver/conv_mlir_igemm_bwd_xdlops.cpp
@@ -39,27 +39,6 @@ namespace solver {
 
 namespace {
 #if MIOPEN_USE_MLIR
-std::tuple<int, int, int> CalculateGemmSize(const ConvolutionContext& ctx)
-{
-    const size_t g  = ConvolutionContextInterpreter::GetGroupCountG(ctx);
-    const size_t n  = ConvolutionContextInterpreter::GetBatchN(ctx);
-    const size_t k  = ConvolutionContextInterpreter::GetOutputChannelK(ctx);
-    const size_t c  = ConvolutionContextInterpreter::GetInputChannelC(ctx);
-    const size_t ho = ConvolutionContextInterpreter::GetOutputHeightHo(ctx);
-    const size_t wo = ConvolutionContextInterpreter::GetOutputWidthWo(ctx);
-    const size_t y  = ConvolutionContextInterpreter::GetFilterHeightY(ctx);
-    const size_t x  = ConvolutionContextInterpreter::GetFilterWidthX(ctx);
-
-    const auto k_per_group = k / g;
-    const auto c_per_group = c / g;
-
-    const auto gemm_m       = c_per_group * y * x;
-    const auto gemm_n       = n * ho * wo;
-    const auto gemm_k_total = k_per_group;
-
-    return std::make_tuple(gemm_m, gemm_n, gemm_k_total);
-}
-
 std::string GetKernelName()
 {
     std::string version   = "_v4r1";
@@ -81,15 +60,6 @@ bool ConvMlirIgemmBwdXdlops::IsApplicable(const ConvolutionContext& ctx) const
     if(!ctx.direction.IsBackwardData())
         return false;
     if(!IsComposableKernelSupportedHardware(ctx))
-        return false;
-
-    int gemm_m = 0;
-    int gemm_n = 0;
-    int gemm_k = 0;
-
-    std::tie(gemm_m, gemm_n, gemm_k) = CalculateGemmSize(ctx);
-
-    if(!IsValidGridGemmXdlops(gemm_m, gemm_n, gemm_k))
         return false;
 
     return MiirIsConfigApplicable(
