b31e66117dcdf31dacaee31a592cc7580b01e51c
Chao Liu <chao.liu2@amd.com>
2021-08-10 22:19:13 +0000

    update CK build script

--- src/composable_kernel/script/cmake-rocm.sh
+++ src/composable_kernel/script/cmake-rocm.sh
@@ -3,40 +3,16 @@ rm -f CMakeCache.txt
 rm -f *.cmake
 rm -rf CMakeFiles
 
-MY_PROJECT_SOURCE=../../../
+MY_PROJECT_SOURCE=../../..
 MY_PROJECT_INSTALL=../install.dir
 
 cmake                                                                                                                                          \
 -D CMAKE_INSTALL_PREFIX=${MY_PROJECT_INSTALL}                                                                                                  \
+-D HALF_INCLUDE_DIR="/root/workspace/external/half/include"                                                                                    \
+-D BUILD_DEV=ON                                                                                                                                \
 -D CMAKE_BUILD_TYPE=Release                                                                                                                    \
--D CMAKE_CXX_FLAGS="-DCK_AMD_GPU_GFX906 -O3 --amdgpu-target=gfx906 -mllvm --amdgpu-spill-vgpr-to-agpr=0 -gline-tables-only -save-temps=$PWD"   \
--D HIP_ONLINE_COMPILER_FLAGS="-DCK_AMD_GPU_GFX906"                                                                                             \
+-D CMAKE_CXX_FLAGS="-DCK_AMD_GPU_GFX908 -O3 --amdgpu-target=gfx908 -mllvm --amdgpu-spill-vgpr-to-agpr=0 -gline-tables-only -save-temps=$PWD"   \
 -D CMAKE_CXX_COMPILER=/opt/rocm/bin/hipcc                                                                                                      \
 -D CMAKE_PREFIX_PATH=/opt/rocm                                                                                                                 \
 -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON                                                                                                              \
 ${MY_PROJECT_SOURCE}
-
-#CXX_FLAG_TMP=-Weverything
-#            -Wno-c++98-compat \
-#            -Wno-c++98-compat-pedantic \
-#            -Wno-conversion \
-#            -Wno-double-promotion \
-#            -Wno-exit-time-destructors \
-#            -Wno-extra-semi \
-#            -Wno-float-conversion \
-#            -Wno-gnu-anonymous-struct \
-#            -Wno-gnu-zero-variadic-macro-arguments \
-#            -Wno-missing-noreturn \
-#            -Wno-missing-prototypes \
-#            -Wno-nested-anon-types \
-#            -Wno-padded \
-#            -Wno-return-std-move-in-c++11 \
-#            -Wno-shorten-64-to-32 \
-#            -Wno-sign-conversion \
-#            -Wno-unknown-warning-option \
-#            -Wno-unused-command-line-argument \
-#            -Wno-weak-vtables \
-#            -Wno-covered-switch-default \
-#            -Wno-disabled-macro-expansion \
-#            -Wno-undefined-reinterpret-cast
-
