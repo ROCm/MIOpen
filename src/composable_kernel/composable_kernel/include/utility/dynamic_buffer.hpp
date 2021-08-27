c07307c0dd1d3b94b140d3190999ebefb97d6a76
Qianfeng <qianfeng.zhang@amd.com>
2021-08-27 09:04:55 +0800

    [SWDEV-281541][MSRCHA-100] Implementation of Dynamic Generic Reduction  (#1108)
    * add solver ConvIgemmFwdV6r1DlopsNchwKcyxNkhw; rename static ck source files
    
    * make inner product compatible on gfx900
    
    * Update src/include/miopen/solver/ck_utility_common.hpp
    
    * compiler parameter use stream
    
    * use int instead of index_t in kernel wrapper
    
    * DynamicBuffer, StaticBuffer, amd_buffer_load support customized value for invalid element
    
    * Add dynamic generic reduction kernel layer (kernel wrappers, kernel implementations and utilities)
    
    * Some updates to dynamic composable kernel facility for the need of dynamic generic reduction
    
    * Update to generic reduction C++ host interface layer to support dynamic generic reduction
    
    * Update to remove tidy complaints in host interface layer
    
    * Change the unary operator form from void op(T &x) to T op(T x)
    
    * Update to pass single workspace pointer for all kernels (fix for OpenCL backend)
    
    * Use cppcheck-suppress to prevent some strange warnings
    
    * Re-use operator [] and () for DynamicBuffer and update to depending codes
    
    * Remove useless codes in first call threadwise/warpwise/blockwise kernel wrappers
    
    * [performance] Remove un-needed local buffer initialization
    
    Co-authored-by: Chao Liu <chao.liu2@amd.com>
    Co-authored-by: JD <Jehandad.Khan@amd.com>

--- src/composable_kernel/composable_kernel/include/utility/dynamic_buffer.hpp
+++ src/composable_kernel/composable_kernel/include/utility/dynamic_buffer.hpp
@@ -38,6 +38,10 @@ struct DynamicBuffer
         return BufferAddressSpace;
     }
 
+    __host__ __device__ constexpr const T& operator[](index_t i) const { return p_data_[i]; }
+
+    __host__ __device__ constexpr T& operator()(index_t i) { return p_data_[i]; }
+
     template <typename X,
               typename enable_if<
                   is_same<typename scalar_type<remove_cv_t<remove_reference_t<X>>>::type,
@@ -234,9 +238,14 @@ __host__ __device__ constexpr auto make_dynamic_buffer(T* p, ElementSpaceSize el
     return DynamicBuffer<BufferAddressSpace, T, ElementSpaceSize, true>{p, element_space_size};
 }
 
-template <AddressSpaceEnum_t BufferAddressSpace, typename T, typename ElementSpaceSize>
+template <
+    AddressSpaceEnum_t BufferAddressSpace,
+    typename T,
+    typename ElementSpaceSize,
+    typename X,
+    typename enable_if<is_same<remove_cvref_t<T>, remove_cvref_t<X>>::value, bool>::type = false>
 __host__ __device__ constexpr auto
-make_dynamic_buffer(T* p, ElementSpaceSize element_space_size, T invalid_element_value)
+make_dynamic_buffer(T* p, ElementSpaceSize element_space_size, X invalid_element_value)
 {
     return DynamicBuffer<BufferAddressSpace, T, ElementSpaceSize, false>{
         p, element_space_size, invalid_element_value};
