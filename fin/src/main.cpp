a717e5836405924d6e4aedadbcfb281b6a9f9221
Chris Erb <Christopher.erb@amd.com>
2021-09-17 18:46:59 +0000

    fin ptr syntax

--- fin/src/main.cpp
+++ fin/src/main.cpp
@@ -154,9 +154,9 @@ int main(int argc, char* argv[], char* envp[])
         }
         else
         {
-            f = new fin::ConvFin<float, float>();
-            dynamic_cast<fin::ConvFin<float,float>*>(f)->job["arch"] = command["arch"];
-            dynamic_cast<fin::ConvFin<float,float>*>(f)->job["num_cu"] = command["num_cu"];
+            f = std::make_unique<fin::ConvFin<float, float>>();
+            dynamic_cast<std::unique_ptr<fin::ConvFin<float, float>>>(f)->job["arch"] = command["arch"];
+            dynamic_cast<std::unique_ptr<fin::ConvFin<float, float>>>(f)->job["num_cu"] = command["num_cu"];
         }
 
         for(auto& step_it : command["steps"])
