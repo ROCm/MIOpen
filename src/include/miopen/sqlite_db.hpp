d7bbbc9bcce87160bb3699f9a8ed5d7418f55021
Vasilii Filippov <vfilippo@amd.com>
2021-09-28 16:04:04 +0300

    format

--- src/include/miopen/sqlite_db.hpp
+++ src/include/miopen/sqlite_db.hpp
@@ -212,7 +212,8 @@ class SQLiteBase
 {
     protected:
     public:
-    SQLiteBase(const std::string& filename_, bool is_system_) : filename(filename_), is_system(is_system_)
+    SQLiteBase(const std::string& filename_, bool is_system_)
+        : filename(filename_), is_system(is_system_)
     {
         if(DisableUserDbFileIO && !is_system)
             return;
