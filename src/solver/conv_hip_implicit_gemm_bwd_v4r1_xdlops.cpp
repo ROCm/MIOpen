c4437cd2dc430c0ca7fd07d5d0a9430be39be08b
Artem Tamazov <artem.tamazov@gmail.com>
2021-10-07 17:49:33 +0300

    [HOTFIX][WORKAROUND][MI100][FP32] W/A for SWDEV-305815 (issue #1206): Disable ConvHipImplicitGemmBwdDataV4R1Xdlops for FP32. (#1211)
    * [HOTFIX][WORKAROUND][MI100][FP32] W/A for SWDEV-305815 (issue #1206). Disable ConvHipImplicitGemmBwdDataV4R1Xdlops for FP32.
    
    * Added regression test for for SWDEV-305815 (issue #1206)
    
    [ROCm/MIOpen commit: baeeac30c471f72516467217301f9fe5fa6877fb]

--- src/solver/conv_hip_implicit_gemm_bwd_v4r1_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_bwd_v4r1_xdlops.cpp
@@ -30,6 +30,10 @@
 #include <miopen/solver/implicitgemm_util.hpp>
 #include <cstddef>
 
+/// Disable ConvHipImplicitGemmBwdDataV4R1Xdlops for FP32 by default.
+/// \ref https://github.com/ROCmSoftwarePlatform/MIOpen/issues/1206.
+#define WORKAROUND_ISSUE_1206 1
+
 MIOPEN_DECLARE_ENV_VAR(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_BWD_V4R1_XDLOPS)
 
 namespace miopen {
@@ -804,8 +808,21 @@ ConvHipImplicitGemmBwdDataV4R1Xdlops::CalculateGemmSize(const ConvolutionContext
 
 bool ConvHipImplicitGemmBwdDataV4R1Xdlops::IsApplicable(const ConvolutionContext& ctx) const
 {
+#if WORKAROUND_ISSUE_1206
+    if(ctx.IsFp32())
+    {
+        if(!miopen::IsEnabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_BWD_V4R1_XDLOPS{}))
+            return false;
+    }
+    else
+    {
+        if(miopen::IsDisabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_BWD_V4R1_XDLOPS{}))
+            return false;
+    }
+#else
     if(miopen::IsDisabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_BWD_V4R1_XDLOPS{}))
         return false;
+#endif
     if(ctx.skip_solutions_that_take_long_time_to_build_and_have_narrow_coverage)
         return false;
     if(!IsComposableKernelSupportedHardware(ctx))
