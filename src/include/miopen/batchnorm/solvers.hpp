75fa1abafd314359ef10cfa5ca1f0c7d81c56101
Vasilii Filippov <DrizztDoUrden@users.noreply.github.com>
2021-11-02 21:37:48 +0300

    [NFC] Bnorm inference solver (#1234)
    * Bnorm inference solver
    
    * Fixed incorrect caching
    
    [ROCm/MIOpen commit: 36807f4653b1634a483160d8cb78c81b4d9f6e27]

--- src/include/miopen/batchnorm/solvers.hpp
+++ src/include/miopen/batchnorm/solvers.hpp
@@ -151,6 +151,24 @@ struct BnBwdTrainingPerActivation : public SolverBase<OldStyleProblemDescription
                              const miopen::batchnorm::ProblemDescription& problem) const;
 };
 
+struct BnFwdInference : public SolverBase<OldStyleProblemDescription>
+{
+    inline bool IsApplicable(const OldStyleProblemDescription& problem) const
+    {
+        return IsApplicable(*std::get<0>(problem), *std::get<1>(problem));
+    }
+
+    inline ConvSolution GetSolution(const OldStyleProblemDescription& problem) const
+    {
+        return GetSolution(*std::get<0>(problem), *std::get<1>(problem));
+    }
+
+    bool IsApplicable(const ExecutionContext& context,
+                      const miopen::batchnorm::ProblemDescription& problem) const;
+    ConvSolution GetSolution(const ExecutionContext& context,
+                             const miopen::batchnorm::ProblemDescription& problem) const;
+};
+
 } // namespace batchnorm
 
 } // namespace solver
