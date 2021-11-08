9dc774bb4d0a5479e34a04a39958a4043923c66d
JD <Jehandad.Khan@amd.com>
2021-11-08 00:38:15 -0600

    Guard hip header for COMGR PCH (#1265)
    * Guard hip header for COMGR PCH

--- src/kernels/gpu_batched_transpose_kernel/batched_transpose.cpp
+++ src/kernels/gpu_batched_transpose_kernel/batched_transpose.cpp
@@ -23,8 +23,10 @@
  * SOFTWARE.
  *
  *******************************************************************************/
+#ifndef MIOPEN_DONT_USE_HIP_RUNTIME_HEADERS
 #include <hip/hip_runtime.h>
 #include <hip/hip_fp16.h>
+#endif
 
 #ifndef BATCHED_TRANSPOSE_OCCUPANCY
 #define BATCHED_TRANSPOSE_OCCUPANCY 4
