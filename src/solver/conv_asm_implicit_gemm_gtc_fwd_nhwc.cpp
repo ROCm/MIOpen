77c6cbc05410ddb46ffbb88f99366a9e336dbe95
carlushuang <carlus.huang@amd.com>
2021-11-08 10:06:45 -0600

    fix bug in asm igemm heuristic init, for fwd/bwd

--- src/solver/conv_asm_implicit_gemm_gtc_fwd_nhwc.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_fwd_nhwc.cpp
@@ -494,9 +494,7 @@ void PerformanceConfigAsmImplicitGemmGTCFwdXdlopsNHWC::HeuristicInit(const Convo
         gemm_k,
         ctx.IsFp32() ? tile_list_fp32 : (ctx.IsFp16() ? tile_list_fp16 : tile_list_bfp16));
 
-    if((m_per_block == 0 && n_per_block == 0 && k_per_block == 0) || not_support_vector_store)
-    {
-        // not found, let's try  gemm_k pad now.
+    auto find_with_gemm_k_pad = [&]() {
         const auto& config_list = GetFwdXdlopsNHWCConfigList();
         size_t min_pad_pixel    = std::numeric_limits<std::size_t>::max();
         size_t selected_index   = 0;
@@ -509,7 +507,8 @@ void PerformanceConfigAsmImplicitGemmGTCFwdXdlopsNHWC::HeuristicInit(const Convo
                 continue;
             if(!(config.tensor_a_thread_lengths[1] == 1 && config.tensor_b_thread_lengths[1] == 1))
                 continue;
-
+            // If we go here, then this is our last hope.
+            // This kind of kernel support any configs
             size_t cur_pad_pixel =
                 ComputeMatrixPadSize(
                     gemm_m, config.gemm_m_per_block, gemm_k, config.gemm_k_per_block) +
@@ -524,6 +523,12 @@ void PerformanceConfigAsmImplicitGemmGTCFwdXdlopsNHWC::HeuristicInit(const Convo
             }
         }
         CopyParameters(config_list[selected_index]);
+    };
+
+    if((m_per_block == 0 && n_per_block == 0 && k_per_block == 0) || not_support_vector_store)
+    {
+        // not found, let's try  gemm_k pad now.
+        find_with_gemm_k_pad();
     }
     else
     {
@@ -562,6 +567,8 @@ void PerformanceConfigAsmImplicitGemmGTCFwdXdlopsNHWC::HeuristicInit(const Convo
 
                 if((unit_conv && config.nxe == 0) || (!unit_conv && config.nxe != 0))
                 {
+                    if(!config.IsValid(ctx)) // last check before assigning a heuristic value
+                        continue;
                     CopyParameters(config);
                     if(need_k_split)
                     {
@@ -569,7 +576,8 @@ void PerformanceConfigAsmImplicitGemmGTCFwdXdlopsNHWC::HeuristicInit(const Convo
                                MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_ASM_PK_ATOMIC_ADD_FP16{}))
                             if(ctx.IsFp16() && gks > 0)
                                 vector_store = 1;
-                        gemm_k_global_split = static_cast<int>(gks);
+                        if(gks > 0)
+                            gemm_k_global_split = static_cast<int>(gks);
                     }
                     return;
                 }
@@ -577,8 +585,8 @@ void PerformanceConfigAsmImplicitGemmGTCFwdXdlopsNHWC::HeuristicInit(const Convo
                     continue;
             }
         }
-        MIOPEN_LOG_E("can't find a suitable heuristic config");
-        MIOPEN_THROW(miopenStatusInternalError);
+        // last try
+        find_with_gemm_k_pad();
     }
 }
 
