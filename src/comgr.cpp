3260a724afabc797b80311971589d1fa2d1d2d38
Artem Tamazov <artem.tamazov@gmail.com>
2021-11-29 19:48:27 +0300

    comgr-default-2(13) Fix comgr-default-2(11)

--- src/comgr.cpp
+++ src/comgr.cpp
@@ -51,7 +51,7 @@
 /// Correctness problems on MI200 with base driver 5.11.14 (~ROCm 4.3.1).
 /// With base driver 5.11.32 the errors disappear.
 /// More info at https://github.com/ROCmSoftwarePlatform/MIOpen/issues/1257.
-#define WORKAROUND_ISSUE_1257 (HIP_PACKAGE_VERSION_FLAT > 4003021331)
+#define WORKAROUND_ISSUE_1257 (HIP_PACKAGE_VERSION_FLAT >= 4003021331)
 
 MIOPEN_DECLARE_ENV_VAR(MIOPEN_DEBUG_COMGR_LOG_CALLS)
 MIOPEN_DECLARE_ENV_VAR(MIOPEN_DEBUG_COMGR_LOG_SOURCE_NAMES)
