7b71403d4be47ea85b7d7dce82c0fd7556a7706a
Artem Tamazov <artem.tamazov@gmail.com>
2021-12-03 01:17:44 +0300

    gfx90a-disable-nonfp16alt-leftover-01(01) ConvCkIgemmFwdV6r1DlopsNchw

--- src/solver/conv_ck_igemm_fwd_v6r1_dlops_nchw.cpp
+++ src/solver/conv_ck_igemm_fwd_v6r1_dlops_nchw.cpp
@@ -97,6 +97,8 @@ bool ConvCkIgemmFwdV6r1DlopsNchw::IsApplicable(const ConvolutionContext& ctx) co
         return false;
     if(ctx.group_counts != 1)
         return false;
+    if(name == "gfx90a" && ctx.conv_problem.IsGfx90aFp16altRequired())
+        return false;
 
     {
         // this kernel use int32_t for memory offset, which covers 2GB of memory maximum
