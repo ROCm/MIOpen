dfd8a415598afc3338bf627b62d1578eb34654a9
shaojiewang <wsjmessi@163.com>
2021-12-03 23:21:17 +0800

    add some comments

--- src/solver/conv_asm_implicit_gemm_gtc_wrw_nhwc.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_wrw_nhwc.cpp
@@ -904,7 +904,7 @@ ConvAsmImplicitGemmGTCDynamicWrwXdlopsNHWC::GetWorkspaceSize(const ConvolutionCo
         if(!trans_output.IsSkippable())
             workspace_size += trans_output.GetSize();
 
-        // need to do 4 bytes alignment to do atomic add
+        // 4 bytes alignment to do atomic add
         workspace_size = ((workspace_size + 3) >> 2) << 2;
     }
 
