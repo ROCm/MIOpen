12c7991f30d4b8d915140053084afdcd32806631
Artem Tamazov <artem.tamazov@gmail.com>
2021-12-18 04:09:40 +0300

    [HOTFIX][Navi21][SWDEV-292187] Do not enforce WAVE64 mode for all OpenCL kernels by default. Restored W/A for #1148. (#1344)
    * [HOTFIX][Navi21][SWDEV-292187] Do not enforce WAVE64 mode for all OpenCL kernels by default.

--- src/ocl/clhelper.cpp
+++ src/ocl/clhelper.cpp
@@ -211,7 +211,7 @@ ClProgramPtr LoadProgram(cl_context ctx,
     else // OpenCL programs.
     {
         ClProgramPtr result{CreateProgram(ctx, source.data(), source.size())};
-        if(!miopen::IsDisabled(MIOPEN_DEBUG_OPENCL_WAVE64_NOWGP{}))
+        if(miopen::IsEnabled(MIOPEN_DEBUG_OPENCL_WAVE64_NOWGP{}))
             params += " -Wf,-mwavefrontsize64 -Wf,-mcumode";
 #if MIOPEN_BUILD_DEV
         params += " -Werror";
