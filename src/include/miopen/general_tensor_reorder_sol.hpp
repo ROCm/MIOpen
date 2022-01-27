7802205a1a4bcfc63c7b167ee103c4b82487cd45
aska-0096 <haocwang@amd.com>
2022-01-27 15:38:48 +0800

    fix bug

--- src/include/miopen/general_tensor_reorder_sol.hpp
+++ src/include/miopen/general_tensor_reorder_sol.hpp
@@ -314,12 +314,12 @@ std::string GeneralReorderSolution<dst_order>::GetKernelName() const
 {
     std::size_t data_size = miopen::GetTypeSize(data_type);
     std::ostringstream kernel_name;
-    std::string type_trait = GetNameTrait(data_size);
-    kernel_name << "general_4d_reorder_" << kparam->tile_x << "x" << kparam->tile_y << "_";
-    if(!(kparam->pack_x == 1 && kparam->pack_y == 1 && kparam->ediv_x == 1 && kparam->ediv_y == 1))
+    std::string type_trait = tensor_reorder::GetNameTrait(data_size);
+    kernel_name << "general_4d_reorder_" << kernel_param_heuristic->tile_x << "x" << kernel_param_heuristic->tile_y << "_";
+    if(!(kernel_param_heuristic->pack_x == 1 && kernel_param_heuristic->pack_y == 1 && kernel_param_heuristic->ediv_x == 1 && kernel_param_heuristic->ediv_y == 1))
     {
-        kernel_name << "pack_" << kparam->pack_x << "x" << kparam->pack_y << "_ediv_"
-                    << kparam->ediv_x << "x" << kparam->ediv_y << "_";
+        kernel_name << "pack_" << kernel_param_heuristic->pack_x << "x" << kernel_param_heuristic->pack_y << "_ediv_"
+                    << kernel_param_heuristic->ediv_x << "x" << kernel_param_heuristic->ediv_y << "_";
     }
     kernel_name << type_trait<<"_r"<<dst_order::at(0)<<dst_order::at(1)<<dst_order::at(2)<<dst_order::at(3);
     return kernel_name.str();
