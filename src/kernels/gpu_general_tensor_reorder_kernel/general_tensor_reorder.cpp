3196935f4a07dfb2f6a155d67ba8cc41793b7add
aska-0096 <haocwang@amd.com>
2022-01-29 01:34:05 +0800

    fixbug try

--- src/kernels/gpu_general_tensor_reorder_kernel/general_tensor_reorder.cpp
+++ src/kernels/gpu_general_tensor_reorder_kernel/general_tensor_reorder.cpp
@@ -23,8 +23,10 @@
  * SOFTWARE.
  *
  *******************************************************************************/
+#ifndef MIOPEN_DONT_USE_HIP_RUNTIME_HEADERS
 #include <hip/hip_runtime.h>
 #include <hip/hip_fp16.h>
+#endif
 #include "order.hpp"
 
 #ifndef TENSOR_REORDER_OCCUPANCY
