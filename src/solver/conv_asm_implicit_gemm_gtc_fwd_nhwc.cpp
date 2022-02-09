2b3f033343d2cb11c6849a26f2ff327c9f21633f
carlushuang <carlus.huang@amd.com>
2022-02-10 02:50:49 +0800

    [MI100][MI200] iGemm ASM GTC XDLOPS: Support for deterministic results (SWDEV-316705) (#1377)
    If `MIOPEN_DEBUG_CONVOLUTION_DETERMINISTIC` is set, then the iGemm ASM GTC XDLOPS Solvers produce only deterministic kernels.

--- src/solver/conv_asm_implicit_gemm_gtc_fwd_nhwc.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_fwd_nhwc.cpp
@@ -655,6 +655,9 @@ bool PerformanceConfigAsmImplicitGemmGTCFwdXdlopsNHWC::IsValid(const Convolution
         if(ctx.IsFp16() && gemm_k_global_split != 0 && vector_store != 1)
             return false;
 
+    if(miopen::IsEnabled(MIOPEN_DEBUG_CONVOLUTION_DETERMINISTIC{}) && gemm_k_global_split != 0)
+        return false;
+
     const auto& c         = ctx.n_inputs;
     const auto& k         = ctx.n_outputs;
     const auto& group     = ctx.group_counts;
