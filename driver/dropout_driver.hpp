35305ad6f7095a19d7f383d1a47692cb120b46cc
Jun Liu <Liu.Jun@amd.com>
2022-02-14 14:19:59 -0800

    [Quality] Fix more verify error with NaNs and Inf (#1408)

--- driver/dropout_driver.hpp
+++ driver/dropout_driver.hpp
@@ -451,9 +451,9 @@ int DropoutDriver<Tgpu, Tref>::VerifyForward()
     auto error = miopen::rms_range(outhost.data, out.data);
 
     const double tolerance = std::is_same<Tgpu, float16>{} ? 5e-4 : 1e-6;
-    if(!(error < tolerance))
+    if(!std::isfinite(error) || error > tolerance)
     {
-        std::cout << "Forward Dropout Failed: " << error << std::endl;
+        std::cout << "Forward Dropout FAILED: " << error << std::endl;
     }
     else
     {
@@ -471,9 +471,9 @@ int DropoutDriver<Tgpu, Tref>::VerifyBackward()
     auto error = miopen::rms_range(din_host.data, din.data);
 
     const double tolerance = std::is_same<Tgpu, float16>{} ? 5e-4 : 1e-6;
-    if(!(error < tolerance))
+    if(!std::isfinite(error) || error > tolerance)
     {
-        std::cout << "Backward Dropout Failed: " << error << std::endl;
+        std::cout << "Backward Dropout FAILED: " << error << std::endl;
     }
     else
     {
