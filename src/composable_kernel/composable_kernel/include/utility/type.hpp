b735eb264d10cf7abd5c9573eb0ce469d96eeb56
Artem Tamazov <artem.tamazov@gmail.com>
2022-02-16 11:44:10 +0300

    Enable HIPRTC support as default from ROCm 5.0 (#1237)

--- src/composable_kernel/composable_kernel/include/utility/type.hpp
+++ src/composable_kernel/composable_kernel/include/utility/type.hpp
@@ -4,6 +4,84 @@
 #include "integral_constant.hpp"
 #include "enable_if.hpp"
 
+#ifdef __HIPCC_RTC__
+#ifdef WORKAROUND_ISSUE_HIPRTC_TRUE_TYPE
+/// We need <type_traits> for std::remove_reference and std::remove_cv.
+/// But <type_traits> also defines std::true_type, per Standard.
+/// However the latter definition conflicts with
+/// /opt/rocm/include/hip/amd_detail/amd_hip_vector_types.h,
+/// which defines std::true_type as well (which is wrong).
+
+namespace std {
+
+template <class T>
+struct remove_reference
+{
+    typedef T type;
+};
+template <class T>
+struct remove_reference<T&>
+{
+    typedef T type;
+};
+template <class T>
+struct remove_reference<T&&>
+{
+    typedef T type;
+};
+
+template <class T>
+using remove_reference_t = typename remove_reference<T>::type;
+
+template <class T>
+struct remove_const
+{
+    typedef T type;
+};
+template <class T>
+struct remove_const<const T>
+{
+    typedef T type;
+};
+
+template <class T>
+struct remove_volatile
+{
+    typedef T type;
+};
+template <class T>
+struct remove_volatile<volatile T>
+{
+    typedef T type;
+};
+
+template <class T>
+struct remove_cv
+{
+    typedef typename remove_volatile<typename remove_const<T>::type>::type type;
+};
+
+template <class T>
+struct is_pointer_helper : std::false_type
+{
+};
+
+template <class T>
+struct is_pointer_helper<T*> : std::true_type
+{
+};
+
+template <class T>
+struct is_pointer : is_pointer_helper<typename std::remove_cv<T>::type>
+{
+};
+
+} // namespace std
+#else
+#include <type_traits> // std::remove_reference, std::remove_cv, is_pointer
+#endif
+#endif // __HIPCC_RTC__
+
 namespace ck {
 
 template <typename X, typename Y>
