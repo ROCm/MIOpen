0def60f2008b9863ffbdfea8a1c157303b2cf3d7
Jun Liu <Liu.Jun@amd.com>
2022-03-10 09:37:28 -0800

    fix clang format issue

--- src/buffer_info.cpp
+++ src/buffer_info.cpp
@@ -162,7 +162,10 @@ MultiBufferWorkspaceTraits::MultiBufferWorkspaceTraits(std::initializer_list<siz
     }
 }
 
-size_t MultiBufferWorkspaceTraits::GetSize() const { return v_offset[v_offset.size() - 2] + v_size.back(); }
+size_t MultiBufferWorkspaceTraits::GetSize() const
+{
+    return v_offset[v_offset.size() - 2] + v_size.back();
+}
 
 size_t MultiBufferWorkspaceTraits::GetOffset(size_t index) const
 {
