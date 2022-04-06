8e6a7f6ccb44a9f5286a3bd399ca976510e08959
jerryyin <zhuoryin@amd.com>
2022-04-06 19:05:18 +0000

    Querying all sub-kernels applicability

--- src/solver/conv_mlir_igemm_fwd.cpp
+++ src/solver/conv_mlir_igemm_fwd.cpp
@@ -91,7 +91,15 @@ bool PerformanceConvMlirIgemm::IsValid(const ConvolutionContext& ctx) const
     if(*this == MlirHeuristicInitRequest())
         return true;
 
-    return MiirIsConfigApplicable(mlir::ConstructBuildOptions(ctx, *this, false));
+    int kernel_count = MiirGetKernelCount(mlir::ConstructBuildOptions(ctx, false));
+    bool isValid     = false;
+    for(int kernel_id = 0; kernel_id < kernel_count; ++kernel_id)
+    {
+        isValid = MiirIsConfigApplicable(mlir::ConstructBuildOptions(ctx, *this, false, kernel_id));
+        if(!isValid)
+            return false;
+    }
+    return isValid;
 #else
     std::ignore = ctx;
     return false;
