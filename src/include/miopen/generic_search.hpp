52795e0e54ae58781aea22d93ffb86bb2a744367
Christopher Erb <christopher.erb@amd.com>
2022-05-18 18:22:09 +0000

    name change

--- src/include/miopen/generic_search.hpp
+++ src/include/miopen/generic_search.hpp
@@ -302,13 +302,13 @@ auto GetAllConfigs(const Solver s, const Context& context)
 {
     using PerformanceConfig = decltype(s.GetDefaultPerformanceConfig(context));
 
-    ComputedContainer<PerformanceConfig, Context> main(context);
-    const int main_size = std::distance(main.begin(), main.end());
+    ComputedContainer<PerformanceConfig, Context> primary(context);
+    const int main_size = std::distance(primary.begin(), primary.end());
     ComputedContainer<PerformanceConfig, Context> spare(context, true);
     const int spare_size = std::distance(spare.begin(), spare.end());
     const bool useSpare  = (main_size == 0);
 
-    ComputedContainer<PerformanceConfig, Context> all_configs = useSpare ? spare : main;
+    ComputedContainer<PerformanceConfig, Context> all_configs = useSpare ? spare : primary;
     const int n_runs_total                                    = useSpare ? spare_size : main_size;
     MIOPEN_LOG_W(s.SolverDbId() << ": Searching the best solution among " << n_runs_total
                                 << (useSpare ? " (spare)" : "") << "...");
