eefb5c18d8953adb00d4cd16641ee52d1bffb770
Jun Liu <Liu.Jun@amd.com>
2022-06-07 08:29:49 -0700

    [MIOpenKernels] fix names for MIOpen precompiled kernels (#1573)
    * [MIOpenKernels] fix names for MIOpen precompiled kernels
    
    * fix install precompilerd kernel script for gfx1*
    
    [ROCm/MIOpen commit: 830eac12d39d078caf8b25dc4393f5c5e881b25d]

--- utils/install_precompiled_kernels.sh
+++ utils/install_precompiled_kernels.sh
@@ -16,16 +16,23 @@ fi
 arches=$($ROCMINFO | grep -e ' gfx' -e 'Compute Unit:' | awk '/Name/{ arch= $2} /Compute Unit:/ {if(arch != "") { all_arches[(arch "-" $3)] }} END { for (a in all_arches) { print a}  }')
 
 while IFS= read -r line ; 
-do 
+do
+    arch=$(echo $line | awk -F"-" '{print $1}')
+    num_cu=$(echo $line | awk -F"-" '{print $2}')
+    if [[ $arch = gfx1* ]]; then
+        echo "using gfx1* architects ..."
+        num_cu=$((num_cu/2))
+    fi
+    package=$arch-$num_cu
     if [ -f /etc/redhat-release ]; then
-          echo sudo yum -y install "miopenkernels-${line}pkg"
-          $SUDO yum -y install --nogpgcheck "miopenkernels-${line}pkg"
+          echo sudo yum -y install "miopenkernels-${package}kdb"
+          $SUDO yum -y install --nogpgcheck "miopenkernels-${package}kdb"
     elif [ -f /etc/lsb-release ]; then
-          echo sudo apt install -y "miopenkernels-${line}pkg"
+          echo sudo apt install -y "miopenkernels-${package}kdb"
           $SUDO apt update
-          $SUDO apt install -y "miopenkernels-${line}pkg"
+          $SUDO apt install -y "miopenkernels-${package}kdb"
     else
         echo "Unknown distribution"
-        echo "Please install the miopenkernels-${line}pkg package using an appropriate package manager"
+        echo "Please install the miopenkernels-${package}kdb package using an appropriate package manager"
     fi
 done <<< "$arches"
