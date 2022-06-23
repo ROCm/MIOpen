7a811d505e9fe9ae9ce90dcd8cb1751dfaeb4dec
Haocong WANG <haocwang@amd.com>
2022-06-24 06:15:17 +0800

    [Solver & Kernel] Integration of MISA NCHWc fp16x4 and fp16x8 asm kernel (#1523)
    [ROCm/MIOpen commit: bb5a97d8edef35a639a1f7370cd7621df2a26c11]

--- src/find_controls.cpp
+++ src/find_controls.cpp
@@ -127,7 +127,9 @@ boost::optional<std::vector<solver::Id>> GetEnvFindOnlySolverImpl()
                              "Invalid value of MIOPEN_DEBUG_FIND_ONLY_SOLVER: " + kinder);
             const auto id = solver::Id{numeric_id};
             if(id.IsValid())
+            {
                 res.push_back(id);
+            }
             else
             {
                 MIOPEN_THROW(miopenStatusBadParm,
