44fbefb36122e985e0435d64cee50dd36df0f21a
Alex Bara <barai@mymail.vcu.edu>
2022-08-15 14:37:46 -0500

    Revert "updated copyright year to 2022"
    This reverts commit fba78227d8f8a2cc9d38f8fb2841ed5d03e54481.

--- src/kernels/conv7x7c3h224w224k64u2v2p3q3f1.s
+++ src/kernels/conv7x7c3h224w224k64u2v2p3q3f1.s
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2022-2019 Advanced Micro Devices, Inc.
+ * Copyright (c) 2017-2019 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
