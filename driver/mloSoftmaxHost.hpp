88bad0faac65cd4bfbafde9aed351fb5758f8952
Alex Bara <barai@mymail.vcu.edu>
2022-08-15 11:56:41 -0500

    updated copyright in /driver

--- driver/mloSoftmaxHost.hpp
+++ driver/mloSoftmaxHost.hpp
@@ -1,5 +1,5 @@
 /**********************************************************************
-Copyright (c)2017 Advanced Micro Devices, Inc. All rights reserved.
+Copyright (c)2022 Advanced Micro Devices, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted
 provided that the following conditions are met:
