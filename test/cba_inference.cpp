22631ede41979e5f58208a93d0acb8d098cd49c8
Alex Bara <barai@mymail.vcu.edu>
2022-08-15 14:18:29 -0500

    updated /test

--- test/cba_inference.cpp
+++ test/cba_inference.cpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2017 Advanced Micro Devices, Inc.
+ * Copyright (c) 2022 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
