84d4399bc5e8d33ee8a2fbf2370154c8f629645e
Chris Erb <chris.erb65@gmail.com>
2022-08-19 01:38:27 -0500

    [Enhancement][Fin] Enhance test for pdb records (#1669)
    * add perf cfg validity test to TestSysDbRecord
    
    * remove debug prints
    
    * removing invalid entries from all perf dbs
    
    * VACUUM sqlite
    
    * Squashed 'fin/' changes from 53d2563fe..30d699b9e
    
    30d699b9e Perf Eval Update (#60)
    3535b948c PerfCompile and PerfEval changes (#59)
    de79468d2 remove unneccessary solution check, add check for previously modified kernel names (#56)
    6924286a2 miopen hash update (#55)
    530399575 Refactor googletest infra to align with MIOpen (#53)
    71c50d146 Datatype fix for BN (#57)
    8abe2f5c6 Perf Eval updates, Add find info (#51)
    e1c1ef0f5 filter find compile by solver input (#54)
    722feea66 sp/chk precomp kernel 264 (#41)
    b9aba2034 Batch norm find compile (#50)
    359f3da80 Fix missing link directives in fin binary (#48)
    a4020c1ba Cache Miss Fixes (#46)
    2ec7ef44d Enable google test and compiling fin in the CI (#47)
    8b6b453bc Applicability support for batch norm (#45)
    44323aae9 Perf compile/eval for fin (#42)
    ebd9aa6bd update member name (#43)
    d6d798efe add cu count (#39)
    8e1989a9f Add find option for selecting only dynamic solvers (#38)
    0e164bf66 setting json version (#37)
    f3f7fed18 Remove function redefinition (#36)
    e1de51a58 Performance DB de-serialize test (#34)
    043cdcdaa Layout support in Fin (#33)
    3a1d58236 Hotfix (#32)
    ee3f0d543 4.4 Tuning Bugfixes (#31)
    832dbe234 Tunability Reporting (#27)
    a564a229f include gfx90a_110 (#28)
    
    git-subtree-dir: fin
    git-subtree-split: 30d699b9edc014c6076a9649f849bd3c4588d4ab
    
    * Squashed 'fin/' changes from 30d699b9e..ea5c844af
    
    ea5c844af fix direction test
    3aa412ee1 Update to use revised testSysDbRecord miopen function
    
    git-subtree-dir: fin
    git-subtree-split: ea5c844aff8b5d46537aa59034a596fd15cd9e1e
    
    * rename pipe step
    
    * Squashed 'fin/' changes from ea5c844af..c702cb968
    
    c702cb968 format
    
    git-subtree-dir: fin
    git-subtree-split: c702cb96800a03b17ee17d03a015dfa38e3883b9
    
    * Squashed 'fin/' changes from c702cb968..d5397abd3
    
    d5397abd3 rename targets
    
    git-subtree-dir: fin
    git-subtree-split: d5397abd37b6908bcd96ef750ea5a3ace04cdf3c
    
    * rename archive
    
    Co-authored-by: Jun Liu <Liu.Jun@amd.com>

--- fin/src/fin.cpp
+++ fin/src/fin.cpp
@@ -22,7 +22,7 @@ miopenDataType_t GetDataType<bfloat16>()
 {
     return miopenBFloat16;
 }
-[[gnu::noreturn]] void Fin::Usage()
+[[gnu::noreturn]] void BaseFin::Usage()
 {
     std::cout << "Usage: ./MIOpenFin *base_arg* *other_args*\n";
     std::cout << "Supported Base Arguments: conv[fp16][bfp16]\n";
@@ -37,7 +37,7 @@ void PadBufferSize(size_t& sz, int datatype_sz)
     }
 }
 
-std::string Fin::ParseBaseArg(const int argc, const char* argv[])
+std::string BaseFin::ParseBaseArg(const int argc, const char* argv[])
 {
     if(argc < 2)
     {
@@ -56,5 +56,52 @@ std::string Fin::ParseBaseArg(const int argc, const char* argv[])
         Usage();
     else
         return arg;
+}
+void BaseFin::InitNoGpuHandle(miopen::Handle& handle,
+                              const std::string& arch,
+                              const unsigned long num_cu)
+{
+#if MIOPEN_MODE_NOGPU
+    handle.impl->device_name        = arch;
+    handle.impl->num_cu             = num_cu;
+    handle.impl->max_mem_alloc_size = 32UL * 1024 * 1024 * 1024; // 32 GB
+    handle.impl->global_mem_size    = 32UL * 1024 * 1024 * 1024;
+    handle.impl->target_properties.Init(&handle);
+#else
+    std::ignore = handle;
+    std::ignore = arch;
+    std::ignore = num_cu;
+#endif
+}
+
+void BaseFin::VerifyDevProps(const std::string& in_arch, const unsigned long in_num_cu)
+{
+    std::cerr << "Verifying device properties" << std::endl;
+    std::string arch    = in_arch;
+    arch                = arch.substr(0, arch.find(':'));
+    const size_t num_cu = in_num_cu;
+    std::ignore         = num_cu;
+    if(arch == "gfx900")
+    {
+        assert(num_cu == 56 || num_cu == 64);
+    }
+    else if(arch == "gfx906")
+    {
+        assert(num_cu == 60 || num_cu == 64);
+    }
+    else if(arch == "gfx908")
+    {
+        assert(num_cu == 120);
+    }
+    else if(arch == "gfx1030")
+    {
+        assert(num_cu == 72 || num_cu == 36);
+    }
+    else if(arch == "gfx90a")
+    {
+        assert(num_cu == 110 || num_cu == 104);
+    }
+    else
+        throw std::runtime_error("Invalid Arch Name");
 }
 } // namespace fin
