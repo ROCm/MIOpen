78ea4b0f2e29fb99fcc47555c687ac1a36df5ca6
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2022-09-12 02:23:05 +0200

    [Solvers] ConvAsm7x7c3h224w224k64u2v2p3q3f1: split ConvolutionContext and ProblemDescription (stage 2) (#1710)

--- src/solver/conv_asm_7x7c3h224w224k64u2v2p3q3f1.cpp
+++ src/solver/conv_asm_7x7c3h224w224k64u2v2p3q3f1.cpp
@@ -37,24 +37,25 @@ MIOPEN_DECLARE_ENV_VAR(MIOPEN_DEBUG_CONV_DIRECT_ASM_7X7C3H224W224)
 namespace miopen {
 namespace solver {
 
-bool ConvAsm7x7c3h224w224k64u2v2p3q3f1::IsApplicable(const ConvolutionContext& params) const
+bool ConvAsm7x7c3h224w224k64u2v2p3q3f1::IsApplicable(const ExecutionContext& ctx,
+                                                     const ProblemDescription& problem) const
 {
     if(miopen::IsDisabled(MIOPEN_DEBUG_CONV_DIRECT_ASM_7X7C3H224W224{}))
         return false;
-    if(!params.use_asm_kernels)
+    if(!ctx.use_asm_kernels)
         return false;
-    if(!params.problem.Is2d())
+    if(!problem.Is2d())
         return false;
-    if(params.problem.IsAsymmetricPadH() || params.problem.IsAsymmetricPadW())
+    if(problem.IsAsymmetricPadH() || problem.IsAsymmetricPadW())
         return false;
-    if(!params.rmv.IsV2orV3())
+    if(!ctx.rmv.IsV2orV3())
         return false;
 
-    const auto target = params.GetStream().GetTargetProperties();
+    const auto target = ctx.GetStream().GetTargetProperties();
     if(target.Xnack() && *target.Xnack())
         return false;
 
-    const std::string name = params.GetStream().GetDeviceName();
+    const std::string name = ctx.GetStream().GetDeviceName();
 #if WORKAROUND_ISSUE_1146
     if(name == "gfx90a")
         return false;
@@ -64,47 +65,48 @@ bool ConvAsm7x7c3h224w224k64u2v2p3q3f1::IsApplicable(const ConvolutionContext& p
     {
         return false;
     }
-    if(!params.problem.direction.IsForward())
+    if(!problem.direction.IsForward())
     {
         return false;
     }
-    if(!params.problem.IsLayoutDefault())
+    if(!problem.IsLayoutDefault())
     {
         return false;
     }
 
     // clang-format off
-    return params.problem.pad_w == 3            // -q
-        && params.problem.pad_h == 3            // -p
-        && params.problem.kernel_stride_w == 2    // -v
-        && params.problem.kernel_stride_h == 2    // -u
-        && params.problem.kernel_size_w == 7    // -x
-        && params.problem.kernel_size_h == 7    // -y
-        && params.problem.kernel_dilation_w == 1
-        && params.problem.kernel_dilation_h == 1
-        && params.problem.n_inputs == 3         // -c
-        && params.problem.n_outputs == 64       // -k
-        && params.problem.in_width == 224       // -W
-        && params.problem.in_height == 224      // -H
-        && params.problem.IsFp32()
-        && params.problem.group_counts == 1
-        && params.problem.in_layout == "NCHW";
+    return problem.pad_w == 3            // -q
+        && problem.pad_h == 3            // -p
+        && problem.kernel_stride_w == 2    // -v
+        && problem.kernel_stride_h == 2    // -u
+        && problem.kernel_size_w == 7    // -x
+        && problem.kernel_size_h == 7    // -y
+        && problem.kernel_dilation_w == 1
+        && problem.kernel_dilation_h == 1
+        && problem.n_inputs == 3         // -c
+        && problem.n_outputs == 64       // -k
+        && problem.in_width == 224       // -W
+        && problem.in_height == 224      // -H
+        && problem.IsFp32()
+        && problem.group_counts == 1
+        && problem.in_layout == "NCHW";
         // && (isForwardDirection() ? _weights_layout == "KCHW" : _weights_layout == "CKHW" )
     // clang-format on
 }
 
-ConvSolution ConvAsm7x7c3h224w224k64u2v2p3q3f1::GetSolution(const ConvolutionContext& params) const
+ConvSolution ConvAsm7x7c3h224w224k64u2v2p3q3f1::GetSolution(const ExecutionContext& ctx,
+                                                            const ProblemDescription& problem) const
 {
     ConvSolution result;
-    const int out_w = (params.problem.in_width + params.problem.pad_w * 2 +
-                       params.problem.kernel_stride_w - params.problem.kernel_size_w) /
-                      params.problem.kernel_stride_w; // (inp_w + 2*pad_w + inp_v - wei_w) / inp_v
-    const int out_h = (params.problem.in_height + params.problem.pad_h * 2 +
-                       params.problem.kernel_stride_h - params.problem.kernel_size_h) /
-                      params.problem.kernel_stride_h; // (inp_h + 2*pad_h + inp_u - wei_h) / inp_u
+    const int out_w =
+        (problem.in_width + problem.pad_w * 2 + problem.kernel_stride_w - problem.kernel_size_w) /
+        problem.kernel_stride_w; // (inp_w + 2*pad_w + inp_v - wei_w) / inp_v
+    const int out_h =
+        (problem.in_height + problem.pad_h * 2 + problem.kernel_stride_h - problem.kernel_size_h) /
+        problem.kernel_stride_h; // (inp_h + 2*pad_h + inp_u - wei_h) / inp_u
 
     std::ostringstream options;
-    GenerateClangDefsym(options, "ROCM_METADATA_VERSION", params.rmv.UseV3() ? 5 : 4);
+    GenerateClangDefsym(options, "ROCM_METADATA_VERSION", ctx.rmv.UseV3() ? 5 : 4);
     KernelInfo constr_params;
     constr_params.comp_options = options.str();
 
@@ -114,8 +116,8 @@ ConvSolution ConvAsm7x7c3h224w224k64u2v2p3q3f1::GetSolution(const ConvolutionCon
 
     // global-work = [align(out_w,64), (align(out_h,4)/4)*align(wei_k/2,8), batch_n]
     constr_params.g_wk.push_back(AlignUp(out_w, 64));
-    constr_params.g_wk.push_back(AlignUp(out_h, 4) / 4 * AlignUp(params.problem.n_outputs / 2, 8));
-    constr_params.g_wk.push_back(params.problem.batch_sz);
+    constr_params.g_wk.push_back(AlignUp(out_h, 4) / 4 * AlignUp(problem.n_outputs / 2, 8));
+    constr_params.g_wk.push_back(problem.batch_sz);
 
     constr_params.kernel_file = "conv7x7c3h224w224k64u2v2p3q3f1.s";
     constr_params.kernel_name = "miopenGcnAsmConv7x7c3h224w224k64u2v2p3q3f1";
