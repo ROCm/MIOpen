3ad0e404dfacae27dcab8be44344e6941dc508ae
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2022-09-19 18:06:50 +0200

    [Solvers] ConvAsmImplicitGemmGTCDynamicWrwXdlops: split ConvolutionContext and ProblemDescription (stage 2) (#1751)
    * ConvAsmImplicitGemmGTCDynamicWrwXdlops: split ConvolutionContext and ProblemDescription

--- src/include/miopen/solver.hpp
+++ src/include/miopen/solver.hpp
@@ -1730,16 +1730,34 @@ struct ConvAsmImplicitGemmV4R1DynamicWrw final : ConvSolver
 
 struct ConvAsmImplicitGemmGTCDynamicWrwXdlops final : ConvSolver
 {
+    // To suppress -Woverloaded-virtual
+    using ConvSolver::GetWorkspaceSize;
+    using ConvSolver::IsApplicable;
+
     const std::string& SolverDbId() const override
     {
         return GetSolverDbId<ConvAsmImplicitGemmGTCDynamicWrwXdlops>();
     }
 
-    bool IsApplicable(const ConvolutionContext& ctx) const override;
+    bool IsApplicable(const ConvolutionContext& ctx) const override
+    {
+        return IsApplicable(ctx, ctx.problem);
+    }
     bool IsDynamic() const override { return true; }
-    size_t GetWorkspaceSize(const ConvolutionContext& ctx) const override;
+    size_t GetWorkspaceSize(const ConvolutionContext& ctx) const override
+    {
+        return GetWorkspaceSize(ctx.problem);
+    }
     bool MayNeedWorkspace() const override { return true; }
-    ConvSolution GetSolution(const ConvolutionContext& ctx) const;
+    ConvSolution GetSolution(const ConvolutionContext& ctx) const
+    {
+        return GetSolution(ctx, ctx.problem);
+    }
+
+private:
+    bool IsApplicable(const ExecutionContext&, const ProblemDescription&) const;
+    size_t GetWorkspaceSize(const ProblemDescription&) const;
+    ConvSolution GetSolution(const ExecutionContext&, const ProblemDescription&) const;
 };
 
 struct ConvAsmImplicitGemmV4R1DynamicBwd final : ConvSolver
