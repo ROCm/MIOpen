20719dfa79684f1a971cbbb18ad1b63662ca9231
Bibek Ghimire <Bibek.Ghimire@amd.com>
2022-11-30 15:02:49 -0600

    bg/LWPMIOPEN-99 : place guard in forward declaration of CK's function

--- src/solver/conv_ck_igemm_fwd_bias_activ.cpp
+++ src/solver/conv_ck_igemm_fwd_bias_activ.cpp
@@ -40,6 +40,7 @@
 #endif
 MIOPEN_DECLARE_ENV_VAR(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_FWD_XDLOPS)
 
+#if MIOPEN_BACKEND_HIP && MIOPEN_USE_COMPOSABLEKERNEL
 // Forward declare CK's function.
 namespace ck {
 namespace tensor_operation {
@@ -58,6 +59,7 @@ void add_device_conv2d_fwd_xdl_c_shuffle_bias_relu_nhwc_kyxc_nhwk_f16_instances(
 } // namespace device
 } // namespace tensor_operation
 } // namespace ck
+#endif
 
 namespace miopen {
 namespace solver {
