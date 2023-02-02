8f8612211d77aa233a100a70307c25f26d4c3fa3
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2023-02-02 19:46:35 +0100

    Fix formatting

--- test/gtest/conv_strided_tensor.cpp
+++ test/gtest/conv_strided_tensor.cpp
@@ -24,13 +24,13 @@
  *
  *******************************************************************************/
 
- // Test Suite for convolution with strided tensor descriptors
+// Test Suite for convolution with strided tensor descriptors
 
 #include <gtest/gtest.h>
 #include <miopen/miopen.h>
 
-#define MIOPEN_CHECK_RET(val)   ASSERT_EQ(val, miopenStatusSuccess)
-#define HIP_CHECK_RET(val)      ASSERT_EQ(val, hipSuccess)
+#define MIOPEN_CHECK_RET(val) ASSERT_EQ(val, miopenStatusSuccess)
+#define HIP_CHECK_RET(val) ASSERT_EQ(val, hipSuccess)
 
 class ConvStridedTensors : public ::testing::Test
 {
@@ -42,11 +42,8 @@ protected:
 
         // Tensor descriptors
         MIOPEN_CHECK_RET(miopenCreateTensorDescriptor(&input_descr));
-        MIOPEN_CHECK_RET(miopenSetTensorDescriptor(input_descr,
-                                                   miopenFloat,
-                                                   input_dims.size(),
-                                                   input_dims.data(),
-                                                   input_strides.data()));
+        MIOPEN_CHECK_RET(miopenSetTensorDescriptor(
+            input_descr, miopenFloat, input_dims.size(), input_dims.data(), input_strides.data()));
 
         MIOPEN_CHECK_RET(miopenCreateTensorDescriptor(&filter_descr));
         MIOPEN_CHECK_RET(miopenSetTensorDescriptor(filter_descr,
@@ -64,21 +61,13 @@ protected:
 
         // Convolution descriptor
         MIOPEN_CHECK_RET(miopenCreateConvolutionDescriptor(&conv_descr));
-        MIOPEN_CHECK_RET(miopenInitConvolutionNdDescriptor(conv_descr,
-                                                           pad.size(),
-                                                           pad.data(),
-                                                           stride.data(),
-                                                           dilation.data(),
-                                                           miopenConvolution));
+        MIOPEN_CHECK_RET(miopenInitConvolutionNdDescriptor(
+            conv_descr, pad.size(), pad.data(), stride.data(), dilation.data(), miopenConvolution));
         MIOPEN_CHECK_RET(miopenSetConvolutionGroupCount(conv_descr, 1));
 
         // Workspace
-        MIOPEN_CHECK_RET(miopenConvolutionForwardGetWorkSpaceSize(handle,
-                                                                  filter_descr,
-                                                                  input_descr,
-                                                                  conv_descr,
-                                                                  output_descr,
-                                                                  &workspace_size));
+        MIOPEN_CHECK_RET(miopenConvolutionForwardGetWorkSpaceSize(
+            handle, filter_descr, input_descr, conv_descr, output_descr, &workspace_size));
         HIP_CHECK_RET(hipMalloc(&d_workspace, workspace_size));
 
         // Data
@@ -131,41 +120,41 @@ protected:
     miopenHandle_t handle = nullptr;
 
     // Tensor descriptors
-    miopenTensorDescriptor_t input_descr = nullptr;
+    miopenTensorDescriptor_t input_descr  = nullptr;
     miopenTensorDescriptor_t filter_descr = nullptr;
     miopenTensorDescriptor_t output_descr = nullptr;
     // Note: All variables are const, but MIOpen's API is not const-correct
-    std::vector<int> input_dims = {4, 4, 16, 9, 16};
-    std::vector<int> input_strides = {10240, 2560, 160, 16, 1};
-    std::vector<int> filter_dims = {8, 4, 3, 3, 3};
+    std::vector<int> input_dims     = {4, 4, 16, 9, 16};
+    std::vector<int> input_strides  = {10240, 2560, 160, 16, 1};
+    std::vector<int> filter_dims    = {8, 4, 3, 3, 3};
     std::vector<int> filter_strides = {108, 27, 9, 3, 1};
-    std::vector<int> output_dims = {4, 8, 8, 4, 8};
+    std::vector<int> output_dims    = {4, 8, 8, 4, 8};
     std::vector<int> output_strides = {2048, 256, 32, 8, 1};
 
     // Convolution descriptor
     miopenConvolutionDescriptor_t conv_descr = nullptr;
     // Note: All variables are const, but MIOpen's API is not const-correct
-    std::vector<int> pad = {1, 0, 1};
-    std::vector<int> stride = {2, 2, 2};
+    std::vector<int> pad      = {1, 0, 1};
+    std::vector<int> stride   = {2, 2, 2};
     std::vector<int> dilation = {1, 1, 1};
 
     // Workspace
     size_t workspace_size;
-    float *d_workspace = nullptr;
+    float* d_workspace = nullptr;
 
     // Data
-    const size_t input_size = input_dims[0] * input_strides[0];
-    const size_t filter_size = filter_dims[0] * filter_strides[0];
-    const size_t output_size = output_dims[0] * output_strides[0];
-    const size_t input_bytes = input_size * sizeof(float);
+    const size_t input_size   = input_dims[0] * input_strides[0];
+    const size_t filter_size  = filter_dims[0] * filter_strides[0];
+    const size_t output_size  = output_dims[0] * output_strides[0];
+    const size_t input_bytes  = input_size * sizeof(float);
     const size_t filter_bytes = filter_size * sizeof(float);
     const size_t output_bytes = output_size * sizeof(float);
-    float *h_input = nullptr;
-    float *h_filter = nullptr;
-    float *h_output = nullptr;
-    float *d_input = nullptr;
-    float *d_filter = nullptr;
-    float *d_output = nullptr;
+    float* h_input            = nullptr;
+    float* h_filter           = nullptr;
+    float* h_output           = nullptr;
+    float* d_input            = nullptr;
+    float* d_filter           = nullptr;
+    float* d_output           = nullptr;
 };
 
 // This test should be replaced when strided tensors are fully implemented
@@ -188,17 +177,18 @@ TEST_F(ConvStridedTensors, ConvStridedTensorsNotImplemented)
                                                     conv_descr,
                                                     output_descr,
                                                     d_output,
-                                                    sizeof(perf_results)/sizeof(perf_results[0]),
+                                                    sizeof(perf_results) / sizeof(perf_results[0]),
                                                     &perf_results_count,
                                                     perf_results,
                                                     d_workspace,
                                                     workspace_size,
-                                                    true), miopenStatusSuccess);
+                                                    true),
+              miopenStatusSuccess);
     EXPECT_GT(perf_results_count, 0);
 
     const float alpha = 1.f;
-    const float beta = 0.f;
-    
+    const float beta  = 0.f;
+
     // miopenConvolutionForward() must return error if the format is not supported
     // TODO replace ASSERT_EQ with ASSERT_NE after testing
     HIP_CHECK_RET(hipDeviceSynchronize());
@@ -214,6 +204,7 @@ TEST_F(ConvStridedTensors, ConvStridedTensorsNotImplemented)
                                        output_descr,
                                        d_output,
                                        d_workspace,
-                                       workspace_size), miopenStatusSuccess);
+                                       workspace_size),
+              miopenStatusSuccess);
     HIP_CHECK_RET(hipDeviceSynchronize());
 }
