93cbf1a37f25a66838ec7a2a3eb33530e738df29
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2023-02-09 21:27:45 +0100

    Fix tests

--- test/solver.cpp
+++ test/solver.cpp
@@ -100,12 +100,12 @@ public:
         return config;
     }
 
-    bool IsValidPerformanceConfig(const ConvolutionContext&, const TestConfig&) const override
+    bool IsValidPerformanceConfig(const ConvolutionContext&, const ProblemDescription&, const TestConfig&) const override
     {
         return true;
     }
 
-    TestConfig Search(const ConvolutionContext&, const AnyInvokeParams&) const override
+    TestConfig Search(const ConvolutionContext&, const ProblemDescription&, const AnyInvokeParams&) const override
     {
         TestConfig config;
         config.str = FileName();
@@ -114,6 +114,7 @@ public:
     }
 
     solver::ConvSolution GetSolution(const ConvolutionContext&,
+                                     const ProblemDescription&,
                                      const TestConfig& config) const override
     {
 
