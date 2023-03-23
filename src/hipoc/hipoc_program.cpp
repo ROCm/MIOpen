277e35c10f036833f781477f3db7bcb7ad52906d
Jun Liu <Liu.Jun@amd.com>
2023-03-23 13:14:33 -0700

    Workaround hipRTC_header.h issues

--- src/hipoc/hipoc_program.cpp
+++ src/hipoc/hipoc_program.cpp
@@ -323,8 +323,11 @@ void HIPOCProgramImpl::BuildCodeObject(std::string params,
 
     if(miopen::EndsWith(filename, ".cpp"))
     {
+        // WORKAROUND: TODO: -Wno-unused-parameter -Wno-extra-semi-stmt bypasses issues with
+        // hipRTC_header.h
 #if MIOPEN_BUILD_DEV
-        params += " -Werror" + HipKernelWarningsString();
+        params +=
+            " -Werror -Wno-unused-parameter -Wno-extra-semi-stmt " + HipKernelWarningsString();
 #else
         params += " -Wno-everything";
 #endif
@@ -332,8 +335,8 @@ void HIPOCProgramImpl::BuildCodeObject(std::string params,
     else if(miopen::EndsWith(filename, ".cl"))
     {
 #if MIOPEN_BUILD_DEV
-        params +=
-            " -Werror" + (is_kernel_str ? MiopengemmWarningsString() : OclKernelWarningsString());
+        params += " -Werror -Wno-unused-parameter -Wno-extra-semi-stmt " +
+                  (is_kernel_str ? MiopengemmWarningsString() : OclKernelWarningsString());
 #else
         params += " -Wno-everything";
 #endif
