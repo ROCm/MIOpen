3bfa55fcfe3774c99d66d9c84d8bc63aaefdcae7
Bibek Ghimire <bghimire@amd.com>
2023-04-03 15:30:03 +0000

    bg/LWPMIOPEN-20: fix minor compile issue

--- src/include/miopen/fusion/solvers.hpp
+++ src/include/miopen/fusion/solvers.hpp
@@ -288,7 +288,7 @@ struct CKGEMMActiv final : FusionTunableSolver<PerformanceConfigCKGEMActiv>
                                   const PerformanceConfigCKGEMActiv& config) const override;
     PerformanceConfigCKGEMActiv
     Search(const FusionContext& ctx,
-           const FusionDescription& fdesc_problem const AnyInvokeParams& invoke_ctx) const override;
+           const FusionDescription& fdesc_problem, const AnyInvokeParams& invoke_ctx) const override;
     bool IsApplicable(const FusionContext& ctx,
                       const FusionDescription& fdesc_problem) const override;
     ConvSolution GetSolution(const FusionContext& ctx,
