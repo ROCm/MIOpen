5f5b4edfb55df39683a57d9f5afdbde196f1c946
Dmantri98 <devanshu.mantri@amd.com>
2023-04-07 16:13:30 +0000

    blank lines

--- src/conv/heuristic_model/tuning_heuristic.cpp
+++ src/conv/heuristic_model/tuning_heuristic.cpp
@@ -30,14 +30,17 @@
 #include <string>
 #include <fstream>
 #if MIOPEN_ENABLE_AI_KERNEL_TUNING
+
 namespace miopen {
 namespace ai {
 namespace tuning {
+
 nlohmann::json GetModelMetadata(const std::string& arch, const std::string& solver)
 {
     std::string file_path = GetSystemDbPath() + "/" + arch + "_" + solver + "_metadata.model";
     return nlohmann::json::parse(std::ifstream(file_path));
 }
+
 } // namespace tuning
 } // namespace ai
 } // namespace miopen
