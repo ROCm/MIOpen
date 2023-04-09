08e855274f34bb6ae5bcfcffc74c042f23c7576d
msaudulhassan <muhammad.saud.ms@gmail.com>
2023-04-09 13:53:30 +0000

    metadata made static

--- src/conv/heuristic_model/tuning_heuristic.cpp
+++ src/conv/heuristic_model/tuning_heuristic.cpp
@@ -24,21 +24,131 @@
  *
  *******************************************************************************/
 
-#include <nlohmann/json.hpp>
-#include <miopen/db_path.hpp>
-#include <miopen/config.h>
-#include <string>
-#include <fstream>
+//#include <miopen/config.h>
+#include <miopen/conv/heuristic_model/tuning_heuristic.hpp>
 #if MIOPEN_ENABLE_AI_KERNEL_TUNING
+#include <fdeep/fdeep.hpp>
 
 namespace miopen {
 namespace ai {
 namespace tuning {
 
-nlohmann::json GetModelMetadata(const std::string& arch, const std::string& solver)
+Metadata::Metadata (const std::string& arch, const std::string& solver)
 {
-    std::string file_path = GetSystemDbPath() + "/" + arch + "_" + solver + "_metadata.ktn.model";
-    return nlohmann::json::parse(std::ifstream(file_path));
+    const nlohmann::json metadata = nlohmann::json::parse(std::ifstream(
+        GetSystemDbPath() + "/" + arch + "_" + solver + "_metadata.ktn.model"));
+
+    num_conv_params   = metadata["num_conv_params"].get<std::size_t>();
+    num_tuning_params = metadata["num_tuning_params"].get<std::size_t>();
+    tuning_decodings  = metadata["decodings"]["tunings"].get<std::unordered_map<std::string, int>>();
+
+    n    = num_conv_params + 1;
+}
+
+std::unordered_map<std::string, Metadata*> GetMetadata(const std::string& arch)
+{
+    const static std::unordered_map<std::string, Metadata*> metadata = {
+        {"ConvAsm1x1U", new Metadata(arch, "ConvAsm1x1U")}
+    };
+    return metadata;
+}
+
+fdeep::model
+GetModel(const std::string& arch, const std::string& solver, const std::string& model_type)
+{
+    std::string file_path =
+        GetSystemDbPath() + "/" + arch + "_" + solver + "_" + model_type + ".ktn.model";
+    return fdeep::load_model(file_path, true, fdeep::dev_null_logger);
+}
+
+std::vector<float> TransformFeatures(const std::string& arch,
+                                     const std::string& solver,
+                                     const ProblemDescription& problem, 
+                                     std::size_t n)
+{
+    if(arch == "gfx908" && solver=="ConvAsm1x1U")
+    {
+        assert(n == 8); // n = 6 (numerical conv params) * 1 + 1 (nominal conv params) * 2(amount of
+                        // values nominal param can take).
+        std::vector<float> features(n * n, 0.0f);
+        features[0]                   = problem.IsFp32() ? 2.0 : 1.0;
+        int offset                    = (problem.direction.IsForward() ? 0 : 1) + 1;
+        features[(offset)*n + offset] = 1.0;
+        features[3 * n + 3] =
+            float(problem.direction.IsForward() ? problem.n_inputs : problem.n_outputs);
+        features[4 * n + 4] =
+            float(problem.direction.IsForward() ? problem.n_outputs : problem.n_inputs);
+        features[5 * n + 5] = float(problem.in_height);
+        features[6 * n + 6] = float(problem.in_width);
+        features[7 * n + 7] = float(problem.batch_sz);
+        return features;
+    }
+    else
+        MIOPEN_THROW("Kernel tuning heuristic doesn't support GPU and/or solver.");
+}
+
+
+bool ModelSetParams(const std::string& arch,
+                    const std::string& solver,
+                    solver::PerformanceConfigConvAsm1x1U& config,
+                    const ProblemDescription& problem)
+{
+    MIOPEN_LOG_I("");
+
+    static const auto encoder       = GetModel(arch, solver, "encoder");
+    static const auto decoder       = GetModel(arch, solver, "decoder");
+    static auto metadata      = GetMetadata(arch);
+
+    metadata[solver];
+
+    std::vector<float> features = TransformFeatures(arch, solver, problem, metadata[solver]->n);
+
+    int dim            = std::sqrt(features.size());
+    auto input_tensor  = fdeep::tensor(fdeep::tensor_shape(dim, dim), features);
+    auto hidden_states = encoder.predict({input_tensor}); // Get hidden states from Encoder LSTM
+
+    std::vector<float> decoder_input_vector(1, 0.0);
+    auto decoder_input_tensor = fdeep::tensor(fdeep::tensor_shape(1), decoder_input_vector);
+    std::vector<fdeep::tensor> decoder_input = {
+        decoder_input_tensor,
+        hidden_states[0],
+        hidden_states[1],
+        hidden_states[2],
+        hidden_states[3]}; // pass in SOS token and hidden states
+
+    for(int i = 0; i < metadata[solver]->num_tuning_params; ++i)
+    {
+        auto output        = decoder.predict({decoder_input});
+        auto output_vector = output[0].to_vector();
+        std::priority_queue<std::pair<float, int>> pq;
+        for(int j = 0; j < output_vector.size(); j++)
+            pq.push(std::make_pair(output_vector[j], j)); // sort by value at index
+
+        int output_token_index = -1;
+        while(!pq.empty())
+        {
+            int token = pq.top().second;
+            int value = metadata[solver]->tuning_decodings[std::to_string(token)]; // convert index to tuning value
+            pq.pop();
+            if(value < 0)
+                return false;
+            if(config.ModelApplyToken(i, value, problem))
+            {
+                output_token_index =
+                    token; // index with largest value that is valid = predicted index
+                break;
+            }
+        }
+        decoder_input_tensor =
+            fdeep::tensor(fdeep::tensor_shape(1), std::vector<float>(1, float(output_token_index)));
+        decoder_input = {decoder_input_tensor,
+                         output[1],
+                         output[2],
+                         output[3],
+                         output[4]}; // index fed into decoder along with hidden states produced
+                                     // from previous step
+    }
+    return true;
 }
 
 } // namespace tuning
--- src/solver/conv_asm_1x1u.cpp
+++ src/solver/conv_asm_1x1u.cpp
@@ -394,36 +394,13 @@ static bool IsModelApplicable(const ConvolutionContext& ctx, const ProblemDescri
     return true;
 }
 
-static std::vector<float> TransformFeatures(const ProblemDescription& problem, std::size_t n)
-{
-    assert(n == 8); // n = 6 (numerical conv params) * 1 + 1 (nominal conv params) * 2(amount of
-                    // values nominal param can take).
-    std::vector<float> features(n * n, 0.0f);
-    features[0]                   = problem.IsFp32() ? 2.0 : 1.0;
-    int offset                    = (problem.direction.IsForward() ? 0 : 1) + 1;
-    features[(offset)*n + offset] = 1.0;
-    features[3 * n + 3] =
-        float(problem.direction.IsForward() ? problem.n_inputs : problem.n_outputs);
-    features[4 * n + 4] =
-        float(problem.direction.IsForward() ? problem.n_outputs : problem.n_inputs);
-    features[5 * n + 5] = float(problem.in_height);
-    features[6 * n + 6] = float(problem.in_width);
-    features[7 * n + 7] = float(problem.batch_sz);
-    return features;
-}
-
 void PerformanceConfigConvAsm1x1U::RunParmeterPredictionModel(const ConvolutionContext& ctx,
                                                               const ProblemDescription& problem,
                                                               bool& valid)
 {
     static const std::string& arch  = ctx.GetStream().GetDeviceName();
     static const std::string solver = "ConvAsm1x1U";
-    static const auto encoder       = ai::tuning::GetModel(arch, solver, "encoder");
-    static const auto decoder       = ai::tuning::GetModel(arch, solver, "decoder");
-    static const auto metadata      = ai::tuning::GetModelMetadata(arch, solver);
-    std::vector<float> features =
-        TransformFeatures(problem, metadata["num_conv_params"].get<std::size_t>() + 1);
-    if(ai::tuning::ModelSetParams(encoder, decoder, metadata, *this, problem, features))
+    if(ai::tuning::ModelSetParams(arch, solver, *this, problem))
     {
         MIOPEN_LOG_I("Params set by AI: " << ToString());
         valid = true;
