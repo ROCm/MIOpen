a670d4e8a6cb7030c4a87337c67cd7a28452930b
Vasilii Filippov <vfilippo@amd.com>
2023-04-11 19:23:51 +0200

    PR comments

--- src/include/miopen/conv/problem_description.hpp
+++ src/include/miopen/conv/problem_description.hpp
@@ -153,16 +153,16 @@ struct ProblemDescription : ProblemDescriptionBase
 {
     ProblemDescription() = default;
 
-    ProblemDescription(TensorDescriptor in_,
-                       TensorDescriptor weights_,
-                       TensorDescriptor out_,
-                       ConvolutionDescriptor conv_,
+    ProblemDescription(const TensorDescriptor& in_,
+                       const TensorDescriptor& weights_,
+                       const TensorDescriptor& out_,
+                       const ConvolutionDescriptor& conv_,
                        Direction direction_,
                        int bias_ = 0)
-        : in(std::move(in_)),
-          weights(std::move(weights_)),
-          out(std::move(out_)),
-          conv(std::move(conv_)),
+        : in(in_),
+          weights(weights_),
+          out(out_),
+          conv(conv_),
           in_layout(ComputeInLayout()),
           weights_layout(ComputeWeightsLayout()),
           out_layout(ComputeOutLayout()),
