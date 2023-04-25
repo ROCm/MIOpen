94d04f086445da474b9e5bfe61ba42cd6664c936
Murali N <33875998+muralinr@users.noreply.github.com>
2023-04-24 23:05:17 -0700

    [MI300] CK new target changes (#2105)
    * [MI300] CK new target changes
    
    * removed some target update changes
    
    [ROCm/MIOpen commit: 6073490784e19f1a65b1a0e18df3f964ab0731b8]

--- src/include/miopen/solver/ck_utility_common.hpp
+++ src/include/miopen/solver/ck_utility_common.hpp
@@ -53,6 +53,7 @@ static inline bool is_ck_supported_hardware(const Handle& handle)
            StartsWith(handle.GetDeviceName(), "gfx906") ||
            StartsWith(handle.GetDeviceName(), "gfx908") ||
            StartsWith(handle.GetDeviceName(), "gfx90a") ||
+           StartsWith(handle.GetDeviceName(), "gfx940") ||
            StartsWith(handle.GetDeviceName(), "gfx1030") ||
            StartsWith(handle.GetDeviceName(), "gfx1031") ||
            StartsWith(handle.GetDeviceName(), "gfx1100") ||
@@ -85,6 +86,8 @@ static inline auto get_ck_common_compiler_flag(const Handle& handle)
         compiler_flag << " -DCK_AMD_GPU_GFX908";
     else if(StartsWith(device_name, "gfx90a"))
         compiler_flag << " -DCK_AMD_GPU_GFX90A";
+    else if(StartsWith(device_name, "gfx940"))
+        compiler_flag << " -DCK_AMD_GPU_GFX940";
     else if(StartsWith(device_name, "gfx1030"))
         compiler_flag << " -DCK_AMD_GPU_GFX1030";
     else if(StartsWith(device_name, "gfx1031"))
