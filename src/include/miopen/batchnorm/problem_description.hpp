a4b0e0f3301359ccb4d58cb0abaf0e1f6a13062a
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2023-04-25 15:11:39 +0200

    Fix gcc

--- src/include/miopen/batchnorm/problem_description.hpp
+++ src/include/miopen/batchnorm/problem_description.hpp
@@ -184,16 +184,16 @@ private:
     TensorDescriptor dxDesc;
     TensorDescriptor scaleBiasDesc;
 
-#if defined(__clang__) || defined(__GNUG__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-private-field"
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-private-field"
 #endif
 
     double expAvgFactor = 0;
     double epsilon;
 
-#if defined(__clang__) || defined(__GNUG__)
-#pragma GCC diagnostic pop
+#ifdef __clang__
+#pragma clang diagnostic pop
 #endif
 
     bool resultsave        = false;
