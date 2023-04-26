b36aa9e206c4e835154eecb5b22ff24d20baf5d4
Chris Erb <chris.erb65@gmail.com>
2023-04-26 10:45:38 -0500

    Update src/solver/conv_asm_implicit_gemm_gtc_fwd_nchwc.cpp
    Co-authored-by: Evgenii Averin <86725875+averinevg@users.noreply.github.com>

--- src/solver/conv_asm_implicit_gemm_gtc_fwd_nchwc.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_fwd_nchwc.cpp
@@ -459,6 +459,8 @@ bool PerformanceConfigAsmImplicitGemmGTCFwdDlopsNCHWC::IsValidValue() const
     if(IsDefaultConstructed())
         return true;
     const auto& config_list = GetFwdDlopsNCHWCConfigList();
+    if(index < config_list.size() && *this == config_list[index])
+        return true;
     return miopen::any_of(config_list, [&](auto v) { return (*this == v); });
 }
 bool PerformanceConfigAsmImplicitGemmGTCFwdDlopsNCHWC::IsValid(
