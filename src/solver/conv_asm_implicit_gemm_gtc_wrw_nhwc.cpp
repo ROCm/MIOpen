5cd06c6539daa8fb14818114bed5fe0d87e0ec4b
Chris Erb <chris.erb65@gmail.com>
2023-04-26 10:46:34 -0500

    Update src/solver/conv_asm_implicit_gemm_gtc_wrw_nhwc.cpp
    Co-authored-by: Evgenii Averin <86725875+averinevg@users.noreply.github.com>

--- src/solver/conv_asm_implicit_gemm_gtc_wrw_nhwc.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_wrw_nhwc.cpp
@@ -725,6 +725,8 @@ bool PerformanceConfigAsmImplicitGemmGTCWrwXdlopsNHWC::IsValidValue() const
     if(IsDefaultConstructed())
         return true;
     const auto& config_list = GetWrwXdlopsNHWCConfigList();
+    if(index < config_list.size() && *this == config_list[index])
+        return true;
     return miopen::any_of(config_list, [&](auto v) { return (*this == v); });
 }
 
