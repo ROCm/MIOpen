dfb8b9a9221dc9e2bc515eacbd1a9830fa3df86f
Chris Erb <chris.erb65@gmail.com>
2023-05-18 23:41:32 -0500

    Fixes for conv_asm perf config validity check. (#2111)

--- src/solver/conv_asm_implicit_gemm_gtc_bwd_nhwc.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_bwd_nhwc.cpp
@@ -734,9 +734,9 @@ bool PerformanceConfigAsmImplicitGemmGTCBwdXdlopsNHWC::IsValidValue() const
     if(IsDefaultConstructed())
         return true;
     const auto& config_list = GetBwdXdlopsNHWCConfigList();
-    if(index >= config_list.size())
-        return false;
-    return *this == config_list[index];
+    if(index < config_list.size() && *this == config_list[index])
+        return true;
+    return miopen::any_of(config_list, [&](auto v) { return (*this == v); });
 }
 bool PerformanceConfigAsmImplicitGemmGTCBwdXdlopsNHWC::SetNextValue(const ProblemDescription&)
 {
