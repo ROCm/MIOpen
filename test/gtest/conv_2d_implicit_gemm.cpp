f5d3cf1c5056089b2a9489d32b2b1f90b1633a45
alecsandraBara <alexbara@amd.com>
2023-05-25 14:29:04 +0000

    renamed file

--- test/gtest/conv_2d_implicit_gemm.cpp
+++ test/gtest/conv_2d_implicit_gemm.cpp
@@ -2,48 +2,10 @@
 
 #include <miopen/miopen.h>
 #include <gtest/gtest.h>
-#include "conv_common.hpp"
+#include "conv_2d.hpp"
 #include "get_handle.hpp"
 
-template <class T>
-struct conv2d_driver : conv_driver<T>
-{
-    conv2d_driver() : conv_driver<T>()
-    {
-        this->add(this->input_dims, "input");
-        this->add(this->weight_tensor_dims, "weights");
-        this->add(this->batch_size,
-                  "batch_size",
-                  this->generate_data_limited(this->get_batch_sizes(), 1));
-        this->add(this->input_channels,
-                  "input_channels",
-                  this->generate_data_limited(this->get_input_channels(), 1, {32}));
-        this->add(this->output_channels,
-                  "output_channels",
-                  this->generate_data_limited(this->get_output_channels(), 1, {64}));
-        this->add(this->spatial_dim_elements,
-                  "spatial_dim_elements",
-                  this->generate_data_limited(this->get_2d_spatial_dims(), 1, {28, 28}));
-        this->add(this->filter_dims,
-                  "filter_dims",
-                  this->generate_data_limited(this->get_2d_filter_dims(), 2, {3, 3}));
-        this->add(this->pads_strides_dilations,
-                  "pads_strides_dilations",
-                  this->generate_data_limited(this->get_2d_pads_strides_dilations(), 2));
-        this->add(this->trans_output_pads,
-                  "trans_output_pads",
-                  this->generate_data(this->get_2d_trans_output_pads()));
-        this->add(this->in_layout, "in_layout", this->generate_data({"NCHW"}));
-        this->add(this->fil_layout, "fil_layout", this->generate_data({"NCHW"}));
-        this->add(this->out_layout, "out_layout", this->generate_data({"NCHW"}));
-        this->add(this->deterministic, "deterministic", this->generate_data({false}));
-        this->add(this->tensor_vect, "tensor_vect", this->generate_data({0}));
-        this->add(this->vector_length, "vector_length", this->generate_data({1}));
-        // Only valid for int8 input and weights
-        this->add(this->output_type, "output_type", this->generate_data({"int32"}));
-        this->add(this->int8_vectorize, "int8_vectorize", this->generate_data({false}));
-    }
-};
+using TestCase = std::tuple<std::vector<std::string>, std::string>;
 
 std::string GetFloatArg()
 {
@@ -55,8 +17,7 @@ std::string GetFloatArg()
     return tmp;
 };
 
-void GetArgs(const std::tuple<std::vector<std::string>, std::string>& param,
-             std::vector<std::string>& tokens)
+void GetArgs(const TestCase& param, std::vector<std::string>& tokens)
 {
     auto env_vars = std::get<0>(param);
     for(auto& elem : env_vars)
@@ -74,25 +35,25 @@ void GetArgs(const std::tuple<std::vector<std::string>, std::string>& param,
         tokens.push_back(*begin++);
 }
 
-class Conv2dHalf : public testing::TestWithParam<std::vector<std::tuple<std::vector<std::string>, std::string>>>
+class Conv2dHalf : public testing::TestWithParam<std::vector<TestCase>>
 {
 };
-class Conv2dInt8 : public testing::TestWithParam<std::vector<std::tuple<std::vector<std::string>, std::string>>>
+class Conv2dInt8 : public testing::TestWithParam<std::vector<TestCase>>
 {
 };
-class Conv2dBFloat16
-    : public testing::TestWithParam<std::vector<std::tuple<std::vector<std::string>, std::string>>>
+class Conv2dBFloat16 : public testing::TestWithParam<std::vector<TestCase>>
 {
 };
-class Conv2dFloat : public testing::TestWithParam<std::vector<std::tuple<std::vector<std::string>, std::string>>>
+class Conv2dFloat : public testing::TestWithParam<std::vector<TestCase>>
 {
 };
+
 TEST_P(Conv2dFloat, FloatTest)
 {
 #if MIOPEN_EMBED_DB
 
     const auto& handle = get_handle();
-    if(!miopen::StartsWith(handle.GetDeviceName(), "gfx908") || GetFloatArg() != "--float")
+    if(!miopen::StartsWith(handle.GetDeviceName(), "gfx906") || GetFloatArg() != "--float")
     {
         GTEST_SKIP();
     }
@@ -120,11 +81,108 @@ TEST_P(Conv2dFloat, FloatTest)
 #endif
 };
 
-std::vector<std::tuple<std::vector<std::string>, std::string>> GetTestCases(const std::string& precision)
+TEST_P(Conv2dHalf, HalfTest)
 {
-const std::vector<std::tuple<std::vector<std::string>, std::string>> test_cases =
+#if MIOPEN_EMBED_DB
+
+    const auto& handle = get_handle();
+    if(!miopen::StartsWith(handle.GetDeviceName(), "gfx906") || GetFloatArg() != "--half")
+    {
+        GTEST_SKIP();
+    }
+    else
+    {
+        auto params = GetParam();
+        for(const auto& test_value : params)
+        {
+            std::vector<std::string> tokens;
+            GetArgs(test_value, tokens);
+            std::vector<const char*> ptrs;
+
+            for(std::string const& str : tokens)
+                ptrs.push_back(str.data());
+
+            testing::internal::CaptureStderr();
+            test_drive<conv2d_driver>(ptrs.size(), ptrs.data());
+            auto capture = testing::internal::GetCapturedStderr();
+            EXPECT_FALSE(capture.find("Perf Db: record not found") != std::string::npos);
+        }
+    }
+
+#else
+    GTEST_SKIP();
+#endif
+};
+
+TEST_P(Conv2dInt8, Int8Test)
 {
-    // clang-format off
+#if MIOPEN_EMBED_DB
+
+    const auto& handle = get_handle();
+    if(!miopen::StartsWith(handle.GetDeviceName(), "gfx906") || GetFloatArg() != "--int8")
+    {
+        GTEST_SKIP();
+    }
+    else
+    {
+        auto params = GetParam();
+        for(const auto& test_value : params)
+        {
+            std::vector<std::string> tokens;
+            GetArgs(test_value, tokens);
+            std::vector<const char*> ptrs;
+
+            for(std::string const& str : tokens)
+                ptrs.push_back(str.data());
+
+            testing::internal::CaptureStderr();
+            test_drive<conv2d_driver>(ptrs.size(), ptrs.data());
+            auto capture = testing::internal::GetCapturedStderr();
+            EXPECT_FALSE(capture.find("Perf Db: record not found") != std::string::npos);
+        }
+    }
+
+#else
+    GTEST_SKIP();
+#endif
+};
+
+TEST_P(Conv2dBFloat16, BFloat16Test)
+{
+#if MIOPEN_EMBED_DB
+
+    const auto& handle = get_handle();
+    if(!miopen::StartsWith(handle.GetDeviceName(), "gfx906") || GetFloatArg() != "--bfloat16")
+    {
+        GTEST_SKIP();
+    }
+    else
+    {
+        auto params = GetParam();
+        for(const auto& test_value : params)
+        {
+            std::vector<std::string> tokens;
+            GetArgs(test_value, tokens);
+            std::vector<const char*> ptrs;
+
+            for(std::string const& str : tokens)
+                ptrs.push_back(str.data());
+
+            testing::internal::CaptureStderr();
+            test_drive<conv2d_driver>(ptrs.size(), ptrs.data());
+            auto capture = testing::internal::GetCapturedStderr();
+            EXPECT_FALSE(capture.find("Perf Db: record not found") != std::string::npos);
+        }
+    }
+
+#else
+    GTEST_SKIP();
+#endif
+};
+std::vector<TestCase> GetTestCases(const std::string& precision)
+{
+    const std::vector<TestCase> test_cases = {
+        // clang-format off
     std::make_tuple<std::vector<std::string>, std::string>(
         {"MIOPEN_DEBUG_AMD_WINOGRAD_RXS_F3X2=0", "MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_WRW_V4R1=0"},
         precision + " --disable-validation --verbose --input 128 1024 14 14 --weights 2048 1024 1 1 --pads_strides_dilations 0 0 2 2 1 1"),
@@ -189,14 +247,13 @@ const std::vector<std::tuple<std::vector<std::string>, std::string>> test_cases 
     std::make_tuple<std::vector<std::string>, std::string>(
         {"MIOPEN_DEBUG_AMD_WINOGRAD_RXS_F3X2=0", "MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_WRW_V4R1=0"},
         precision + " --disable-validation --verbose --input 128 64 56 56 --weights 64 64 3 3 --pads_strides_dilations 1 1 1 1 1 1")
-    // clang-format on
-};
+        // clang-format on
+    };
 
     return test_cases;
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    Conv2dGroup,
-    Conv2dFloat,
-    testing::Values(GetTestCases("--float")
-    ));
+INSTANTIATE_TEST_SUITE_P(Conv2dGroup, Conv2dFloat, testing::Values(GetTestCases("--float")));
+INSTANTIATE_TEST_SUITE_P(Conv2dGroup, Conv2dHalf, testing::Values(GetTestCases("--half")));
+INSTANTIATE_TEST_SUITE_P(Conv2dGroup, Conv2dInt8, testing::Values(GetTestCases("--int8")));
+INSTANTIATE_TEST_SUITE_P(Conv2dGroup, Conv2dBFloat16, testing::Values(GetTestCases("--bfloat16")));
