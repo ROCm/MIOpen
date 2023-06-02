f2cb061b211122ab41e5487e49c0e546615f6fe6
Vasilii Filippov <vfilippo@amd.com>
2023-06-02 19:15:13 +0200

    Fixed fin build

--- src/include/miopen/kernel_info.hpp
+++ src/include/miopen/kernel_info.hpp
@@ -50,7 +50,9 @@ struct KernelInfo
     friend std::ostream& operator<<(std::ostream& os, const KernelInfo& k);
 };
 
-std::vector<Program> PrecompileKernels(const Handle& h, const std::vector<KernelInfo>& kernels);
+std::vector<Program> PrecompileKernels(const Handle& h,
+                                       const std::vector<KernelInfo>& kernels,
+                                       bool force_attach_binary = false);
 
 } // namespace solver
 } // namespace miopen
