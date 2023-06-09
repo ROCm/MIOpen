ba2f12fe4e392597e28175f7097159d7c5a571ce
gonidelis <gonidelis@hotmail.com>
2023-06-09 18:07:13 +0000

    Check whole tensor instead of just the first element.

--- test/gtest/fusion_test.cpp
+++ test/gtest/fusion_test.cpp
@@ -71,18 +71,23 @@ protected:
 
         auto&& handle = get_handle();
         std::fill(h_input.begin(), h_input.end(), 1.0);
+        // h_input.generate(tensor_elem_gen_integer{17});     // tolerance is off for 3 decimal
+        // points
         d_input = handle.Write(h_input.data);
 
         std::fill(h_filter1.begin(), h_filter1.end(), 1.0);
+        // h_filter1.generate(tensor_elem_gen_integer{17});
         d_filter1 = handle.Write(h_filter1.data);
 
         std::fill(h_filter2.begin(), h_filter2.end(), 2.0);
+        // h_filter2.generate(tensor_elem_gen_integer{17});
         d_filter2 = handle.Write(h_filter2.data);
 
         std::fill(h_bias.begin(), h_bias.end(), 0.0);
+        // h_bias.generate(tensor_elem_gen_integer{17});
         d_bias = handle.Write(h_bias.data);
 
-        d_output = handle.Write(h_output.data); // not sure
+        d_output = handle.Write(h_output.data);
     }
 
     void TearDown() override
@@ -91,35 +96,104 @@ protected:
             return;
 
         auto&& handle = get_handle();
-        h_output.data = handle.Read<T>(d_output, h_output.data.size()); // read
+        EXPECT_EQ(miopenCompileFusionPlan(&handle, fusion_plan), 0);
+        EXPECT_EQ(miopenSetOpArgsConvForward(fusion_args, conv_op, &alpha, &beta, d_filter1.get()),
+                  0);
+        EXPECT_EQ(miopenSetOpArgsBiasForward(fusion_args, bias_op, &alpha, &beta, d_bias.get()), 0);
+        EXPECT_EQ(miopenExecuteFusionPlan(&handle,
+                                          fusion_plan,
+                                          &h_input.desc,
+                                          d_input.get(),
+                                          &h_output.desc,
+                                          d_output.get(),
+                                          fusion_args),
+                  0);
+        h_output.data = handle.Read<float>(d_output, h_output.data.size());
+        handle.Finish();
+
+        miopen::OperatorArgs* fus_args = reinterpret_cast<miopen::OperatorArgs*>(fusion_args);
+        using ConvParam                = miopen::fusion::ConvolutionOpInvokeParam;
+        using BiasParam                = miopen::fusion::BiasOpInvokeParam;
+
+        ConvParam* conv_param = dynamic_cast<ConvParam*>(fus_args->params[0].get());
+        BiasParam* bias_param = dynamic_cast<BiasParam*>(fus_args->params[1].get());
+
+        ASSERT_EQ(conv_param->weights, d_filter1.get());
+        ASSERT_EQ(bias_param->bdata, d_bias.get());
+
         miopen::TensorDescriptor conv_output_desc =
-            conv_desc.GetForwardOutputTensor(h_input.desc, h_filter2.desc, GetDataType<T>());
-        // miopen::TensorDescriptor bias_output_desc =
-        //     bias_desc.GetForwardOutputTensor(conv_output_desc.desc, h_bias.desc,
-        //     /*GetDataType<T>()*/float);
-        ref_out = tensor<T>{conv_output_desc.GetLengths()};
+            conv_desc.GetForwardOutputTensor(h_input.desc, h_filter1.desc, GetDataType<T>());
+        ref_out1 = tensor<T>{conv_output_desc.GetLengths()};
         cpu_convolution_forward(conv_desc.GetSpatialDimension(),
                                 h_input,
-                                h_filter2,
-                                ref_out,
+                                h_filter1,
+                                ref_out1,
                                 conv_desc.GetConvPads(),
                                 conv_desc.GetConvStrides(),
                                 conv_desc.GetConvDilations(),
                                 conv_desc.GetGroupCount());
 
-        EXPECT_FALSE(miopen::range_zero(ref_out)) << "Cpu data is all zeros";
+        EXPECT_FALSE(miopen::range_zero(ref_out1)) << "Cpu data is all zeros";
         EXPECT_FALSE(miopen::range_zero(h_output)) << "Gpu data is all zeros";
-        EXPECT_TRUE(miopen::range_distance(ref_out) == miopen::range_distance(h_output));
+        EXPECT_TRUE(miopen::range_distance(ref_out1) == miopen::range_distance(h_output));
 
         const double tolerance = 80;
         double threshold       = std::numeric_limits<T>::epsilon() * tolerance;
-        auto error             = miopen::rms_range(ref_out, h_output);
+        auto error             = miopen::rms_range(ref_out1, h_output);
 
-        EXPECT_FALSE(miopen::find_idx(ref_out, miopen::not_finite) >= 0)
+        EXPECT_FALSE(miopen::find_idx(ref_out1, miopen::not_finite) >= 0)
             << "Non finite number found in the CPU data";
 
         EXPECT_TRUE(error < threshold)
             << "Error beyond tolerance Error:" << error << ",  Threshold: " << threshold;
+
+        // Change fusion parameters (filter), see if it still works properly.
+        EXPECT_EQ(miopenSetOpArgsConvForward(fusion_args, conv_op, &alpha, &beta, d_filter2.get()),
+                  0);
+        EXPECT_EQ(miopenSetOpArgsBiasForward(fusion_args, bias_op, &alpha, &beta, d_bias.get()), 0);
+        EXPECT_EQ(miopenExecuteFusionPlan(&handle,
+                                          fusion_plan,
+                                          &h_input.desc,
+                                          d_input.get(),
+                                          &h_output.desc,
+                                          d_output.get(),
+                                          fusion_args),
+                  0);
+        h_output.data = handle.Read<float>(d_output, h_output.data.size());
+        handle.Finish();
+
+        conv_param = dynamic_cast<ConvParam*>(fus_args->params[0].get());
+        bias_param = dynamic_cast<BiasParam*>(fus_args->params[1].get());
+
+        ASSERT_EQ(conv_param->weights, d_filter2.get());
+
+        conv_output_desc =
+            conv_desc.GetForwardOutputTensor(h_input.desc, h_filter2.desc, GetDataType<T>());
+        ref_out2 = tensor<T>{conv_output_desc.GetLengths()};
+        cpu_convolution_forward(conv_desc.GetSpatialDimension(),
+                                h_input,
+                                h_filter2,
+                                ref_out2,
+                                conv_desc.GetConvPads(),
+                                conv_desc.GetConvStrides(),
+                                conv_desc.GetConvDilations(),
+                                conv_desc.GetGroupCount());
+
+        EXPECT_FALSE(miopen::range_zero(ref_out2)) << "Cpu data is all zeros";
+        EXPECT_FALSE(miopen::range_zero(h_output)) << "Gpu data is all zeros";
+        EXPECT_TRUE(miopen::range_distance(ref_out2) == miopen::range_distance(h_output));
+
+        error = miopen::rms_range(ref_out2, h_output);
+
+        EXPECT_FALSE(miopen::find_idx(ref_out2, miopen::not_finite) >= 0)
+            << "Non finite number found in the CPU data";
+
+        EXPECT_TRUE(error < threshold)
+            << "Error beyond tolerance Error:" << error << ",  Threshold: " << threshold;
+
+        miopenDestroyConvolutionDescriptor(conv);
+        miopenDestroyFusionPlan(fusion_plan);
+        miopenDestroyOperatorArgs(fusion_args);
     }
 
     miopenFusionPlanDescriptor_t fusion_plan;
@@ -152,9 +226,9 @@ protected:
     tensor<T> h_output;
     miopen::Allocator::ManageDataPtr d_output;
 
-    tensor<T> ref_out;
+    tensor<T> ref_out1;
+    tensor<T> ref_out2;
     miopen::ConvolutionDescriptor conv_desc;
-    // miopen::BiasDescriptor bias_desc; // does not exist
 
     bool test_skipped = false;
 };
@@ -165,38 +239,53 @@ struct FusionTestApiFloat : FusionTestApi<float>
 
 TEST_F(FusionTestApiFloat, TestFusionPlanCompilation)
 {
-    auto&& handle = get_handle();
-    EXPECT_EQ(miopenCompileFusionPlan(&handle, fusion_plan), 0);
-    EXPECT_EQ(miopenSetOpArgsConvForward(fusion_args, conv_op, &alpha, &beta, d_filter1.get()), 0);
-    EXPECT_EQ(miopenSetOpArgsBiasForward(fusion_args, bias_op, &alpha, &beta, d_bias.get()), 0);
-    EXPECT_EQ(miopenExecuteFusionPlan(&handle,
-                                      fusion_plan,
-                                      &h_input.desc,
-                                      d_input.get(),
-                                      &h_output.desc,
-                                      d_output.get(),
-                                      fusion_args),
-              0);
-    h_output.data = handle.Read<float>(d_output, h_output.data.size());
-    handle.Finish();
-    // EXPECT_EQ(h_output.data[0], 64);
-    miopen::OperatorArgs* fus_args = static_cast<miopen::OperatorArgs*>(fusion_args);
-    ASSERT_EQ(fus_args->params[0].get(), conv_op);
+    // ---- DISCLAIMER ----
+    // I KEEP THIS SECTION IN CASE WE WANT TO COMPATMENTALIZE TEST CASE FROM TEAR DOWN CHECKS
+    // ---- DISCLAIMER ----
 
-    // Change fusion parameters (filter), see if it still works properly.
-    EXPECT_EQ(miopenSetOpArgsConvForward(fusion_args, conv_op, &alpha, &beta, d_filter2.get()), 0);
-    EXPECT_EQ(miopenSetOpArgsBiasForward(fusion_args, bias_op, &alpha, &beta, d_bias.get()), 0);
-    EXPECT_EQ(miopenExecuteFusionPlan(&handle,
-                                      fusion_plan,
-                                      &h_input.desc,
-                                      d_input.get(),
-                                      &h_output.desc,
-                                      d_output.get(),
-                                      fusion_args),
-              0);
-    h_output.data = handle.Read<float>(d_output, h_output.data.size());
-    handle.Finish();
-    EXPECT_EQ(h_output.data[0], 128);
+    // auto&& handle = get_handle();
+    // EXPECT_EQ(miopenCompileFusionPlan(&handle, fusion_plan), 0);
+    // EXPECT_EQ(miopenSetOpArgsConvForward(fusion_args, conv_op, &alpha, &beta, d_filter1.get()),
+    // 0); EXPECT_EQ(miopenSetOpArgsBiasForward(fusion_args, bias_op, &alpha, &beta, d_bias.get()),
+    // 0); EXPECT_EQ(miopenExecuteFusionPlan(&handle,
+    //                                   fusion_plan,
+    //                                   &h_input.desc,
+    //                                   d_input.get(),
+    //                                   &h_output.desc,
+    //                                   d_output.get(),
+    //                                   fusion_args),
+    //           0);
+    // h_output.data = handle.Read<float>(d_output, h_output.data.size());
+    // handle.Finish();
+
+    // miopen::OperatorArgs* fus_args  = reinterpret_cast<miopen::OperatorArgs*>(fusion_args);
+    // using ConvParam = miopen::fusion::ConvolutionOpInvokeParam;
+    // using BiasParam = miopen::fusion::BiasOpInvokeParam;
+
+    // ConvParam* conv_param = dynamic_cast<ConvParam*>(fus_args->params[0].get());
+    // BiasParam* bias_param = dynamic_cast<BiasParam*>(fus_args->params[1].get());
+
+    // ASSERT_EQ(conv_param->weights, d_filter1.get());
+    // ASSERT_EQ(bias_param->bdata, d_bias.get());
+
+    // // Change fusion parameters (filter), see if it still works properly.
+    // EXPECT_EQ(miopenSetOpArgsConvForward(fusion_args, conv_op, &alpha, &beta, d_filter2.get()),
+    // 0); EXPECT_EQ(miopenSetOpArgsBiasForward(fusion_args, bias_op, &alpha, &beta, d_bias.get()),
+    // 0); EXPECT_EQ(miopenExecuteFusionPlan(&handle,
+    //                                   fusion_plan,
+    //                                   &h_input.desc,
+    //                                   d_input.get(),
+    //                                   &h_output.desc,
+    //                                   d_output.get(),
+    //                                   fusion_args),
+    //           0);
+    // h_output.data = handle.Read<float>(d_output, h_output.data.size());
+    // handle.Finish();
+
+    // conv_param = dynamic_cast<ConvParam*>(fus_args->params[0].get());
+    // bias_param = dynamic_cast<BiasParam*>(fus_args->params[1].get());
+
+    // ASSERT_EQ(conv_param->weights, d_filter2.get());
 }
 
 #endif
