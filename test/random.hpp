e61e1477989350ada94769b29d14df18805c2b6a
Aleksandr Eremin <CAHEK7@yandex.ru>
2023-08-15 16:22:07 +0300

    remove legacy PRNG support from tests

--- test/random.hpp
+++ test/random.hpp
@@ -28,36 +28,30 @@
 
 #include <miopen/env.hpp>
 #include <iostream>
-#include <cstdlib>
 #include <random>
 
-std::minstd_rand& get_minstd_gen()
+MIOPEN_DECLARE_ENV_VAR(MIOPEN_TEST_PRNG_SEED)
+
+inline std::minstd_rand& get_minstd_gen()
 {
     static thread_local std::minstd_rand minstd_gen{[]() {
-        auto external_seed = miopen::EnvvarValue("MIOPEN_DRIVER_PRNG_SEED", 100500);
+        auto external_seed = miopen::Value(MIOPEN_TEST_PRNG_SEED{}, 100500);
 
         auto seed = external_seed == 0
                         ? std::random_device{}()
                         : static_cast<std::random_device::result_type>(external_seed);
-        std::cout << "Random seed: " << seed << "\n";
+        std::cout << "PRNG seed: " << seed << "\n";
         return seed;
     }()};
 
     return minstd_gen;
 }
 
-bool use_legacy_prng()
-{
-    static bool legacy_prng = miopen::IsEnvvarValueEnabled("MIOPEN_USE_LEGACY_PRNG");
-    return legacy_prng;
-}
-
 // template <typename T>
 // inline T FRAND()
 // {
 //
-//     return use_legacy_prng() ? static_cast<T>(rand() / (static_cast<double>(RAND_MAX)))
-//                              : std::generate_canonical<T, 1>(get_minstd_gen());
+//     return std::generate_canonical<T, 1>(get_minstd_gen());
 // }
 
 /// Basically, this is a copy of driver/random.hpp. Why:
@@ -66,7 +60,7 @@ bool use_legacy_prng()
 /// and vice versa. This independency could be important, because, for example, the driver
 /// implements its own cache of verification data and change or GET_RAND() would break it.
 
-inline int GET_RAND() { return use_legacy_prng() ? rand() : get_minstd_gen()(); }
+inline int GET_RAND() { return get_minstd_gen()(); }
 
 // template <typename T>
 // inline T RAN_GEN(T A, T B)
