0da7bc3cf5ccdf80aaaab6a705cb34f97e01d8ae
Jun Liu <Liu.Jun@amd.com>
2023-08-20 23:36:59 -0700

    suppress UNSAN

--- src/include/miopen/hipoc_kernel.hpp
+++ src/include/miopen/hipoc_kernel.hpp
@@ -55,8 +55,10 @@ struct KernelArgsPair
     static const int padding      = (alignment - (sizeof(T) % alignment)) % alignment;
     static const int second_index = sizeof(T) + padding;
     KernelArgsPair(T x, U y)
+#if defined(__clang__)
+        __attribute__((no_sanitize("undefined")))
+#endif
     {
-
         new(buffer) T(x); // NOLINT (clang-analyzer-cplusplus.PlacementNew)
         new(buffer + second_index) U(y);
     }
