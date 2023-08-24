5906e6d129c8f04ff81ba0c456f724ca6317b69c
Vasilii Filippov <vfilippo@amd.com>
2023-08-24 18:55:23 +0200

    unrelated tidy fix

--- src/solver/conv_asm_1x1u_bias_activ_fused.cpp
+++ src/solver/conv_asm_1x1u_bias_activ_fused.cpp
@@ -94,9 +94,9 @@ bool ConvBiasActivAsm1x1U::IsValidPerformanceConfig(
 PerformanceConfigConvBiasActivAsm1x1U
 ConvBiasActivAsm1x1U::Search(const FusionContext& context,
                              const FusionDescription& problem,
-                             const AnyInvokeParams& invoke_ctx) const
+                             const AnyInvokeParams& invoke_params) const
 {
-    return GenericSearch(*this, context, problem, invoke_ctx);
+    return GenericSearch(*this, context, problem, invoke_params);
 }
 
 ConvSolution
