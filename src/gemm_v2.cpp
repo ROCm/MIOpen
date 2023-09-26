39a8081f711067c8c49bc11bcb066390e465beb6
Jun Liu <Liu.Jun@amd.com>
2023-09-25 21:08:13 -0700

    clear clang format issue

--- src/gemm_v2.cpp
+++ src/gemm_v2.cpp
@@ -262,8 +262,7 @@ std::ostream& operator<<(std::ostream& stream, const GemmDescriptor& gemm_desc)
                   << "beta " << gemm_desc.beta << ", "
                   << "dataType " << GetDataType(gemm_desc.dataType) << ", "
                   << "a_cast_type " << GetDataType(gemm_desc.a_cast_type) << ", "
-                  << "b_cast_type " << GetDataType(gemm_desc.b_cast_type)
-                  << "} ";
+                  << "b_cast_type " << GetDataType(gemm_desc.b_cast_type) << "} ";
 }
 
 #if MIOPEN_USE_ROCBLAS
