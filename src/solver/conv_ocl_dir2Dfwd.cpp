22658a64592fecd9fb8e0db0564476dc51ae7a5e
amberhassaan <amber_474@yahoo.com>
2023-10-25 17:59:26 -0400

    Add a check for packed tensors for convolution solvers (#2471)

--- src/solver/conv_ocl_dir2Dfwd.cpp
+++ src/solver/conv_ocl_dir2Dfwd.cpp
@@ -48,17 +48,16 @@ bool ConvOclDirectFwd::IsApplicable(const ExecutionContext& ctx,
         return false;
     if(!(problem.direction.IsForward() || problem.direction.IsBackwardData()))
         return false;
+    if(problem.HasNonPackedTensors())
+        return false;
     if(problem.IsAsymmetricPadH() || problem.IsAsymmetricPadW())
         return false;
     if(!(problem.IsFp32() || problem.IsFp16() || problem.IsBfp16()))
         return false;
-
     if(problem.IsTensorsCasted())
         return false;
     if(!problem.IsLayoutDefault())
-    {
         return false;
-    }
 
     // clang-format off
     // Cases when dy has negative padding are not supported (issue 918)
