b14887edd049e6d7d53576f84e12d89ad2977eca
seungmanhan <seungman.han@moreh.io>
2023-11-20 09:32:03 +0000

    remove MIOPEN_BETA_API

--- test/gtest/layernorm.hpp
+++ test/gtest/layernorm.hpp
@@ -23,7 +23,6 @@
  * SOFTWARE.
  *
  *******************************************************************************/
-#define MIOPEN_BETA_API 1
 #include <miopen/miopen.h>
 #include <gtest/gtest.h>
 #include <miopen/layernorm.hpp>
