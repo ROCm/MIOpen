5046fa5d0ff694f2300082092e54922debc46a17
Christopher Erb <Christopher.Erb@amd.com>
2023-11-22 22:59:50 +0000

    immed_conv2d test update

--- test/gtest/immed_conv2d.cpp
+++ test/gtest/immed_conv2d.cpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2019 Advanced Micro Devices, Inc.
+ * Copyright (c) 2023 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,12 +23,27 @@
  * SOFTWARE.
  *
  *******************************************************************************/
+
 #include "conv_common.hpp"
+#include "get_handle.hpp"
+#include <miopen/env.hpp>
+#include <gtest/gtest.h>
+#include <boost/algorithm/string.hpp>
+
+MIOPEN_DECLARE_ENV_VAR_BOOL(MIOPEN_TEST_ALL)
+MIOPEN_DECLARE_ENV_VAR_BOOL(CODECOV_TEST)
+MIOPEN_DECLARE_ENV_VAR_STR(MIOPEN_TEST_FLOAT_ARG)
+MIOPEN_DECLARE_ENV_VAR_STR(MIOPEN_TEST_FLAGS_ARGS)
+
+using TestCase = std::string;
+struct ImmedConv2DTest : public testing::TestWithParam<std::vector<TestCase>>
+{
+};
 
 template <class T>
-struct conv2d_driver : conv_driver<T, ConvApi::Immediate>
+struct imm_conv2d_driver : conv_driver<T, ConvApi::Immediate>
 {
-    conv2d_driver() : conv_driver<T, ConvApi::Immediate>()
+    imm_conv2d_driver() : conv_driver<T, ConvApi::Immediate>()
     {
         this->add(this->input_dims, "input");
         this->add(this->weight_tensor_dims, "weights");
@@ -59,4 +74,48 @@ struct conv2d_driver : conv_driver<T, ConvApi::Immediate>
     }
 };
 
-int main(int argc, const char* argv[]) { test_drive<conv2d_driver>(argc, argv); }
+
+void RunImmedConv2DDriver(std::string cmd)
+{
+    std::vector<std::string> ptrs;
+    std::cout << cmd << std::endl;
+    boost::split(ptrs, cmd, boost::is_any_of(" \t"), boost::token_compress_on);
+    ptrs.insert(ptrs.begin(), "test_immed_conv2d");
+    std::vector<const char*> char_ptrs;
+    std::transform(ptrs.begin(), ptrs.end(), std::back_inserter(char_ptrs), [](const auto& str) {
+        return str.c_str();
+    });
+
+    test_drive<imm_conv2d_driver>(char_ptrs.size(), char_ptrs.data());
+}
+
+TEST_P(ImmedConv2DTest, test_immed_conv2d_codecov)
+{
+    if(miopen::IsEnabled(ENV(CODECOV_TEST)))
+    {
+	const auto& float_arg = miopen::GetStringEnv(ENV(MIOPEN_TEST_FLOAT_ARG));
+	const auto& flag_arg = miopen::GetStringEnv(ENV(MIOPEN_TEST_FLAGS_ARGS));
+        // clang-format off
+        RunImmedConv2DDriver(" "+float_arg+" --input  2 2 14 14 --weights 8 2 3 3 --pads_strides_dilations 0 0 1 1 1 1 "+flag_arg);
+        // clang-format on
+    }
+    else
+    {
+        GTEST_SKIP();
+    }
+}
+
+
+std::vector<std::string> GetTestCases()
+{
+    std::vector<std::string> test_cases;
+    return test_cases;
+}
+
+INSTANTIATE_TEST_SUITE_P(ImmedConv2D, ImmedConv2DTest, testing::Values(GetTestCases()));
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
