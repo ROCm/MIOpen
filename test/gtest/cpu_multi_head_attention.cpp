26f0d381bcf523dc572919b24dd66c82857b35af
Bibek Ghimire <bghimire@amd.com>
2023-11-30 07:32:01 +0000

    lwpmiopen-230 : fix indexing issue

--- test/gtest/cpu_multi_head_attention.cpp
+++ test/gtest/cpu_multi_head_attention.cpp
@@ -3,16 +3,31 @@
 int main()
 {
     size_t num_heads = 3;
-    size_t problem_dimension = 15;
-    size_t sequence_length = 10;
+    size_t problem_dimension = 6;
+    size_t sequence_length = 7;
     size_t batch_size = 2;
-    tensor<float> mask_val;
-    float drop_out_rate = 0.5;
-
-    test::cpu::multi_head_attention(num_heads,
+    float drop_out_rate = 0.2;
+    std::vector<size_t> mask_val_lens = {sequence_length, sequence_length};
+    tensor<float> mask_val({mask_val_lens});
+    // std::mutex coutMutex;
+    mask_val.par_for_each(
+        [&](size_t s_id, size_t p_id){
+            if(p_id > s_id)
+            {
+                // std::lock_guard<std::mutex> guard(coutMutex);
+                // std::cout << s_id << "," << p_id << std::endl;
+                mask_val(s_id, p_id) = 
+                    -std::numeric_limits<float>::infinity();
+            }
+        });
+        
+    tensor<float> mha = 
+            test::cpu::multi_head_attention(num_heads,
                          problem_dimension,
                          sequence_length,
                          batch_size, 
                          mask_val,
                          drop_out_rate);
+    
+    test::cpu::print<float>(mha, problem_dimension);
 }
--- test/gtest/cpu_multi_head_attention.hpp
+++ test/gtest/cpu_multi_head_attention.hpp
@@ -39,26 +39,56 @@ auto squareRoot(T value) -> decltype(std::sqrt(value)) {
 }
 
 template <class T>
+tensor<T> concatenateHeads(std::vector<tensor<T>>& heads, size_t batch_size, 
+    size_t seq_length, size_t problem_dim, size_t d_k)
+{
+    std::vector<size_t> multi_head_lens = {batch_size, seq_length, problem_dim};
+    tensor<T> multi_head({multi_head_lens});
+
+    for(size_t b = 0; b < batch_size; ++b)
+    {
+        for(size_t s = 0; s < seq_length; ++s)
+        {
+            size_t dim = 0;
+
+            for(const auto&each_head : heads)
+            {
+                for(size_t d = 0; d < d_k; ++d,++dim)
+                {
+                    multi_head(b, s, dim) = each_head(b, s, d);
+                }
+            }
+        }
+    }
+    return multi_head;
+
+}
+
+template <class T>
 void softmax(tensor<T>& tensor_val)
 {
     // assert(tensor_val.desc.GetLengths() == static_cast<size_t>(2));
-    size_t rows = tensor_val.desc.GetLengths()[0];
-    size_t cols = tensor_val.desc.GetLengths()[1];
-
-    for(int i = 0; i < rows; ++i)
+    size_t batch = tensor_val.desc.GetLengths()[0];
+    size_t rows = tensor_val.desc.GetLengths()[1];
+    size_t cols = tensor_val.desc.GetLengths()[2];
+    for(int b = 0; b < batch; ++b)
     {
-        T max_score_in_row = *std::max_element(tensor_val.begin(), tensor_val.begin() + cols); 
-        // computing sum of exp for each row
-        T sum(0);
-        for(int j = 0; j < cols; ++j)
+        for(int i = 0; i < rows; ++i)
         {
-            sum += std::exp(tensor_val(i, j) - max_score_in_row);
-        }
+            // T max_score_in_row = *std::max_element(tensor_val.begin(), tensor_val.begin() + cols); 
+            // computing sum of exp for each row
+            T sum(0);
+            for(int j = 0; j < cols; ++j)
+            {
+                tensor_val(b, i, j) = std::exp(tensor_val(b, i, j));
+                sum += tensor_val(b, i, j);
+            }
 
-        // applying the softmax
-        for(int j = 0; j < cols; ++j)
-        {
-            tensor_val(i,j) = std::exp(tensor_val(i,j) - max_score_in_row) / sum;
+            // applying the softmax
+            for(int j = 0; j < cols; ++j)
+            {
+                tensor_val(b, i, j) = tensor_val(b, i,j)/sum;
+            }
         }
     }
 }
@@ -82,7 +112,26 @@ void par_compute_val(size_t num_heads, size_t problem_dimension, std::vector<ten
 }
 
 template<typename T>
-void multi_head_attention(size_t num_heads,
+void print(const tensor<T>& tensor_val,
+           size_t problem_dimension)
+{
+    size_t count = 0;
+    for(const auto& val : tensor_val)
+    {
+        if(count%problem_dimension == 0)
+            std::cout << "\n";
+        if(count == tensor_val.data.size()/2)
+        {
+            std::cout << "\n\n";
+        }
+        std::cout << std::fixed << std::setprecision(2) << val << " , ";
+        count++;
+    }
+    std::cout << "\n======\n";
+
+}
+template<typename T>
+tensor<T> multi_head_attention(size_t num_heads,
                           size_t problem_dimension,
                           size_t sequence_length,
                           size_t batch_size, 
@@ -113,8 +162,8 @@ void multi_head_attention(size_t num_heads,
     
     std::vector<size_t> lens = {batch_size, sequence_length, problem_dimension};
     tensor<T> word_position({lens});
-    // final output stored here
-    tensor<T> concatinated_multi_head_attention{lens};
+    std::mutex coutMutex;
+
 
     word_position.generate(GenData<T>{});
 
@@ -125,88 +174,88 @@ void multi_head_attention(size_t num_heads,
         set_of_v_weights[i].generate(GenData<T>{});
         set_of_head_weights[i].generate(GenData<T>{});
     }
+
     
     par_compute_val(num_heads, problem_dimension, set_of_q_weights, word_position, set_of_q);
     par_compute_val(num_heads, problem_dimension, set_of_k_weights, word_position, set_of_k);
     par_compute_val(num_heads, problem_dimension, set_of_v_weights, word_position, set_of_v);
-    
-    for(size_t head = 0; head < num_heads; ++head){
-        set_of_k_transpose[head].par_for_each([&](size_t b, size_t s, size_t d)
-        {
-            set_of_k_transpose[head](b, d, s) = set_of_k[head](b, s, d);
-        });
-    }
+
 
     for(size_t head = 0; head < num_heads; ++head){
-        set_of_q_dot_k_transpose[head].par_for_each(
-            [&](size_t b_id, size_t s_r_id, size_t s_c_id){
-                T sum(0);
-                for(size_t dk_id = 0; dk_id < d_k; ++dk_id)
-                {
-                    // quick change is pd_id and dk_id
-                    sum += set_of_q[head](b_id, s_r_id, dk_id) * set_of_k_transpose[head](dk_id, s_c_id);
-                }
-                set_of_q_dot_k_transpose[head](b_id, s_r_id, s_c_id) = sum;
-            });
+        set_of_k_transpose[head].par_for_each([&](size_t b_id, size_t dk_id, size_t sl_id)
+        {
+            set_of_k_transpose[head](b_id, dk_id, sl_id) = set_of_k[head](b_id, sl_id, dk_id);
+        });
     }
+    // print(set_of_k_transpose[0], problem_dimension);
+
 
     double sqrt_dk = squareRoot(d_k);
     for(size_t head = 0; head < num_heads; ++head){
         set_of_q_dot_k_transpose[head].par_for_each(
-            [&](int i, int j) { 
-                set_of_q_dot_k_transpose[head](i, j) = set_of_q_dot_k_transpose[head](i, j)/sqrt_dk;
+            [&](size_t b_id, size_t sl_r_id, size_t sl_c_id){
+                T sum(0);
+                for(size_t dk_id = 0; dk_id < d_k; ++dk_id)
+                {
+                    sum += set_of_q[head](b_id, sl_r_id, dk_id) * set_of_k_transpose[head](b_id, dk_id, sl_c_id);
+                }
+                sum = (sum/sqrt_dk) + mask_val(sl_r_id, sl_c_id);
+                set_of_q_dot_k_transpose[head](b_id, sl_r_id, sl_c_id) = sum;
             });
     }
 
-    par_compute_val(num_heads, problem_dimension, set_of_v_weights, word_position, set_of_v);
-    
-
-    // masking
     for(size_t head = 0; head < num_heads; ++head){
-        set_of_q_dot_k_transpose[head].par_for_each(
-            [&](int i, int j) { 
-                set_of_q_dot_k_transpose[head](i, j) += mask_val(i, j);
-        });
-    }
-
-    for(size_t head = 0; head < num_heads; ++head){
+        // std::cout << "compute softmax of " << head << std::endl;
         softmax(set_of_q_dot_k_transpose[head]);
+
         // dropout
         tensor<T> rand_dis(set_of_q_dot_k_transpose[head].desc.GetLengths());
         rand_dis.generate(GenData<T>{});
         set_of_q_dot_k_transpose[head].par_for_each( 
-            [&](int i, int j) { if(rand_dis(i,j) < drop_out_rate){
-                set_of_q_dot_k_transpose[head](i,j) = T(0);
-            }});
+            [&](size_t b_id, size_t sl_r_id, size_t sl_c_id) { 
+                if(rand_dis(b_id, sl_r_id, sl_c_id) < drop_out_rate){
+                    set_of_q_dot_k_transpose[head](b_id, sl_r_id, sl_c_id) 
+                        = T(0);
+                }
+            });
     }
 
     for(size_t head = 0; head < num_heads; ++head){
         atten_heads[head].par_for_each(
-            [&](size_t b_id, size_t s_r_id, size_t s_c_id){
+            [&](size_t b_id, size_t s_id, size_t p_id){
                 T sum(0);
-                for(size_t dk_id = 0; dk_id < d_k; ++dk_id)
+                for(size_t sl_id = 0; sl_id < sequence_length; ++sl_id)
                 {
-                    sum += set_of_q[head](b_id, s_r_id, dk_id) * set_of_v[head](dk_id, s_c_id);
+                    sum += set_of_q_dot_k_transpose[head](b_id, s_id, sl_id) * set_of_v[head](b_id, p_id, sl_id);
                 }
-                atten_heads[head](b_id, s_r_id, s_c_id) = sum;
+                atten_heads[head](b_id, s_id, p_id) = sum;
+            });
+        // print(atten_heads[head], d_k);
+    }
+
+
+    // concatinate attention heads 
+    tensor<T> multi_head = concatenateHeads(atten_heads,
+                            batch_size, sequence_length, problem_dimension, d_k);
+
+    tensor<T> linear_multi_head(multi_head.desc.GetLengths());
+
+    for(size_t head = 0; head < num_heads; ++head){
+        multi_head.par_for_each(
+            [&](size_t b_id, size_t s_id, size_t p_id){
+                std::lock_guard<std::mutex> guard(coutMutex);
+
+                T sum(0);
+                for(size_t pd_id = 0; pd_id < problem_dimension; ++pd_id)
+                {
+                    sum += multi_head(b_id, s_id, pd_id) * 
+                           set_of_head_weights[head](p_id, pd_id);
+                }
+                linear_multi_head(b_id, s_id, p_id) = sum;
             });
     }
 
-    // concatinate single_attn 
-    std::vector<T> concatinated_data;
-
-    for(size_t head = 0; head < num_heads; ++head)
-    {
-        concatinated_data.insert(concatinated_data.end(),
-                                set_of_head_weights[head].data.begin(),
-                                set_of_head_weights[head].data.end());
-    }
-    std::vector<size_t> multi_head_lens = {batch_size, sequence_length, problem_dimension};
-    tensor<T> multi_head({multi_head_lens});
-    multi_head.data = concatinated_data;
-
-    // multi_head dot with multi_head_weights
-
+    return linear_multi_head;
 }
 
 }
