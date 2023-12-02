29525520336a2c68aa9feec81dd4bfa0c2d08a4a
xinlipn <xinlipn@gmail.com>
2023-12-01 22:41:21 -0800

    Added two tests, removed miopenInt8x4 from output message

--- test/gtest/gru_extra.cpp
+++ test/gtest/gru_extra.cpp
@@ -60,7 +60,7 @@ void Run2dDriver(miopenDataType_t prec)
     case miopenBFloat16:
     case miopenInt32:
     case miopenDouble:
-        FAIL() << "miopenHalf, miopenInt8, miopenBFloat16, miopenInt8x4, miopenInt32, miopenDouble "
+        FAIL() << "miopenHalf, miopenInt8, miopenBFloat16, miopenInt32, miopenDouble "
                   "data type not supported by "
                   "rnn_vanilla test";
 
