88e6ea356ec3c96d89c64f4917ee48efd154182d
Dmantri98 <109552294+Dmantri98@users.noreply.github.com>
2023-12-08 16:50:34 -0600

    AI Based Parameter Prediction Model for conv_hip_igemm_group_fwd_xdlops Solver (#2523)
    [ROCm/MIOpen commit: 4d5a18419990c99e05af64835a35e465f0dbd5b2]

--- test/gtest/kernel_tuning_net.cpp
+++ test/gtest/kernel_tuning_net.cpp
@@ -6,130 +6,114 @@
 
 struct KernelTuningNetTestCase : AIModelTestCase
 {
-    bool expected_valid;
     std::string expected_config;
 };
 
-std::vector<KernelTuningNetTestCase> GetConvAsm1x1UFloatTestCases()
+std::vector<KernelTuningNetTestCase> GetConvAsm1x1UTestCases()
 {
     return {{{{512, 192, 56, 56, 288, 1, 1, 0, 0, 1, 1, 1, 1, miopenConvolution},
               miopen::conv::Direction::BackwardData,
               miopenFloat,
               miopenTensorNCHW},
-             true,
              "1,16,1,64,2,2,1,4"},
-
-            {{{1, 4, 2, 2, 4, 1, 1, 0, 0, 1, 1, 1, 1, miopenConvolution},
-              miopen::conv::Direction::Forward,
-              miopenFloat,
-              miopenTensorNCHW},
-             false,
-             ""}};
-}
-
-std::vector<KernelTuningNetTestCase> GetConvAsm1x1UHalfTestCases()
-{
-    return {{{{256, 2048, 7, 7, 512, 1, 1, 0, 0, 1, 1, 1, 1, miopenConvolution},
+            {{{256, 2048, 7, 7, 512, 1, 1, 0, 0, 1, 1, 1, 1, miopenConvolution},
               miopen::conv::Direction::Forward,
               miopenHalf,
               miopenTensorNCHW},
-             true,
              "2,8,4,16,1,4,1,4"}};
 }
 
-template <typename G>
+std::vector<KernelTuningNetTestCase> GetConvHipIgemmGroupFwdXdlopsTestCases()
+{
+    return {
+        {{{128, 64, 209, 209, 128, 3, 3, 0, 0, 2, 2, 1, 1, miopenConvolution},
+          miopen::conv::Direction::Forward,
+          miopenFloat,
+          miopenTensorNHWC},
+         "DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle<256, 128, 128, 16, Default, 32, 32, 2, 2, "
+         "4, 4, 4, 1, 1>"}};
+}
+
 struct KernelTuningNetTest : public ::testing::TestWithParam<KernelTuningNetTestCase>
 {
 protected:
     void SetUp() override
     {
 #if MIOPEN_ENABLE_AI_KERNEL_TUNING
-        auto test_case           = GetParam();
-        tensor<G> input_tensor   = tensor<G>(test_case.layout, test_case.conv.GetInput());
-        tensor<G> weights_tensor = tensor<G>(test_case.layout, test_case.conv.GetWeights());
-        auto conv_desc           = test_case.conv.GetConv();
+        auto test_case                             = GetParam();
+        miopen::TensorDescriptor input_tensor_desc = miopen::TensorDescriptor(
+            test_case.data_type, test_case.layout, test_case.conv.GetInput());
+        miopen::TensorDescriptor weights_tensor_desc = miopen::TensorDescriptor(
+            test_case.data_type, test_case.layout, test_case.conv.GetWeights());
+        auto conv_desc                       = test_case.conv.GetConv();
         miopen::TensorDescriptor output_desc = conv_desc.GetForwardOutputTensor(
-            input_tensor.desc, weights_tensor.desc, test_case.data_type);
-
-        problem = (test_case.direction == miopen::conv::Direction::Forward)
-                      ? miopen::conv::ProblemDescription(input_tensor.desc,
-                                                         weights_tensor.desc,
+            input_tensor_desc, weights_tensor_desc, test_case.data_type);
+        problem  = (test_case.direction == miopen::conv::Direction::Forward)
+                       ? miopen::conv::ProblemDescription(input_tensor_desc,
+                                                         weights_tensor_desc,
                                                          output_desc,
                                                          conv_desc,
                                                          test_case.direction)
-                      : miopen::conv::ProblemDescription(output_desc,
-                                                         weights_tensor.desc,
-                                                         input_tensor.desc,
+                       : miopen::conv::ProblemDescription(output_desc,
+                                                         weights_tensor_desc,
+                                                         input_tensor_desc,
                                                          conv_desc,
                                                          test_case.direction);
-
-        expected_valid = test_case.expected_valid;
-        expected       = test_case.expected_config;
+        expected = test_case.expected_config;
 #else
         GTEST_SKIP();
 #endif
     }
     miopen::conv::ProblemDescription problem;
-    bool expected_valid;
     std::string expected;
 };
 
-struct KernelTuningNetTestFloat : KernelTuningNetTest<float>
-{
-};
-
-struct KernelTuningNetTestHalf : KernelTuningNetTest<half_float::half>
-{
-};
-
 template <typename T>
-void TestParameterPredictionModel(miopen::conv::ProblemDescription problem,
-                                  bool expected_valid,
-                                  std::string expected)
+void TestParameterPredictionModel(miopen::conv::ProblemDescription problem, std::string expected)
 {
 #if MIOPEN_ENABLE_AI_KERNEL_TUNING
     auto&& handle = get_handle();
-    if(handle.GetDeviceName() != "gfx908")
-        GTEST_SKIP();
     miopen::ExecutionContext ctx;
     ctx.SetStream(&handle);
     T perf_config;
-    bool valid = false;
-    perf_config.RunParmeterPredictionModel(ctx, problem, valid);
-    ASSERT_EQ(valid, expected_valid)
-        << "Expected parameters to be "
-        << (expected_valid ? std::string("valid") : std::string("invalid")) << " but were "
-        << (valid ? std::string("valid") : std::string("invalid"));
-    if(expected_valid)
-    {
-        EXPECT_EQ(perf_config.ToString(), expected)
-            << "Expected parameters: " << expected
-            << "\nPredicted parameters: " << perf_config.ToString();
-    }
+    if(!perf_config.IsModelApplicable(ctx, problem))
+        GTEST_SKIP();
+    perf_config.HeuristicInit(ctx, problem);
+    EXPECT_EQ(perf_config.ToString(), expected)
+        << "Expected parameters: " << expected
+        << "\nPredicted parameters: " << perf_config.ToString();
 #else
     std::ignore = problem;
-    std::ignore = expected_valid;
     std::ignore = expected;
     GTEST_SKIP();
 #endif
 }
 
-TEST_P(KernelTuningNetTestFloat, ConvAsm1x1UParameterPredictionModelFloat)
+struct KernelTuningNetTestConvAsm1x1U : KernelTuningNetTest
 {
-    TestParameterPredictionModel<miopen::solver::conv::PerformanceConfigConvAsm1x1U>(
-        problem, expected_valid, expected);
+};
+
+struct KernelTuningNetTestConvHipIgemmGroupFwdXdlops : KernelTuningNetTest
+{
+};
+
+TEST_P(KernelTuningNetTestConvAsm1x1U, ConvAsm1x1UParameterPredictionModel)
+{
+    TestParameterPredictionModel<miopen::solver::conv::PerformanceConfigConvAsm1x1U>(problem,
+                                                                                     expected);
 }
 
-TEST_P(KernelTuningNetTestHalf, ConvAsm1x1UParameterPredictionModelHalf)
+TEST_P(KernelTuningNetTestConvHipIgemmGroupFwdXdlops,
+       ConvHipIgemmGroupFwdXdlopsParameterPredictionModel)
 {
-    TestParameterPredictionModel<miopen::solver::conv::PerformanceConfigConvAsm1x1U>(
-        problem, expected_valid, expected);
+    TestParameterPredictionModel<
+        miopen::solver::conv::PerformanceConfigHipImplicitGemmGroupFwdXdlops>(problem, expected);
 }
 
-INSTANTIATE_TEST_SUITE_P(ConvAsm1x1UParameterPredictionModelFloatTest,
-                         KernelTuningNetTestFloat,
-                         testing::ValuesIn(GetConvAsm1x1UFloatTestCases()));
+INSTANTIATE_TEST_SUITE_P(ConvAsm1x1UParameterPredictionModelTest,
+                         KernelTuningNetTestConvAsm1x1U,
+                         testing::ValuesIn(GetConvAsm1x1UTestCases()));
 
-INSTANTIATE_TEST_SUITE_P(ConvAsm1x1UParameterPredictionModelHalfTest,
-                         KernelTuningNetTestHalf,
-                         testing::ValuesIn(GetConvAsm1x1UHalfTestCases()));
+INSTANTIATE_TEST_SUITE_P(ConvHipIgemmGroupFwdXdlopsParameterPredictionModelTest,
+                         KernelTuningNetTestConvHipIgemmGroupFwdXdlops,
+                         testing::ValuesIn(GetConvHipIgemmGroupFwdXdlopsTestCases()));
