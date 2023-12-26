1cf460da532686a6a590020647c871f7a6f49ad4
Artur Wojcik <artur.wojcik@outlook.com>
2023-12-26 08:35:10 +0100

    [Windows] add missing symbol export (#2556)
    * add missing symbol export
    
    * more missing exports
    
    * fix format issues
    
    ---------
    
    Co-authored-by: Jun Liu <Liu.Jun@amd.com>
    Co-authored-by: Alex Eremin <CAHEK7@yandex.ru>
    
    [ROCm/MIOpen commit: 1cc63e29acfb6622e214aa7436125ef937603757]

--- src/logger.cpp
+++ src/logger.cpp
@@ -68,7 +68,8 @@ namespace miopen {
 
 namespace debug {
 
-bool LoggingQuiet = false; // NOLINT (cppcoreguidelines-avoid-non-const-global-variables)
+// NOLINTNEXTLINE (cppcoreguidelines-avoid-non-const-global-variables)
+MIOPEN_EXPORT bool LoggingQuiet = false;
 
 } // namespace debug
 
