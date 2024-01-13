72c45757ca14396a972fee676cbafbe56af260c5
Jun Liu <Liu.Jun@amd.com>
2024-01-12 23:54:49 -0800

    [HotFix] fix clang format issue from #2661

--- src/ocl/convolutionocl.cpp
+++ src/ocl/convolutionocl.cpp
@@ -149,7 +149,7 @@ static Invoker PrepareInvoker(ExecutionContext ctx,
                               solver::Id solver_id)
 {
     problem.SetupFloats(ctx);
-    ctx.do_search = false;
+    ctx.do_search              = false;
     ctx.disable_search_enforce = true;
 
     const auto solver = solver_id.GetSolver();
