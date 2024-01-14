40a3f9d779f6347a98191d45041d98d919095a52
Artur Wojcik <artur.wojcik@outlook.com>
2024-01-14 07:45:14 +0100

    [Windows] addkernels: fix operations on path for Windows (#2657)

--- addkernels/include_inliner.hpp
+++ addkernels/include_inliner.hpp
@@ -24,11 +24,13 @@
  *
  *******************************************************************************/
 #ifndef SOURCE_INLINER_HPP
-
 #define SOURCE_INLINER_HPP
+
 #include "source_file_desc.hpp"
-#include <ostream>
+#include <exception>
+#include <filesystem>
 #include <memory>
+#include <ostream>
 #include <stack>
 
 class InlineException : public std::exception
@@ -118,8 +120,8 @@ public:
 
     void Process(std::istream& input,
                  std::ostream& output,
-                 const std::string& root,
-                 const std::string& file_name,
+                 const std::filesystem::path& root,
+                 const std::filesystem::path& file_name,
                  const std::string& directive,
                  bool allow_angle_brackets,
                  bool recurse);
@@ -131,8 +133,8 @@ private:
 
     void ProcessCore(std::istream& input,
                      std::ostream& output,
-                     const std::string& root,
-                     const std::string& file_name,
+                     const std::filesystem::path& root,
+                     const std::filesystem::path& file_name,
                      int line_number,
                      const std::string& directive,
                      bool allow_angle_brackets,
