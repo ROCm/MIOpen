a0c3e386e85e37f539e6fa983ef8f2e17c0090ea
xinlipn <xinlipn@gmail.com>
2024-02-09 13:09:56 -0800

    [Tests] Convert test_rnn_vanilla , test_gru, test_rnn_extra and test_gru_extra gTests (#2550)
    [ROCm/MIOpen commit: efaafebc51c4db6f0f2731df7e2e366f5b0952fd]

--- test/gru.cpp
+++ test/gru.cpp
@@ -24,45 +24,7 @@
  *
  *******************************************************************************/
 
-#include "gru_common.hpp"
-
-template <class T>
-struct gru_driver : gru_basic_driver<T>
-{
-    gru_driver() : gru_basic_driver<T>()
-    {
-        std::vector<int> modes(2, 0);
-        modes[1] = 1;
-        std::vector<int> defaultBS(1);
-
-        this->add(this->batchSize, "batch-size", this->generate_data(get_gru_batchSize(), {17}));
-        this->add(this->seqLength, "seq-len", this->generate_data(get_gru_seq_len(), {2}));
-        this->add(this->inVecLen, "vector-len", this->generate_data(get_gru_vector_len()));
-        this->add(this->hiddenSize, "hidden-size", this->generate_data(get_gru_hidden_size()));
-        this->add(this->numLayers, "num-layers", this->generate_data(get_gru_num_layers()));
-        this->add(this->nohx, "no-hx", this->flag());
-        this->add(this->nodhy, "no-dhy", this->flag());
-        this->add(this->nohy, "no-hy", this->flag());
-        this->add(this->nodhx, "no-dhx", this->flag());
-        this->add(this->flatBatchFill, "flat-batch-fill", this->flag());
-        this->add(this->useDropout, "use-dropout", this->generate_data({0}));
-
-#if(MIO_GRU_TEST_DEBUG == 3)
-        this->biasMode  = 0;
-        this->dirMode   = 1;
-        this->inputMode = 0;
-#else
-        this->add(this->inputMode, "in-mode", this->generate_data(modes));
-        this->add(this->biasMode, "bias-mode", this->generate_data(modes));
-        this->add(this->dirMode, "dir-mode", this->generate_data(modes));
-#endif
-        this->add(
-            this->batchSeq,
-            "batch-seq",
-            this->lazy_generate_data(
-                [=] { return generate_batchSeq(this->batchSize, this->seqLength); }, defaultBS));
-    }
-};
+#include "gru.hpp"
 
 int main(int argc, const char* argv[])
 {
