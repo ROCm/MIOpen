1907fbc8b5a2342ef1643d09456613f7c0eb011a
Artur Wojcik <artur.wojcik@amd.com>
2024-02-19 17:41:32 +0100

    fix clang-format

--- src/include/miopen/tmp_dir.hpp
+++ src/include/miopen/tmp_dir.hpp
@@ -12,12 +12,12 @@ struct TmpDir
     explicit TmpDir(std::string_view prefix = "");
 
     TmpDir(TmpDir&&) = default;
-    TmpDir& operator = (TmpDir&&) = default;
+    TmpDir& operator=(TmpDir&&) = default;
 
-    fs::path operator / (std::string_view other) const { return path / other; }
+    fs::path operator/(std::string_view other) const { return path / other; }
 
-    operator const fs::path& () const { return path; }
-    operator std::string () const { return path.string(); }
+    operator const fs::path&() const { return path; }
+    operator std::string() const { return path.string(); }
 
     int Execute(std::string_view exe, std::string_view args) const;
 
