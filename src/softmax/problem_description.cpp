3e001865182a1ef048f8a40ebee67c67ba203fe7
Vsevolod Golovko <vsevolod.golovko2@dxc.com>
2024-03-04 13:04:01 +0100

    Problem description slightly  changed. softmax driver patched

--- src/softmax/problem_description.cpp
+++ src/softmax/problem_description.cpp
@@ -73,7 +73,7 @@ NetworkConfig ProblemDescription::MakeNetworkConfig() const
     }
 
     ss << "a" << alpha;
-    ss << "b" << beta; 
+    ss << "b" << beta;
     ss << "algo" << static_cast<int>(algorithm);
     ss << "mode" << static_cast<int>(mode);
 
