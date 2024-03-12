08cd9c7676c2dc7d222dcc559a963f25b37742d2
Jun Liu <Liu.Jun@amd.com>
2024-03-11 17:31:34 -0700

    Revert "Revert "[HotFix] Fixed incorrectly generated files (#2769)""
    This reverts commit 0b32700d307f07dacdd6e296937840fdd0e39d2b.

--- src/include/miopen/execution_context.hpp
+++ src/include/miopen/execution_context.hpp
@@ -282,7 +282,7 @@ struct ExecutionContext
 #if MIOPEN_ENABLE_SQLITE
         filename << "_" << SQLitePerfDb::MIOPEN_PERFDB_SCHEMA_VER << ".udb";
 #else
-        filename << "." << GetUserDbSuffix() << ".cd.updb.txt";
+        filename << "." << GetUserDbSuffix() << ".udb.txt";
 #endif
         return (udb / filename.str()).string();
     }
