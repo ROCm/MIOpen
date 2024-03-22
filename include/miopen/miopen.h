64c4750fcb695f0674e91817ac60ebcc2b2021c8
seungmanhan <seungman.han@moreh.io>
2024-03-22 15:23:06 +0000

    fix doc build error

--- include/miopen/miopen.h
+++ include/miopen/miopen.h
@@ -5832,6 +5832,7 @@ MIOPEN_EXPORT miopenStatus_t miopenT5LayerNormForward(miopenHandle_t handle,
  * @param weightDesc               Tensor descriptor for data input tensor weight (input)
  * @param rstdDesc                 Tensor descriptor for data input tensor rstd (input)
  * @param dxDesc                   Tensor descriptor for output data tensor dx (input)
+ * @param dwDesc                   Tensor descriptor for output data tensor dw (input)
  * @param sizeInBytes              Pointer to data to return the minimum workspace size
  * @return                         miopenStatus_t
  */
