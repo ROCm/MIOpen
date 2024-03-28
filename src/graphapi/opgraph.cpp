3f919802bee7e34681c8e15b523326ceae99318c
M. Amber Hassaan <amber_474@yahoo.com>
2024-03-28 15:05:00 +0000

    address comments

---
+++ src/graphapi/opgraph.cpp
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#include <miopen/graphapi/opgraph.h>
+
+namespace miopen {
+namespace graphapi {
+
+OpNode::~OpNode() = default;
+
+OpGraph OpGraphBuilder::build()
+{
+
+    OpGraph graph;
+
+    // key = tensor ptr, value = vec. of dest nodes
+    std::unordered_map<Tensor*, EdgeInfo> e_map;
+
+    for(OpNode* n : mNodes)
+    {
+
+        for(Tensor* i : n->getInTensors())
+        {
+            auto [iter, _ignore] =
+                e_map.emplace(i, EdgeInfo{}); // add empty EdgeInfo if not present
+
+            iter->second.mDests.emplace_back(n);
+        }
+
+        for(Tensor* o : n->getOutTensors())
+        {
+            auto [iter, _ignore] =
+                e_map.emplace(o, EdgeInfo{}); // add empty EdgeInfo if not present
+
+            assert(iter->second.mSrc == nullptr);
+            iter->second.mSrc = n;
+        }
+    }
+
+    graph.addNodes(std::move(mNodes));
+
+    for(const auto& [tens_ptr, edge_info] : e_map)
+    {
+
+        if(edge_info.mSrc != nullptr && !edge_info.mDests.empty())
+        {
+            for(const auto& d : edge_info.mDests)
+            {
+                graph.addEdge(edge_info.mSrc, tens_ptr, d);
+            }
+        }
+        else if(edge_info.mSrc == nullptr)
+        {
+
+            // tens_ptr is a non-virtual input tensor
+            assert(!tens_ptr->isVirtual()); // tensor pointer can't be virtual
+            assert(!edge_info.mDests.empty());
+
+            // NOTE(Amber): we may take out this step if we decide not to add a source
+            // and sink in the graph
+            for(const auto& d : edge_info.mDests)
+            {
+                graph.addEdgeFromSrc(d, tens_ptr);
+            }
+        }
+        else if(edge_info.mDests.empty())
+        {
+            // tens_ptr is a non-virtual output tensor
+            assert(!tens_ptr->isVirtual());
+            assert(edge_info.mSrc != nullptr);
+            graph.addEdgeToSink(edge_info.mSrc, tens_ptr);
+        }
+        else
+        {
+            // both can't be true at the same time
+            assert(!(edge_info.mSrc == nullptr && edge_info.mDests.empty()));
+        }
+    }
+
+    return graph;
+}
+
+} // end namespace graphapi
+} // end namespace miopen
--- src/include/miopen/graphapi/opgraph.h
+++ src/include/miopen/graphapi/opgraph.h
@@ -1,15 +1,39 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
 #pragma once
 
 #include <miopen/graphapi/graphapi_tensor.hpp>
 
 #include <algorithm>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 #include <cassert>
 
 namespace miopen {
-
 namespace graphapi {
 
 namespace internal {
@@ -23,10 +47,15 @@ bool contains(const C& container, const typename C::value_type& val) noexcept
 class OpGraphBuilder;
 class OpGraph;
 
-struct OpNode
+class OpNode
 {
-
+public:
     using Edge = std::pair<OpNode*, Tensor*>;
+    virtual ~OpNode();
+
+private:
+    std::vector<Edge> mInEdges;
+    std::vector<Edge> mOutEdges;
 
     friend class OpGraphBuilder;
     friend class OpGraph;
@@ -71,10 +100,6 @@ protected:
             mInEdges.emplace_back(src, tens_ptr);
         }
     }
-
-private:
-    std::vector<Edge> mInEdges;
-    std::vector<Edge> mOutEdges;
 };
 
 using OpEdge = OpNode::Edge;
@@ -82,6 +107,7 @@ using OpEdge = OpNode::Edge;
 class SourceOpNode : public OpNode
 {
 protected:
+    std::vector<Tensor*> mOutTensors;
     friend class OpGraph;
 
     const std::string& signName() const final
@@ -101,14 +127,12 @@ protected:
         assert(!hasOutTensor(tens_ptr));
         mOutTensors.emplace_back(tens_ptr);
     }
-
-private:
-    std::vector<Tensor*> mOutTensors;
 };
 
 class SinkOpNode : public OpNode
 {
 protected:
+    std::vector<Tensor*> mInTensors;
     friend class OpGraph;
 
     const std::string& signName() const final
@@ -128,12 +152,13 @@ protected:
         assert(!hasInTensor(tens_ptr));
         mInTensors.emplace_back(tens_ptr);
     }
-
-    std::vector<Tensor*> mInTensors;
 };
 
 class OpGraph
 {
+    SourceOpNode mSrcNode{};
+    SinkOpNode mSinkNode{};
+    std::vector<OpNode*> mNodes{};
 
 public:
     bool hasNode(OpNode* n) const { return internal::contains(mNodes, n); }
@@ -169,14 +194,12 @@ private:
         mSinkNode.addInTensor(tens_ptr);
         addEdge(src, tens_ptr, &mSinkNode);
     }
-
-    SourceOpNode mSrcNode{};
-    SinkOpNode mSinkNode{};
-    std::vector<OpNode*> mNodes{};
 };
 
 class OpGraphBuilder
 {
+private:
+    std::vector<OpNode*> mNodes;
 
 public:
     bool hasNode(OpNode* node) const { return internal::contains(mNodes, node); }
@@ -193,82 +216,8 @@ public:
         std::vector<OpNode*> mDests{};
     };
 
-    OpGraph build()
-    {
-
-        OpGraph graph;
-
-        // key = tensor ptr, value = vec. of dest nodes
-        std::unordered_map<Tensor*, EdgeInfo> e_map;
-
-        for(OpNode* n : mNodes)
-        {
-
-            for(Tensor* i : n->getInTensors())
-            {
-                auto [iter, _ignore] =
-                    e_map.emplace(i, EdgeInfo{}); // add empty EdgeInfo if not present
-
-                iter->second.mDests.emplace_back(n);
-            }
-
-            for(Tensor* o : n->getOutTensors())
-            {
-                auto [iter, _ignore] =
-                    e_map.emplace(o, EdgeInfo{}); // add empty EdgeInfo if not present
-
-                assert(iter->second.mSrc == nullptr);
-                iter->second.mSrc = n;
-            }
-        }
-
-        graph.addNodes(std::move(mNodes));
-
-        for(const auto& [tens_ptr, edge_info] : e_map)
-        {
-
-            if(edge_info.mSrc != nullptr && !edge_info.mDests.empty())
-            {
-                for(const auto& d : edge_info.mDests)
-                {
-                    graph.addEdge(edge_info.mSrc, tens_ptr, d);
-                }
-            }
-            else if(edge_info.mSrc == nullptr)
-            {
-
-                // tens_ptr is a non-virtual input tensor
-                assert(!tens_ptr->isVirtual()); // tensor pointer can't be virtual
-                assert(!edge_info.mDests.empty());
-
-                // NOTE(Amber): we may take out this step if we decide not to add a source
-                // and sink in the graph
-                for(const auto& d : edge_info.mDests)
-                {
-                    graph.addEdgeFromSrc(d, tens_ptr);
-                }
-            }
-            else if(edge_info.mDests.empty())
-            {
-                // tens_ptr is a non-virtual output tensor
-                assert(!tens_ptr->isVirtual());
-                assert(edge_info.mSrc != nullptr);
-                graph.addEdgeToSink(edge_info.mSrc, tens_ptr);
-            }
-            else
-            {
-                // both can't be true at the same time
-                assert(!(edge_info.mSrc == nullptr && edge_info.mDests.empty()));
-            }
-        }
-
-        return graph;
-    }
-
-private:
-    std::vector<OpNode*> mNodes;
+    OpGraph build();
 };
 
 } // end namespace graphapi
-
 } // end namespace miopen
