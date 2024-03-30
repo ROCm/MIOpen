3fa20250ff5a19cc0f981f812ba529c0ea1303e6
Sergei Grigoriev <102019268+scerzh@users.noreply.github.com>
2024-03-30 22:58:00 +0100

    Implement MIOPEN_BACKEND_REDUCTION_DESCRIPTOR Builder

---
+++ src/include/miopen/graphapi/reduction.hpp
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+#pragma once
+
+#include <miopen/miopen.h>
+
+namespace miopen {
+
+namespace graphapi {
+
+class Reduction
+{
+private:
+    miopenReduceTensorOp_t mReductionOperator = MIOPEN_REDUCE_TENSOR_ADD;
+    miopenDataType_t mCompType                = miopenFloat;
+
+    friend class ReductionBuilder;
+
+public:
+    Reduction() noexcept = default;
+    Reduction(miopenReduceTensorOp_t reductionOperator, miopenDataType_t compType) noexcept
+        : mReductionOperator(reductionOperator), mCompType(compType)
+    {
+    }
+
+    miopenReduceTensorOp_t getReductionOperator() const { return mReductionOperator; }
+    miopenDataType_t getCompType() const { return mCompType; }
+};
+
+class ReductionBuilder
+{
+private:
+    Reduction mReduction;
+    bool mReductionOperatorSet = false;
+    bool mCompTypeSet          = false;
+
+public:
+    ReductionBuilder& setReductionOperator(miopenReduceTensorOp_t reductionOperator) noexcept
+    {
+        mReduction.mReductionOperator = reductionOperator;
+        mReductionOperatorSet         = true;
+        return *this;
+    }
+
+    ReductionBuilder& setCompType(miopenDataType_t compType) noexcept
+    {
+        mReduction.mCompType = compType;
+        mCompTypeSet         = true;
+        return *this;
+    }
+
+    Reduction build();
+};
+
+} // namespace graphapi
+
+} // namespace miopen
