b9d3995ada364bbb11a0a97ab5127eb453161727
Vasilii Filippov <DrizztDoUrden@users.noreply.github.com>
2024-04-04 00:36:26 +0200

    Find 2.0 scalar run-time parameters (#2826)
    * Find 2.0 scalar run-time parameters
    
    * Update include/miopen/miopen.h
    
    Co-authored-by: Artem Tamazov <artem.tamazov@gmail.com>
    
    * Added miopenTensorArgumentIsScalar value serialization
    
    * Fixed build after renaming enum field
    
    * format
    
    * tidy fix
    
    ---------
    
    Co-authored-by: Artem Tamazov <artem.tamazov@gmail.com>

--- include/miopen/miopen.h
+++ include/miopen/miopen.h
@@ -5348,6 +5348,7 @@ typedef enum
     miopenTensorSoftmaxDY    = 31,
 #endif
 
+    miopenTensorArgumentIsScalar = 1U << 31,
 } miopenTensorArgumentId_t;
 
 /*! @enum miopenTensorArgumentId_t
@@ -5563,7 +5564,7 @@ MIOPEN_EXPORT miopenStatus_t miopenFindSolutions(miopenHandle_t handle,
                                                  size_t* numSolutions,
                                                  size_t maxSolutions);
 
-/*! @brief Values of a tensor argument for the miopenRunSolution function.
+/*! @brief Values of a tensor or scalar argument for the miopenRunSolution function.
  */
 struct miopenTensorArgument_t
 {
@@ -5576,7 +5577,8 @@ struct miopenTensorArgument_t
      * is no way to tell from the API. Intended for the future use.
      */
     miopenTensorDescriptor_t* descriptor;
-    /* @brief Pointer to the device memory buffer to use for the operation.
+    /* @brief Pointer to the device memory buffer to use for the operation or to the host memory if
+     * the value is scalar.
      */
     void* buffer;
 };
