8cda6790fe9e47167a3370594fd4f2ee6d7c0498
hieule88 <hieu.le@moreh.com.vn>
2024-04-15 04:23:13 +0000

    fix INPUT OUTPUT TYPE

--- src/solver/nllloss/forward_nllloss.cpp
+++ src/solver/nllloss/forward_nllloss.cpp
@@ -89,8 +89,8 @@ NLLLossForward::GetSolution(const ExecutionContext& context,
             {"MIOPEN_USE_FP32", static_cast<int>(dtype == miopenFloat)},
             {"MIOPEN_USE_FP64", static_cast<int>(dtype == miopenDouble)},
             {"MIOPEN_USE_BFP16", static_cast<int>(dtype == miopenBFloat16)},
-            {"INPUT_TYPE", input_dtype},
-            {"OUTPUT_TYPE", output_dtype},
+            {"INPUT_TYPE", input_dtype == "bfloat16" ? "ushort" : input_dtype},
+            {"OUTPUT_TYPE", output_dtype == "bfloat16" ? "ushort" : output_dtype},
             {"LOCAL_SIZE", LOCAL_SIZE},
         };
 
