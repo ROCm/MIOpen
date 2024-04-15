fcbcc17bcfbc6ab52c1bd6cc47d7d9866931644b
hieule88 <hieu.le@moreh.com.vn>
2024-04-15 06:31:05 +0000

    resolve license and /

--- driver/mloNLLLossHost.hpp
+++ driver/mloNLLLossHost.hpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2023 Advanced Micro Devices, Inc.
+ * Copyright (c) 2024 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,10 +27,6 @@
 #define MLO_NLLLOSSHOST_H_
 
 #include <miopen/tensor.hpp>
-
-////////////////////////////////////////////////////////////
-//
-///////////////////////////////////////////////////////////
 
 template <typename Tgpu, typename Tcheck>
 int32_t mloNLLLossForwardRunHost(miopenTensorDescriptor_t inputDesc,
