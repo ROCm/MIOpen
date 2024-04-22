7e4a6cc88bd97c03f12a6b6b24654942efa7abb6
Bibek Ghimire <bghimire@amd.com>
2024-04-22 05:00:09 +0000

    bg/lwp-730-gpu_ref_kernel: cleanups

--- test/gtest/nonpack_conv3d_fwd.hpp
+++ test/gtest/nonpack_conv3d_fwd.hpp
@@ -49,52 +49,53 @@ struct NonPackTestCase : Conv3DTestCase
     std::vector<size_t> GetInputStrides() { return {i0, i1, i2, i3, i4}; }
     std::vector<size_t> GetWeightStrides() { return {w0, w1, w2, w3, w4}; }
     std::vector<size_t> GetOutputStrides() { return {o0, o1, o2, o3, o4}; }
-    float GetAlpha() {return alpha;}
-    float GetBeta() {return beta;}
+    float GetAlpha() { return alpha; }
+    float GetBeta() { return beta; }
 };
 
 template <>
 std::vector<NonPackTestCase> ConvTestConfigs()
-{          // g  n  c  d  h  w  k  z  y  x  pad_x pad_y pad_z stri_x stri_y stri_z dia_x dia_y dia_z
-    return {{{1, 4, 16, 4, 9, 16, 16, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, miopenConvolution},
-             10240,
-             1,
-             2560,
-             160,
-             16,
-             432,
-             1,
-             144,
-             48,
-             16,
-             9216,
-             1,
-             2304,
-             256,
-             16,
-             1.0,// alpha
-             0.0 // beta
-             },
-            {{1, 1, 64, 3, 16, 16, 128, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, miopenConvolution},
-             65536,
-             1,
-             24000,
-             2048,
-             64,
-             1728,
-             1,
-             576,
-             192,
-             64,
-             98304,
-             1,
-             32768,
-             2048,
-             128,
-             1.0,// alpha
-             0.0 // beta
-             }
-            };
+{ // g  n  c  d  h  w  k  z  y  x  pad_x pad_y pad_z stri_x stri_y stri_z dia_x dia_y dia_z
+    return {{
+                {1, 4, 16, 4, 9, 16, 16, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, miopenConvolution},
+                10240,
+                1,
+                2560,
+                160,
+                16,
+                432,
+                1,
+                144,
+                48,
+                16,
+                9216,
+                1,
+                2304,
+                256,
+                16,
+                1.0, // alpha
+                0.0  // beta
+            },
+            {
+                {1, 1, 64, 3, 16, 16, 128, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, miopenConvolution},
+                65536,
+                1,
+                24000,
+                2048,
+                64,
+                1728,
+                1,
+                576,
+                192,
+                64,
+                98304,
+                1,
+                32768,
+                2048,
+                128,
+                1.0, // alpha
+                0.0  // beta
+            }};
 }
 
 template <typename T = float>
@@ -130,52 +131,28 @@ protected:
     }
     void TearDown() override
     {
-        
+        if(test_skipped)
+            return;
 
         auto&& handle = get_handle();
 
         miopen::TensorDescriptor output_desc =
             conv_desc.GetForwardOutputTensor(input.desc, weights.desc, miopen_type<T>{});
-        ref_out     = tensor<T>{tensor_layout, output_desc.GetLengths()};
-        ref_out     = ref_conv_fwd(input, weights, output, conv_desc, 
-                        static_cast<const void*>(&alpha_val), static_cast<const void*>(&beta_val));
-        if(test_skipped)
-            return;
+        ref_out = tensor<T>{tensor_layout, output_desc.GetLengths()};
+        ref_out = ref_conv_fwd(input,
+                               weights,
+                               output,
+                               conv_desc,
+                               static_cast<const void*>(&alpha_val),
+                               static_cast<const void*>(&beta_val));
+
         output.data = handle.Read<T>(out_dev, output.data.size());
         EXPECT_FALSE(miopen::range_zero(ref_out)) << "Cpu data is all zeros";
         EXPECT_FALSE(miopen::range_zero(output)) << "Gpu data is all zeros";
         EXPECT_TRUE(miopen::range_distance(ref_out) == miopen::range_distance(output));
 
-
-
-
-        std::cout << "\nin  : " << input.data.size() << std::endl;
-        for(const auto& it : input.data)
-        {
-           std::cout << static_cast<float>(it) << ", ";
-        }
-
-        std::cout << "\nwei  : " << weights.data.size() << std::endl;
-        for(const auto& it : weights.data)
-        {
-           std::cout << static_cast<float>(it) << ", ";
-        }
-
-        std::cout << "\noutput  : " << output.data.size() << std::endl;
-        for(const auto& it : output.data)
-        {
-           std::cout << static_cast<float>(it) << ", ";
-        }
-
-        std::cout << "\nref_out  : " << ref_out.data.size() << std::endl;
-        for(const auto& it : ref_out.data)
-        {
-           std::cout << static_cast<float>(it) << ", ";
-        }
-
-
-        // const double tolerance = 80;
-        double threshold       = std::numeric_limits<T>::epsilon();
+        const double tolerance = 80;
+        double threshold       = std::numeric_limits<T>::epsilon() * tolerance;
         auto error             = miopen::rms_range(ref_out, output);
 
         EXPECT_FALSE(miopen::find_idx(ref_out, miopen::not_finite) >= 0)
@@ -183,9 +160,6 @@ protected:
 
         EXPECT_TRUE(error < threshold)
             << "Error beyond tolerance Error:" << error << ",  Threshold: " << threshold;
-        std::cout << "Exiting early\n" << error << std::endl;
-        std::cout << "Exiting early threshold\n" << threshold << std::endl;
-        // exit(1);
     }
     NonPackTestCase conv_config;
     miopen::ConvolutionDescriptor conv_desc;
@@ -195,7 +169,6 @@ protected:
     tensor<T> ref_out;
     float alpha_val;
     float beta_val;
-
 
     miopen::Allocator::ManageDataPtr in_dev;
     miopen::Allocator::ManageDataPtr wei_dev;
