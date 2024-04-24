ae57240759d00d0a478ed43e9621f267f188d199
Vsevolod Golovko <vsevolod.golovko2@dxc.com>
2024-04-24 18:26:56 +0200

    tensors creation mainly done

--- test/gtest/graphapi_capi_mha_forward.cpp
+++ test/gtest/graphapi_capi_mha_forward.cpp
@@ -74,7 +74,7 @@ private:
 typedef std::unique_ptr<DescriptorWrapper> DescriptorWrapperPtr;
 
 DescriptorWrapperPtr
-MakeTensorDescriptor(int64_t uniqueId, int64_t n = 1, int64_t h = 1, int64_t s = 1, int64_t d = 1)
+MakeTensorDescriptor(int64_t uniqueId, bool isVirtual = false, int64_t n = 1, int64_t h = 1, int64_t s = 1, int64_t d = 1)
 {
     DescriptorWrapperPtr descWrapperPtr =
         std::make_unique<DescriptorWrapper>(MIOPEN_BACKEND_TENSOR_DESCRIPTOR);
@@ -110,16 +110,75 @@ MakeTensorDescriptor(int64_t uniqueId, int64_t n = 1, int64_t h = 1, int64_t s =
             descriptor, MIOPEN_ATTR_TENSOR_BYTE_ALIGNMENT, MIOPEN_TYPE_INT64, 1, &alignment),
         "miopenBackendSetAttribute for MIOPEN_ATTR_TENSOR_BYTE_ALIGNMENT failed");
 
+    CheckStatusAndThrow(
+        miopenBackendSetAttribute(
+            descriptor, MIOPEN_ATTR_TENSOR_IS_VIRTUAL, MIOPEN_TYPE_BOOLEAN, 1, &isVirtual),
+        "miopenBackendSetAttribute for MIOPEN_ATTR_TENSOR_IS_VIRTUAL failed");
+
     CheckStatusAndThrow(miopenBackendFinalize(descriptor),
                         "miopenBackendFinalize for Tensor descriptor failed");
 
     return descWrapperPtr;
 }
 
+int64_t GetNextId()
+{
+    static int64_t counter = 0;
+
+    return counter++;
+}
+
 TEST(TestCGraphApi, MhaForward)
 {
+    const int64_t test_n = 2;
+    const int64_t test_h = 4;
+    const int64_t test_s = 8;
+    const int64_t test_d = 16;
+
     try
     {
+        // real tensors
+        auto k = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, test_d);
+        auto v = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, test_d);
+        auto q = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, test_d);
+        auto descale_k = MakeTensorDescriptor(GetNextId());
+        auto descale_q = MakeTensorDescriptor(GetNextId());
+        auto descale_v = MakeTensorDescriptor(GetNextId());
+        auto descale_s = MakeTensorDescriptor(GetNextId());
+        auto scale_s = MakeTensorDescriptor(GetNextId());
+        auto scale_d = MakeTensorDescriptor(GetNextId());
+
+        auto d_p = MakeTensorDescriptor(GetNextId());
+        auto d_s = MakeTensorDescriptor(GetNextId());
+        auto d_o = MakeTensorDescriptor(GetNextId());
+
+        // This scale param is just a float in Find 2.0
+        auto atn_scl = MakeTensorDescriptor(GetNextId());
+
+        auto o = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, test_d);
+        auto amax_o = MakeTensorDescriptor(GetNextId());
+        auto amax_s = MakeTensorDescriptor(GetNextId());
+        auto m = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, 1);
+        auto z_inv = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, 1);
+
+        // virtual tensors
+        auto t_mm_0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pw_s_0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pw_s_1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pw_s_2 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+
+        auto t_sub = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto t_exp = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto t_sum = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, 1);
+        auto t_mult_0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto t_rnd = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto t_mult_1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pw_s_3 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pw_s_4 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+
+        auto t_mm_1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
+        auto pw_s_5 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
+        auto pw_s_6 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
     }
     catch(const miopen::Exception& ex)
     {
