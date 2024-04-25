c6275d4dacb65302b95a47b1f6e95f4b5d73b881
Vsevolod Golovko <vsevolod.golovko2@dxc.com>
2024-04-25 18:48:35 +0200

    pwscale node and matmul node creation

--- test/gtest/graphapi_capi_mha_forward.cpp
+++ test/gtest/graphapi_capi_mha_forward.cpp
@@ -43,6 +43,9 @@ miopenStatus_t CheckStatusAndThrow(miopenStatus_t status, const std::string& msg
     return status;
 }
 
+class DescriptorWrapper;
+typedef std::shared_ptr<DescriptorWrapper> DescriptorWrapperPtr;
+
 class DescriptorWrapper
 {
 public:
@@ -56,11 +59,35 @@ public:
 
     ~DescriptorWrapper()
     {
+        m_refsToKeep.clear();
+
         EXPECT_NE(m_descriptor, nullptr) << "m_descriptor is nullptr";
 
         miopenStatus_t status = miopenBackendDestroyDescriptor(m_descriptor);
         EXPECT_EQ(status, miopenStatusSuccess)
             << "Error while destroying descriptor, type: " << m_descriptorType;
+    }
+
+    void SetAttribute(miopenBackendAttributeName_t attributeName,
+                        miopenBackendAttributeType_t attributeType,
+                        int64_t elementCount,
+                        void* arrayOfElements)
+    {
+        miopenStatus_t status = miopenBackendSetAttribute(m_descriptor, attributeName, attributeType, elementCount, arrayOfElements);
+
+        CheckStatusAndThrow(status, "miopenBackendSetAttribute failed: descriptorType = " + std::to_string(m_descriptorType) + 
+        ", attributeName=" + std::to_string(attributeName) + 
+        ", attributeType=" + std::to_string(attributeType));
+    }
+
+    void Finalize()
+    {
+        CheckStatusAndThrow(miopenBackendFinalize(m_descriptor), "miopenBackendFinalize failed: descriptorType = " + std::to_string(m_descriptorType)); 
+    }
+
+    void AddRef(DescriptorWrapperPtr refToKeep)
+    {
+        m_refsToKeep.push_back(refToKeep);
     }
 
     miopenBackendDescriptor_t GetDescriptor() const { return m_descriptor; }
@@ -69,15 +96,19 @@ public:
 private:
     miopenBackendDescriptorType_t m_descriptorType;
     miopenBackendDescriptor_t m_descriptor;
+
+    std::vector<DescriptorWrapperPtr> m_refsToKeep;
 };
 
-typedef std::unique_ptr<DescriptorWrapper> DescriptorWrapperPtr;
+DescriptorWrapperPtr MakeDescriptor(miopenBackendDescriptorType_t descriptorType)
+{
+    return std::make_shared<DescriptorWrapper>(descriptorType);
+}
 
 DescriptorWrapperPtr
 MakeTensorDescriptor(int64_t uniqueId, bool isVirtual = false, int64_t n = 1, int64_t h = 1, int64_t s = 1, int64_t d = 1)
 {
-    DescriptorWrapperPtr descWrapperPtr =
-        std::make_unique<DescriptorWrapper>(MIOPEN_BACKEND_TENSOR_DESCRIPTOR);
+    DescriptorWrapperPtr descWrapperPtr = MakeDescriptor(MIOPEN_BACKEND_TENSOR_DESCRIPTOR);
 
     miopenBackendDescriptor_t descriptor = descWrapperPtr->GetDescriptor();
 
@@ -121,11 +152,67 @@ MakeTensorDescriptor(int64_t uniqueId, bool isVirtual = false, int64_t n = 1, in
     return descWrapperPtr;
 }
 
+
+// just simple id generator, might be redone if necessary
 int64_t GetNextId()
 {
     static int64_t counter = 0;
 
     return counter++;
+}
+
+DescriptorWrapperPtr MakeMatmul(DescriptorWrapperPtr tensor1, DescriptorWrapperPtr tensor2, DescriptorWrapperPtr output)
+{
+    DescriptorWrapperPtr matmul = MakeDescriptor(MIOPEN_BACKEND_MATMUL_DESCRIPTOR);
+
+    miopenDataType_t dType = miopenFloat;
+    matmul->SetAttribute(MIOPEN_ATTR_MATMUL_COMP_TYPE, MIOPEN_TYPE_DATA_TYPE, 1, &dType);
+    matmul->Finalize();
+
+    miopenBackendDescriptor_t childDesc = matmul->GetDescriptor();
+
+    miopenBackendDescriptor_t tensor1Desc = tensor1->GetDescriptor();
+    miopenBackendDescriptor_t tensor2Desc = tensor2->GetDescriptor();
+    miopenBackendDescriptor_t outputDesc = output->GetDescriptor();
+
+    DescriptorWrapperPtr matmulOperation = MakeDescriptor(MIOPEN_BACKEND_OPERATION_MATMUL_DESCRIPTOR);
+    matmulOperation->SetAttribute(MIOPEN_ATTR_OPERATION_MATMUL_DESC, MIOPEN_TYPE_BACKEND_DESCRIPTOR, 1, &childDesc);
+    matmulOperation->SetAttribute(MIOPEN_ATTR_OPERATION_MATMUL_ADESC, MIOPEN_TYPE_BACKEND_DESCRIPTOR, 1, &tensor1Desc);
+    matmulOperation->SetAttribute(MIOPEN_ATTR_OPERATION_MATMUL_BDESC, MIOPEN_TYPE_BACKEND_DESCRIPTOR, 1, &tensor2Desc);
+    matmulOperation->SetAttribute(MIOPEN_ATTR_OPERATION_MATMUL_CDESC, MIOPEN_TYPE_BACKEND_DESCRIPTOR, 1, &outputDesc);
+
+    matmulOperation->AddRef(matmul);
+
+    matmulOperation->Finalize();
+
+    return matmulOperation;
+}
+
+DescriptorWrapperPtr MakePwScale(DescriptorWrapperPtr tensor1, DescriptorWrapperPtr tensor2, DescriptorWrapperPtr output)
+{
+    DescriptorWrapperPtr pointwise = MakeDescriptor(MIOPEN_BACKEND_POINTWISE_DESCRIPTOR);
+
+    miopenPointwiseMode_t mode = MIOPEN_POINTWISE_MUL;
+    pointwise->SetAttribute(MIOPEN_ATTR_POINTWISE_MODE, MIOPEN_TYPE_POINTWISE_MODE, 1, &mode);
+    pointwise->Finalize();
+
+    miopenBackendDescriptor_t childDesc = pointwise->GetDescriptor();
+
+    miopenBackendDescriptor_t tensor1Desc = tensor1->GetDescriptor();
+    miopenBackendDescriptor_t tensor2Desc = tensor2->GetDescriptor();
+    miopenBackendDescriptor_t outputDesc = output->GetDescriptor();
+
+    DescriptorWrapperPtr pointwiseOperation = MakeDescriptor(MIOPEN_BACKEND_OPERATION_POINTWISE_DESCRIPTOR);
+    pointwiseOperation->SetAttribute(MIOPEN_ATTR_OPERATION_POINTWISE_PW_DESCRIPTOR, MIOPEN_TYPE_BACKEND_DESCRIPTOR, 1, &childDesc);
+    pointwiseOperation->SetAttribute(MIOPEN_ATTR_OPERATION_POINTWISE_XDESC, MIOPEN_TYPE_BACKEND_DESCRIPTOR, 1, &tensor1Desc);
+    pointwiseOperation->SetAttribute(MIOPEN_ATTR_OPERATION_POINTWISE_BDESC, MIOPEN_TYPE_BACKEND_DESCRIPTOR, 1, &tensor2Desc);
+    pointwiseOperation->SetAttribute(MIOPEN_ATTR_OPERATION_POINTWISE_YDESC, MIOPEN_TYPE_BACKEND_DESCRIPTOR, 1, &outputDesc);
+
+    pointwiseOperation->AddRef(pointwise);
+
+    pointwiseOperation->Finalize();
+
+    return pointwiseOperation;
 }
 
 TEST(TestCGraphApi, MhaForward)
@@ -141,44 +228,47 @@ TEST(TestCGraphApi, MhaForward)
         auto k = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, test_d);
         auto v = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, test_d);
         auto q = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, test_d);
-        auto descale_k = MakeTensorDescriptor(GetNextId());
-        auto descale_q = MakeTensorDescriptor(GetNextId());
-        auto descale_v = MakeTensorDescriptor(GetNextId());
-        auto descale_s = MakeTensorDescriptor(GetNextId());
-        auto scale_s = MakeTensorDescriptor(GetNextId());
-        auto scale_d = MakeTensorDescriptor(GetNextId());
+        auto descaleK = MakeTensorDescriptor(GetNextId());
+        auto descaleQ = MakeTensorDescriptor(GetNextId());
+        auto descaleV = MakeTensorDescriptor(GetNextId());
+        auto descaleS = MakeTensorDescriptor(GetNextId());
+        auto scaleS = MakeTensorDescriptor(GetNextId());
+        auto scaleD = MakeTensorDescriptor(GetNextId());
 
-        auto d_p = MakeTensorDescriptor(GetNextId());
-        auto d_s = MakeTensorDescriptor(GetNextId());
-        auto d_o = MakeTensorDescriptor(GetNextId());
+        auto dp = MakeTensorDescriptor(GetNextId());
+        auto ds = MakeTensorDescriptor(GetNextId());
+        auto doff = MakeTensorDescriptor(GetNextId());
 
         // This scale param is just a float in Find 2.0
-        auto atn_scl = MakeTensorDescriptor(GetNextId());
+        auto atnScl = MakeTensorDescriptor(GetNextId());
 
         auto o = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, test_d);
-        auto amax_o = MakeTensorDescriptor(GetNextId());
-        auto amax_s = MakeTensorDescriptor(GetNextId());
+        auto amaxO = MakeTensorDescriptor(GetNextId());
+        auto amaxS = MakeTensorDescriptor(GetNextId());
         auto m = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, 1);
-        auto z_inv = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, 1);
+        auto zinv = MakeTensorDescriptor(GetNextId(), false, test_n, test_h, test_s, 1);
 
         // virtual tensors
-        auto t_mm_0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto pw_s_0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto pw_s_1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto pw_s_2 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto tMM0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pwS0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pwS1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pwS2 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
 
-        auto t_sub = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto t_exp = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto t_sum = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, 1);
-        auto t_mult_0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto t_rnd = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto t_mult_1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto pw_s_3 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
-        auto pw_s_4 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto tSub = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto tExp = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto tSum = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, 1);
+        auto tMult0 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto tRnd = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto tMult1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pwS3 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
+        auto pwS4 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_s);
 
-        auto t_mm_1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
-        auto pw_s_5 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
-        auto pw_s_6 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
+        auto tMM1 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
+        auto pwS5 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
+        auto pwS6 = MakeTensorDescriptor(GetNextId(), true, test_n, test_h, test_s, test_d);
+
+        auto matmul1 = MakeMatmul(q, k, tMM0);
+        auto pwScale1 = MakePwScale(tMM0, atnScl, pwS0);
     }
     catch(const miopen::Exception& ex)
     {
