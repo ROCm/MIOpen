75fa112867560ba7e1224f557ddef8789b30feb3
Alexey Akimov <kikimych@gmail.com>
2024-04-26 22:38:26 +0300

    [MHA] Implement MIOPEN_BACKEND_OPERATION_MATMUL_DESCRIPTOR (#2902)

--- src/include/miopen/graphapi/matmul.hpp
+++ src/include/miopen/graphapi/matmul.hpp
@@ -25,6 +25,9 @@
  *******************************************************************************/
 #include <miopen/graphapi/graphapi.hpp>
 
+#include <miopen/graphapi/tensor.hpp>
+#include <miopen/graphapi/opgraph.hpp>
+
 namespace miopen {
 namespace graphapi {
 
@@ -80,6 +83,114 @@ public:
 
     const Matmul* getMatmul() const noexcept { return &mMatmul; }
     Matmul* getMatmul() noexcept { return &mMatmul; }
+};
+
+class OperationMatmul : public OpNode
+{
+private:
+    Tensor* mA;
+    Tensor* mB;
+    Tensor* mC;
+    int64_t mBatchCount = 1;
+    Tensor* mGemmMOverride;
+    Tensor* mGemmNOverride;
+    Tensor* mGemmKOverride;
+    Matmul* mMatmul;
+
+public:
+    OperationMatmul(Tensor* A,
+                    Tensor* B,
+                    Tensor* C,
+                    int batchCount,
+                    Tensor* MOverride,
+                    Tensor* NOverride,
+                    Tensor* KOverride,
+                    Matmul* matmul) noexcept
+        : mA(A),
+          mB(B),
+          mC(C),
+          mBatchCount(batchCount),
+          mGemmMOverride(MOverride),
+          mGemmNOverride(NOverride),
+          mGemmKOverride(KOverride),
+          mMatmul(matmul)
+    {
+    }
+
+    OperationMatmul() = default;
+    Tensor* getA() const { return mA; }
+    Tensor* getB() const { return mB; }
+    Tensor* getC() const { return mC; }
+    int64_t getBatchCount() const { return mBatchCount; }
+    Tensor* getMOverride() { return mGemmMOverride; }
+    Tensor* getNOverride() { return mGemmNOverride; }
+    Tensor* getKOverride() { return mGemmKOverride; }
+    Matmul* getMatmul() { return mMatmul; }
+    virtual std::vector<Tensor*> getInTensors() const override { return {getA(), getB()}; }
+    virtual std::vector<Tensor*> getOutTensors() const override { return {getC()}; }
+    virtual const std::string& signName() const override
+    {
+        static const std::string name = "OP_MATMUL";
+        return name;
+    }
+
+private:
+    friend class OperationMatmulBuilder;
+};
+
+class OperationMatmulBuilder
+{
+private:
+    OperationMatmul mOperationMatmul;
+    bool mASet      = false;
+    bool mBSet      = false;
+    bool mCSet      = false;
+    bool mMatmulSet = false;
+
+public:
+    OperationMatmulBuilder& setA(Tensor* A);
+
+    OperationMatmulBuilder& setB(Tensor* B);
+
+    OperationMatmulBuilder& setC(Tensor* C);
+
+    OperationMatmulBuilder& setBatchCount(int64_t count);
+
+    OperationMatmulBuilder& setGemmMOverride(Tensor* overrideTensor);
+
+    OperationMatmulBuilder& setGemmNOverride(Tensor* overrideTensor);
+
+    OperationMatmulBuilder& setGemmKOverride(Tensor* overrideTensor);
+
+    OperationMatmulBuilder& setMatmulDescriptor(Matmul* mMatmul);
+
+    OperationMatmul build();
+};
+
+class BackendOperationMatmulDescriptor : public BackendDescriptor
+{
+private:
+    OperationMatmulBuilder mBuilder;
+    OperationMatmul mMatmul;
+    miopenBackendDescriptor_t mA               = nullptr;
+    miopenBackendDescriptor_t mB               = nullptr;
+    miopenBackendDescriptor_t mC               = nullptr;
+    miopenBackendDescriptor_t mGemmMOverride   = nullptr;
+    miopenBackendDescriptor_t mGemmNOverride   = nullptr;
+    miopenBackendDescriptor_t mGemmKOverride   = nullptr;
+    miopenBackendDescriptor_t mMatmuDescriptor = nullptr;
+
+public:
+    virtual void setAttribute(miopenBackendAttributeName_t attributeName,
+                              miopenBackendAttributeType_t attributeType,
+                              int64_t elementCount,
+                              void* arrayOfElements) override;
+    virtual void finalize() override;
+    virtual void getAttribute(miopenBackendAttributeName_t attributeName,
+                              miopenBackendAttributeType_t attributeType,
+                              int64_t requestedElementCount,
+                              int64_t* elementCount,
+                              void* arrayOfElements) override;
 };
 
 } // namespace graphapi
