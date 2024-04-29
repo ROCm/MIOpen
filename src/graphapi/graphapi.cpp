ROCm/MIOpen

commit 7d5eda40837394e06e7440038eac2abd912727ee

[MHA] Implement MIOPEN_BACKEND_OPERATIONGRAPH_DESCRIPTOR C API Interface (#2894)

src/graphapi/graphapi.cpp
@@ -26,6 +26,7 @@
 #include <miopen/errors.hpp>
 #include <miopen/graphapi/convolution.hpp>
 #include <miopen/graphapi/graphapi.hpp>
+#include <miopen/graphapi/opgraph.hpp>
 #include <miopen/graphapi/pointwise.hpp>
 #include <miopen/graphapi/reduction.hpp>
 #include <miopen/graphapi/rng.hpp>
@@ -42,7 +43,7 @@ miopenBackendCreateDescriptor(miopenBackendDescriptorType_t descriptorType,
 {
     MIOPEN_LOG_FUNCTION(descriptorType, descriptor);
     return miopen::try_([&] {
-        auto& outputDesciptor = miopen::deref(descriptor);
+        auto& outputDescriptor = miopen::deref(descriptor);
 
         switch(descriptorType)
         {
@@ -53,43 +54,46 @@ miopenBackendCreateDescriptor(miopenBackendDescriptorType_t descriptorType,
          */
         // clang-format off
         case MIOPEN_BACKEND_CONVOLUTION_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendConvolutionDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendConvolutionDescriptor(); break;
 
         case MIOPEN_BACKEND_OPERATION_CONVOLUTION_FORWARD_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendOperationConvolutionForwardDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendOperationConvolutionForwardDescriptor(); break;
 
         case MIOPEN_BACKEND_OPERATION_CONVOLUTION_BACKWARD_FILTER_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendOperationConvolutionBackwardFilterDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendOperationConvolutionBackwardFilterDescriptor(); break;
 
         case MIOPEN_BACKEND_OPERATION_CONVOLUTION_BACKWARD_DATA_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendOperationConvolutionBackwardDataDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendOperationConvolutionBackwardDataDescriptor(); break;
 
         case MIOPEN_BACKEND_OPERATION_POINTWISE_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendOperationPointwiseDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendOperationPointwiseDescriptor(); break;
 
         case MIOPEN_BACKEND_OPERATION_REDUCTION_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendOperationReductionDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendOperationReductionDescriptor(); break;
 
         case MIOPEN_BACKEND_OPERATION_RNG_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendOperationRngDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendOperationRngDescriptor(); break;
+
+        case MIOPEN_BACKEND_OPERATIONGRAPH_DESCRIPTOR:
+            outputDescriptor = new miopen::graphapi::BackendOperationGraphDescriptor(); break;
 
         case MIOPEN_BACKEND_POINTWISE_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendPointwiseDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendPointwiseDescriptor(); break;
 
         case MIOPEN_BACKEND_REDUCTION_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendReductionDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendReductionDescriptor(); break;
 
         case MIOPEN_BACKEND_RNG_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendRngDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendRngDescriptor(); break;
 
         case MIOPEN_BACKEND_TENSOR_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendTensorDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendTensorDescriptor(); break;
 
         case MIOPEN_BACKEND_VARIANT_PACK_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendVariantPackDescriptor(); break;
+            outputDescriptor = new miopen::graphapi::BackendVariantPackDescriptor(); break;
 
         case MIOPEN_BACKEND_MATMUL_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendMatmulDescriptor();
+            outputDescriptor = new miopen::graphapi::BackendMatmulDescriptor();
             break;
 
         case MIOPEN_BACKEND_OPERATION_MATMUL_DESCRIPTOR:
@@ -231,6 +235,9 @@ extern "C" miopenStatus_t miopenBackendInitialize(miopenBackendDescriptor_t desc
 
         case MIOPEN_BACKEND_OPERATION_RNG_DESCRIPTOR:
             initializeBackendDescriptor<miopen::graphapi::BackendOperationRngDescriptor>(descriptor, sizeInBytes); break;
+
+        case MIOPEN_BACKEND_OPERATIONGRAPH_DESCRIPTOR:
+            initializeBackendDescriptor<miopen::graphapi::BackendOperationGraphDescriptor>(descriptor, sizeInBytes); break;
 
         case MIOPEN_BACKEND_POINTWISE_DESCRIPTOR:
             initializeBackendDescriptor<miopen::graphapi::BackendPointwiseDescriptor>(descriptor, sizeInBytes); break;

src/graphapi/opgraph.cpp
@@ -35,10 +35,18 @@ namespace graphapi {
 
 OpNode::~OpNode() = default;
 
+void OpGraphBuilder::setHandle(miopenHandle_t handle) { mHandle = checkPtr(handle); }
+
 OpGraph OpGraphBuilder::build() &&
 {
+    if(mHandle == nullptr || mNodes.empty())
+    {
+        MIOPEN_THROW(miopenStatusBadParm);
+    }
 
     OpGraph graph;
+
+    graph.mHandle = mHandle;
 
     // key = tensor ptr, value = vec. of dest nodes
     /// \todo might eventually move this state to the graph class during build()
@@ -308,6 +316,135 @@ bool isIsomorphic(const OpGraph& left, const OpGraph& right)
     }
 
     return true;
+}
+
+void BackendOperationGraphDescriptor::setAttribute(miopenBackendAttributeName_t attributeName,
+                                                   miopenBackendAttributeType_t attributeType,
+                                                   int64_t elementCount,
+                                                   void* arrayOfElements)
+{
+    if(mFinalized)
+    {
+        MIOPEN_THROW(miopenStatusNotInitialized);
+    }
+
+    switch(attributeName)
+    {
+    case MIOPEN_ATTR_OPERATIONGRAPH_HANDLE:
+        if(attributeType == MIOPEN_TYPE_HANDLE && elementCount == 1)
+        {
+            mBuilder.setHandle(*static_cast<miopenHandle_t*>(arrayOfElements));
+        }
+        else
+        {
+            MIOPEN_THROW(miopenStatusBadParm);
+        }
+        break;
+
+    case MIOPEN_ATTR_OPERATIONGRAPH_OPS:
+        if(attributeType == MIOPEN_TYPE_BACKEND_DESCRIPTOR && elementCount > 0)
+        {
+            std::vector<miopenBackendDescriptor_t> descriptors;
+            descriptors.reserve(elementCount);
+            std::vector<OpNode*> nodes;
+            nodes.reserve(elementCount);
+
+            std::for_each_n(static_cast<miopenBackendDescriptor_t*>(arrayOfElements),
+                            elementCount,
+                            [&descriptors, &nodes](miopenBackendDescriptor_t apiDescriptor) {
+                                BackendDescriptor& backendDescriptor = deref(apiDescriptor);
+                                if(backendDescriptor.isFinalized())
+                                {
+                                    descriptors.push_back(apiDescriptor);
+                                    nodes.push_back(backendDescriptor.getOperation());
+                                }
+                                else
+                                {
+                                    MIOPEN_THROW(miopenStatusBadParm);
+                                }
+                            });
+
+            if(!internal::noRepetitions(nodes))
+            {
+                MIOPEN_THROW(miopenStatusBadParm);
+            }
+
+            mBuilder.setNodes(std::move(nodes));
+            mOps = std::move(descriptors);
+        }
+        else
+        {
+            MIOPEN_THROW(miopenStatusBadParm);
+        }
+        break;
+
+    default: MIOPEN_THROW(miopenStatusBadParm);
+    }
+}
+
+void BackendOperationGraphDescriptor::finalize()
+{
+    if(mFinalized)
+    {
+        MIOPEN_THROW(miopenStatusNotInitialized);
+    }
+    mOpGraph   = std::move(mBuilder).build();
+    mFinalized = true;
+}
+
+void BackendOperationGraphDescriptor::getAttribute(miopenBackendAttributeName_t attributeName,
+                                                   miopenBackendAttributeType_t attributeType,
+                                                   int64_t requestedElementCount,
+                                                   int64_t* elementCount,
+                                                   void* arrayOfElements)
+{
+    if(!mFinalized)
+    {
+        MIOPEN_THROW(miopenStatusNotInitialized);
+    }
+
+    switch(attributeName)
+    {
+    case MIOPEN_ATTR_OPERATIONGRAPH_HANDLE:
+        if(attributeType == MIOPEN_TYPE_HANDLE && requestedElementCount == 1)
+        {
+            *elementCount                                  = 1;
+            *static_cast<miopenHandle_t*>(arrayOfElements) = mOpGraph.getHandle();
+        }
+        else
+        {
+            MIOPEN_THROW(miopenStatusBadParm);
+        }
+        break;
+
+    case MIOPEN_ATTR_OPERATIONGRAPH_OPS:
+        if(attributeType == MIOPEN_TYPE_BACKEND_DESCRIPTOR && requestedElementCount >= 0)
+        {
+            *elementCount = mOps.size();
+            std::copy_n(mOps.cbegin(),
+                        std::min(*elementCount, requestedElementCount),
+                        static_cast<miopenBackendDescriptor_t*>(arrayOfElements));
+        }
+        else
+        {
+            MIOPEN_THROW(miopenStatusBadParm);
+        }
+        break;
+
+    case MIOPEN_ATTR_OPERATIONGRAPH_ENGINE_GLOBAL_COUNT:
+        if(attributeType == MIOPEN_TYPE_INT64 && requestedElementCount == 1)
+        {
+            *elementCount                           = 1;
+            *static_cast<int64_t*>(arrayOfElements) = mOpGraph.getEngines().size();
+        }
+        else
+        {
+            MIOPEN_THROW(miopenStatusBadParm);
+        }
+        break;
+
+    default: MIOPEN_THROW(miopenStatusBadParm);
+    }
 }
 
 } // end namespace graphapi

src/include/miopen/graphapi/engine.hpp
@@ -1,0 +1,58 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#pragma once
+
+#include <miopen/graphapi/graphapi.hpp>
+#include <miopen/solution.hpp>
+
+namespace miopen {
+
+namespace graphapi {
+
+class Engine
+{
+private:
+    Solution mSolution;
+    friend class EngineBuilder;
+
+public:
+    Engine()              = default;
+    Engine(const Engine&) = default;
+    Engine(Engine&&)      = default;
+    Engine& operator=(const Engine&) = default;
+    Engine& operator=(Engine&&) = default;
+
+    Engine(const Solution& solution) : mSolution(solution) {}
+    Engine(Solution&& solution) : mSolution(std::move(solution)) {}
+
+    const Solution& getSolution() const { return mSolution; }
+    Solution& getSolution() { return mSolution; }
+};
+
+} // namespace graphapi
+
+} // namespace miopen

src/include/miopen/graphapi/opgraph.hpp
@@ -25,11 +25,13 @@
  *******************************************************************************/
 #pragma once
 
+#include <miopen/graphapi/engine.hpp>
 #include <miopen/graphapi/tensor.hpp>
 
 #include <algorithm>
 #include <memory>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 #include <cassert>
@@ -42,6 +44,40 @@ template <typename C, typename T>
 bool contains(const C& container, const T& val) noexcept
 {
     return std::find(container.cbegin(), container.cend(), val) != container.cend();
+}
+
+template <typename R, int threshold = 500>
+bool noRepetitions(const R& r)
+{
+    auto begin = r.cbegin();
+    auto end   = r.cend();
+
+    if(std::distance(begin, end) < threshold)
+    {
+        // time = O(n^2) mem = O(1)
+        bool result = true;
+        while(result && begin != end)
+        {
+            const auto& val = *begin++;
+            result          = std::find(begin, end, val) == end;
+        }
+        return result;
+    }
+    else
+    {
+        // time = O(n) mem = O(n)
+        std::unordered_set<std::remove_cv_t<std::remove_reference_t<decltype(*begin)>>> seen;
+        for(; begin != end; ++begin)
+        {
+            const auto& val = *begin;
+            if(seen.find(val) != seen.end())
+            {
+                return false;
+            }
+            seen.insert(val);
+        }
+        return true;
+    }
 }
 } // end namespace internal
 
@@ -179,6 +215,10 @@ class OpGraph
     std::unique_ptr<SinkOpNode> mSinkNode  = std::make_unique<SinkOpNode>();
     std::vector<OpNode*> mNodes{};
 
+    // Descriptor related members
+    miopenHandle_t mHandle = nullptr;
+    std::vector<Engine> mEngines;
+
 public:
     OpGraph(const OpGraph&) = delete;
     OpGraph& operator=(const OpGraph&) = delete;
@@ -247,6 +287,9 @@ public:
         return hasEdge(src, tens_ptr, mSinkNode.get());
     }
 
+    miopenHandle_t getHandle() const noexcept { return mHandle; }
+    const std::vector<Engine>& getEngines() const noexcept { return mEngines; }
+
 private:
     friend class OpGraphBuilder;
 
@@ -277,14 +320,29 @@ class OpGraphBuilder
 {
 private:
     std::vector<OpNode*> mNodes;
+    miopenHandle_t mHandle = nullptr;
 
 public:
+    void setHandle(miopenHandle_t handle);
+
     bool hasNode(OpNode* node) const { return internal::contains(mNodes, node); }
 
     void addNode(OpNode* node)
     {
         assert(!hasNode(node));
         mNodes.emplace_back(node);
+    }
+
+    void setNodes(const std::vector<OpNode*>& nodes)
+    {
+        assert(internal::noRepetitions(nodes));
+        mNodes = nodes;
+    }
+
+    void setNodes(std::vector<OpNode*>&& nodes)
+    {
+        assert(internal::noRepetitions(nodes));
+        mNodes = std::move(nodes);
     }
 
     struct EdgeInfo
@@ -300,6 +358,29 @@ public:
 bool isIsomorphic(const OpGraph& left, const OpGraph& right);
 
 std::string pathToStr(const Path& path);
+
+class BackendOperationGraphDescriptor : public BackendDescriptor
+{
+private:
+    OpGraphBuilder mBuilder;
+    OpGraph mOpGraph;
+    std::vector<miopenBackendDescriptor_t> mOps; // to return them in getAttribute
+
+public:
+    void setAttribute(miopenBackendAttributeName_t attributeName,
+                      miopenBackendAttributeType_t attributeType,
+                      int64_t elementCount,
+                      void* arrayOfElements) override;
+    void finalize() override;
+    void getAttribute(miopenBackendAttributeName_t attributeName,
+                      miopenBackendAttributeType_t attributeType,
+                      int64_t requestedElementCount,
+                      int64_t* elementCount,
+                      void* arrayOfElements) override;
+
+    const OpGraph* getOperationGraph() const noexcept { return &mOpGraph; }
+    OpGraph* getOperationGraph() noexcept { return &mOpGraph; }
+};
 
 } // end namespace graphapi
 } // end namespace miopen

test/gtest/graphapi_operationgraph_descriptor.cpp
@@ -1,0 +1,179 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#include <miopen/graphapi/opgraph.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "graphapi_gtest_common.hpp"
+
+namespace {
+
+using miopen::graphapi::BackendDescriptor;
+using miopen::graphapi::OpNode;
+using miopen::graphapi::Tensor;
+
+static int64_t id = 0;
+
+class GMockNode : public OpNode
+{
+private:
+    std::shared_ptr<Tensor> mIn;
+    std::shared_ptr<Tensor> mOut;
+
+public:
+    GMockNode()
+        : mIn(std::make_shared<Tensor>(miopenFloat,
+                                       std::vector<int64_t>{8, 64, 64},
+                                       std::vector<int64_t>{64 * 64, 64, 1},
+                                       ++id,
+                                       false)),
+          mOut(std::make_shared<Tensor>(miopenFloat,
+                                        std::vector<int64_t>{8, 64, 64},
+                                        std::vector<int64_t>{64 * 64, 64, 1},
+                                        ++id,
+                                        false))
+    {
+    }
+
+    std::vector<Tensor*> getInTensors() const override { return {mIn.get()}; }
+
+    std::vector<Tensor*> getOutTensors() const override { return {mOut.get()}; }
+
+    const std::string& signName() const override
+    {
+        static const std::string name = "OP_MOCK";
+        return name;
+    }
+};
+
+class GMockBackendOperationDescriptor : public BackendDescriptor
+{
+private:
+    GMockNode mNode;
+
+public:
+    GMockBackendOperationDescriptor(bool finalized) { mFinalized = finalized; }
+    void setAttribute(miopenBackendAttributeName_t attributeName,
+                      miopenBackendAttributeType_t attributeType,
+                      int64_t elementCount,
+                      void* arrayOfElements) override
+    {
+    }
+    void finalize() override {}
+    void getAttribute(miopenBackendAttributeName_t attributeName,
+                      miopenBackendAttributeType_t attributeType,
+                      int64_t requestedElementCount,
+                      int64_t* elementCount,
+                      void* arrayOfElements) override
+    {
+    }
+    OpNode* getOperation() override { return &mNode; }
+};
+
+using miopen::graphapi::GTestDescriptorAttribute;
+using miopen::graphapi::GTestDescriptorSingleValueAttribute;
+using miopen::graphapi::GTestDescriptorVectorAttribute;
+using miopen::graphapi::GTestGraphApiExecute;
+
+} // namespace
+
+TEST(GraphApiOperationGraphDescriptor, CFunctions)
+{
+    miopenHandle_t handle = nullptr;
+    auto status           = miopenCreate(&handle);
+    ASSERT_EQ(status, miopenStatusSuccess) << "Handle wasn't obtained, cannot continue";
+
+    GTestDescriptorSingleValueAttribute<miopenHandle_t, char> handleAttribute{
+        true,
+        "MIOPEN_ATTR_OPERATIONGRAPH_HANDLE",
+        MIOPEN_ATTR_OPERATIONGRAPH_HANDLE,
+        MIOPEN_TYPE_HANDLE,
+        MIOPEN_TYPE_CHAR,
+        0,
+        handle};
+
+    GMockBackendOperationDescriptor goodNode(true);
+
+    GTestDescriptorVectorAttribute<miopenBackendDescriptor_t, char> goodOp{
+        true,
+        "MIOPEN_ATTR_OPERATIONGRAPH_OPS",
+        MIOPEN_ATTR_OPERATIONGRAPH_OPS,
+        MIOPEN_TYPE_BACKEND_DESCRIPTOR,
+        MIOPEN_TYPE_CHAR,
+        -1,
+        {&goodNode}};
+    ;
+
+    GTestGraphApiExecute<GTestDescriptorAttribute*> execute{
+        {"MIOPEN_BACKEND_OPERATIONGRAPH_DESCRIPTOR",
+         MIOPEN_BACKEND_OPERATIONGRAPH_DESCRIPTOR,
+         true,
+         {&handleAttribute, &goodOp}}};
+
+    execute();
+
+    execute.descriptor.attributes = {&handleAttribute, &goodOp, &goodOp};
+    execute();
+
+    GMockBackendOperationDescriptor badNode(false);
+
+    GTestDescriptorVectorAttribute<miopenBackendDescriptor_t, char> badOp{
+        false,
+        "MIOPEN_ATTR_OPERATIONGRAPH_OPS",
+        MIOPEN_ATTR_OPERATIONGRAPH_OPS,
+        MIOPEN_TYPE_BACKEND_DESCRIPTOR,
+        MIOPEN_TYPE_CHAR,
+        -1,
+        {&badNode}};
+    ;
+
+    execute.descriptor.attrsValid = false;
+
+    execute.descriptor.attributes = {&handleAttribute};
+    execute();
+
+    execute.descriptor.attributes = {&goodOp};
+    execute();
+
+    execute.descriptor.attributes = {&handleAttribute, &badOp};
+    execute();
+
+    badOp = {false,
+             "MIOPEN_ATTR_OPERATIONGRAPH_OPS",
+             MIOPEN_ATTR_OPERATIONGRAPH_OPS,
+             MIOPEN_TYPE_BACKEND_DESCRIPTOR,
+             MIOPEN_TYPE_CHAR,
+             -1,
+             {&goodNode, &goodNode}};
+    execute();
+
+    miopenDestroy(handle);
+}

test/gtest/graphapi_opgraph.cpp
@@ -56,7 +56,15 @@ TEST(GraphAPI, BuildDiamond)
     auto t_c = makeDummyTensor("t_c");
     auto t_d = makeDummyTensor("t_d");
 
+    miopenHandle_t handle = nullptr;
+    miopenStatus_t status = miopenCreate(&handle);
+
+    ASSERT_EQ(status, miopenStatusSuccess) << "Handle wasn't created";
+    ASSERT_NE(handle, nullptr) << "miopenCreate() created a null handle";
+
     gr::OpGraphBuilder graph_builder;
+
+    graph_builder.setHandle(handle);
 
     DummyNode top{"top", {&t_in}, {&t_a, &t_b}};
     DummyNode left{"left", {&t_a}, {&t_c}};
@@ -84,4 +92,6 @@ TEST(GraphAPI, BuildDiamond)
     ASSERT_TRUE(graph.numEdges() == 4);
     ASSERT_TRUE(graph.hasEdgeToSink(&bottom, &t_out));
     ASSERT_TRUE(graph.hasEdgeFromSource(&top, &t_in));
+
+    miopenDestroy(handle);
 }

test/gtest/graphapi_opgraph_common.hpp
@@ -86,9 +86,11 @@ private:
     std::unordered_map<std::string, gr::Tensor> mTensorMap;
     std::vector<DummyNode> mNodes;
     gr::OpGraph mGraph;
+    miopenHandle_t mHandle = nullptr;
 
     DummyOpGraphGenerator(const std::vector<DummyNodeGenSpec>& node_specs)
     {
+        miopenCreate(&mHandle);
 
         for(const auto& ns : node_specs)
         {
@@ -112,6 +114,8 @@ private:
 
         gr::OpGraphBuilder builder;
 
+        builder.setHandle(mHandle);
+
         for(auto& n : mNodes)
         {
             builder.addNode(&n);
@@ -125,7 +129,8 @@ public:
     DummyOpGraphGenerator(DummyOpGraphGenerator&&)      = delete;
     DummyOpGraphGenerator& operator=(const DummyOpGraphGenerator&) = delete;
     DummyOpGraphGenerator& operator=(DummyOpGraphGenerator&&) = delete;
-    ~DummyOpGraphGenerator()                                  = default;
+
+    ~DummyOpGraphGenerator() { miopenDestroy(mHandle); }
 
     static std::unique_ptr<DummyOpGraphGenerator>
     Make(const std::vector<DummyNodeGenSpec>& node_specs)

commit 1e0987938da985c2a2435497d6ee45cc908ac2c0

[HOTFIX] Fix typo introduced by #2894 and #2902. (#2934)

src/graphapi/graphapi.cpp
@@ -97,7 +97,7 @@ miopenBackendCreateDescriptor(miopenBackendDescriptorType_t descriptorType,
             break;
 
         case MIOPEN_BACKEND_OPERATION_MATMUL_DESCRIPTOR:
-            outputDesciptor = new miopen::graphapi::BackendOperationMatmulDescriptor();
+            outputDescriptor = new miopen::graphapi::BackendOperationMatmulDescriptor();
             break;
 
         default: MIOPEN_THROW(miopenStatusUnsupportedOp);