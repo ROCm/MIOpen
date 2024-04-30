61dd067864d2f6f21cf150f91c29a544c09dffa3
M. Amber Hassaan <amber_474@yahoo.com>
2024-04-30 15:19:54 +0000

    WIP: finished creating MHA graph

--- test/gtest/graphapi_mha_fwd_f8.cpp
+++ test/gtest/graphapi_mha_fwd_f8.cpp
@@ -29,6 +29,8 @@
 #include "tensor_holder.hpp"
 
 #include <miopen/graphapi/opgraph.hpp>
+#include <miopen/graphapi/pointwise.hpp>
+#include <miopen/graphapi/reduction.hpp>
 
 #include <numeric>
 #include <string>
@@ -73,6 +75,8 @@ struct Deleter {
 class MhaGraphTest: public testing::TestWithParam<std::array<int64_t, 4>> {
 
   std::vector<Deleter> mPtrsToFree;
+  std::unique_ptr<gr::OpGraphBuilder> mGraphBuilder;
+  gr::OpGraph mGraph;
 
   template <typename T>
   T* alloc(T&& val) {
@@ -93,19 +97,73 @@ class MhaGraphTest: public testing::TestWithParam<std::array<int64_t, 4>> {
     std::exclusive_scan(dims.begin(), dims.end(), strides.begin(), T{1ll}, std::multiplies<T>{});
 
 
-    auto t = gr::TensorBuilder{}
+    return alloc(gr::TensorBuilder{}
     .setDataType(miopenFloat)
       .setDim(dims)
       .setStride(strides)
       .setId(id)
       .setVirtual(isVirtual)
-      .build();
-
-    return alloc(std::move(t));
+      .build());
   }
 
+  auto* makePointWiseDesc(miopenPointwiseMode_t mode) {
+      return alloc(gr::PointwiseBuilder{}
+          .setMode(MIOPEN_POINTWISE_MUL)
+          .setMathPrecision(miopenFloat)
+          .build());
+  }
 
-  gr::OpNode* makeNode(std::string_view name, 
+  void addBinaryPointwiseNode( gr::Pointwise* pw,
+      std::initializer_list<gr::Tensor*> in_tensors, 
+      std::initializer_list<gr::Tensor*> out_tensors) {
+
+      assert(in_tensors.size() == 2);
+      assert(out_tensors.size() == 1);
+
+      mGraphBuilder->addNode(alloc(gr::OperationPointwiseBuilder{}
+            .setPointwise(pw)
+            .setX(in_tensors[0])
+            .setB(in_tensors[1])
+            .setY(out_tensors[0])
+            .build()));
+
+  }
+
+  void addUnaryPointwiseNode( gr::Pointwise* pw,
+      std::initializer_list<gr::Tensor*> in_tensors, 
+      std::initializer_list<gr::Tensor*> out_tensors) {
+
+      assert(in_tensors.size() == 1);
+      assert(out_tensors.size() == 1);
+
+      mGraphBuilder->addNode(alloc(gr::OperationPointwiseBuilder{}
+            .setPointwise(pw)
+            .setX(in_tensors[0])
+            .setY(out_tensors[0])
+            .build()));
+
+  }
+
+  void addReductionNode(miopenReduceTensorOp_t red_op,
+      std::initializer_list<gr::Tensor*> in_tensors, 
+      std::initializer_list<gr::Tensor*> out_tensors) {
+
+      auto* red_desc = alloc(gr::ReductionBuilder{}
+        .setCompType(miopenFloat)
+        .setReductionOperator(red_op)
+        .build());
+      
+      assert(in_tensors.size() == 1);
+      assert(out_tensors.size() == 1);
+
+      mGraphBuilder->addNode(alloc(gr::OperationReductionBuilder{}
+            .setReduction(red_desc)
+            .setX(in_tensors[0])
+            .setY(out_tensors[0])
+            .build()));
+  }
+
+  void addNode(std::string_view name, 
       std::initializer_list<gr::Tensor*> in_tensors, 
       std::initializer_list<gr::Tensor*> out_tensors) {
     
@@ -113,13 +171,58 @@ class MhaGraphTest: public testing::TestWithParam<std::array<int64_t, 4>> {
       assert(in_tensors.size() == 2);
       assert(out_tensors.size() == 1);
 
-      auto mm_desc = 
-      auto node = gr::OperationMatmulBuilder{}
+      auto* mm_desc = alloc(gr::MatmulBuilder().setComputeType(miopenFloat8).build());
+      mGraphBuilder->addNode(alloc(gr::OperationMatmulBuilder{}
         .setA(in_tensors[0])
         .setB(in_tensors[1])
         .setC(out_tensors[0])
-        .setMatmulDescriptor(
+        .setMatmulDescriptor(mm_desc)
+        .build()));
 
+    } else if (name == "OP_POINTWISE:MUL") {
+
+      auto* pw = makePointWiseDesc(MIOPEN_POINTWISE_MUL);
+      addBinaryPointwiseNode(pw, in_tensors, out_tensors);
+
+    } else if (name == "OP_POINTWISE:SUB") {
+      auto* pw = makePointWiseDesc(MIOPEN_POINTWISE_SUB);
+      addBinaryPointwiseNode(pw, in_tensors, out_tensors);
+
+    } else if (name == "OP_POINTWISE:EXP") {
+      auto* pw = makePointWiseDesc(MIOPEN_POINTWISE_EXP);
+      addUnaryPointwiseNode(pw, in_tensors, out_tensors);
+
+    } else if (name == "OP_POINTWISE:RECIPROCAL) {
+      auto* pw = makePointWiseDesc(MIOPEN_POINTWISE_RECIPROCAL);
+      addUnaryPointwiseNode(pw, in_tensors, out_tensors);
+
+    } else if (name == "OP_REDUCTION:MAX") {
+      addReductionNode(MIOPEN_REDUCE_TENSOR_MAX, in_tensors, out_tensors);
+
+    } else if (name == "OP_REDUCTION:SUM") {
+      addReductionNode(MIOPEN_REDUCE_TENSOR_MAX, in_tensors, out_tensors);
+
+    } else if (name == "OP_RNG") {
+      constexpr double BERNOULLI = 0.5; 
+      auto* rng_desc = alloc(gr::RngBuilder{}
+          .setDistribution(MIOPEN_RNG_DISTRIBUTION_BERNOULLI)
+          .setBernoulliProb(BERNOULLI)
+          .build());
+
+      assert(in_tensors.size() == 2);// first is seed tensor, second is offset
+      assert(out_tensors.size() == 1);
+
+      mGraphBuilder->addNode(alloc(gr::OperationRngBuilder{}
+            .setRng(rng_desc)
+            .setSeed(in_tensors[0])
+            .setOffset(in_tensors[1])
+            .setOutput(out_tensors[0])
+            .build()));
+
+
+    } else {
+      std::cerr << "Unknown graph node type" << std::endl;
+      std::abort();
     }
 
   }
@@ -127,12 +230,12 @@ class MhaGraphTest: public testing::TestWithParam<std::array<int64_t, 4>> {
 
   void createMhaGraph(int64_t n, int64_t h, int64_t s, int64_t d) {
 
+    mGraphBuilder = std::make_unique<gr::OpGraphBuilder>();
+
     std::vector<int64_t> nhsd = {n, h, s, d};
     std::vector<int64_t> nhss = {n, h, s, s};
     std::vector<int64_t> nhs1 = {n, h, s, 1};
     std::vector<int64_t> all1s = {1, 1, 1, 1};
-
-    OpGraphBuilder graph_builder;
 
     auto Q = makeTensor("Q", nhsd, false);
     auto K = makeTensor("K", nhsd, false);
@@ -141,102 +244,83 @@ class MhaGraphTest: public testing::TestWithParam<std::array<int64_t, 4>> {
 #define MAKE_TENSOR(name, dims) auto* name = makeTensor(#name, dims)
 
     MAKE_TENSOR(T_MM_0, nhss);
-    auto mm_node_0 = makeNode("OP_MATMUL", {Q, K}, {T_MM_0});
-    graph_builder.addNode(mm_node_0);
+    addNode("OP_MATMUL", {Q, K}, {T_MM_0});
 
     MAKE_TENSOR(T_SCL_0, nhss);
     MAKE_TENSOR(ATN_SCL, all1s);
 
-    auto scale_node_0 = makeNode("OP_POINTWISE:MUL", {T_MM_0, ATN_SCL}, {T_SCL_0});
-    graph_builder.addNode(scale_node_0);
+    addNode("OP_POINTWISE:MUL", {T_MM_0, ATN_SCL}, {T_SCL_0});
 
     MAKE_TENSOR(T_SCL_1, nhss);
     MAKE_TENSOR(DSCL_Q, all1s);
 
-    auto scale_node_1 = makeNode("OP_POINTWISE:MUL", {T_SCL_0, DSCL_Q}, {T_SCL_1});
-    graph_builder.addNode(scale_node_1);
+    addNode("OP_POINTWISE:MUL", {T_SCL_0, DSCL_Q}, {T_SCL_1});
 
     MAKE_TENSOR(T_SCL_2, nhss);
     MAKE_TENSOR(DSCL_K, all1s);
 
-    auto scale_node_2 = makeNode("OP_POINTWISE:MUL", {T_SCL_1, DSCL_K}, {T_SCL_2});
-    graph_builder.addNode(scale_node_2);
+    addNode("OP_POINTWISE:MUL", {T_SCL_1, DSCL_K}, {T_SCL_2});
 
     MAKE_TENSOR(M, nhs1);
-    auto reduce_node_0 = makeNode("OP_REDUCTION:MAX", {T_SCL_2}, {M});
-    graph_builder.addNode(reduce_node_0);
+    addNode("OP_REDUCTION:MAX", {T_SCL_2}, {M});
 
     MAKE_TENSOR(T_SUB, nhss);
-    auto sub_node = makeNode("OP_POINTWISE:SUB", {T_SCL_2, M}, {T_SUB});
-    graph_builder.addNode(sub_node);
+    addNode("OP_POINTWISE:SUB", {T_SCL_2, M}, {T_SUB});
 
     MAKE_TENSOR(T_EXP, nhss);
-    auto exp_node = makeNode("OP_POINTWISE:EXP", {T_SUB}, {T_EXP});
-    graph_builder.addNode(sub_node);
+    addNode("OP_POINTWISE:EXP", {T_SUB}, {T_EXP});
 
     MAKE_TENSOR(T_SUM, nhs1);
-    auto reduce_node_1 = makeNode("OP_REDUCTION:SUM", {T_EXP}, {T_SUM});
-    graph_builder.addNode(reduce_node_1);
+    addNode("OP_REDUCTION:SUM", {T_EXP}, {T_SUM});
 
     MAKE_TENSOR(Z_INV, nhs1);
-    auto inv_node = makeNode("OP_POINTWISE:RECIPROCAL", {T_SUM}, {Z_INV});
-    graph_builder.addNode(inv_node);
+    addNode("OP_POINTWISE:RECIPROCAL", {T_SUM}, {Z_INV});
 
     MAKE_TENSOR(T_MUL_0, nhss);
-    auto mul_node_0 = makeNode("OP_POINTWISE:MUL", {T_EXP, Z_INV}, {T_MUL_0});
-    graph_builder.addNode(mul_node_0);
+    addNode("OP_POINTWISE:MUL", {T_EXP, Z_INV}, {T_MUL_0});
 
     MAKE_TENSOR(AMAX_S, all1s);
-    auto reduce_node_2 = makeNode("OP_REDUCTION:MAX", {T_MUL_0}, {AMAX_S});
-    graph_builder.addNode(reduce_node_2);
+    addNode("OP_REDUCTION:MAX", {T_MUL_0}, {AMAX_S});
 
     MAKE_TENSOR(RND_SD, all1s);
     MAKE_TENSOR(RND_OFF, all1s);
-    MAKE_TENSOR(RND_PRB, all1s);
 
     MAKE_TENSOR(T_RND, nhss);
-    auto mul_node_1 = makeNode("OP_POINTWISE:MUL", {T_MUL_0, T_RND}, {T_MUL_1});
-    graph_builder.addNode(mul_node_1);
+    addNode("OP_RNG", {RND_SD, RND_OFF}, {T_RND});
 
+    MAKE_TENSOR(T_MUL_1, nhss);
+    addNode("OP_POINTWISE:MUL", {T_MUL_0, T_RND}, {T_MUL_1});
 
+    MAKE_TENSOR(RND_PRB, all1s); // TODO(Amber): revisit
     MAKE_TENSOR(T_SCL_3, nhss);
-    auto scale_node_3 = makeNode("OP_POINTWISE:MUL", {T_MUL_1, RND_PRB}, {T_SCL_3});
-    graph_builder.addNode(scale_node_3);
+    addNode("OP_POINTWISE:MUL", {T_MUL_1, RND_PRB}, {T_SCL_3});
 
     MAKE_TENSOR(T_SCL_4, nhss);
     MAKE_TENSOR(SCL_S, all1s);
-    auto scale_node_4 = makeNode("OP_POINTWISE:MUL", {T_SCL_3, SCL_S}, {T_SCL_4});
-    graph_builder.addNode(scale_node_4);
+    addNode("OP_POINTWISE:MUL", {T_SCL_3, SCL_S}, {T_SCL_4});
 
     MAKE_TENSOR(T_MM_1, nhsd);
-    auto mm_node_1 = makeNode("OP_MATMUL", {T_SCL_4, V}, {T_MM_1});
-    graph_builder.addNode(mm_node_1);
+    addNode("OP_MATMUL", {T_SCL_4, V}, {T_MM_1});
 
     MAKE_TENSOR(T_SCL_5, nhsd);
     MAKE_TENSOR(DSCL_S, all1s);
-    auto scale_node_5 = makeNode("OP_POINTWISE:MUL", {T_MM_1, DSCL_S}, {T_SCL_5});
-    graph_builder.addNode(scale_node_5);
+    addNode("OP_POINTWISE:MUL", {T_MM_1, DSCL_S}, {T_SCL_5});
 
     MAKE_TENSOR(T_SCL_6, nhsd);
     MAKE_TENSOR(DSCL_V, all1s);
-    auto scale_node_6 = makeNode("OP_POINTWISE:MUL", {T_SCL_5, DSCL_V}, {T_SCL_6});
-    graph_builder.addNode(scale_node_6);
+    addNode("OP_POINTWISE:MUL", {T_SCL_5, DSCL_V}, {T_SCL_6});
 
     MAKE_TENSOR(T_SCL_7, nhsd);
     MAKE_TENSOR(SCL_O, all1s);
-    auto scale_node_7 = makeNode("OP_POINTWISE:MUL", {T_SCL_6, SCL_O}, {T_SCL_7});
-    graph_builder.addNode(scale_node_7);
+    addNode("OP_POINTWISE:MUL", {T_SCL_6, SCL_O}, {T_SCL_7});
 
     MAKE_TENSOR(AMAX_O, all1s);
-    auto reduce_node_3 = makeNode("OP_REDUCTION:MAX", {T_SCL_6}, {AMAX_O});
-    graph_builder.addNode(reduce_node_2);
+    addNode("OP_REDUCTION:MAX", {T_SCL_6}, {AMAX_O});
 
-
-    auto graph = std::move(graph_builder).build(); 
+    mGraph = std::move(*mGraphBuilder).build(); 
+    mGraphBuilder.reset(nullptr);
 
 #undef MAKE_TENSOR
-
-    return graph; // FIXME(amber): all local pointers will be invalidated upon return
 
   }
 };
