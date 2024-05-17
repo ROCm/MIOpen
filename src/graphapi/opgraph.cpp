b50aab97d39399b73a23127adaedf12eed9948f1
Jun Liu <Liu.Jun@amd.com>
2024-05-17 15:20:56 -0700

    [HotFix] for_each_n not available on RHEL 8 or SLES and bump CK to avoid ENV conflicts (#2974)
    * update CK commit hash to avoid ENV conflicts
    
    * fix SLES/RHEL: no member named 'for_each_n' in namespace 'std'
    
    [ROCm/MIOpen commit: 2ed1448c0fc06a2ef472e05286452030635dc3d1]

--- src/graphapi/opgraph.cpp
+++ src/graphapi/opgraph.cpp
@@ -349,20 +349,20 @@ void BackendOperationGraphDescriptor::setAttribute(miopenBackendAttributeName_t 
             std::vector<OpNode*> nodes;
             nodes.reserve(elementCount);
 
-            std::for_each_n(static_cast<miopenBackendDescriptor_t*>(arrayOfElements),
-                            elementCount,
-                            [&descriptors, &nodes](miopenBackendDescriptor_t apiDescriptor) {
-                                BackendDescriptor& backendDescriptor = deref(apiDescriptor);
-                                if(backendDescriptor.isFinalized())
-                                {
-                                    descriptors.push_back(apiDescriptor);
-                                    nodes.push_back(backendDescriptor.getOperation());
-                                }
-                                else
-                                {
-                                    MIOPEN_THROW(miopenStatusBadParm);
-                                }
-                            });
+            std::for_each(static_cast<miopenBackendDescriptor_t*>(arrayOfElements),
+                          static_cast<miopenBackendDescriptor_t*>(arrayOfElements) + elementCount,
+                          [&descriptors, &nodes](miopenBackendDescriptor_t apiDescriptor) {
+                              BackendDescriptor& backendDescriptor = deref(apiDescriptor);
+                              if(backendDescriptor.isFinalized())
+                              {
+                                  descriptors.push_back(apiDescriptor);
+                                  nodes.push_back(backendDescriptor.getOperation());
+                              }
+                              else
+                              {
+                                  MIOPEN_THROW(miopenStatusBadParm);
+                              }
+                          });
 
             if(!internal::noRepetitions(nodes))
             {
