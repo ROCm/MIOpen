4f6dbbe041c5d6a914f0100b178e633c8630abf0
seungmanhan <seungman.han@moreh.io>
2024-05-27 18:59:10 +0000

    fix kernel error

--- src/kernels/MIOpenRoPE.cpp
+++ src/kernels/MIOpenRoPE.cpp
@@ -36,32 +36,33 @@ __device__ void ropefwdcontiguous(const TI* __restrict__ x,
                                   const TI* __restrict__ cos,
                                   const TI* __restrict__ sin,
                                   TO* __restrict__ y,
-                                  tensor_view_t<5> x_tv,
+                                  tensor_view_t<4> x_tv,
                                   tensor_view_t<3> cos_tv,
                                   tensor_view_t<3> sin_tv,
-                                  tensor_view_t<5> y_tv,
-                                  uint64_t output_numele)
+                                  tensor_view_t<4> y_tv,
+                                  uint64_t output_numel)
 {
     const uint64_t gid = threadIdx.x + blockIdx.x * blockDim.x;
     if(gid >= output_numel)
         return;
 
-    tensor_layout_t<5> ncdhw(x_tv, gid);
+    tensor_layout_t<4> ncdhw(x_tv, gid);
 
     FLOAT_ACCUM input = CVT_FLOAT2ACCUM(x[x_tv.get_tensor_view_idx(ncdhw)]);
     FLOAT_ACCUM input_rotate_half =
-        (ncdhw.layout[4] % 2 == 0)
-            ? CVT_FLOAT2ACCUM(-x[x_tv.get_tensor_view_idx(ncdhw.add_tensor_layout_t(4, 1))])
-            : CVT_FLOAT2ACCUM(x[x_tv.get_tensor_view_idx(ncdhw.sub_tensor_layout_t(4, 1))]);
+        (x_tv.get_tensor_view_idx(ncdhw) % 2 == 0)
+            ? CVT_FLOAT2ACCUM(
+                  -x[x_tv.get_tensor_view_idx(ncdhw.add_tensor_layout_t(3, 1)) % output_numel])
+            : CVT_FLOAT2ACCUM(x[x_tv.get_tensor_view_idx(ncdhw.sub_tensor_layout_t(3, 1))]);
 
-    tensor_layout_t<3> ncw(ncdhw.layout[2], ncdhw.layout[3], ncdhw.layout[4]);
+    tensor_layout_t<3> ncw(ncdhw.layout[1], ncdhw.layout[2], ncdhw.layout[3]);
 
     FLOAT_ACCUM cos_val = CVT_FLOAT2ACCUM(cos[cos_tv.get_tensor_view_idx(ncw)]);
     FLOAT_ACCUM sin_val = CVT_FLOAT2ACCUM(sin[sin_tv.get_tensor_view_idx(ncw)]);
 
-    FLOAT_ACCUM val = (input * cos_val) + (input_rotate_half * sin_val);
+    FLOAT_ACCUM val = (input * cos_val); // + (input_rotate_half * sin_val);
 
-    y[y_tv.get_tensor_view_idx(ncdhw)] = CVT_ACCUM2FLOAT(val);
+    y[y_tv.get_tensor_view_idx(ncdhw)] = CVT_ACCUM2FLOAT(input);
 }
 
 template <typename TI, typename TO>
@@ -69,30 +70,33 @@ __device__ void ropebwdcontiguous(const TI* __restrict__ dy,
                                   const TI* __restrict__ cos,
                                   const TI* __restrict__ sin,
                                   TO* __restrict__ dx,
-                                  tensor_view_t<5> dy_tv,
+                                  tensor_view_t<4> dy_tv,
                                   tensor_view_t<3> cos_tv,
                                   tensor_view_t<3> sin_tv,
-                                  tensor_view_t<5> dx_tv,
-                                  uint64_t output_numele)
+                                  tensor_view_t<4> dx_tv,
+                                  uint64_t output_numel,
+                                  uint64_t rotary_numel)
 {
     const uint64_t gid = threadIdx.x + blockIdx.x * blockDim.x;
     if(gid >= output_numel)
         return;
 
-    tensor_layout_t<5> ncdhw(dy_tv, gid);
+    tensor_layout_t<4> ncdhw(dy_tv, gid);
 
     FLOAT_ACCUM output_grad = CVT_FLOAT2ACCUM(dy[dy_tv.get_tensor_view_idx(ncdhw)]);
     FLOAT_ACCUM output_grad_rotate_half =
-        (ncdhw.layout[4] % 2 == 0)
-            ? CVT_FLOAT2ACCUM(dy[dy_tv.get_tensor_view_idx(ncdhw.add_tensor_layout_t(4, 1))])
-            : CVT_FLOAT2ACCUM(-dy[dy_tv.get_tensor_view_idx(ncdhw.sub_tensor_layout_t(4, 1))]);
+        (dy_tv.get_tensor_view_idx(ncdhw) % 2 == 0)
+            ? CVT_FLOAT2ACCUM(
+                  dy[dy_tv.get_tensor_view_idx(ncdhw.add_tensor_layout_t(3, 1)) % output_numel])
+            : CVT_FLOAT2ACCUM(-dy[dy_tv.get_tensor_view_idx(ncdhw.sub_tensor_layout_t(3, 1))]);
 
-    tensor_layout_t<3> ncw(ncdhw.layout[2], ncdhw.layout[3], ncdhw.layout[4]);
+    tensor_layout_t<3> ncw(ncdhw.layout[1], ncdhw.layout[2], ncdhw.layout[3]);
 
     FLOAT_ACCUM cos_val = CVT_FLOAT2ACCUM(cos[cos_tv.get_tensor_view_idx(ncw)]);
     FLOAT_ACCUM sin_val =
-        (ncw.layout[2] % 2 == 0)
-            ? CVT_FLOAT2ACCUM(sin[sin_tv.get_tensor_view_idx(ncw.add_tensor_layout_t(2, 1))])
+        (cos_tv.get_tensor_view_idx(ncw) % 2 == 0)
+            ? CVT_FLOAT2ACCUM(
+                  sin[sin_tv.get_tensor_view_idx(ncw.add_tensor_layout_t(2, 1)) % rotary_numel])
             : CVT_FLOAT2ACCUM(sin[sin_tv.get_tensor_view_idx(ncw.sub_tensor_layout_t(2, 1))]);
 
     FLOAT_ACCUM val = (output_grad * cos_val) + (output_grad_rotate_half * sin_val);
@@ -104,10 +108,10 @@ extern "C" __global__ void RoPEFwdContiguous(const INPUT_TYPE* __restrict__ x,
                                              const INPUT_TYPE* __restrict__ cos,
                                              const INPUT_TYPE* __restrict__ sin,
                                              OUTPUT_TYPE* __restrict__ y,
-                                             tensor_view_t<5> x_tv,
+                                             tensor_view_t<4> x_tv,
                                              tensor_view_t<3> cos_tv,
                                              tensor_view_t<3> sin_tv,
-                                             tensor_view_t<5> y_tv,
+                                             tensor_view_t<4> y_tv,
                                              uint64_t output_numel)
 {
     // instantiate the kernel
@@ -119,13 +123,14 @@ extern "C" __global__ void RoPEBwdContiguous(const INPUT_TYPE* __restrict__ dy,
                                              const INPUT_TYPE* __restrict__ cos,
                                              const INPUT_TYPE* __restrict__ sin,
                                              OUTPUT_TYPE* __restrict__ dx,
-                                             tensor_view_t<5> dy_tv,
+                                             tensor_view_t<4> dy_tv,
                                              tensor_view_t<3> cos_tv,
                                              tensor_view_t<3> sin_tv,
-                                             tensor_view_t<5> dx_tv,
-                                             uint64_t output_numel)
+                                             tensor_view_t<4> dx_tv,
+                                             uint64_t output_numel,
+                                             uint64_t rotary_numel)
 {
     // instantiate the kernel
     ropebwdcontiguous<INPUT_TYPE, OUTPUT_TYPE>(
-        dy, cos, sin, dx, dy_tv, cos_tv, sin_tv, dx_tv, output_numel);
+        dy, cos, sin, dx, dy_tv, cos_tv, sin_tv, dx_tv, output_numel, rotary_numel);
 }
