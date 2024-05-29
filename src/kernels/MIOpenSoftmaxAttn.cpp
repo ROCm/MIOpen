ebfbab946e85544e002b48df23e691ba8eb47213
Alex Eremin <CAHEK7@yandex.ru>
2024-05-29 06:47:21 +0200

    [MHA] turn on fp8 matmul for mha (#2971)
    * turn on fp8 matmul for mha
    
    * use rocblas-compatible fp8 mode
    
    * shut-up clang-tidy
    
    * fix clang-tidy internal error
    
    * fix nocomgr compilation
    
    * turn on 'negative zero as nan' for FP8 numbers, since it's default for the HW
    
    * Revert "turn on 'negative zero as nan'"
    And override it in-place
    
    This reverts commit d2cc4a5e1e4eb33ab9e1c00b2f80831320cc46d0.
    
    * get rid of overriding and disable the test unless the defaults got fixed

--- src/kernels/MIOpenSoftmaxAttn.cpp
+++ src/kernels/MIOpenSoftmaxAttn.cpp
@@ -24,16 +24,36 @@
  *
  *******************************************************************************/
 
+// rocblas operates with non-ieee FP8
+#define MIOPEN_FP8_IEEE_EXPONENT_BIAS 0
+
 #ifndef MIOPEN_DONT_USE_HIP_RUNTIME_HEADERS
+#include <hip/hip_fp16.h>
 #include <hip/hip_runtime.h>
+#include <hip/hip_bfloat16.h>
 #endif
 
 #include "miopen_cstdint.hpp"
 #include "miopen_limits.hpp"
 #include "miopen_rocrand.hpp"
 
+#define MIOPEN_ENABLE_F8_DEVICE_CODE 1
+#include <hip_float8.hpp>
+
+// Some versions of amd-clang treats MIOPEN_ENABLE_F8_DEVICE_CODE as unused despite the fact that
+// it's used in the subsequent include. Some other versions amd-clang failed to compile everything
+// without MIOPEN_ENABLE_F8_DEVICE_CODE explicitely defined.
+// Fake-using it.
+#ifndef MIOPEN_ENABLE_F8_DEVICE_CODE
+#message "MIOPEN_ENABLE_F8_DEVICE_CODE must be defined"
+#endif
+
 #ifndef THREADS
 #define THREADS 64
+#endif
+
+#ifndef OUT_TYPE
+#define OUT_TYPE float
 #endif
 
 namespace {
@@ -160,7 +180,7 @@ __forceinline__ __device__ bool doDropout(float dropout, rocrand_device::xorwow_
 
 extern "C" __global__ void __launch_bounds__(THREADS)
     SoftMaxWarp(const float* in,
-                float* out,
+                OUT_TYPE* out,
                 float* __restrict__ M,
                 float* __restrict__ Z,
                 float* __restrict__ Amax,
@@ -195,7 +215,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
     for(uint64_t gid = blockIdx.x * NumWarps + warpId; gid < nhs; gid += gridDim.x * NumWarps)
     {
         const float* line = in + gid * seq_len + laneId;
-        float* res        = out + gid * seq_len + laneId;
+        auto res          = out + gid * seq_len + laneId;
 
         float local_val =
             (laneId < seq_len) ? (*line) * descaler : std::numeric_limits<float>::lowest();
@@ -215,7 +235,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 
         if(laneId < seq_len)
         {
-            *res = doDropout(dropout, &rng) ? 0.0f : local_val * scaler;
+            *res = static_cast<OUT_TYPE>(doDropout(dropout, &rng) ? 0.0f : local_val * scaler);
         }
 
         if(save_stats)
@@ -237,7 +257,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 
 extern "C" __global__ void __launch_bounds__(THREADS)
     SoftMaxBlock(const float* in,
-                 float* out,
+                 OUT_TYPE* out,
                  float* __restrict__ M,
                  float* __restrict__ Z,
                  float* __restrict__ Amax,
@@ -272,7 +292,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
     for(uint64_t gid = blockIdx.x; gid < nhs; gid += gridDim.x)
     {
         const float* line = in + gid * seq_len + lid;
-        float* res        = out + gid * seq_len + lid;
+        auto res          = out + gid * seq_len + lid;
 
         float local_val =
             (lid < seq_len) ? (*line) * descaler : std::numeric_limits<float>::lowest();
@@ -291,7 +311,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 
         if(lid < seq_len)
         {
-            *res = doDropout(dropout, &rng) ? 0.0f : local_val * scaler;
+            *res = static_cast<OUT_TYPE>(doDropout(dropout, &rng) ? 0.0f : local_val * scaler);
         }
 
         if(save_stats)
@@ -313,7 +333,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 
 extern "C" __global__ void __launch_bounds__(THREADS)
     SoftMaxCommon(const float* in,
-                  float* out,
+                  OUT_TYPE* out,
                   float* __restrict__ M,
                   float* __restrict__ Z,
                   float* __restrict__ Amax,
@@ -348,7 +368,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
     for(uint64_t gid = blockIdx.x; gid < nhs; gid += gridDim.x)
     {
         const float* line = in + gid * seq_len;
-        float* res        = out + gid * seq_len;
+        auto res          = out + gid * seq_len;
 
         float r_max = reductionCommon<NumWarps>(
             line,
@@ -379,7 +399,8 @@ extern "C" __global__ void __launch_bounds__(THREADS)
             // non-negative value. Plain max() is enough.
             r_Amax = fmaxf_op(r_Amax, local_val);
 
-            res[loop_lid] = doDropout(dropout, &rng) ? 0.0f : local_val * scaler;
+            res[loop_lid] =
+                static_cast<OUT_TYPE>(doDropout(dropout, &rng) ? 0.0f : local_val * scaler);
         }
 
         if(save_stats)
@@ -401,7 +422,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 
 extern "C" __global__ void __launch_bounds__(THREADS)
     ScaleReduce(const float* __restrict__ in,
-                float* __restrict__ out,
+                OUT_TYPE* __restrict__ out,
                 float* __restrict__ Amax,
                 const float* __restrict__ descale_S,
                 const float* __restrict__ descale_V,
@@ -426,7 +447,7 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 
         r_Amax = fmaxf_op(r_Amax, fabsf(res));
 
-        *out_ptr = res * scaler;
+        *out_ptr = static_cast<OUT_TYPE>(res * scaler);
 
         in_ptr += step;
         out_ptr += step;
@@ -445,8 +466,8 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 }
 
 extern "C" __global__ void __launch_bounds__(THREADS)
-    ScaleRowReduceWarp(const float* __restrict__ dO,
-                       const float* __restrict__ O,
+    ScaleRowReduceWarp(const OUT_TYPE* __restrict__ dO,
+                       const OUT_TYPE* __restrict__ O,
                        float* __restrict__ out,
                        const float* __restrict__ descale_dO,
                        const float* __restrict__ descale_O,
@@ -466,10 +487,10 @@ extern "C" __global__ void __launch_bounds__(THREADS)
         float local_val = 0.0f;
         if(laneId < d)
         {
-            const float* dO_ptr = dO + gid * d + laneId;
-            const float* O_ptr  = O + gid * d + laneId;
+            const auto dO_ptr = dO + gid * d + laneId;
+            const auto O_ptr  = O + gid * d + laneId;
 
-            local_val = (*dO_ptr) * (*O_ptr) * scaler;
+            local_val = static_cast<float>(*dO_ptr) * static_cast<float>(*O_ptr) * scaler;
         }
 
         local_val = reductionFullWarp<warpSize>(local_val, laneId, plus_op);
@@ -482,8 +503,8 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 }
 
 extern "C" __global__ void __launch_bounds__(THREADS)
-    ScaleRowReduceBlock(const float* __restrict__ dO,
-                        const float* __restrict__ O,
+    ScaleRowReduceBlock(const OUT_TYPE* __restrict__ dO,
+                        const OUT_TYPE* __restrict__ O,
                         float* __restrict__ out,
                         const float* __restrict__ descale_dO,
                         const float* __restrict__ descale_O,
@@ -500,13 +521,13 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 
     for(uint64_t gid = blockIdx.x; gid < nhs; gid += gridDim.x)
     {
-        const float* dO_ptr = dO + gid * d + lid;
-        const float* O_ptr  = O + gid * d + lid;
+        const auto dO_ptr = dO + gid * d + lid;
+        const auto O_ptr  = O + gid * d + lid;
 
         float local_val = 0.0f;
         if(lid < d)
         {
-            local_val = (*dO_ptr) * (*O_ptr) * scaler;
+            local_val = static_cast<float>(*dO_ptr) * static_cast<float>(*O_ptr) * scaler;
         }
 
         local_val = reductionBlock<NumWarps>(local_val, plus_op, lid, laneId, warpId);
@@ -519,8 +540,8 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 }
 
 extern "C" __global__ void __launch_bounds__(THREADS)
-    ScaleRowReduceCommon(const float* __restrict__ dO,
-                         const float* __restrict__ O,
+    ScaleRowReduceCommon(const OUT_TYPE* __restrict__ dO,
+                         const OUT_TYPE* __restrict__ O,
                          float* __restrict__ out,
                          const float* __restrict__ descale_dO,
                          const float* __restrict__ descale_O,
@@ -537,10 +558,12 @@ extern "C" __global__ void __launch_bounds__(THREADS)
 
     for(uint64_t gid = blockIdx.x; gid < nhs; gid += gridDim.x)
     {
-        const float* dO_ptr = dO + gid * d;
-        const float* O_ptr  = O + gid * d;
+        const auto dO_ptr = dO + gid * d;
+        const auto O_ptr  = O + gid * d;
 
-        float local_val = (lid < d) ? dO_ptr[lid] * O_ptr[lid] * scaler : 0.0f;
+        float local_val =
+            (lid < d) ? static_cast<float>(dO_ptr[lid]) * static_cast<float>(O_ptr[lid]) * scaler
+                      : 0.0f;
 
         for(uint32_t loop_lid = lid + blockDim.x; loop_lid < d; loop_lid += blockDim.x)
             local_val += dO_ptr[loop_lid] * O_ptr[loop_lid] * scaler;
