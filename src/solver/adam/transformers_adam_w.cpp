28863aa7f66e2840e567d4f72af9f831821a9629
jungkeun kim <et16kr@gmail.com>
2024-06-05 13:39:10 +0000

    transformers_adam_w: fix for CICD

--- src/solver/adam/transformers_adam_w.cpp
+++ src/solver/adam/transformers_adam_w.cpp
@@ -58,11 +58,13 @@ ConvSolution TransformersAdamW::GetSolution(const ExecutionContext& context,
                                ? miopen::GetDataType(problem.GetGradDesc().GetType())
                                : "float";
 
-        const auto build_params = KernelBuildParameters{
-            {"PTYPE", param_dtype},
-            {"GTYPE", grad_dtype},
-            {"CTYPE", ptype_size > 4 ? "double" : "float"},
-        };
+        const auto build_params =
+            KernelBuildParameters{
+                {"PTYPE", param_dtype},
+                {"GTYPE", grad_dtype},
+                {"CTYPE", ptype_size > 4 ? "double" : "float"},
+            }
+            << GetDataTypeKBP(problem.GetParamDesc().GetType());
 
         constexpr size_t local_size = 256;
         auto& handle                = context.GetStream();
