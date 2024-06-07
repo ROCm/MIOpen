f089e754a8a6d10ef3dbca81dd2be3a102933a6f
seungmanhan <seungman.han@moreh.io>
2024-06-07 08:18:04 +0000

    fix build error

--- src/solver/getitem/backward_getitem.cpp
+++ src/solver/getitem/backward_getitem.cpp
@@ -244,8 +244,8 @@ ConvSolution GetitemBackward::GetSolution(const ExecutionContext& /*context*/,
                 handle_.EnableProfiling(true);
                 hipEventSynchronize(stop.get());
                 hipEventElapsedTime(&elapsed, start.get(), stop.get());
-                hipEventDestroy(start);
-                hipEventDestroy(stop);
+                hipEventDestroy(start.get());
+                hipEventDestroy(stop.get());
                 handle_.ResetKernelTime();
                 handle_.AccumKernelTime(elapsed);
             };
