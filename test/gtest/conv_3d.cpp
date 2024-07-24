ac3d4bdb86c82d19306e3e9c08c68e6ebc7a3f75
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2024-07-24 02:22:29 +0200

    [bug][tests] Fix broken tests (#3123)
    * Fix broken gtests
    
    * Fix formatting
    
    * Fix ctests
    
    ---------
    
    Co-authored-by: Jun Liu <Liu.Jun@amd.com>

--- test/gtest/conv_3d.cpp
+++ test/gtest/conv_3d.cpp
@@ -43,9 +43,9 @@ void GetArgs(const std::string& param, std::vector<std::string>& tokens)
         tokens.push_back(*begin++);
 }
 
-std::vector<std::string> GetTestCases(void)
+std::vector<std::string> GetTestCases(const std::string& precision)
 {
-    std::string cmd_v = "test_conv3d --verbose ";
+    std::string cmd_v = "test_conv3d --verbose " + precision + " ";
 
     // clang-format off
     return std::vector<std::string>{
@@ -70,7 +70,7 @@ std::vector<std::string> GetTestCases(void)
     // clang-format on
 }
 
-using TestCase = decltype(GetTestCases())::value_type;
+using TestCase = decltype(GetTestCases(std::string{}))::value_type;
 
 class ConfigWithFloat_conv_3d : public testing::TestWithParam<std::vector<TestCase>>
 {
@@ -116,4 +116,4 @@ using namespace conv_3d;
 
 TEST_P(ConfigWithFloat_conv_3d, FloatTest_conv_3d) { Run2dDriver(); };
 
-INSTANTIATE_TEST_SUITE_P(Conv3d, ConfigWithFloat_conv_3d, testing::Values(GetTestCases()));
+INSTANTIATE_TEST_SUITE_P(Conv3d, ConfigWithFloat_conv_3d, testing::Values(GetTestCases("--float")));
