c9588d1e9f177a38e1a58bd5494a04198a0fffde
Vasilii Filippov <vasilii.filippov@amd.com>
2024-07-24 02:43:14 +0200

    Changed Fusion to have a separate find mode env variable and default to fast (#3121)

--- src/include/miopen/handle.hpp
+++ src/include/miopen/handle.hpp
@@ -254,7 +254,13 @@ public:
     {
         invokers.Register({config, solver}, invoker);
         if(algo.has_value())
-            invokers.SetAsFound1_0(config, *algo, solver);
+            SetAsFound1_0(config, *algo, solver);
+    }
+
+    void
+    SetAsFound1_0(const NetworkConfig& config, const AlgorithmName& algo, const std::string& solver)
+    {
+        invokers.SetAsFound1_0(config, algo, solver);
     }
 
     std::optional<Invoker> GetInvoker(const NetworkConfig& config,
