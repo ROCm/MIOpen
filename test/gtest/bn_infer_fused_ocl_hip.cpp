a6a27ab106f34107cfea95293b0af2a6bdfd0836
Sree Charan Gundabolu <sgundabo@amd.com>
2024-07-31 20:09:36 +0000

    warpSize windows fix

--- test/gtest/bn_infer_fused_ocl_hip.cpp
+++ test/gtest/bn_infer_fused_ocl_hip.cpp
@@ -38,6 +38,10 @@
 
 #define PERF_ENABLE 0
 
+#ifndef warpSize
+#define warpSize 32
+#endif
+
 void BatchNormFusedInferencGPU(miopen::Handle& handle,
                                miopenBatchNormMode_t bn_mode,
                                miopenActivationMode_t activ_mode,
