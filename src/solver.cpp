bf7332c5c5b0c9e613e56102db35130f54a2df15
long10024070 <long.luong@moreh.com.vn>
2024-08-02 03:05:08 +0000

    fix  error

--- src/solver.cpp
+++ src/solver.cpp
@@ -654,8 +654,6 @@ inline SolverRegistrar::SolverRegistrar(IdRegistryData& registry)
 
     Register(registry, ++id, Primitive::Softmax, softmax::Softmax{}.SolverDbId());
     Register(registry, ++id, Primitive::Softmax, softmax::AttnSoftmax{}.SolverDbId());
-    Register(registry, ++id, Primitive::ReLU, prelu::SingleWeightBackward{}.SolverDbId());
-    Register(registry, ++id, Primitive::ReLU, prelu::MultiWeightsBackward{}.SolverDbId());
 
     Register(registry, ++id, Primitive::Reduce, reduce::ArgminForward{}.SolverDbId());
     Register(registry, ++id, Primitive::Reduce, reduce::MaxForward{}.SolverDbId());
@@ -666,27 +664,9 @@ inline SolverRegistrar::SolverRegistrar(IdRegistryData& registry)
 
     Register(registry, ++id, Primitive::Cat, cat::CatForward{}.SolverDbId());
     Register(registry, ++id, Primitive::Adam, adam::Adam{}.SolverDbId());
-
-    Register(registry, ++id, Primitive::Adam, adam::TransformersAdamW{}.SolverDbId());
-
-    Register(registry,
-             ++id,
-             Primitive::Fusion,
-             fusion::ConvWinoFuryRxSFused<2, 3>{}.SolverDbId(),
-             miopenConvolutionAlgoWinograd);
-
-    Register(registry, ++id, Primitive::Reduce, reduce::ArgminForward{}.SolverDbId());
-    Register(registry, ++id, Primitive::Reduce, reduce::MaxForward{}.SolverDbId());
-    Register(registry, ++id, Primitive::Reduce, reduce::MinForward{}.SolverDbId());
-
-    Register(registry, ++id, Primitive::Mha, mha::MhaForward{}.SolverDbId());
-    Register(registry, ++id, Primitive::Mha, mha::MhaBackward{}.SolverDbId());
-
-    Register(registry, ++id, Primitive::Cat, cat::CatForward{}.SolverDbId());
-    Register(registry, ++id, Primitive::Adam, adam::Adam{}.SolverDbId());
-    Register(registry, ++id, Primitive::Adam, adam::TransformersAdamW{}.SolverDbId());
-
     Register(registry, ++id, Primitive::Item, getitem::GetitemBackward{}.SolverDbId());
+
+    Register(registry, ++id, Primitive::Adam, adam::TransformersAdamW{}.SolverDbId());
 
     Register(registry,
              ++id,
