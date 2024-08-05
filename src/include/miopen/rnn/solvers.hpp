1b4b64d1eec2844a563b4f5ff664b1f5b50488bf
Kamil Nasyrov <shurale.nkn@gmail.com>
2024-08-05 07:40:22 +0200

    windows fix, build without GEMM (#3179)
    * windows fix
    
    * Copyright
    
    [ROCm/MIOpen commit: a54b21c84d60c69291bac7417fa9aa285995d447]

--- src/include/miopen/rnn/solvers.hpp
+++ src/include/miopen/rnn/solvers.hpp
@@ -1,3 +1,29 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
 #pragma once
 
 #include <miopen/rnn.hpp>
@@ -146,6 +172,14 @@ public:
                 SequenceDirection direction,
                 size_t gemm_batch_offset,
                 size_t gemm_batch_size) const;
+    static bool IsApplicable()
+    {
+#if MIOPEN_USE_GEMM && MIOPEN_BACKEND_HIP
+        return true;
+#else
+        return false;
+#endif // MIOPEN_USE_GEMM&& MIOPEN_BACKEND_HIP
+    }
 
 private:
     RNNBackwardDataModularAlgo(GeneralLstmRedBuffer rb_layout,
@@ -284,9 +318,17 @@ public:
     {
     }
 
-    bool isApplicable();
+    static bool IsApplicable()
+    {
+#if MIOPEN_USE_GEMM && MIOPEN_BACKEND_HIP
+        return true;
+#else
+        return false;
+#endif // MIOPEN_USE_GEMM&& MIOPEN_BACKEND_HIP
+    }
 
-    size_t GetWsSize();
+    // TODO
+    static size_t GetWsSize() { return 0; };
 
     void ComputeBWD(Handle& handle,
                     ConstData_t dy,
@@ -318,9 +360,17 @@ public:
     {
     }
 
-    bool isApplicable();
+    static bool IsApplicable()
+    {
+#if MIOPEN_USE_GEMM && MIOPEN_BACKEND_HIP
+        return true;
+#else
+        return false;
+#endif // MIOPEN_USE_GEMM&& MIOPEN_BACKEND_HIP
+    }
 
-    size_t GetWsSize();
+    // TODO
+    static size_t GetWsSize() { return 0; };
 
     struct runtimeArgsBwd
     {
