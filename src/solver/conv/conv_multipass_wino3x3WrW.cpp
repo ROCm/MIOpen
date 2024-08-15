df27b9645935f9b6ac2e2779e5bf8b6eff5a60a8
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2024-08-15 17:42:19 +0200

    Fix ConvWinograd3x3MultipassWrW device applicability

--- src/solver/conv/conv_multipass_wino3x3WrW.cpp
+++ src/solver/conv/conv_multipass_wino3x3WrW.cpp
@@ -379,6 +379,13 @@ bool ConvWinograd3x3MultipassWrW<WinoDataH, WinoFilterH, WinoDataW, WinoFilterW>
     // ROCBLAS for GEMM step
 
 #if(MIOPEN_BACKEND_HIP && MIOPEN_USE_ROCBLAS)
+    const std::string name = ctx.GetStream().GetDeviceName();
+    if(!(StartsWith(name, "gfx8") || name == "gfx900" || name == "gfx906" || name == "gfx908" ||
+         name == "gfx90a"))
+    {
+        return false;
+    }
+
     static const int wino_data_tile   = std::max(WinoDataH, WinoDataW);
     static const int wino_filter_tile = std::max(WinoFilterH, WinoFilterW);
 
@@ -397,7 +404,6 @@ bool ConvWinograd3x3MultipassWrW<WinoDataH, WinoFilterH, WinoDataW, WinoFilterW>
         }
     }
 
-    const std::string name = ctx.GetStream().GetDeviceName();
 #if WORKAROUND_SWDEV_234193
     if(problem.IsFp16() && (StartsWith(name, "gfx908") || StartsWith(name, "gfx906")))
     {
@@ -486,8 +492,6 @@ bool ConvWinograd3x3MultipassWrW<WinoDataH, WinoFilterH, WinoDataW, WinoFilterW>
                                                                                        problem)))
         return false;
 
-    if(!(StartsWith(name, "gfx8") || StartsWith(name, "gfx9")) || StartsWith(name, "gfx94"))
-        return false;
     if(name == "gfx90a" && problem.IsGfx90aFp16altRequired())
         return false;
 
