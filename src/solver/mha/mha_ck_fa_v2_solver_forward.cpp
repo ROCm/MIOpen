5b4a58bd4111cbda3382ec9d54ee5b63e7135705
Brian Harrison <Brian.Harrison@amd.com>
2024-09-27 22:46:53 +0000

    Apply formatting

--- src/solver/mha/mha_ck_fa_v2_solver_forward.cpp
+++ src/solver/mha/mha_ck_fa_v2_solver_forward.cpp
@@ -47,25 +47,23 @@ static std::string Convert(miopenDataType_t dataType)
 {
     switch(dataType)
     {
-        case miopenHalf:
-        {
-            return "fp16";
-        }
-        case miopenBFloat16:
-        {
-            return "bfp16";
-        }
-        default:
-        {
-            MIOPEN_THROW("Unsupported datatype provided");
-        }
+    case miopenHalf: {
+        return "fp16";
+    }
+    case miopenBFloat16: {
+        return "bfp16";
+    }
+    default: {
+        MIOPEN_THROW("Unsupported datatype provided");
+    }
     }
 }
 
-bool MhaCKFlashAttentionV2Forward::IsApplicable([[maybe_unused]] const ExecutionContext& context,
-                                const miopen::mha::ProblemDescription& problem) const
+bool MhaCKFlashAttentionV2Forward::IsApplicable(
+    [[maybe_unused]] const ExecutionContext& context,
+    const miopen::mha::ProblemDescription& problem) const
 {
-    #if MIOPEN_USE_COMPOSABLEKERNEL
+#if MIOPEN_USE_COMPOSABLEKERNEL
     if(!problem.IsForward())
     {
         return false;
@@ -76,37 +74,39 @@ bool MhaCKFlashAttentionV2Forward::IsApplicable([[maybe_unused]] const Execution
         return false;
     }
 
-    auto& descsFwd = problem.GetDescsForward();
+    auto& descsFwd            = problem.GetDescsForward();
     auto [N_k, H_k, S_k, D_k] = miopen::tien<4>(descsFwd.kDesc.GetLengths());
     auto [N_q, H_q, S_q, D_q] = miopen::tien<4>(descsFwd.qDesc.GetLengths());
 
-    return !env::disabled(MIOPEN_DEBUG_FA_CK_V2_FWD)                 //
-           && H_q == H_k                                            // Replace with H_q % H_k == 0 once we add support for MQA & GQA.
-           && H_q <= 256                                            //
-           && H_q % 8 == 0                                          // No padding support yet which means it needs to be multiple of 8.
-           && descsFwd.kDesc.IsPacked()                             //
-           && descsFwd.qDesc.IsPacked()                             //
-           && descsFwd.vDesc.IsPacked()                             //
-           && descsFwd.oDesc.IsPacked()                             //
-           && descsFwd.biasDesc.IsPacked()                          //
-           && descsFwd.biasDesc.GetType() == miopenHalf             //
-           && descsFwd.kDesc.GetType() == miopenHalf                //
-           && descsFwd.qDesc.GetType() == miopenHalf                //
-           && descsFwd.vDesc.GetType() == miopenHalf                //
-           && descsFwd.oDesc.GetType() == miopenHalf;               //
+    return !env::disabled(MIOPEN_DEBUG_FA_CK_V2_FWD) //
+           && H_q == H_k   // Replace with H_q % H_k == 0 once we add support for MQA & GQA.
+           && H_q <= 256   //
+           && H_q % 8 == 0 // No padding support yet which means it needs to be multiple of 8.
+           && descsFwd.kDesc.IsPacked()                 //
+           && descsFwd.qDesc.IsPacked()                 //
+           && descsFwd.vDesc.IsPacked()                 //
+           && descsFwd.oDesc.IsPacked()                 //
+           && descsFwd.biasDesc.IsPacked()              //
+           && descsFwd.biasDesc.GetType() == miopenHalf //
+           && descsFwd.kDesc.GetType() == miopenHalf    //
+           && descsFwd.qDesc.GetType() == miopenHalf    //
+           && descsFwd.vDesc.GetType() == miopenHalf    //
+           && descsFwd.oDesc.GetType() == miopenHalf;   //
 #else
     return false;
 #endif
 }
 
-std::size_t MhaCKFlashAttentionV2Forward::GetWorkspaceSize([[maybe_unused]] const ExecutionContext& context,
-                                           [[maybe_unused]] const miopen::mha::ProblemDescription& problem) const
+std::size_t MhaCKFlashAttentionV2Forward::GetWorkspaceSize(
+    [[maybe_unused]] const ExecutionContext& context,
+    [[maybe_unused]] const miopen::mha::ProblemDescription& problem) const
 {
     return 0;
 }
 
-ConvSolution MhaCKFlashAttentionV2Forward::GetSolution([[maybe_unused]] const ExecutionContext& context,
-                                       const miopen::mha::ProblemDescription& problem) const
+ConvSolution
+MhaCKFlashAttentionV2Forward::GetSolution([[maybe_unused]] const ExecutionContext& context,
+                                          const miopen::mha::ProblemDescription& problem) const
 {
     auto result         = ConvSolution{miopenStatusSuccess};
     result.workspace_sz = 0;
@@ -138,30 +138,30 @@ ConvSolution MhaCKFlashAttentionV2Forward::GetSolution([[maybe_unused]] const Ex
     const ck_tile::index_t max_seqlen_k   = seqlen_k;
 
     fmha_fwd_traits fmha_traits;
-    fmha_traits.hdim_q = hdim_q;
-    fmha_traits.hdim_v = hdim_v;
-    fmha_traits.data_type = Convert(descsFwd.qDesc.GetType());
-    fmha_traits.is_group_mode = is_group_mode;
-    fmha_traits.is_v_rowmajor = false;
-    fmha_traits.mask_type = mask_enum::no_mask;
-    fmha_traits.has_lse = false;
-    fmha_traits.is_v_rowmajor = false;
+    fmha_traits.hdim_q              = hdim_q;
+    fmha_traits.hdim_v              = hdim_v;
+    fmha_traits.data_type           = Convert(descsFwd.qDesc.GetType());
+    fmha_traits.is_group_mode       = is_group_mode;
+    fmha_traits.is_v_rowmajor       = false;
+    fmha_traits.mask_type           = mask_enum::no_mask;
+    fmha_traits.has_lse             = false;
+    fmha_traits.is_v_rowmajor       = false;
     fmha_traits.do_fp8_static_quant = false;
 
     fmha_fwd_args fmha_args;
-    fmha_args.batch = batch;
-    fmha_args.hdim_q = hdim_q;
-    fmha_args.hdim_v = hdim_v;
-    fmha_args.nhead_q = nhead_q;
-    fmha_args.nhead_k = nhead_k;
-    fmha_args.stride_q = (i_perm ? hdim_q : nhead * hdim_q);
-    fmha_args.stride_k = (i_perm ? hdim_q : nhead_k * hdim_q);
-    fmha_args.stride_v = (i_perm ? shape_seqlen_k : nhead_k * shape_seqlen_k);
+    fmha_args.batch          = batch;
+    fmha_args.hdim_q         = hdim_q;
+    fmha_args.hdim_v         = hdim_v;
+    fmha_args.nhead_q        = nhead_q;
+    fmha_args.nhead_k        = nhead_k;
+    fmha_args.stride_q       = (i_perm ? hdim_q : nhead * hdim_q);
+    fmha_args.stride_k       = (i_perm ? hdim_q : nhead_k * hdim_q);
+    fmha_args.stride_v       = (i_perm ? shape_seqlen_k : nhead_k * shape_seqlen_k);
     fmha_args.batch_stride_q = (nhead * shape_seqlen_q * hdim_q);
     fmha_args.batch_stride_k = (nhead_k * shape_seqlen_k * hdim_q);
     fmha_args.batch_stride_v = (nhead_k * hdim_v * shape_seqlen_k);
-    fmha_args.seqlen_k = shape_seqlen_k;
-    fmha_args.max_seqlen_q = max_seqlen_q;
+    fmha_args.seqlen_k       = shape_seqlen_k;
+    fmha_args.max_seqlen_q   = max_seqlen_q;
 
     // These are used for group mode, and we are in batch right now.
     fmha_args.seqstart_q_ptr = nullptr;
@@ -170,53 +170,63 @@ ConvSolution MhaCKFlashAttentionV2Forward::GetSolution([[maybe_unused]] const Ex
     // Batch does not support padding, and we aren't using kvcache yet.
     fmha_args.seqlen_k_ptr = nullptr;
 
-    fmha_args.scale_s = scale_s;
-    fmha_args.scale_p = scale_p;
-    fmha_args.scale_o = scale_o;
-    fmha_args.stride_bias = (i_perm ? shape_seqlen_k : 1 * shape_seqlen_k);
-    fmha_args.stride_o = (o_perm ? hdim_v : nhead * hdim_v);
+    fmha_args.scale_s           = scale_s;
+    fmha_args.scale_p           = scale_p;
+    fmha_args.scale_o           = scale_o;
+    fmha_args.stride_bias       = (i_perm ? shape_seqlen_k : 1 * shape_seqlen_k);
+    fmha_args.stride_o          = (o_perm ? hdim_v : nhead * hdim_v);
     fmha_args.nhead_stride_bias = i_perm ? 0 * shape_seqlen_q * shape_seqlen_k : 0 * shape_seqlen_k;
-    fmha_args.nhead_stride_lse = shape_seqlen_q;
-    fmha_args.nhead_stride_o = o_perm ? shape_seqlen_q * hdim_v : hdim_v;
-    fmha_args.window_size_left = 0;
+    fmha_args.nhead_stride_lse  = shape_seqlen_q;
+    fmha_args.nhead_stride_o    = o_perm ? shape_seqlen_q * hdim_v : hdim_v;
+    fmha_args.window_size_left  = 0;
     fmha_args.window_size_right = 0;
-    fmha_args.mask_type = static_cast<ck_tile::index_t>(fmha_traits.mask_type);
+    fmha_args.mask_type         = static_cast<ck_tile::index_t>(fmha_traits.mask_type);
 
     fmha_args.s_randval = false;
     // Since we aren't storing the random values these will be unused for now.
-    fmha_args.stride_randval = max_seqlen_k;
+    fmha_args.stride_randval       = max_seqlen_k;
     fmha_args.nhead_stride_randval = (shape_seqlen_q * max_seqlen_k);
-    fmha_args.batch_stride_randval = (nhead * shape_seqlen_q * max_seqlen_k); 
+    fmha_args.batch_stride_randval = (nhead * shape_seqlen_q * max_seqlen_k);
 
-    result.invoker_factory                = [=](const std::vector<Kernel>&) {
-        return [=] (const Handle& handle_, const AnyInvokeParams& raw_params) {
+    result.invoker_factory = [=](const std::vector<Kernel>&) {
+        return [=](const Handle& handle_, const AnyInvokeParams& raw_params) {
             decltype(auto) params = raw_params.CastTo<miopen::mha::InvokeParams>();
             const auto& dataFwd   = params.GetDataForward();
 
             fmha_fwd_traits fmha_runtime_traits = fmha_traits;
-            fmha_fwd_args fmha_runtime_args = fmha_args;
+            fmha_fwd_args fmha_runtime_args     = fmha_args;
 
-            fmha_runtime_traits.bias_type = dataFwd.biasData != nullptr ? bias_enum::elementwise_bias : bias_enum::no_bias;
+            fmha_runtime_traits.bias_type =
+                dataFwd.biasData != nullptr ? bias_enum::elementwise_bias : bias_enum::no_bias;
             fmha_runtime_traits.has_dropout = dataFwd.dropoutProbabilityData != nullptr;
 
             float probability = 0;
-            uint64_t seed = 0;
-            uint64_t offset = 0;
+            uint64_t seed     = 0;
+            uint64_t offset   = 0;
             if(fmha_runtime_traits.has_dropout)
             {
-                hipMemcpy(&probability, dataFwd.dropoutProbabilityData, sizeof(float), hipMemcpyKind::hipMemcpyDeviceToHost);
-                hipMemcpy(&seed, dataFwd.dropoutSeedData, sizeof(uint64_t), hipMemcpyKind::hipMemcpyDeviceToHost);
-                hipMemcpy(&offset, dataFwd.dropoutOffsetData, sizeof(uint64_t), hipMemcpyKind::hipMemcpyDeviceToHost);
+                hipMemcpy(&probability,
+                          dataFwd.dropoutProbabilityData,
+                          sizeof(float),
+                          hipMemcpyKind::hipMemcpyDeviceToHost);
+                hipMemcpy(&seed,
+                          dataFwd.dropoutSeedData,
+                          sizeof(uint64_t),
+                          hipMemcpyKind::hipMemcpyDeviceToHost);
+                hipMemcpy(&offset,
+                          dataFwd.dropoutOffsetData,
+                          sizeof(uint64_t),
+                          hipMemcpyKind::hipMemcpyDeviceToHost);
             }
-            fmha_runtime_args.p_drop = probability;
+            fmha_runtime_args.p_drop           = probability;
             fmha_runtime_args.drop_seed_offset = {seed, offset};
 
-            fmha_runtime_args.bias_ptr = dataFwd.biasData;
-            fmha_runtime_args.q_ptr = dataFwd.qData;
-            fmha_runtime_args.k_ptr = dataFwd.kData;
-            fmha_runtime_args.v_ptr = dataFwd.vData;
+            fmha_runtime_args.bias_ptr     = dataFwd.biasData;
+            fmha_runtime_args.q_ptr        = dataFwd.qData;
+            fmha_runtime_args.k_ptr        = dataFwd.kData;
+            fmha_runtime_args.v_ptr        = dataFwd.vData;
             fmha_runtime_args.rand_val_ptr = nullptr;
-            fmha_runtime_args.o_ptr = dataFwd.oData;
+            fmha_runtime_args.o_ptr        = dataFwd.oData;
 
             // Create stream_config, and set it to not time kernel.
             ck_tile::stream_config stream_config;
@@ -228,7 +238,7 @@ ConvSolution MhaCKFlashAttentionV2Forward::GetSolution([[maybe_unused]] const Ex
             }
         };
     };
-    
+
     return result;
 }
 
