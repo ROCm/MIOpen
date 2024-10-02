3756f5ab8cb47556a7d80fd24fcc8bc328c11723
Christopher Erb <christopher.erb@amd.com>
2024-10-02 15:03:25 +0000

    fix

--- test/gtest/kernel_tuning_net.cpp
+++ test/gtest/kernel_tuning_net.cpp
@@ -79,8 +79,8 @@ std::vector<KernelTuningNetTestCase> GetConvHipIgemmGroupWrwXdlopsTestCases_FP32
               miopen::conv::Direction::BackwardWeights,
               miopenFloat,
               miopenTensorNHWC},
-             "DeviceGroupedConvBwdWeight_Xdl_CShuffle<128, 128, 32, 4, Default, 4, 2, 1, 4, 4, 1, "
-             "1, 1, 1, 1>+128",
+             "DeviceGroupedConvBwdWeight_Xdl_CShuffle<64, 64, 64, 4, Default, 4, 2, 2, 1, 4, 1, "
+             "4, 1, 1, 1>+1",
              "gfx942"}};
 }
 
