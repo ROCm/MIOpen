eb94399dd2a4a5d29b05669c18b3d2ff5b94c7ca
Brian Harrison <Brian.Harrison@amd.com>
2024-10-15 16:34:27 +0000

    Use device pointer instead of value for probability params

--- src/solver/mha/mha_ck_fa_v2_solver_forward.cpp
+++ src/solver/mha/mha_ck_fa_v2_solver_forward.cpp
@@ -233,26 +233,16 @@ MhaCKFlashAttentionV2Forward::GetSolution([[maybe_unused]] const ExecutionContex
 
             fmha_runtime_traits.has_dropout = dataFwd.dropoutProbabilityData != nullptr;
             float probability               = 0;
-            uint64_t seed                   = 0;
-            uint64_t offset                 = 0;
             if(fmha_runtime_traits.has_dropout)
             {
                 hipMemcpy(&probability,
                           dataFwd.dropoutProbabilityData,
                           sizeof(float),
                           hipMemcpyKind::hipMemcpyDeviceToHost);
-                hipMemcpy(&seed,
-                          dataFwd.dropoutSeedData,
-                          sizeof(uint64_t),
-                          hipMemcpyKind::hipMemcpyDeviceToHost);
-                hipMemcpy(&offset,
-                          dataFwd.dropoutOffsetData,
-                          sizeof(uint64_t),
-                          hipMemcpyKind::hipMemcpyDeviceToHost);
             }
             fmha_runtime_args.p_drop = probability;
-            // TODO: update to use GPU pointers once available from CK hash update.
-            fmha_runtime_args.drop_seed_offset = std::make_pair(seed, offset);
+            fmha_runtime_args.drop_seed_offset =
+                std::make_pair(dataFwd.dropoutSeedData, dataFwd.dropoutOffsetData);
 
             // Create stream_config, and set it to not time kernel.
             ck_tile::stream_config stream_config;
