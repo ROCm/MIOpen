c6b426efbfb028a8658afb40dfe9f62bafc2af1b
Bibek Ghimire <bghimire@amd.com>
2024-10-27 06:20:41 +0000

    fix review comments

--- src/include/miopen/batchnorm/problem_description.hpp
+++ src/include/miopen/batchnorm/problem_description.hpp
@@ -52,9 +52,6 @@ struct ProblemDescriptionTag
 {
 };
 
-using index_t                           = int32_t;
-constexpr index_t NumBatchNormReduceDim = 3;
-
 struct MIOPEN_INTERNALS_EXPORT ProblemDescription : ProblemDescriptionBase,
                                                     ProblemDescriptionTag
 #if MIOPEN_ENABLE_SQLITE
@@ -320,17 +317,7 @@ private:
     NetworkConfig MakeForwardInferenceNetworkConfig() const;
     NetworkConfig MakeBackwardNetworkConfig() const;
 
-    std::string ComputeLayout(const TensorDescriptor& td) const
-    {
-        if(spatial_dim == 2)
-        {
-            return td.GetLayout("NCHW");
-        }
-        else
-        {
-            return td.GetLayout("NCDHW");
-        }
-    }
+    std::string ComputeLayout(const TensorDescriptor& td) const { return td.GetLayout_str(); }
     std::string ComputeInLayout() const { return ComputeLayout(xDesc); }
     std::string ComputeOutLayout() const { return ComputeLayout(yOrDyDesc); }
     std::string ComputeDinLayout() const { return ComputeLayout(dxDesc); }
