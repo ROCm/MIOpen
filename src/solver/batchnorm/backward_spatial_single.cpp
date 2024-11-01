43aa47950aedded462c971e87d2edc2d6e4c6c45
Brian Harrison <Brian.Harrison@amd.com>
2024-11-01 03:07:35 +0000

    Add bf16 support for bwd OCL spatial

--- src/solver/batchnorm/backward_spatial_single.cpp
+++ src/solver/batchnorm/backward_spatial_single.cpp
@@ -79,9 +79,10 @@ BnBwdTrainingSpatialSingle::GetSolution(const ExecutionContext& context,
     const auto& handle      = context.GetStream();
     const unsigned wavesize = (miopen::StartsWith(handle.GetDeviceName(), "gfx10") ? 32 : 64);
 
-    bool bfpmixparm = false;
-    bool bfp16parm  = false;
-    bool bfp32parm  = true;
+    bool bfpmixparm   = false;
+    bool bbfpmixparam = false;
+    bool bfp16parm    = false;
+    bool bfp32parm    = true;
 
     if(problem.GetXDesc().GetType() == miopenHalf &&
        problem.GetScaleBiasDiffDesc().GetType() == miopenHalf)
@@ -94,6 +95,12 @@ BnBwdTrainingSpatialSingle::GetSolution(const ExecutionContext& context,
     {
         bfpmixparm = true;
         bfp32parm  = false;
+    }
+    else if(problem.GetXDesc().GetType() == miopenBFloat16 &&
+            problem.GetScaleBiasDiffDesc().GetType() == miopenFloat)
+    {
+        bbfpmixparam = true;
+        bfp32parm    = false;
     }
 
     int n, c, h, w;
@@ -208,6 +215,7 @@ BnBwdTrainingSpatialSingle::GetSolution(const ExecutionContext& context,
             {"MIOPEN_USE_FP16", static_cast<int>(bfp16parm)},
             {"MIOPEN_USE_FP32", static_cast<int>(bfp32parm)},
             {"MIOPEN_USE_FPMIX", static_cast<int>(bfpmixparm)},
+            {"MIOPEN_USE_BFPMIX", static_cast<int>(bbfpmixparam)},
             {"MIO_BN_USESAVED", static_cast<int>(problem.UseSaved())},
             {"MIO_BN_N", static_cast<int>(n)},
             {"MIO_BN_C", static_cast<int>(c)},
