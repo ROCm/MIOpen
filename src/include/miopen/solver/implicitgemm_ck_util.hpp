fc25be9931ae9708c68964f25bf710221b8bcd05
Daming Feng <dmfeng8898@gmail.com>
2024-11-11 23:14:13 +0000

    clear buffer for fwd output to avoid Nan issue

--- src/include/miopen/solver/implicitgemm_ck_util.hpp
+++ src/include/miopen/solver/implicitgemm_ck_util.hpp
@@ -759,7 +759,8 @@ ConvSolution InitInvokerFactoryNCHW(const ExecutionContext& ctx,
             /// \todo: Will need SetTensor() to properly zero out non-packed tensors
             /// Note: Need to clear buffer memory for BWD data since all values may not be set.
             if(output_tr_inst.GetConvOperandTag() == internal::ConvOperandTag::Weights ||
-               output_tr_inst.GetConvOperandTag() == internal::ConvOperandTag::Input)
+               output_tr_inst.GetConvOperandTag() == internal::ConvOperandTag::Input ||
+               output_tr_inst.GetConvOperandTag() == internal::ConvOperandTag::Output)
             {
                 output_tr_inst.ZeroOutBuffer(handle);
             }
