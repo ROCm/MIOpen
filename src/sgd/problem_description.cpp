97712416d5097fe64c0998c25125c0c3d4c73456
hieule88 <hieu.le@moreh.com.vn>
2024-11-25 16:21:56 +0700

    2023->2024

--- src/sgd/problem_description.cpp
+++ src/sgd/problem_description.cpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2023 Advanced Micro Devices, Inc.
+ * Copyright (c) 2024 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,6 +26,8 @@
 
 #include <miopen/sgd/problem_description.hpp>
 #include <miopen/names.hpp>
+
+#include <sstream>
 
 namespace miopen {
 
