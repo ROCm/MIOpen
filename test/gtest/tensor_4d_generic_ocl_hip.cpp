a641ba0275a916f82fcf8db833c87fca29774184
novakovicdj <novakovicdj27@gmail.com>
2024-11-25 12:32:14 +0200

    minor changes

--- test/gtest/tensor_4d_generic_ocl_hip.cpp
+++ test/gtest/tensor_4d_generic_ocl_hip.cpp
@@ -38,7 +38,7 @@
 #include <miopen/float_equal.hpp>
 
 #define MAX_TENSOR_ELEM 17
-#define PERF_ENABLE 1
+#define PERF_ENABLE 0
 #define POW_2 1
 
 struct TensorsConfig
@@ -129,11 +129,11 @@ std::vector<TensorsConfig> TensorsConfigs()
         {
             for(size_t N = 1; N <= maxTotalSize; N *= 2)
             {
-                for(size_t C = 1; C <= maxTotalSize / N; C *= 8)
+                for(size_t C = 1; C <= maxTotalSize / N; C *= 2)
                 {
-                    for(size_t H = 1; H <= maxTotalSize / (N * C); H *= 8)
+                    for(size_t H = 1; H <= maxTotalSize / (N * C); H *= 2)
                     {
-                        for(size_t W = 1; W <= maxTotalSize / (N * C * H); W *= 16)
+                        for(size_t W = 1; W <= maxTotalSize / (N * C * H); W *= 2)
                         {
                             size_t totalSize = N * C * H * W;
                             // Ensure the total size does not exceed the maximum limit
@@ -224,10 +224,6 @@ protected:
         tensC = tensor<T>{tensorsConfig.aclens, tensorsConfig.acstrides}.generate(
             tensor_elem_gen_integer{MAX_TENSOR_ELEM});
 
-        // print_tensor(tensA, "A", 1);
-        // print_tensor(tensB, "B", 1);
-        // print_tensor(tensC, "C", 1);
-
         // Write the device tensors
         tensA_dev = handle.Write(tensA.data);
         tensB_dev = handle.Write(tensB.data);
@@ -267,21 +263,12 @@ protected:
             }
         }
 
-        if(std::accumulate(tensorsConfig.blens.begin(),
-                           tensorsConfig.blens.end(),
-                           4,
-                           std::multiplies<std::size_t>()) == 1)
-        {
-            bitmap = 4;
-        }
-        // auto fwd_conv_bias = bitmap == (1 << 2) ? 1 : 0;
-        // // This block gives off indexing for 5d tensors, skipping
-        // if(fwd_conv_bias == 1 && tensorsConfig.aclens.size() < 5 && num_wg < 640 &&
-        //    work_per_wg > 256 && tensorsConfig.aclens[0] > 0)
-        // { // 640 workgroups of size 256 needed to completely fill the GPU
-
-        //     work_per_wg /= tensorsConfig.aclens[0]; // c_n;
-        //     num_wg *= tensorsConfig.aclens[0];      // c_n;
+        // if(std::accumulate(tensorsConfig.blens.begin(),
+        //                    tensorsConfig.blens.end(),
+        //                    4,
+        //                    std::multiplies<std::size_t>()) == 1)
+        // {
+        //     bitmap = 4;
         // }
 
         num_wg_orig = num_wg;
@@ -290,23 +277,8 @@ protected:
 
         size_t local_threads = 256;
 
-        // bool leading_ones = true;
-
-        // for(int i = (d - 2); i >= 0; i--)
-        // {
-        //     bool is_one = (bitmap & (1 << (tensorsConfig.aclens.size() - 1 - i))) != 0u;
-        //     leading_ones &= is_one;
-        // }
-
-        // if(leading_ones && work_per_wg < 64)
-        // {
-        //     local_threads = 64;
-        // }
-
         size_t global_threads;
-        global_threads =
-            /*(static_cast<int>(leading_ones) == 1 && (d - 1) == 3) ? num_wg : */ num_wg *
-            local_threads;
+        global_threads = num_wg * local_threads;
         global_threads = (global_threads < local_threads) ? local_threads : global_threads;
 
         vld = {local_threads, 1, 1};
@@ -314,14 +286,6 @@ protected:
 
         network_config += std::to_string(data_type) + "-miopenTensorOpAdd-" +
                           std::to_string(global_threads) + "-" + std::to_string(local_threads);
-        std::cout << "A: " << std::to_string(tensorsConfig.aclens[0]) << "x"
-                  << std::to_string(tensorsConfig.aclens[1]) << "x"
-                  << std::to_string(tensorsConfig.aclens[2]) << "x"
-                  << std::to_string(tensorsConfig.aclens[3]) << std::endl;
-        std::cout << "B: " << std::to_string(tensorsConfig.blens[0]) << "x"
-                  << std::to_string(tensorsConfig.blens[1]) << "x"
-                  << std::to_string(tensorsConfig.blens[2]) << "x"
-                  << std::to_string(tensorsConfig.blens[3]) << std::endl;
     }
 
     void runCPU()
@@ -341,18 +305,10 @@ protected:
         size_t a_hstride = tensorsConfig.acstrides[2];
         size_t a_wstride = tensorsConfig.acstrides[3];
 
-        size_t b_c = tensorsConfig.blens[1] == 1 ? a_c : tensorsConfig.blens[1];
-        size_t b_h = tensorsConfig.blens[2] == 1 ? a_h : tensorsConfig.blens[2];
-        size_t b_w = tensorsConfig.blens[3] == 1 ? a_w : tensorsConfig.blens[3];
-
         size_t b_nstride = tensorsConfig.blens[0] == 1 ? 0 : tensorsConfig.bstrides[0];
         size_t b_cstride = tensorsConfig.blens[1] == 1 ? 0 : tensorsConfig.bstrides[1];
         size_t b_hstride = tensorsConfig.blens[2] == 1 ? 0 : tensorsConfig.bstrides[2];
         size_t b_wstride = tensorsConfig.blens[3] == 1 ? 0 : tensorsConfig.bstrides[3];
-
-        // std::cout << b_wstride << std::endl;
-
-        int tid = 0;
 
         for(int i = 0; i < a_n; i++)
         {
@@ -362,29 +318,14 @@ protected:
                 {
                     for(int l = 0; l < a_w; l++)
                     {
-                        // int a_index = (tid / (a_c * a_h * a_w)) * a_nstride +
-                        //               ((tid % (a_c * a_h * a_w)) / (a_h * a_w)) * a_cstride +
-                        //               ((tid % (a_h * a_w)) / a_w) * a_hstride +
-                        //               (tid % a_w) * a_wstride;
-                        // int b_index = (tid / (b_c * b_h * b_w)) * b_nstride +
-                        //               ((tid % (b_c * b_h * b_w)) / (b_h * b_w)) * b_cstride +
-                        //               ((tid % (b_h * b_w)) / b_w) * b_hstride +
-                        //               (tid % b_w) * b_wstride;
                         int a_index = i * a_nstride + j * a_cstride + k * a_hstride + l * a_wstride;
                         int b_index = i * b_nstride + j * b_cstride + k * b_hstride + l * b_wstride;
                         C[a_index]  = alpha0 * A[a_index] + alpha1 * B[b_index] + beta * C[a_index];
-                        // std::cout << "C[" << a_index << "] (" << C[a_index] << ") = A[" <<
-                        // a_index
-                        //           << "] (" << A[a_index] << ") + B[" << b_index << "] ("
-                        //           << B[b_index] << ")" << std::endl;
-
-                        tid += 1;
                     }
                 }
             }
         }
         tensC_cpu.data = C;
-        // print_tensor(tensC_cpu, "C cpu", 1);
     }
 
     void runOCL()
@@ -437,7 +378,7 @@ protected:
                                  static_cast<int>(num_wg_orig));
 
         tensC_ocl.data = handle.Read<T>(tensC_dev, tensC_ocl.data.size());
-        // print_tensor(tensC_ocl, "C ocl", 1);
+
         if constexpr(PERF_ENABLE)
         {
             ph.perfTest(handle,
@@ -638,6 +579,7 @@ TEST_P(GPU_Op4dTensorGenericTest_FP32, PortTest)
     runOCL();
     // run HIP kernel
     runHIP();
+    // run CPU implementation
     // runCPU();
     // verify if the output tensors are same
     verify();
