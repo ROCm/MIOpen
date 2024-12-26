dde96e811586fec6891ecc6dbbd80020dc22c176
Anh Duong <anh.duong@moreh.com.vn>
2024-12-26 04:54:47 +0000

    rm unnecessary change in tensor_view.hpp

--- src/kernels/tensor_view.hpp
+++ src/kernels/tensor_view.hpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2023 Advanced Micro Devices, Inc.
+ * Copyright (c) 2024 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,7 @@
  * SOFTWARE.
  *
  *******************************************************************************/
+
 #ifndef GUARD_TENSOR_VIEW_HPP
 #define GUARD_TENSOR_VIEW_HPP
 
