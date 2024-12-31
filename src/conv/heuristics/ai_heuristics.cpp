0fe2904fc53908701d11b01dfd2ce6c17429f98e
Muhammad Saud <muhammad.saud.ms@gmail.com>
2024-12-31 16:47:01 +0000

    ck fwd, wrw, bwd KTN updated

--- src/conv/heuristics/ai_heuristics.cpp
+++ src/conv/heuristics/ai_heuristics.cpp
@@ -707,6 +707,11 @@ bool ModelSetParams(const std::string& arch,
 {
     auto model = GetModel(arch, solver);
 
+    std::cout << "\nFeatures: ";
+    for (int i = 0; i < 17; ++i)
+        std::cout << features[i * 17 + i] << ", ";
+    std::cout << "\n";
+
     // get context
     int dim = 0;
     if(transform_features)
@@ -727,10 +732,11 @@ bool ModelSetParams(const std::string& arch,
     default: return false;
     }
 
+    std::cout << "\n PREDICT TYPE: " << model->metadata.predict_type << "\n";
+
     // run decoder to set kernel parameters
     for(size_t i = 0, num_tuning_params = 1; i < num_tuning_params; ++i)
     {
-
         if(i == 0 && (model->metadata.predict_type == 0u))
             num_tuning_params = model->metadata.num_tuning_params[dir];
 
@@ -739,8 +745,9 @@ bool ModelSetParams(const std::string& arch,
                                                                        // score of the k-th token
         // order tokens according to their scores
         std::priority_queue<std::pair<float, int>> pq;
-        for(int j = 0; j < token_scores.size(); j++)
+        for(int j = 0; j < token_scores.size(); j++) {
             pq.push(std::make_pair(token_scores[j], j)); // sort by value at index
+        }
 
         // find a token whose value is a valid kernel parameter for the i-th position
         int output_token_index = -1;
@@ -751,11 +758,19 @@ bool ModelSetParams(const std::string& arch,
             std::string value = model->metadata.tuning_decodings[std::to_string(token)];
             pq.pop();
 
+            std::cout << "\n";
+            std::cout << i;
+            std::cout << " " + std::to_string(token) + " " + value;
+            //std::cout << "\nTOKEN SCORES";
+            //for(int j = 0; j < token_scores.size(); j++) {
+            //    std::cout << token_scores[j] << " ";
+            //}
+            //std::cout << std::endl;
             if(value == "-1") // if token-value is "-1", then decoding has finished
             {
                 auto stop     = std::chrono::high_resolution_clock::now();
                 auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-                MIOPEN_LOG_I2("Model ran for " << duration.count() << " micro-seconds");
+                MIOPEN_LOG_I2("KTN ran for " << duration.count() << " micro-seconds. Ended at -1.");
                 return false;
             }
             if(validator(i, value)) // if token-value is a valid kernel parameter, it's set
@@ -773,7 +788,7 @@ bool ModelSetParams(const std::string& arch,
 
     auto stop     = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    MIOPEN_LOG_I2("Model ran for " << duration.count() << " micro-seconds");
+    MIOPEN_LOG_I2("KTN ran for " << duration.count() << " micro-seconds");
     return true;
 }
 
