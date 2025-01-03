23a67000ed1cfd2870029a14845ac2b496e65163
novakovicdj <novakovicdj27@gmail.com>
2025-01-03 15:21:44 +0200

    changed mistake from merge

--- src/kernels/MIOpenTensorKernelsHip.cpp
+++ src/kernels/MIOpenTensorKernelsHip.cpp
@@ -62,13 +62,13 @@ extern "C" __global__ void Op1dTensorGeneric(const MIOPEN_TYPE* a,
                                              const uint64_t Aoffset,
                                              const uint64_t Boffset,
                                              const uint64_t Coffset,
-                                             const uint32_t a_nstride,
-                                             const uint32_t b_nstride,
-                                             const uint32_t c_nstride,
+                                             const DIM_TYPE a_nstride,
+                                             const DIM_TYPE b_nstride,
+                                             const DIM_TYPE c_nstride,
                                              const MIOPEN_TYPE alpha0,
                                              const MIOPEN_TYPE alpha1,
                                              const MIOPEN_TYPE beta,
-                                             const uint32_t total_work,
+                                             const DIM_TYPE total_work,
                                              const bool use_beta)
 {
     const MIOPEN_TYPE* a_off = a + Aoffset;
