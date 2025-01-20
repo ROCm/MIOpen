2f84e1be6fa7b8e1aa8bb8e0c4e5d9fa8f001e25
Christopher Erb <christopher.erb@amd.com>
2025-01-20 10:48:51 -0600

    update docker image usage

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -400,45 +400,46 @@ def RunPerfTest(Map conf=[:]){
     try {
         //(retimage, image) = getDockerImage(conf)
         image = conf.get("docker_image").pull()
-        echo "docker image: ${image}"
-        withDockerContainer(image: image, args: dockerOpts + ' -v=/var/jenkins/:/var/jenkins') {
-        timeout(time: 100, unit: 'MINUTES')
+        //echo "docker image: ${image}"
+        image.inside(dockerOpts + ' -v=/var/jenkins/:/var/jenkins') 
         {
-            //cmake_build(conf)
-            //unstash 'miopen_tar'
-            //sh "tar -zxvf build/miopen-hip-*-Linux-runtime.tar.gz"
-            ld_lib="${env.WORKSPACE}/install/lib"
-            def filename = conf.get("filename", "")
-            if(params.PERF_TEST_OVERRIDE != '')
+            timeout(time: 100, unit: 'MINUTES')
             {
-                echo "Appending MIOpenDriver cmd env vars: ${params.PERF_TEST_OVERRIDE}"
-                sh "export LD_LIBRARY_PATH=${ld_lib} && ${env.WORKSPACE}/install/bin/test_perf.py  --filename ${filename} --install_path ${env.WORKSPACE}/install/ --override ${params.PERF_TEST_OVERRRIDE}"
-            }else
-            {
-                sh "export LD_LIBRARY_PATH=${ld_lib} && ${env.WORKSPACE}/install/bin/test_perf.py  --filename ${filename} --install_path ${env.WORKSPACE}/install/"
-            }
-            archiveArtifacts artifacts: "install/bin/perf_results/${filename}", allowEmptyArchive: true, fingerprint: true
-            //sh "export LD_LIBRARY_PATH=${ld_lib} && ${env.WORKSPACE}/install/bin/test_perf.py  --filename ${filename} --install_path ${env.WORKSPACE}/install/"
-            jenkins_url = "${env.artifact_path}/MIOpenPerf/lastSuccessfulBuild/artifact"
-            if(params.COMPARE_TO_BASE)
-            {
-              try {
-                  sh "rm -rf ${env.WORKSPACE}/install/bin/old_results/"
-                  sh "wget -P ${env.WORKSPACE}/install/bin/old_results/ ${jenkins_url}/install/bin/perf_results/${filename}"
-              }
-              catch (Exception err){
-                  currentBuild.result = 'SUCCESS'
-              }
+                //cmake_build(conf)
+                //unstash 'miopen_tar'
+                //sh "tar -zxvf build/miopen-hip-*-Linux-runtime.tar.gz"
+                ld_lib="${env.WORKSPACE}/install/lib"
+                def filename = conf.get("filename", "")
+                if(params.PERF_TEST_OVERRIDE != '')
+                {
+                    echo "Appending MIOpenDriver cmd env vars: ${params.PERF_TEST_OVERRIDE}"
+                    sh "export LD_LIBRARY_PATH=${ld_lib} && ${env.WORKSPACE}/install/bin/test_perf.py  --filename ${filename} --install_path ${env.WORKSPACE}/install/ --override ${params.PERF_TEST_OVERRRIDE}"
+                }else
+                {
+                    sh "export LD_LIBRARY_PATH=${ld_lib} && ${env.WORKSPACE}/install/bin/test_perf.py  --filename ${filename} --install_path ${env.WORKSPACE}/install/"
+                }
+                archiveArtifacts artifacts: "install/bin/perf_results/${filename}", allowEmptyArchive: true, fingerprint: true
+                //sh "export LD_LIBRARY_PATH=${ld_lib} && ${env.WORKSPACE}/install/bin/test_perf.py  --filename ${filename} --install_path ${env.WORKSPACE}/install/"
+                jenkins_url = "${env.artifact_path}/MIOpenPerf/lastSuccessfulBuild/artifact"
+                if(params.COMPARE_TO_BASE)
+                {
+                  try {
+                      sh "rm -rf ${env.WORKSPACE}/install/bin/old_results/"
+                      sh "wget -P ${env.WORKSPACE}/install/bin/old_results/ ${jenkins_url}/install/bin/perf_results/${filename}"
+                  }
+                  catch (Exception err){
+                      currentBuild.result = 'SUCCESS'
+                  }
 
-              try{
-                 sh "${env.WORKSPACE}/install/bin/test_perf.py --compare_results --old_results_path ${env.WORKSPACE}/install/bin/old_results --filename ${filename}"
-              }
-              catch (Exception err){
-                  currentBuild.result = 'SUCCESS'
-              }
-              //cleanWs()
+                  try{
+                     sh "${env.WORKSPACE}/install/bin/test_perf.py --compare_results --old_results_path ${env.WORKSPACE}/install/bin/old_results --filename ${filename}"
+                  }
+                  catch (Exception err){
+                      currentBuild.result = 'SUCCESS'
+                  }
+                  //cleanWs()
+                }
             }
-        }
         }
     }
     catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e){
