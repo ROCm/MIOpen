d58f67dbf308df9dd2b0a693fe3248273dbd5f11
Brian Harrison <Brian.Harrison@amd.com>
2025-01-23 02:32:14 +0000

    Fix hanging test

--- test/main.cpp
+++ test/main.cpp
@@ -206,14 +206,7 @@ struct conv_forward : output_tensor_fixture
             int ret_algo_count;
             miopenConvAlgoPerf_t perf;
 
-#if MIOPEN_BUILD_DEV
-            miopenHandle_t handle2{};
-            STATUS(miopenCreate(&handle2));
-
-            miopenHandle_t& used_handle = handle2;
-#else
             miopenHandle_t& used_handle = handle;
-#endif
 
             STATUS(miopenEnableProfiling(used_handle, Profile));
 
@@ -248,10 +241,6 @@ struct conv_forward : output_tensor_fixture
                                             wspace.size()));
 
             STATUS(miopenGetKernelTime(used_handle, &time));
-
-#if MIOPEN_BUILD_DEV
-            STATUS(miopenDestroy(handle2));
-#endif
         }).join();
 
         if(Profile)
