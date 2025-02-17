b6b7d877cce82112d148ff425dd3b4233dcf79e0
Randy J. Spaulding <rspauldi@amd.com>
2025-02-17 18:57:35 +0000

    new f8 refs->fnuz

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1484,9 +1484,9 @@ std::string GetCastTensorBuildOptionFromType(const std::string& buildOption, mio
     case miopenHalf: return option += "2";
     case miopenFloat: return option += "3";
     case miopenBFloat16: return option += "4";
-    case miopenFloat8:
+    case miopenFloat8_fnuz:
         MIOPEN_THROW(miopenStatusBadParm, "miopenFloat8 data type not supported in cast tensor.");
-    case miopenBFloat8:
+    case miopenBFloat8_fnuz:
         MIOPEN_THROW(miopenStatusBadParm, "miopenBFloat8 data type not supported in cast tensor.");
     case miopenDouble:
         // TODO
