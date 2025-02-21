4d03def2d1d55f8a6ff053896b11a2d47660e55f
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2025-02-21 18:44:30 +0100

    Fix formatting

--- test/gtest/unit_conv_solver.cpp
+++ test/gtest/unit_conv_solver.cpp
@@ -171,7 +171,7 @@ ConvTestCase::ConvTestCase(TensorDescriptorParams&& x_,
 {
     const auto num_spatial_dims = conv.GetNumSpatialDims();
     const auto num_tensor_dims  = num_spatial_dims + 2;
-    const auto group_count = conv.GetGroupCount();
+    const auto group_count      = conv.GetGroupCount();
 
     if(x.GetNumDims() != num_tensor_dims || w.GetNumDims() != num_tensor_dims ||
        x.GetLens()[1] != w.GetLens()[1] * group_count)
