7291445e1091e0f77fcb33ecaace3b330f3d5338
novakovicdj <novakovicdj27@gmail.com>
2025-02-24 14:46:21 +0200

    fixed error in building strides in IOBufferDescriptor::build

--- src/include/miopen/rnn/tmp_buffer_utils.hpp
+++ src/include/miopen/rnn/tmp_buffer_utils.hpp
@@ -902,8 +902,9 @@ public:
     {
         //{batch, seq_cnt, vector}
         auto lens = std::vector<size_t>(xyDesc.GetLengths().begin(), xyDesc.GetLengths().end());
+        auto paddedStrides = xyDesc.GetPaddedStrides();
         auto strides =
-            std::vector<size_t>(xyDesc.GetPaddedStrides().begin(), xyDesc.GetPaddedStrides().end());
+            std::vector<size_t>(paddedStrides.begin(), paddedStrides.end());
 
         //{ combine(batch, seq_cnt), vector}
         std::vector<size_t> packed_lens{xyDesc.GetTotalSequenceLen(), lens[2]};
