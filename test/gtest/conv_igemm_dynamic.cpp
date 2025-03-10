879443bd87642cd8efbeebf1c1083cae1a37baaf
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2025-03-10 13:47:10 +0100

    Add unit test for ConvAsmImplicitGemmV4R1DynamicWrw, remove old tests

--- test/gtest/conv_igemm_dynamic.cpp
+++ test/gtest/conv_igemm_dynamic.cpp
@@ -37,20 +37,14 @@ auto GetTestCases()
     const auto env_1x1 = std::tuple{
         std::pair{MIOPEN_FIND_MODE, "normal"},
         std::pair{MIOPEN_DEBUG_FIND_ONLY_SOLVER, "ConvAsmImplicitGemmV4R1DynamicFwd_1x1"}};
-    const auto env_wrw =
-        std::tuple{std::pair{MIOPEN_FIND_MODE, "normal"},
-                   std::pair{MIOPEN_DEBUG_FIND_ONLY_SOLVER, "ConvAsmImplicitGemmV4R1DynamicWrw"}};
 
     const std::string v           = " --verbose";
     const std::string dis_bk_data = " --disable-backward-data";
     const std::string dis_bk_wei  = " --disable-backward-weights";
-    const std::string dis_fwd     = " --disable-forward";
 
     auto basic_tests = std::vector{
         // clang-format off
     std::pair{env_1x1, v + " --input  16 384  8  8 --weights 64 384 1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_bk_data + dis_bk_wei},
-    std::pair{env_wrw, v + " --input  64  64 28 28 --weights 32  64 1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_fwd + dis_bk_data},
-    std::pair{env_wrw, v + " --input  16  128 36 36 --weights 32  128 1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_fwd + dis_bk_data},
         // clang-format on
     };
 
@@ -60,12 +54,6 @@ auto GetTestCases()
     std::pair{env_1x1, v + " --input 128  256 28 28 --weights 128  256 1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_bk_data + dis_bk_wei},
     std::pair{env_1x1, v + " --input  64 1536  8  8 --weights 256 1536 1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_bk_data + dis_bk_wei},
     std::pair{env_1x1, v + " --input 128  768 17 17 --weights 128  768 1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_bk_data + dis_bk_wei},
-    std::pair{env_wrw, v + " --input  64   64 56 56 --weights 256  64  1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_fwd + dis_bk_data},
-    std::pair{env_wrw, v + " --input  32  128 34 34 --weights 64  128  3 3 --pads_strides_dilations 0 0 1 1 1 1" + dis_fwd + dis_bk_data},
-    std::pair{env_wrw, v + " --input 128  128 35 35 --weights 128  128 3 3 --pads_strides_dilations 1 1 1 1 1 1" + dis_fwd + dis_bk_data},
-    std::pair{env_wrw, v + " --input 128  256 56 56 --weights 64  256 1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_fwd + dis_bk_data},
-    std::pair{env_wrw, v + " --input  64  512 28 28 --weights 256 512 1 1 --pads_strides_dilations 0 0 2 2 1 1" + dis_fwd + dis_bk_data},
-    std::pair{env_wrw, v + " --input  64  512 14 14 --weights 256 512 1 1 --pads_strides_dilations 0 0 1 1 1 1" + dis_fwd + dis_bk_data},
                            // clang-format on
                        });
 
--- test/gtest/unit_conv_solver_ConvAsmImplicitGemmV4R1DynamicWrw.cpp
+++ test/gtest/unit_conv_solver_ConvAsmImplicitGemmV4R1DynamicWrw.cpp
@@ -34,7 +34,7 @@ auto GetConvTestCases(miopenDataType_t datatype)
 
     return std::vector{
         // clang-format off
-        TestCase{{64, 16, 14, 14}, {64, 16, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype},
+        TestCase{{1, 32, 28, 28}, {32, 32, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype},
         // clang-format on
     };
 }
@@ -45,13 +45,15 @@ auto GetConvTestCasesFull(miopenDataType_t datatype)
 
     return std::vector{
         // clang-format off
-        // https://github.com/ROCm/MIOpen/pull/272
-        //TestCase{{ 64,  64, 28, 28}, { 16,  64, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype}, // not applicable
-        //TestCase{{ 16, 128, 36, 36}, { 32, 128, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype}, // not applicable
+        // https://github.com/ROCm/MIOpen/pull/317
+        TestCase{{ 64,  64, 28, 28}, { 32,  64, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype},
+        TestCase{{ 16, 128, 36, 36}, { 32, 128, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype},
         TestCase{{ 64,  64, 56, 56}, {256,  64, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype},
-        //TestCase{{ 32, 128, 34, 34}, { 64, 128, 3, 3}, {0, 0}, {1, 1}, {1, 1}, datatype}, // not applicable
+        TestCase{{ 32, 128, 34, 34}, { 64, 128, 3, 3}, {0, 0}, {1, 1}, {1, 1}, datatype},
         TestCase{{128, 128, 35, 35}, {128, 128, 3, 3}, {1, 1}, {1, 1}, {1, 1}, datatype},
         TestCase{{128, 256, 56, 56}, { 64, 256, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype},
+        TestCase{{ 64, 512, 28, 28}, {256, 512, 1, 1}, {0, 0}, {2, 2}, {1, 1}, datatype},
+        TestCase{{ 64, 512, 14, 14}, {256, 512, 1, 1}, {0, 0}, {1, 1}, {1, 1}, datatype},
         // clang-format on
     };
 }
@@ -69,37 +71,37 @@ const auto& GetTestParams()
 
 } // namespace
 
-using GPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicBwd_FP32 = GPU_UnitTestConvSolverBwd_FP32;
-using CPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicDevApplicabilityBwd_NONE =
-    CPU_UnitTestConvSolverDevApplicabilityBwd_NONE;
+using GPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicWrw_FP32 = GPU_UnitTestConvSolverWrw_FP32;
+using CPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicDevApplicabilityWrw_NONE =
+    CPU_UnitTestConvSolverDevApplicabilityWrw_NONE;
 
-TEST_P(GPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicBwd_FP32, ConvAsmImplicitGemmV4R1DynamicBwd)
+TEST_P(GPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicWrw_FP32, ConvAsmImplicitGemmV4R1DynamicWrw)
 {
-    this->RunTest(miopen::solver::conv::ConvAsmImplicitGemmV4R1DynamicBwd{});
+    this->RunTest(miopen::solver::conv::ConvAsmImplicitGemmV4R1DynamicWrw{});
 };
 
-TEST_P(CPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicDevApplicabilityBwd_NONE,
-       ConvAsmImplicitGemmV4R1DynamicBwd)
+TEST_P(CPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicDevApplicabilityWrw_NONE,
+       ConvAsmImplicitGemmV4R1DynamicWrw)
 {
-    this->RunTest(miopen::solver::conv::ConvAsmImplicitGemmV4R1DynamicBwd{});
+    this->RunTest(miopen::solver::conv::ConvAsmImplicitGemmV4R1DynamicWrw{});
 };
 
 // Smoke tests
 INSTANTIATE_TEST_SUITE_P(Smoke,
-                         GPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicBwd_FP32,
+                         GPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicWrw_FP32,
                          testing::Combine(testing::Values(GetTestParams()),
                                           testing::Values(miopenConvolutionAlgoImplicitGEMM),
                                           testing::ValuesIn(GetConvTestCases(miopenFloat))));
 
 // Device applicability test
 INSTANTIATE_TEST_SUITE_P(Smoke,
-                         CPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicDevApplicabilityBwd_NONE,
+                         CPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicDevApplicabilityWrw_NONE,
                          testing::Combine(testing::Values(GetTestParams()),
                                           testing::Values(GetConvTestCases(miopenFloat)[0])));
 
 // Full tests
 INSTANTIATE_TEST_SUITE_P(Full,
-                         GPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicBwd_FP32,
+                         GPU_UnitTestConvSolverAsmImplicitGemmV4R1DynamicWrw_FP32,
                          testing::Combine(testing::Values(GetTestParams()),
                                           testing::Values(miopenConvolutionAlgoImplicitGEMM),
                                           testing::ValuesIn(GetConvTestCasesFull(miopenFloat))));
