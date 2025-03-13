4b728ca30535ce2291077e03535bc659c1f4e2a8
Daming Feng <dmfeng8898@gmail.com>
2025-03-13 18:21:07 +0000

    add gtest

--- test/gtest/group_conv.hpp
+++ test/gtest/group_conv.hpp
@@ -434,13 +434,15 @@ public:
         {
             DispatchSolver<miopen::solver::conv::ConvHipImplicitGemmGroupFwdXdlops,
                            miopen::solver::conv::ConvHipImplicitGemmGroupBwdXdlops,
-                           miopen::solver::conv::ConvHipImplicitGemmGroupWrwXdlops>();
+                           miopen::solver::conv::ConvHipImplicitGemmGroupWrwXdlops,
+                           miopen::solver::conv::ConvHipImplicitGemmGroupWrwCKNCHWXdlops>();
         }
         else
         {
             DispatchSolver<miopen::solver::conv::ConvHipImplicitGemm3DGroupFwdXdlops,
                            miopen::solver::conv::ConvHipImplicitGemm3DGroupBwdXdlops,
-                           miopen::solver::conv::ConvHipImplicitGemm3DGroupWrwXdlops>();
+                           miopen::solver::conv::ConvHipImplicitGemm3DGroupWrwXdlops,
+                           miopen::solver::conv::ConvHipImplicitGemm3DGroupWrwCKNCHWXdlops>();
         }
     }
 
