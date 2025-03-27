e39977002e7032735df48f7404a13650e4529d3c
Vsevolod Golovko <gvsevolo@amd.com>
2025-03-27 17:41:42 +0100

    dont run tests if CK is not installed

--- test/gtest/unit_conv_solver_ConvHipImplicitGemmBwdXdlops.cpp
+++ test/gtest/unit_conv_solver_ConvHipImplicitGemmBwdXdlops.cpp
@@ -73,8 +73,13 @@ auto GetConvFullTestCases(miopenDataType_t datatype)
 
 auto GetTestParams(miopenDataType_t datatype)
 {
+// If CK is not installed these tests would fail
+#if MIOPEN_BACKEND_HIP && MIOPEN_USE_COMPOSABLEKERNEL
     Gpu supportedDevices = Gpu::gfx908 | Gpu::gfx90A | Gpu::gfx94X;
-    auto params          = miopen::unit_tests::UnitTestConvSolverParams(supportedDevices);
+#else
+    Gpu supportedDevices = Gpu::None;
+#endif
+    auto params = miopen::unit_tests::UnitTestConvSolverParams(supportedDevices);
     params.Tunable(5);
     if(datatype == miopenHalf)
     {
