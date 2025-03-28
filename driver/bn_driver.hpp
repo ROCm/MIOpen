d84035571473413218e6f9772273150dc968f0c3
Bibek Ghimire <bghimire@amd.com>
2025-03-28 20:19:04 +0000

    fix bn driver signed init

--- driver/bn_driver.hpp
+++ driver/bn_driver.hpp
@@ -196,7 +196,7 @@ int BatchNormDriver<TInput, Tref, TAcc, TScaleBias, TOut>::GetandSetData()
 
     in.AllocOnHost(tensor<TInput>{bn_layout, in_len});
     // -2.0 to 2.0
-    in.GetTensor().generate(uniform_signed_initializer<TInput>(2e-3 /*scale*/, 1000 /*range*/));
+    in.GetTensor().generate(uniform_unsigned_initializer<TInput>(2e-3 /*scale*/, 1000 /*range*/));
 
     auto derivedBnDesc = miopen::TensorDescriptor{};
     miopen::DeriveBNTensorDescriptor(derivedBnDesc, in.GetTensor().desc, bn_mode);
