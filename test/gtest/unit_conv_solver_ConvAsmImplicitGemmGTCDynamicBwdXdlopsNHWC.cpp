a2dde88f966e26644593dd16ad0a35f301332f66
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2025-03-28 14:04:12 +0100

    Fix tolerances for ConvAsmImplicitGemmGTCDynamicFwdXdlopsNHWC, ConvAsmImplicitGemmGTCDynamicBwdXdlopsNHWC and ConvAsmImplicitGemmGTCDynamicWrwXdlopsNHWC

--- test/gtest/unit_conv_solver_ConvAsmImplicitGemmGTCDynamicBwdXdlopsNHWC.cpp
+++ test/gtest/unit_conv_solver_ConvAsmImplicitGemmGTCDynamicBwdXdlopsNHWC.cpp
@@ -85,6 +85,7 @@ auto GetFullTestParams(miopenDataType_t datatype)
     auto testParams = miopen::unit_tests::UnitTestConvSolverParams(supportedDevices);
     testParams.Tunable(1000);
     testParams.CheckXnackDisabled();
+    testParams.SetTolerance(Gpu::gfx90A, miopenFloat, 2.0f);
     testParams.SetTolerance(Gpu::gfx90A, miopenHalf, 2.0f);
 
     return testParams;
