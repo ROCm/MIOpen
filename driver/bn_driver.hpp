c5ae97be1fc8089c46c87fac13d869dbea9b21ab
Bibek Ghimire <bghimire@amd.com>
2025-03-28 20:20:33 +0000

    fix comment

--- driver/bn_driver.hpp
+++ driver/bn_driver.hpp
@@ -195,7 +195,7 @@ int BatchNormDriver<TInput, Tref, TAcc, TScaleBias, TOut>::GetandSetData()
     SetBNParametersFromCmdLineArgs();
 
     in.AllocOnHost(tensor<TInput>{bn_layout, in_len});
-    // -2.0 to 2.0
+    // 0.0 to 2.0
     in.GetTensor().generate(uniform_unsigned_initializer<TInput>(2e-3 /*scale*/, 1000 /*range*/));
 
     auto derivedBnDesc = miopen::TensorDescriptor{};
