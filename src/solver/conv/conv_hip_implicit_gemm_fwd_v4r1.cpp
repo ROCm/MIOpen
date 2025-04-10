01eae5c0a3fd3fa85c8139cf5b6ace9823b8fd07
Kamil Nasyrov <shurale.nkn@gmail.com>
2025-04-10 14:22:02 +0200

    missing solver

--- src/solver/conv/conv_hip_implicit_gemm_fwd_v4r1.cpp
+++ src/solver/conv/conv_hip_implicit_gemm_fwd_v4r1.cpp
@@ -379,8 +379,11 @@ ConvHipImplicitGemmV4R1Fwd::GetSolution(const ExecutionContext& ctx,
         std::string(" -DCK_PARAM_EPACK_LENGTH=") + std::to_string(GetEPackLength(ctx, problem, false)) +
         std::string(" -DCK_THREADWISE_GEMM_USE_AMD_INLINE_ASM=") + (use_amd_inline_asm(ctx, problem) ? '1' : '0') +
         std::string(" -DCK_USE_AMD_INLINE_ASM=") + (use_amd_inline_asm(ctx, problem) ? '1' : '0') +
+#if HIP_PACKAGE_VERSION_FLAT >= 6004000000
+        " -DCK_USE_AMD_BUFFER_PTR_TYPE=1" +
+#endif
         get_static_ck_common_compiler_flag(ctx) +
-        ctx.general_compile_options;
+        ctx.general_compile_options  + " --std=c++17";
     // clang-format on
 
     if(problem.IsFp32())
