3093f17f27333581896619706dc3fccbcee87c99
Randy J. Spaulding <rspauldi@amd.com>
2025-04-23 04:35:05 -0500

    more intrinsics not mangled correctly

--- test/gtest/unit_conv_solver_ConvHipImplicitGemmForwardV4R4Xdlops.cpp
+++ test/gtest/unit_conv_solver_ConvHipImplicitGemmForwardV4R4Xdlops.cpp
@@ -65,7 +65,7 @@ auto GetConvTestCasesFull(miopenDataType_t datatype)
 const auto& GetTestParams()
 {
     static const auto params = [] {
-        Gpu supported_gpus = Gpu::gfx908 | Gpu::gfx90A | Gpu::gfx94X;
+        Gpu supported_gpus = Gpu::gfx908 | Gpu::gfx90A;
         auto p             = miopen::unit_tests::UnitTestConvSolverParams(supported_gpus);
         p.EnableDeprecatedSolvers();
         p.Tunable(5);
