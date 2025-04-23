391c86f34f3498907ce4e03a46d75666b9d1c3bd
Randy Spaulding <167460321+randyspauldingamd@users.noreply.github.com>
2025-04-23 12:26:17 -0700

    Disable igemm bwd v1r1 on gfx942 (#3682)
    * disable igemm bwd v1r1 on gfx942
    
    * disable solvers on gfx942,gfx950 that call kernels that use v_mac_f32
    
    * clang-format
    
    * remove type-specific
    
    [ROCm/MIOpen commit: 8a81e7a416c3fca0b9f9198eca0dcc4b6583f828]

--- test/gtest/unit_conv_solver_ConvHipImplicitGemmBwdDataV4R1.cpp
+++ test/gtest/unit_conv_solver_ConvHipImplicitGemmBwdDataV4R1.cpp
@@ -87,9 +87,8 @@ auto GetConvTestCases(miopenDataType_t datatype)
 const auto& GetTestParams()
 {
     static const auto params = [] {
-        Gpu supported_gpus =
-            Gpu::gfx900 | Gpu::gfx906 | Gpu::gfx908 | Gpu::gfx90A | Gpu::gfx94X | Gpu::gfx103X;
-        auto p = miopen::unit_tests::UnitTestConvSolverParams(supported_gpus);
+        Gpu supported_gpus = Gpu::gfx900 | Gpu::gfx906 | Gpu::gfx908 | Gpu::gfx90A | Gpu::gfx103X;
+        auto p             = miopen::unit_tests::UnitTestConvSolverParams(supported_gpus);
         p.EnableDeprecatedSolvers();
         p.Tunable(5);
         return p;
