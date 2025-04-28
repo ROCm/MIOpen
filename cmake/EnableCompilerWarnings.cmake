2c992a2594a8f010982521a919a268df94c7df59
Artur Wojcik <artur.wojcik@amd.com>
2025-04-28 13:16:36 +0200

    Add more exclusions

--- cmake/EnableCompilerWarnings.cmake
+++ cmake/EnableCompilerWarnings.cmake
@@ -109,6 +109,7 @@ set(__clang_cxx_compile_options
     -Wno-c++20-extensions)
 if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "19")
     list(APPEND __clang_cxx_compile_options
+        -Wno-unique-object-duplication
         -Wno-switch-default
         -Wno-nontrivial-memcall)
 endif()
