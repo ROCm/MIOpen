3f04930c360d4af0582c6cb6a60f74574db1ed7c
Adam Dickin <adam.dickin@amd.com>
2025-06-06 21:36:20 +0000

    pull the first elapsed time out of the zerooutputs if.  It is accumulating time for the convertFrom functions.

--- src/include/miopen/solver/implicitgemm_ck_util.hpp
+++ src/include/miopen/solver/implicitgemm_ck_util.hpp
@@ -868,12 +868,12 @@ ConvSolution InitInvokerFactoryNCHW(const ExecutionContext& ctx,
             input1_tr_inst.ConvertFrom(handle, kernels, conv_tensors);
             input2_tr_inst.ConvertFrom(handle, kernels, conv_tensors);
             output_init_tr_inst.ConvertFrom(handle, kernels, conv_tensors);
+            /// \todo: Will need SetTensor() to properly zero out non-packed tensors
+            /// Note: Need to clear buffer memory for output since all values may not be set.
+            elapsed = handle.IsProfilingEnabled() ? handle.GetKernelTime() : 0.0f;
 
             if constexpr(ZeroOutputs)
             {
-                /// \todo: Will need SetTensor() to properly zero out non-packed tensors
-                /// Note: Need to clear buffer memory for output since all values may not be set.
-                elapsed = handle.IsProfilingEnabled() ? handle.GetKernelTime() : 0.0f;
                 output_tr_inst.ZeroOutBuffer(handle);
                 if(handle.IsProfilingEnabled())
                     elapsed += handle.GetKernelTime();
