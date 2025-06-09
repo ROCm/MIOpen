d3cdb52df34923bd40a80ebd62c049a004f1ec46
Adam Dickin <adam.dickin@amd.com>
2025-06-09 19:05:14 +0000

    fix formatting

--- driver/driver.hpp
+++ driver/driver.hpp
@@ -84,7 +84,7 @@ struct GPUMem
     };
 
 #if MIOPEN_BACKEND_OPENCL
-    GPUMem() {};
+    GPUMem(){};
     GPUMem(cl_context& ctx, size_t psz, size_t pdata_sz, Check ch = Check::None)
         : sz(psz), data_sz(pdata_sz)
     {
@@ -111,7 +111,7 @@ struct GPUMem
 
 #elif MIOPEN_BACKEND_HIP
 
-    GPUMem() {};
+    GPUMem(){};
     GPUMem(uint32_t ctx, size_t psz, size_t pdata_sz, Check ch = Check::None)
         : _ctx(ctx), sz(psz), data_sz(pdata_sz), check(ch)
     {
