7db8ab35bf173b9afe643b94f902d552f9ddf9a9
Adam Dickin <adam.dickin@amd.com>
2025-06-09 18:00:14 +0000

    correct the hipmemset usage

--- driver/driver.hpp
+++ driver/driver.hpp
@@ -285,7 +285,38 @@ public:
             // init the buffers with NaNs.
             // Note, we only do this for the gpu buffers, adding the behaviour for the host buffers
             // causes a crash as the host code doesnt handle NaNs.
-            hipMemset(dev->GetMem(), std::numeric_limits<Tgpu>::quiet_NaN(), dev->GetSize());
+            if(std::is_same<Tgpu, float>::value)
+            {
+                hipMemsetD32(
+                    dev->GetMem(), std::numeric_limits<float>::quiet_NaN(), dev->GetSize());
+            }
+            else if(std::is_same<Tgpu, bfloat16>::value)
+            {
+                hipMemsetD16(
+                    dev->GetMem(), std::numeric_limits<bfloat16>::quiet_NaN(), dev->GetSize());
+            }
+            else if(std::is_same<Tgpu, half_float::half>::value)
+            {
+                hipMemsetD16(dev->GetMem(),
+                             std::numeric_limits<half_float::half>::quiet_NaN(),
+                             dev->GetSize());
+            }
+            else if(std::is_same<Tgpu, bfloat8_fnuz>::value)
+            {
+                hipMemset(
+                    dev->GetMem(), std::numeric_limits<bfloat8_fnuz>::quiet_NaN(), dev->GetSize());
+            }
+            else if(std::is_same<Tgpu, float8_fnuz>::value)
+            {
+                hipMemset(
+                    dev->GetMem(), std::numeric_limits<float8_fnuz>::quiet_NaN(), dev->GetSize());
+            }
+            else if(std::is_same<Tgpu, int8_t>::value)
+            {
+                // ints dont have Nan so use min value.
+                hipMemset(dev->GetMem(), std::numeric_limits<int8_t>::min(), dev->GetSize());
+            }
+
             return 0;
         }
 
