4ed05c0f0b13ce229aae3018db6d42db00eb85a2
Jin Zhou <jin.zhou@amd.com>
2025-06-27 05:43:50 +0000

    add transpose

--- src/include/miopen/conv/solvers.hpp
+++ src/include/miopen/conv/solvers.hpp
@@ -2917,7 +2917,7 @@ struct ConvJinMDConvBwd final : ConvSolver
 
     MIOPEN_INTERNALS_EXPORT ConvSolution
     GetBestSolution(const ExecutionContext&, const miopen::conv::ProblemDescription&) const;
-    bool FindCachedSolution(size_t hashcode, const miopen::conv::ProblemDescription& problem, ConvSolution& sol) const;
+    bool FindCachedSolution(const ExecutionContext& ctx, size_t hashcode, const miopen::conv::ProblemDescription& problem, ConvSolution& sol) const;
     uint32_t GetSupportedSolutionCount(const ExecutionContext&, const miopen::conv::ProblemDescription&) const;
 };
 
