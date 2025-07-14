0991e088efb35ae943081acb5973c7f2f3ed9558
Chris Erb <chris.erb65@gmail.com>
2025-07-14 17:20:21 -0500

    Rework KTN gtest (#3733)
    * drafting
    
    * test invocation of model results
    
    * test model result, solver, and invocation
    
    * clang format
    
    * hip tidy
    
    * correct data size for ktn test, add export definitions for functions used in test
    
    * clang-format
    
    * additional test cases for ktn coverage
    
    ---------
    
    Co-authored-by: BrianHarrisonAMD <169072757+BrianHarrisonAMD@users.noreply.github.com>
    
    [ROCm/MIOpen commit: 4970f4ed02a1b787a6d6804c97c44097a765c973]

--- src/include/miopen/conv/solver_finders.hpp
+++ src/include/miopen/conv/solver_finders.hpp
@@ -181,11 +181,11 @@ FindCoreResult FindCore(const AnyInvokeParams& invoke_ctx,
                         bool force_attach_binary                  = false);
 
 namespace conv {
-bool IsAlgorithmDisabled(miopenConvAlgorithm_t algo);
-bool IsEnoughWorkspace(std::string_view where,
-                       const miopen::solver::Id& solver_id,
-                       std::size_t required_size,
-                       const miopen::AnyInvokeParams* invokeParams);
+bool MIOPEN_INTERNALS_EXPORT IsAlgorithmDisabled(miopenConvAlgorithm_t algo);
+bool MIOPEN_INTERNALS_EXPORT IsEnoughWorkspace(std::string_view where,
+                                               const miopen::solver::Id& solver_id,
+                                               std::size_t required_size,
+                                               const miopen::AnyInvokeParams* invokeParams);
 
 struct ConvFindParameters : PrimitiveFindParameters
 {
