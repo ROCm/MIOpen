c03d682a0848ae211a804cd9a81e45802ee22c8e
John Robbins <John.Robbins@amd.com>
2025-07-16 15:22:58 -0600

    [Jenkins] check manual submodule update works without changes to scm method

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -315,7 +315,7 @@ def getDockerImage(Map conf=[:])
 def buildHipClangJob(Map conf=[:]){
         show_node_info()
         miopenCheckout()
-        sh(script: "git submodule update --init --recursive || true")
+        sh(script: "git submodule update --init --recursive")
         checkout scm
         env.HSA_ENABLE_SDMA=0
         env.DOCKER_BUILDKIT=1
