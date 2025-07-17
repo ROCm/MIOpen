67a70f72985406e30edcbd637d51ddc0dc47d7d0
Jonathan Lichtner <195780826+JonathanLichtnerAMD@users.noreply.github.com>
2025-07-17 00:28:24 +0000

    TMP: workaround jenkins git submodule issue

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -315,7 +315,7 @@ def getDockerImage(Map conf=[:])
 def buildHipClangJob(Map conf=[:]){
         show_node_info()
         miopenCheckout()
-        sh(script: "file ./fin/CMakeLists.txt || git submodule update --init --recursive")
+        sh(script: "git submodule update --init --recursive || true")
         checkout scm
         env.HSA_ENABLE_SDMA=0
         env.DOCKER_BUILDKIT=1
