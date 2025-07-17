98814ee955b4ef199eb9fa73fefa208f031d300c
Jonathan Lichtner <195780826+JonathanLichtnerAMD@users.noreply.github.com>
2025-07-17 03:09:39 +0000

    TMP: try the latest workaround

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -315,8 +315,8 @@ def getDockerImage(Map conf=[:])
 def buildHipClangJob(Map conf=[:]){
         show_node_info()
         miopenCheckout()
-        sh(script: "file ./fin/CMakeLists.txt || git submodule update --init --recursive")
         checkout scm
+	sh(script: "git submodule update --init --recursive || true")
         env.HSA_ENABLE_SDMA=0
         env.DOCKER_BUILDKIT=1
         def image
