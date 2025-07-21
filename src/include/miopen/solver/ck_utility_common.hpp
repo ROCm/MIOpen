07ea7116db47c3c5b034039b1a1303cd7f01284f
sriram kuamr k <sriramkumar.kishorekumar@amd.com>
2025-07-21 05:42:53 +0000

    Remove is_ck_whitelist for BN tests

--- src/include/miopen/solver/ck_utility_common.hpp
+++ src/include/miopen/solver/ck_utility_common.hpp
@@ -42,8 +42,7 @@ namespace ck_utility {
 static inline bool is_ck_whitelist(const std::string& device_name)
 {
     return (StartsWith(device_name, "gfx908") || StartsWith(device_name, "gfx90a") ||
-            StartsWith(device_name, "gfx942") || StartsWith(device_name, "gfx950") ||
-            StartsWith(device_name, "gfx110") || StartsWith(device_name, "gfx120"));
+            StartsWith(device_name, "gfx942") || StartsWith(device_name, "gfx950"));
 }
 
 static inline bool is_ck_whitelist(const Handle& handle)
