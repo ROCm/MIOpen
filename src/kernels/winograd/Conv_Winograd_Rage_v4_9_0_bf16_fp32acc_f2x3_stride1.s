062315449b6b866cd6c31e8d075baf4488375fdf
Sergei Grigoriev <102019268+scerzh@users.noreply.github.com>
2025-09-12 13:45:07 +0200

    [miopen] Integrate Winograd Rage 4.9.0 (#1396)
    ## Motivation
    
    <!-- Explain the purpose of this PR and the goals it aims to achieve.
    -->
    
    An integration of Winograd Rage 4.9.0, a functional extension of the
    Winograd Rage 4.7.0 with numerical improvements for sigmoid function.
    
    ## Technical Details
    
    - Added support for FP32 data type on MI300 ASICs.
    - A performance improvement of up to 2.26X and 1.87X on average is
    observed for convolution cases on MI300X compared to Winograd v40.
    - Added support for BF16 data type on MI300 ASICs.
      - Input, filter, bias and output data are stored in BF16.
    - The input and filter Winograd transform use mixed BF16 precision: data
    is converted to FP32, the transform is performed in FP32, and the result
    is converted back to BF16.
      - Accumulation is in FP32 using WMMA instructions.
      - The output Winograd transform is computed in FP32.
    - Bias and activation are computed in FP32. The output is converted back
    to BF16.
    - Improved numerical accuracy for the sigmoid activation function.
    - The updated implementation achieves 0.5 ULP accuracy for FP16 and 2.64
    ULP for FP32.
    - A minor performance drop is expected in convolution cases with fused
    sigmoid activation enabled.
    - For the FP16 data type, performance difference is expected to be
    minimal.
    
    ## Test Plan
    
    - [x] Unit tests for the solver
    - [x] Performance tests
    
    ## Test Result
    
    Performance is the same as of Winograd Rage 4.7.0 (we extended data type
    support, not performance) and around 2X better compared to Winograd v40.
    
    ## Submission Checklist
    
    - [x] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- src/kernels/winograd/Conv_Winograd_Rage_v4_9_0_bf16_fp32acc_f2x3_stride1.s
+++ src/kernels/winograd/Conv_Winograd_Rage_v4_9_0_bf16_fp32acc_f2x3_stride1.s
@@ -27,9 +27,9 @@
 .include "Conv_Winograd_Rage_v4_X_X_metadata.inc"
 
 .if (.amdgcn.gfx_generation_number == 9 && .amdgcn.gfx_generation_minor == 4 && .amdgcn.gfx_generation_stepping == 2)
-    KERNEL_PROLOG 4_6_0, _fp16_fp32acc_f2x3_stride1
-    .include "Conv_Winograd_Rage_v4_6_0_gfx94x_fp16_fp32acc_f2x3_stride1.inc"
-    KERNEL_EPILOG 80, 164, 164, 768, 4_6_0, _fp16_fp32acc_f2x3_stride1
+    KERNEL_PROLOG 4_9_0, _bf16_fp32acc_f2x3_stride1
+    .include "Conv_Winograd_Rage_v4_9_0_gfx94x_bf16_fp32acc_f2x3_stride1.inc"
+    KERNEL_EPILOG 89, 164, 4, 768, 4_9_0, _bf16_fp32acc_f2x3_stride1
 .else
     .error "Unsupported gfx version"
 .endif
