691cc9ba472aa4ab1f9bb2fa1901556d7c67befc
JonathanLichtnerAMD <195780826+JonathanLichtnerAMD@users.noreply.github.com>
2025-09-23 16:40:44 +0000

    [rocm-libraries] ROCm/rocm-libraries#1740 (commit 31d89f1)
    =?UTF-8?q?Revert=20"[MIOpen]=20Implement=20kernel=20tunin?=
     =?UTF-8?q?g=20heuristic=20model=20for=203D=20conv=20=E2=80=A6=20(#1740)?=
    MIME-Version: 1.0
    Content-Type: text/plain; charset=UTF-8
    Content-Transfer-Encoding: 8bit
    
    …ops (two tower model) (#1154)"
    
    This reverts commit 422e87293e9d52dd399a1801313f9017be29291d.
    
    ## Motivation
    
    That commit broke MI300 unit tests.
    
    ## Test Plan
    
    Reverted this change and verified that the failing build now passes
    
    ## Submission Checklist
    
    - [x] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- src/include/miopen/conv/solvers.hpp
+++ src/include/miopen/conv/solvers.hpp
@@ -4498,7 +4498,6 @@ struct PerformanceConfigHipImplicitGemm3DGroupFwdXdlops
     : PerfConfigBaseCK<PerformanceConfigHipImplicitGemm3DGroupFwdXdlops>
 {
     int index             = 0;
-    int split_k           = 0; // not used for this solver, but required for AI heuristics interface
     std::string kernel_id = "";
     std::vector<std::string> valid_kernels;
 
@@ -4513,8 +4512,7 @@ struct PerformanceConfigHipImplicitGemm3DGroupFwdXdlops
         : PerformanceConfigHipImplicitGemm3DGroupFwdXdlops(0, "")
     {
     }
-    MIOPEN_INTERNALS_EXPORT void HeuristicInit(const ExecutionContext&,
-                                               const miopen::conv::ProblemDescription&);
+    MIOPEN_INTERNALS_EXPORT void HeuristicInit(const miopen::conv::ProblemDescription&);
     MIOPEN_INTERNALS_EXPORT bool SetNextValue(const miopen::conv::ProblemDescription&);
     MIOPEN_INTERNALS_EXPORT bool IsValidValue() const;
     bool IsValid(const ExecutionContext&, const miopen::conv::ProblemDescription& problem) const
@@ -4530,7 +4528,6 @@ private:
     void Init(const miopen::conv::ProblemDescription&);
     template <typename DataType>
     bool CheckIsSupportCKArgs(const miopen::conv::ProblemDescription&) const;
-    void InitValidKernels(const miopen::conv::ProblemDescription& problem);
 };
 
 struct ConvHipImplicitGemm3DGroupFwdXdlops final
@@ -4590,8 +4587,7 @@ struct PerformanceConfigHipImplicitGemm3DGroupWrwXdlops
         : PerformanceConfigHipImplicitGemm3DGroupWrwXdlops(0, "")
     {
     }
-    MIOPEN_INTERNALS_EXPORT void HeuristicInit(const ExecutionContext&,
-                                               const miopen::conv::ProblemDescription&);
+    MIOPEN_INTERNALS_EXPORT void HeuristicInit(const miopen::conv::ProblemDescription&);
     MIOPEN_INTERNALS_EXPORT bool SetNextValue(const miopen::conv::ProblemDescription&);
     MIOPEN_INTERNALS_EXPORT bool IsValidValue() const;
     bool IsValid(const ExecutionContext&, const miopen::conv::ProblemDescription& problem) const
@@ -4607,7 +4603,6 @@ private:
     void Init(const miopen::conv::ProblemDescription&);
     template <typename DataType>
     bool CheckIsSupportCKArgs(const miopen::conv::ProblemDescription&) const;
-    void InitValidKernels(const miopen::conv::ProblemDescription& problem);
 };
 
 struct ConvHipImplicitGemm3DGroupWrwXdlops final
@@ -4655,7 +4650,6 @@ struct PerformanceConfigHipImplicitGemm3DGroupBwdXdlops
     : PerfConfigBaseCK<PerformanceConfigHipImplicitGemm3DGroupBwdXdlops>
 {
     int index;
-    int split_k = 0; // not used for this solver, but required for AI heuristics interface
     std::string kernel_id;
     std::vector<std::string> valid_kernels;
     PerformanceConfigHipImplicitGemm3DGroupBwdXdlops(int idx, std::string kernl_id)
@@ -4670,8 +4664,7 @@ struct PerformanceConfigHipImplicitGemm3DGroupBwdXdlops
         : PerformanceConfigHipImplicitGemm3DGroupBwdXdlops(0, "")
     {
     }
-    MIOPEN_INTERNALS_EXPORT void HeuristicInit(const ExecutionContext&,
-                                               const miopen::conv::ProblemDescription&);
+    MIOPEN_INTERNALS_EXPORT void HeuristicInit(const miopen::conv::ProblemDescription&);
     MIOPEN_INTERNALS_EXPORT bool SetNextValue(const miopen::conv::ProblemDescription&);
     MIOPEN_INTERNALS_EXPORT bool IsValidValue() const;
     bool IsValid(const ExecutionContext&, const miopen::conv::ProblemDescription& problem) const
@@ -4687,7 +4680,6 @@ private:
     void Init(const miopen::conv::ProblemDescription&);
     template <typename DataType>
     bool CheckIsSupportCKArgs(const miopen::conv::ProblemDescription&) const;
-    void InitValidKernels(const miopen::conv::ProblemDescription& problem);
 };
 
 struct ConvHipImplicitGemm3DGroupBwdXdlops final
