c2ce26a9abaa0ca07697e704fad33ee33c618c7c
Chris Erb <24392813+cderb@users.noreply.github.com>
2025-11-14 16:58:01 +0000

    [rocm-libraries] ROCm/rocm-libraries#2672 (commit 1775320)
    Patch LockFile unit test for sharding
    
    Gtest sharding was layering the LockFile unit tests causing
    non-deterministic behavior in CI. This patch allows the tests to be run
    in parallel.

--- test/gtest/unit_lock_file.cpp
+++ test/gtest/unit_lock_file.cpp
@@ -38,8 +38,8 @@ static boost::posix_time::ptime ToPTime(TDuration duration)
 
 TEST(CPU_UnitTestLockFile_NONE, TryLock)
 {
-    miopen::fs::remove_all(miopen::fs::path{"/tmp/config/miopen"});
-    auto lockpath = miopen::LockFilePath(miopen::fs::path{"/tmp/config/miopen/test"});
+    auto lockpath = miopen::LockFilePath(
+        miopen::fs::path{"/tmp/config/miopen/test." + std::to_string(getpid())});
     miopen::fs::path fs_lock_path = lockpath.string() + ".fslock";
     auto lockfile                 = miopen::FSLockFile(lockpath);
     auto unique_handle            = lockfile.get_unique_handle();
@@ -57,8 +57,8 @@ TEST(CPU_UnitTestLockFile_NONE, TryLock)
 
 TEST(CPU_UnitTestLockFile_NONE, TimedLock)
 {
-    miopen::fs::remove_all(miopen::fs::path{"/tmp/config/miopen"});
-    auto lockpath = miopen::LockFilePath(miopen::fs::path{"/tmp/config/miopen/test"});
+    auto lockpath = miopen::LockFilePath(
+        miopen::fs::path{"/tmp/config/miopen/test." + std::to_string(getpid())});
     auto lockfile = miopen::FSLockFile(lockpath);
     EXPECT_TRUE(lockfile.timed_lock(ToPTime(std::chrono::seconds{1})));
     EXPECT_FALSE(lockfile.timed_lock(ToPTime(std::chrono::seconds{1})));
@@ -68,9 +68,9 @@ TEST(CPU_UnitTestLockFile_NONE, TimedLock)
 
 TEST(CPU_UnitTestLockFile_NONE, OrphanLock)
 {
-    miopen::fs::remove_all(miopen::fs::path{"/tmp/config/miopen"});
-    auto lockpath = miopen::LockFilePath(miopen::fs::path{"/tmp/config/miopen/test"});
-    auto lockfile = miopen::FSLockFile(lockpath);
+    auto lockpath = miopen::LockFilePath(
+        miopen::fs::path{"/tmp/config/miopen/test." + std::to_string(getpid())});
+    auto lockfile                 = miopen::FSLockFile(lockpath);
     miopen::fs::path fs_lock_path = lockpath.string() + ".fslock";
     auto unique_handle            = lockfile.get_unique_handle();
 
@@ -87,9 +87,9 @@ TEST(CPU_UnitTestLockFile_NONE, OrphanLock)
 
 TEST(CPU_UnitTestLockFile_NONE, LostLockFile)
 {
-    miopen::fs::remove_all(miopen::fs::path{"/tmp/config/miopen"});
-    auto lockpath = miopen::LockFilePath(miopen::fs::path{"/tmp/config/miopen/test"});
-    auto lockfile = miopen::FSLockFile(lockpath);
+    auto lockpath = miopen::LockFilePath(
+        miopen::fs::path{"/tmp/config/miopen/test." + std::to_string(getpid())});
+    auto lockfile                 = miopen::FSLockFile(lockpath);
     miopen::fs::path fs_lock_path = lockpath.string() + ".fslock";
     auto unique_handle            = lockfile.get_unique_handle();
 
@@ -102,8 +102,8 @@ TEST(CPU_UnitTestLockFile_NONE, LostLockFile)
 
 TEST(CPU_UnitTestLockFile_NONE, RefreshHold)
 {
-    miopen::fs::remove_all(miopen::fs::path{"/tmp/config/miopen"});
-    auto lockpath = miopen::LockFilePath(miopen::fs::path{"/tmp/config/miopen/test"});
+    auto lockpath = miopen::LockFilePath(
+        miopen::fs::path{"/tmp/config/miopen/test." + std::to_string(getpid())});
     miopen::fs::path fs_lock_path = lockpath.string() + ".fslock";
     auto lockfile                 = miopen::FSLockFile(lockpath);
     auto unique_handle            = lockfile.get_unique_handle();
